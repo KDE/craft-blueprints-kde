296b857ec07f29154a0313f61868ce469d0856b5
Kevin Funk <kfunk@kde.org>
2017-08-31 23:25:50 +0200

    Add kdegraphics-mobipocket

---
+++ kde/kdegraphics/kdegraphics-mobipocket/kdegraphics-mobipocket.py
@@ -0,0 +1,21 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        self.description = "A collection of plugins to handle mobipocket files"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
+        self.buildDependencies["libs/qt5/qtbase"] = "default"
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
