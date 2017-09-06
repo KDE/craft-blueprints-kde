ffc00f5734ea66c9c8f35d34d1a901798f38e074
Hannah von Reth <vonreth@kde.org>
2017-09-06 10:25:37 +0200

    Update ruby to 2.4.1-2

--- dev-util/ruby/ruby.py
+++ dev-util/ruby/ruby.py
@@ -3,10 +3,12 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets["1.9.2"] = "ftp://ftp.ruby-lang.org/pub/ruby/binaries/mswin32/ruby-1.9.2-p136-i386-mswin32.zip"
-        self.defaultTarget = "1.9.2"
-        self.targetDigests["1.9.2"] = "e8cbc6215d1f08433b105661a1ac72c197e2e5b1"
-        self.targetInstallPath["1.9.2"] = "dev-utils"
+        for ver in ["2.4.1-2"]:
+            self.targets[ver] = f"https://github.com/oneclick/rubyinstaller2/releases/download/{ver}/rubyinstaller-{ver}-{craftCompiler.architecture}.7z"
+            self.targetInstSrc[ver] = f"rubyinstaller-{ver}-{craftCompiler.architecture}"
+            self.targetInstallPath[ver] = "dev-utils"
+        self.targetDigestsX64["2.4.1-2"] = (['2ad3c7f68404ba0805cf91e682ecf2d85b75c42a5809e73e54898f24a1e014bb'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "2.4.1-2"
 
 
 from Package.BinaryPackageBase import *
@@ -15,11 +17,3 @@ from Package.BinaryPackageBase import *
 class Package(BinaryPackageBase):
     def __init__(self):
         BinaryPackageBase.__init__(self)
-
-    def unpack(self):
-        res = BinaryPackageBase.unpack(self)
-        # ruby package contains a file called MANIFEST that we need to get
-        # out of the way so we can make the manifest dir
-        if res:
-            os.remove(os.path.join(self.sourceDir(), "MANIFEST"))
-        return res
