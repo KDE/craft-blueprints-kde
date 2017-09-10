e437a11d083734dbd0f04003114b948c2d61c0dc
Ben Cooksley <bcooksley@kde.org>
2017-09-10 17:06:55 +1200

    Add 0.26 for Exiv2, as exiv2.org is down and 0.25 isn't available anywhere else

--- win32libs/exiv2/exiv2.py
+++ win32libs/exiv2/exiv2.py
@@ -6,15 +6,23 @@ class subinfo(info.infoclass):
         for ver in ['0.24', '0.25']:
             self.targets[ver] = 'http://www.exiv2.org/exiv2-%s.tar.gz' % ver
             self.targetInstSrc[ver] = 'exiv2-%s' % ver
+
+        for ver in ['0.26']:
+            self.targets[ver] = 'https://github.com/Exiv2/exiv2/archive/v%s.tar.gz' % ver
+            self.archiveNames[ver] = "exiv2-{ver}.tar.gz"
+            self.targetInstSrc[ver] = "exiv2-{ver}"
+
         self.targetDigests['0.24'] = '2f19538e54f8c21c180fa96d17677b7cff7dc1bb'
         self.patchToApply['0.24'] = ('exiv2-0.22-20120117.diff', 1)
         self.patchToApply['0.25'] = ('exiv2-0.25-20150826.diff', 1)
         self.targetDigests['0.25'] = 'adb8ffe63916e7c27bda9792e690d1330ec7273d'
+        self.patchToApply['0.26'] = ('exiv2-0.25-20150826.diff', 1)
+        self.targetDigests['0.26'] = '25b9b93dc1df3bd449fa250ddfb84bc11fb4812b'
 
         self.svnTargets['svnHEAD'] = 'svn://dev.exiv2.org/svn/trunk'
 
         self.description = "an image metadata library"
-        self.defaultTarget = '0.25'
+        self.defaultTarget = '0.26'
 
     def setDependencies(self):
         self.runtimeDependencies["win32libs/win_iconv"] = "default"
