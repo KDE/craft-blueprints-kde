dd38219bdb4704e9a4354d66a14e17153c2f8967
Matthieu Gallien <matthieu_gallien@yahoo.fr>
2017-10-08 15:32:44 +0200

    initial version of windows blueprint for elisa music player
    Summary: blueprint for elisa music player
    
    Test Plan:
    generates a working elisa win32 executable
    
    still need to check that icons are properly handled and need to minimize the size of executable through blacklist.txt file
    
    Reviewers: #craft, vonreth
    
    Reviewed By: #craft, vonreth
    
    Differential Revision: https://phabricator.kde.org/D8198

---
+++ kde/unreleased/elisa/elisa.py
@@ -0,0 +1,53 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.svnTargets['master'] = '[git]kde:elisa'
+        self.defaultTarget = 'master'
+        self.shortDescription = "the Elisa music player"
+
+    def setDependencies( self ):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
+        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["libs/qtmultimedia"] = "default"
+        self.runtimeDependencies["libs/qtquickcontrols"] = "default"
+        self.runtimeDependencies["libs/qtquickcontrols2"] = "default"
+        self.runtimeDependencies["libs/qtsvg"] = "default"
+        self.runtimeDependencies["libs/qtwinextras"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kconfig"] = "default"
+        self.runtimeDependencies["frameworks/tier3/kcmutils"] = "default"
+        self.runtimeDependencies["frameworks/tier2/kfilemetadata"] = "default"
+        self.runtimeDependencies["frameworks/tier1/ki18n"] = "default"
+        self.runtimeDependencies["frameworks/tier3/kdeclarative"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kcoreaddons"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kdoctools"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kcrash"] = "default"
+        self.runtimeDependencies["frameworks/tier2/kxmlgui"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+
+    def createPackage(self):
+        self.defines["productname"] = "Elisa"
+        self.defines["executable"] = "bin\\elisa.exe"
+        self.defines["icon"] = os.path.join(self.packageDir(), "elisa.ico")
+
+        self.blacklist_file.append(os.path.join(self.packageDir(), 'blacklist.txt'))
+
+        self.ignoredPackages.append("binary/mysql")
+
+        return TypePackager.createPackage(self)
+
+    def preArchive(self):
+        archiveDir = self.archiveDir()
+        # TODO: Why is that needed?
+        os.mkdir(os.path.join(archiveDir, "etc", "dbus-1", "session.d"))
+
+        # TODO: Just blacklisting this doesn't work. WTF?
+        utils.rmtree(os.path.join(archiveDir, "dev-utils"))
