d49fa4eae6f2cb309310db0d4abc73276a1caf67
Kevin Funk <kfunk@kde.org>
2017-10-25 13:56:54 +0200

    Add recipe for KDE's falkon

---
+++ kde/unreleased/falkon/falkon.py
@@ -0,0 +1,31 @@
+import info
+from CraftOS.osutils import OsUtils
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        self.description = "Cross-platform Qt web-browser"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
+        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["libs/qt5/qtwebengine"] = "default"
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+
+    def createPackage(self):
+        self.defines["productname"] = "Falkon"
+        self.defines["executable"] = "bin\\falkon.exe"
+
+        self.ignoredPackages.append("binary/mysql")
+
+        return TypePackager.createPackage(self)
