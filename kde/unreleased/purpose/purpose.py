e3505c04663dda35d7f462036b865133fb17d46b
Kevin Funk <kfunk@kde.org>
2017-10-25 12:15:16 +0200

    Add recipe for KDE purpose

---
+++ kde/unreleased/purpose/purpose.py
@@ -0,0 +1,22 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["master"] = "git://anongit.kde.org/purpose"
+        self.defaultTarget = "master"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kconfig"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kcoreaddons"] = "default"
+        self.runtimeDependencies["frameworks/tier1/ki18n"] = "default"
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
