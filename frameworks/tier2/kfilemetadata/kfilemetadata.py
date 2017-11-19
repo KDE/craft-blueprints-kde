275616f5901bf08c191ee7fe2a6d0d46972f4dd3
Kevin Funk <kfunk@kde.org>
2017-11-19 22:25:44 +0100

    kfilemetadata: Apply crash fix to older versions

--- frameworks/tier2/kfilemetadata/kfilemetadata.py
+++ frameworks/tier2/kfilemetadata/kfilemetadata.py
@@ -5,6 +5,9 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
+        for ver in ['5.37.0', '5.38.0']:
+            self.patchToApply[ver] = [("0001-fix-crash-when-more-than-one-instances-of-ExtractorC.patch", 1)]
+
         self.description = "A file metadata and text extraction library"
 
     def setDependencies(self):
