1b6502acc968d9afcb8fdf80148b5dadd9531e20
Hannah von Reth <vonreth@kde.org>
2017-12-05 22:14:15 +0100

    Upgrade boost to 1_65_1 wich adds proper msvc2017 supprot

--- win32libs/boost/boost-headers/boost-headers.py
+++ win32libs/boost/boost-headers/boost-headers.py
@@ -10,6 +10,7 @@ class subinfo(info.infoclass):
         self.targetDigests['1_56_0'] = '3a65d31ad443a46ce6bb5011a5d5a0750765f157'
         self.targetDigests['1_57_0'] = 'cc08224035b6aa642599bbf28c76ee5ec8061c10'
         self.targetDigests['1_59_0'] = (['2bc99f7b358cc0882d1e26a1d98290be82c23cfe97b348a8d217411f35e79331'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1_65_1'] = (['df7bea4ce58076bd8e9cd63966562d8cf969b431e1c587969e6aa408e51de606'], CraftHash.HashAlgorithm.SHA256)
 
         self.patchToApply['1_55_0'] = [('boost_1_54_0-spirit-20131114.diff', 1)]
         self.patchToApply['1_56_0'] = [
