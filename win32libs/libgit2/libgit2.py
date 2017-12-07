61c02a13fbd35d24234530ae588d8808f5b944a8
Dan Leinir Turthra Jensen <admin@leinir.dk>
2017-12-07 11:30:29 +0000

    Add ssl login support for libgit2 (by adding libssh2 blueprint and using it)
    Add a blueprint for the libssh2 library, and make use of it to
    enable ssl support in libgit2 (which otherwise only supported
    standard username/password authentication).
    
    Differential Revision: https://phabricator.kde.org/D9069

--- win32libs/libgit2/libgit2.py
+++ win32libs/libgit2/libgit2.py
@@ -10,7 +10,11 @@ class subinfo(info.infoclass):
         self.defaultTarget = 'master'
 
     def setDependencies(self):
+        self.buildDependencies['dev-util/pkg-config'] = 'default'
         self.runtimeDependencies["virtual/base"] = "default"
+        self.runtimeDependencies['win32libs/win_iconv'] = 'default'
+        self.runtimeDependencies['win32libs/libssh2'] = 'default'
+        self.runtimeDependencies['win32libs/openssl'] = 'default'
 
 
 from Package.CMakePackageBase import *
@@ -19,5 +23,6 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.defines = "-DUSE_ICONV=ON -DBUILD_CLAR=OFF"
 
 # self.subinfo.options.configure.args = "-DDBUS_REPLACE_LOCAL_DIR=ON "
---
+++ win32libs/libssh2/libssh2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import info
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        self.svnTargets['master'] = 'https://github.com/libssh2/libssh2.git||libssh2-1.8.0'
+        self.targets['1.8.0'] = "https://www.libssh2.org/download/libssh2-1.8.0.tar.gz"
+        self.targetInstSrc['1.8.0'] = "libssh2-1.8.0"
+        self.patchToApply['master'] = ('0001-Ensure-other-libraries-are-told-the-correct-linkage-.patch', 1)
+        self.defaultTarget = 'master'
+
+    def setDependencies( self ):
+        self.buildDependencies['virtual/base'] = 'default'
+        self.runtimeDependencies['win32libs/zlib'] = 'default'
+        self.runtimeDependencies['win32libs/openssl'] = 'default'
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__( self, **args ):
+        CMakePackageBase.__init__( self )
+        self.subinfo.options.configure.defines = "-DENABLE_ZLIB_COMPRESSION=ON "
+
