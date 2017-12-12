6778b010d315dcf399c7440deeb61953d0cbfeb7
Dan Leinir Turthra Jensen <admin@leinir.dk>
2017-12-12 13:15:02 +0000

    Remove non-existent URL, point default to one that does exist

--- data/aspell-data/aspell-data.py
+++ data/aspell-data/aspell-data.py
@@ -3,13 +3,9 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        repoUrl = """http://winkde.org/pub/kde/ports/win32/repository/aspell"""
-
-        self.targets['0.60'] = (repoUrl + """/aspell-en-6.0-41-bin.tar.bz2 """
-                                + repoUrl + """/aspell-de-0.60.20030222.1-10-bin.tar.bz2 """)
         self.targets['0.60.6'] = """ http://files.kolab.org/local/windows-ce/aspell-0.60.6-data.zip"""
 
-        self.defaultTarget = '0.60'
+        self.defaultTarget = '0.60.6'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/bin-base"] = "default"
