59105e13f1dd1cfe5f67b842993479e5103076df
Hannah von Reth <vonreth@kde.org>
2017-12-12 12:33:39 +0100

    Fix typo

--- frameworks/tier1/breeze-icons/breeze-icons.py
+++ frameworks/tier1/breeze-icons/breeze-icons.py
@@ -19,14 +19,14 @@ class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
         self.subinfo.options.configure.args = " -DBINARY_ICONS_RESOURCE=ON"
-        self.subinfo.options.dynamic.registerOption("useBreezeDrark", False)
+        self.subinfo.options.dynamic.registerOption("useBreezeDark", False)
 
     def install(self):
         if OsUtils.isWin() or OsUtils.isMac():
             dest = os.path.join(self.installDir(), "bin", "data")
             if not os.path.exists(dest):
                 os.makedirs(dest)
-            if not self.subinfo.options.dynamic.useBreezeDrark:
+            if not self.subinfo.options.dynamic.useBreezeDark:
                 theme = os.path.join(self.buildDir(), "icons", "breeze-icons.rcc")
             else:
                 theme = os.path.join(self.buildDir(), "icons-dark", "breeze-icons-dark.rcc")
