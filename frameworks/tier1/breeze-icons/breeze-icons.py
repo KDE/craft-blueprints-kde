9750ef899af264246a44e8c9a1f339915207bd3d
Hannah von Reth <vonreth@kde.org>
2017-12-12 09:18:28 +0100

    Add option to install dark theme

--- frameworks/tier1/breeze-icons/breeze-icons.py
+++ frameworks/tier1/breeze-icons/breeze-icons.py
@@ -18,16 +18,19 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-
         self.subinfo.options.configure.args = " -DBINARY_ICONS_RESOURCE=ON"
+        self.subinfo.options.dynamic.registerOption("useBreezeDrark", False)
 
     def install(self):
         if OsUtils.isWin() or OsUtils.isMac():
             dest = os.path.join(self.installDir(), "bin", "data")
             if not os.path.exists(dest):
                 os.makedirs(dest)
-            utils.copyFile(os.path.join(self.buildDir(), "icons", "breeze-icons.rcc"),
-                           os.path.join(dest, "icontheme.rcc"))
+            if not self.subinfo.options.dynamic.useBreezeDrark:
+                theme = os.path.join(self.buildDir(), "icons", "breeze-icons.rcc")
+            else:
+                theme = os.path.join(self.buildDir(), "icons-dark", "breeze-icons-dark.rcc")
+            utils.copyFile(theme, os.path.join(dest, "icontheme.rcc"))
             return True
         else:
             return CMakePackageBase.install(self)
