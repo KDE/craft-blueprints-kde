ed77b4609a9506fc0aa7ecb5b32c71d7e4d1bd67
Hannah von Reth <vonreth@kde.org>
2017-12-15 14:28:52 +0100

    make kcalcore build

--- kde/pim/kcalcore/kcalcore.py
+++ kde/pim/kcalcore/kcalcore.py
@@ -8,6 +8,11 @@ class subinfo(info.infoclass):
         self.description = "KCalcore library"
         self.patchToApply['17.04.1'] = [("kcalcore-fix-linking.diff", 1)]
         self.patchToApply['17.04.2'] = [("kcalcore-fix-linking.diff", 1)]
+        self.patchToApply['17.12.0'] = [("0003-Remove-strings.h-include.patch", 1),
+                                        ("0001-define-strncasecmp-and-strcasecmp-on-Windows.patch", 1),
+                                        ("0003-Remove-unused-unistd.h-include.patch", 1),
+                                        ("0002-Use-interface-lib-for-libical.patch", 1),
+                                        ("0004-Fix-a-porting-bug-when-persing-timezones.patch", 1)]
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
