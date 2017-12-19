0be2d6083a976f3f8f4b02b58e51f5f9a8387cbc
Kevin Funk <kfunk@kde.org>
2017-12-19 13:25:27 +0100

    kirigami: Tarballs are still named kirigami2
    Reflect this in the recipe...

--- frameworks/tier1/kirigami/kirigami.py
+++ frameworks/tier1/kirigami/kirigami.py
@@ -3,7 +3,11 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.versionInfo.setDefaultValues()
+        self.versionInfo.setDefaultValues(
+            tarballUrl="http://download.kde.org/stable/frameworks/${VERSION_MAJOR}.${VERSION_MINOR}/kirigami2-${VERSION}.tar.xz",
+            tarballDigestUrl="http://download.kde.org/stable/frameworks/${VERSION_MAJOR}.${VERSION_MINOR}/kirigami2-${VERSION}.tar.xz.sha1",
+            tarballInstallSrc = "kirigami2-${VERSION}"
+        )
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = "default"
