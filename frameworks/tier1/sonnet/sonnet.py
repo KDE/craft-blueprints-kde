a4e263eb9aa92726f07500a68c498505e93bb629
Hannah von Reth <vonreth@kde.org>
2017-12-20 15:29:49 +0100

    Fix

--- frameworks/tier1/sonnet/sonnet.py
+++ frameworks/tier1/sonnet/sonnet.py
@@ -4,7 +4,7 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
-        self.patchToApply["5.41.0"] = [("0001-Don-t-cause-circular-linking-on-Windows.patch", 1)
+        self.patchToApply["5.41.0"] = [("0001-Don-t-cause-circular-linking-on-Windows.patch", 1),
                                        ("0001-Remove-anchient-and-broken-workaround.patch", 1)]
         self.patchLevel["5.41.0"] = 2
 
