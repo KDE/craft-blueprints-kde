db02ae28b77b49c223f0013f097e3181c95a44f9
Hannah von Reth <vonreth@kde.org>
2018-01-05 11:12:09 +0100

    Require short path for kdstatemachine

--- qt-apps/kdstatemachineeditor/kdstatemachineeditor.py
+++ qt-apps/kdstatemachineeditor/kdstatemachineeditor.py
@@ -10,10 +10,8 @@ class subinfo(info.infoclass):
             self.archiveNames[ver] = "kdstatemachineeditor-%s.tar.gz" % ver
             self.targetInstSrc[ver] = 'KDStateMachineEditor-%s' % ver
 
-        self.targetDigests['1.2.0'] = (
-            ['c43b864e60c025b1d4eb03f0b3073e00ebb642aecf10dd8b5b29f4a2da2b1c07'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests['1.2.1'] = (
-            ['a97597aa3c67356bba7f6c1503018ddf3369ada5534f33129adab29a53f6ed11'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.2.0'] = (['c43b864e60c025b1d4eb03f0b3073e00ebb642aecf10dd8b5b29f4a2da2b1c07'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.2.1'] = (['a97597aa3c67356bba7f6c1503018ddf3369ada5534f33129adab29a53f6ed11'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "The KDAB State Machine Editor Library is a framework that can be used to help develop full-featured State Machine Editing graphical user interfaces and tools."
         self.defaultTarget = "1.2.1"
@@ -32,3 +30,4 @@ class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
         self.subinfo.options.configure.args = "-DWITH_INTERNAL_GRAPHVIZ=OFF"
+        self.subinfo.options.needsShortPath = True
