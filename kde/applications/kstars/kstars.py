c47d683532d1ce3a8321c2632d570ed0ed5b3820
Jasem Mutlaq <mutlaqja@ikarustech.com>
2018-01-10 08:31:45 +0300

    KStars v2.9.0 release

--- kde/applications/kstars/kstars.py
+++ kde/applications/kstars/kstars.py
@@ -5,10 +5,10 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
         self.description = 'a desktop planetarium'
-        for ver in ['2.8.9']:
+        for ver in ['2.9.0']:
             self.targets[ver] = 'http://download.kde.org/stable/kstars/kstars-%s.tar.xz' % ver
             self.targetInstSrc[ver] = 'kstars-%s' % ver
-        self.defaultTarget = '2.8.9'
+        self.defaultTarget = '2.9.0'
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt5/qtbase"] = "default"
