159bf6f0dbf2c436b82fa658419d7ff7aeb35661
Kevin Funk <kfunk@kde.org>
2018-01-18 12:09:04 +0100

    Add blueprint for labplot
    CCMAIL: alexander.semke@web.de

---
+++ kde/applications/labplot/labplot.py
@@ -0,0 +1,39 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+        self.description = "Interactive graphing and analysis of scientific data"
+
+        for ver in ['2.4.0']:
+            self.targets[ver] = 'http://download.kde.org/stable/labplot/%s/labplot-%s-kf5.tar.xz' % (ver, ver)
+            self.targetInstSrc[ver] = 'labplot-%s-kf5' % ver
+
+        # FIXME: note: 2.4.0 does not build. Wait for a new release.
+        # b/c of: cl : Command line error D8021 : invalid numeric argument '/Wextra'
+        self.defaultTarget = 'master' # '2.4.0'
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
+        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["frameworks/tier1/karchive"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kconfig"] = "default"
+        self.runtimeDependencies["frameworks/tier1/ki18n"] = "default"
+        self.runtimeDependencies["frameworks/tier1/sonnet"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kcoreaddons"] = "default"
+        self.runtimeDependencies["frameworks/tier2/kcrash"] = "default"
+        self.runtimeDependencies["frameworks/tier2/kdoctools"] = "default"
+        self.runtimeDependencies["frameworks/tier3/kdeclarative"] = "default"
+        self.runtimeDependencies["frameworks/tier3/kio"] = "default"
+        self.runtimeDependencies["frameworks/tier3/knewstuff"] = "default"
+        self.runtimeDependencies["frameworks/tier4/kdelibs4support"] = "default"
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
