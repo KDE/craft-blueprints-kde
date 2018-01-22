ae3d738e342a765668549432e441f080a9ba5e44
Dan Leinir Turthra Jensen <admin@leinir.dk>
2018-01-22 13:20:06 +0000

    Install Aspell data in a more Crafty location
    Without this patch, the data ended up in a not very easily
    manageable (craftroot)/lib/aspell-(not-quite-version-number)
    location. This moves the data into (craftroot)/bin/data/aspell
    which makes it much simpler to manage and find.
    
    Differential Revision: https://phabricator.kde.org/D10018

--- data/aspell-data/aspell-data.py
+++ data/aspell-data/aspell-data.py
@@ -3,9 +3,9 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['0.60.6'] = """ http://files.kolab.org/local/windows-ce/aspell-0.60.6-data.zip"""
+        self.targets['0.60'] = """ http://files.kolab.org/local/windows-ce/aspell-0.60.6-data.zip"""
 
-        self.defaultTarget = '0.60.6'
+        self.defaultTarget = '0.60'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/bin-base"] = "default"
@@ -17,3 +17,9 @@ from Package.BinaryPackageBase import *
 class Package(BinaryPackageBase):
     def __init__(self):
         BinaryPackageBase.__init__(self)
+
+    def install(self):
+        utils.createDir(os.path.join(self.imageDir(), "bin", "data"))
+        utils.copyDir(os.path.join(self.sourceDir(), "lib", "aspell-" + self.version),
+                       os.path.join(self.imageDir(), "bin", "data", "aspell"))
+        return True
