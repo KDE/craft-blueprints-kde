7d6c0fb292cf17bc540246aeca12b2d1af734c4e
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2018-01-26 16:23:44 +0100

    Make MPIR 3.0.0 work with MSVC 2017
    In order to compile MPIR 3.0.0 on MSVC 2017 one have to:
    1) provide WindowsTargetPlatformVersion higher than Windows 8.1,
    2) enable targets in mpir.sln for compilation.
    
    It seems that maintainer of MPIR prefers to build it from msbuild.bat,
    so he left mpir.sln unselected inside for users to make selections. In
    effect, if you provide targets to be build in command line, then they
    won't be built, because they weren't preselected for compilation.
    
    I see no other way to preselect targets in mpir.sln than edyting it
    forcefully with python.
    
    Differential Revision: https://phabricator.kde.org/D10127

--- win32libs/mpir/mpir.py
+++ win32libs/mpir/mpir.py
@@ -1,16 +1,20 @@
+# copyright:
+# Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
+
 import info
+import re
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['2.7.2']:
+        for ver in ['3.0.0']:
             self.targets[ver] = 'http://www.mpir.org/mpir-' + ver + '.tar.bz2'
             self.targetInstSrc[ver] = "mpir-" + ver
-        self.targetDigests['2.7.2'] = 'a285352d4299eb18d4f02a97e3232efab225e174'
-        self.targetInstSrc['2.7.2'] = 'mpir-2.7.2'
+        self.targetDigests['3.0.0'] = (['52f63459cf3f9478859de29e00357f004050ead70b45913f2c2269d9708675bb'], CraftHash.HashAlgorithm.SHA256)
+        self.targetInstSrc['3.0.0'] = 'mpir-3.0.0'
 
         self.description = "Library for arbitrary precision integer arithmetic derived from version 4.2.1 of gmp"
-        self.defaultTarget = '2.7.2'
+        self.defaultTarget = '3.0.0'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
@@ -37,14 +41,47 @@ class PackageMinGW(AutoToolsPackageBase):
 class PackageMSVC(MSBuildPackageBase):
     def __init__(self, **args):
         MSBuildPackageBase.__init__(self)
-        self.mpirBuildDir = os.path.join(self.sourceDir(), "build.vc14")
+        if CraftCore.compiler.getInternalVersion() == 14:
+            self.mpirBuildDir = os.path.join(self.sourceDir(), "build.vc14")
+        else:
+            self.mpirBuildDir = os.path.join(self.sourceDir(), "build.vc15")
+
         if CraftCore.compiler.isX86():
             self.subinfo.options.configure.args = " /p:Platform=win32"
+
+        self.subinfo.options.configure.args = self.subinfo.options.configure.args + " /p:WindowsTargetPlatformVersion=" + os.getenv('WINDOWSSDKVERSION')[:-1]
         self.subinfo.options.configure.projectFile = os.path.join(self.mpirBuildDir, "mpir.sln")
         self.msbuildTargets = ["dll_mpir_gc", "lib_mpir_cxx"]
 
+    def adjustProjectFile(self):
+        path = self.subinfo.options.configure.projectFile
+        targets = self.msbuildTargets
+        #open project file to add default build targets
+        f = open(str(path), "r+")
+        projectFileContents = f.readlines()
+        IDS = [[] for _ in range(len(targets))]
+        for i in range(len(projectFileContents)):
+            for j in range(len(targets)):
+                #fetch target ids, to add them latter for building
+                m = re.search(targets[j] + "[^{]*{(?P<ID>[^{}]*)", projectFileContents[i])
+                if m is not None:
+                    IDS[j] = m.group('ID')
+
+            if "ProjectConfigurationPlatforms" in projectFileContents[i]:
+                for ID in IDS:
+                    projectFileContents.insert(i + 1,
+                                            "{"+ ID + "}.Release|Win32.Build.0 = Release|Win32\n")  #3.0.0 version needs those lines, otherwise it won't compile anything
+                    projectFileContents.insert(i + 1,
+                                            "{"+ ID + "}.Release|x64.Build.0 = Release|x64\n")
+                break
+
+        f.seek(0)
+        f.write(''.join(projectFileContents))
+        f.close()
+
     def make(self):
         utils.putenv('YASMPATH', os.path.join(self.rootdir, 'dev-utils', 'bin'))
+        self.adjustProjectFile()
         return MSBuildPackageBase.make(self)
 
     def install(self):
