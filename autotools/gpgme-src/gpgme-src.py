15e1e9c82856a8cec3da400268c91c437cc56711
Filipe Azevedo <filipe.azevedo@kdab.com>
2018-01-30 15:34:51 +0100

    Allow to build Gcrypt, Gpgme and assuan2 on macOS
    This make is less dependent to brew.

--- autotools/gpgme-src/gpgme-src.py
+++ autotools/gpgme-src/gpgme-src.py
@@ -1,4 +1,5 @@
 import info
+import CraftCore
 
 
 class subinfo(info.infoclass):
@@ -24,7 +25,36 @@ from Package.AutoToolsPackageBase import *
 class Package(AutoToolsPackageBase):
     def __init__( self, **args ):
         AutoToolsPackageBase.__init__( self )
-        self.subinfo.options.configure.args = "--enable-languages=no"
+        if OsUtils.isWin():
+            self.subinfo.options.configure.args = "--enable-languages=no"
+        elif OsUtils.isMac():
+            # The configure script does not honnor the env var is PKG_CONFIG is not installed / env var not set
+            # This is problematic especially on macOS, let manually fill the env var to make configure happy finding Qt
+            PKG_CONFIG = "echo"
+            # Gpgme rely on pkg-config to discover Qt, but pkg config files are not shipped / generated...
+            # So we need to help it to discover Qt
+            _, QT_BINS = CraftCore.cache.getCommandOutput("qmake", "-query QT_INSTALL_BINS")
+            _, QT_LIBS = CraftCore.cache.getCommandOutput("qmake", "-query QT_INSTALL_LIBS")
+            if QT_BINS:
+                QT_BINS = QT_BINS.strip();
+            if QT_LIBS:
+                QT_LIBS = QT_LIBS.strip();
+            print(f"QT_BINS: {QT_BINS}")
+            print(f"QT_LIBS: {QT_LIBS}")
+            MOC = f"{QT_BINS}/moc"
+            GPGME_QT_CFLAGS = f"-F{QT_LIBS} -I{QT_LIBS}/QtCore.framework/Headers -DQT_NO_DEBUG -DQT_CORE_LIB"
+            GPGME_QT_LIBS = f"-F{QT_LIBS} -framework QtCore"
+            GPGME_QTTEST_CFLAGS = f"-F{QT_LIBS} -I{QT_LIBS}/QtTest.framework/Headers -DQT_NO_DEBUG -DQT_TEST_LIB"
+            GPGME_QTTEST_LIBS = f"-F{QT_LIBS} -framework QtTest"
+            self.subinfo.options.configure.args = (f"PKG_CONFIG='{PKG_CONFIG}'"
+                f" MOC='{MOC}'"
+                f" GPGME_QT_CFLAGS='{GPGME_QT_CFLAGS}'"
+                f" GPGME_QT_LIBS='{GPGME_QT_LIBS}'"
+                f" GPGME_QTTEST_CFLAGS='{GPGME_QTTEST_CFLAGS}'"
+                f" GPGME_QTTEST_LIBS='{GPGME_QTTEST_LIBS}'"
+                " --enable-languages=cpp,qt")
+        else:
+            self.subinfo.options.configure.args = "--enable-languages=cpp,qt"
 
     def install(self):
         if not AutoToolsPackageBase.install(self):
