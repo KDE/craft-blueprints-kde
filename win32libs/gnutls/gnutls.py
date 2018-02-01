3222b72d82cbdf370eaa0ad46599b5666222ad18
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2018-02-01 19:35:32 +0100

    Update GnuTLS
    Differential Revision: https://phabricator.kde.org/D10239

--- win32libs/gnutls/gnutls.py
+++ win32libs/gnutls/gnutls.py
@@ -7,17 +7,19 @@ from Package.VirtualPackageBase import VirtualPackageBase
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["3.3.10"]:
-            self.targets[ver] = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.3/gnutls-%s.tar.xz" % ver
+        for ver in ["3.5.17"]:
+            self.targets[ver] = "ftp://ftp.gnutls.org/gcrypt/gnutls/v3.5/gnutls-%s.tar.xz" % ver
             self.targetInstSrc[ver] = "gnutls-%s" % ver
-        self.patchToApply["3.3.10"] = ("0005-fix-strtok-conflict.mingw.patch", 1)
-        self.targetDigests['3.3.10'] = 'b47af4ee116ba2099a24ff7a8e686079f80ec23a'
+        # self.patchToApply["3.5.17"] = ("0005-fix-strtok-conflict.mingw.patch", 1)
+        self.targetDigests['3.5.17'] = (['86b142afef587c118d63f72ccf307f3321dbc40357aae528202b65d913d20919'], CraftHash.HashAlgorithm.SHA256)
         self.description = "A library which provides a secure layer over a reliable transport layer"
-        self.defaultTarget = "3.3.10"
+        self.defaultTarget = "3.5.17"
 
     def setDependencies(self):
         self.runtimeDependencies["win32libs/gcrypt"] = "default"
         self.runtimeDependencies["win32libs/nettle"] = "default"
+        self.runtimeDependencies["autotools/libtasn1"] = "default"
+        self.runtimeDependencies["autotools/p11kit"] = "default"
         if CraftCore.compiler.isMinGW():
             self.buildDependencies["dev-util/msys"] = "default"
 
@@ -25,7 +27,7 @@ class subinfo(info.infoclass):
 class PackageMinGW(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.configure.args = "--with-zlib --enable-shared --disable-static --enable-cxx --enable-nls --disable-rpath --disable-gtk-doc --disable-guile --disable-libdane "
+        self.subinfo.options.configure.args = "--with-zlib --enable-shared --disable-static --enable-cxx --enable-nls --disable-rpath --disable-gtk-doc --disable-guile --disable-libdane --with-included-unistring "
         self.subinfo.options.configure.cflags = "-I%s/usr/include " % utils.toMSysPath(
             CraftStandardDirs.msysDir())  # could cause problems but we need the autotools headers
         self.subinfo.options.configure.ldflags = "-L%s/usr/lib " % utils.toMSysPath(
