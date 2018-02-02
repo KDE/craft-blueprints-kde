fb930b4cbcc6fe7aca2f292353f639950a11a1a5
Hannah von Reth <vonreth@kde.org>
2018-02-02 16:17:09 +0100

    Build shared

--- win32libs/qrencode/qrencode.py
+++ win32libs/qrencode/qrencode.py
@@ -23,4 +23,4 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args = "-DWITH_TOOLS=OFF"
+        self.subinfo.options.configure.args = "-DWITH_TOOLS=OFF -DBUILD_SHARED_LIBS=ON"
