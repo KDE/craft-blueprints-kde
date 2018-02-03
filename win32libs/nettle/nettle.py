f62a28278f0040b042c415a54f99c3e6f6c91dac
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2018-02-03 09:10:19 +0100

    Update nettle
    Differential Revision: https://phabricator.kde.org/D10264

--- win32libs/nettle/nettle.py
+++ win32libs/nettle/nettle.py
@@ -7,14 +7,14 @@ from Package.VirtualPackageBase import VirtualPackageBase
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2.7.1"]:
+        for ver in ["3.4"]:
             self.targets[ver] = "http://www.lysator.liu.se/~nisse/archive/nettle-%s.tar.gz" % ver
             self.targetInstSrc[ver] = "nettle-%s" % ver
-            self.patchToApply[ver] = ("nettle-2.7.1-20140630.diff", 1)
-        self.targetDigests['2.7.1'] = 'e7477df5f66e650c4c4738ec8e01c2efdb5d1211'
+            self.patchToApply[ver] = ("nettle-3.4-20180203.diff", 1)
+        self.targetDigests['3.4'] = (['ae7a42df026550b85daca8389b6a60ba6313b0567f374392e54918588a411e94'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "A low-level cryptographic library"
-        self.defaultTarget = "2.7.1"
+        self.defaultTarget = "3.4"
 
     def setDependencies(self):
         if CraftCore.compiler.isMinGW():
