9c7e11f46556fd6cbdfd1a2371d0dd1da00b1c0f
Jaroslaw Staniek <staniek@kde.org>
2018-02-11 23:47:43 +0100

    Add kreport blueprint
    Test Plan:
    craft kreport
    craft --test kreport
    Run kreportexample from BUILDDDIR/bin/
    
    Reviewers: vonreth, #craft, piggz
    
    Reviewed By: vonreth, #craft
    
    Differential Revision: https://phabricator.kde.org/D10399

---
+++ extragear/kreport/kreport.py
@@ -0,0 +1,33 @@
+import info
+from CraftOS.osutils import OsUtils
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        versions = ['3.1', 'master']
+        for ver in versions:
+            self.svnTargets[ver] = f"git://anongit.kde.org/kreport|{ver}"
+        self.defaultTarget = versions[0]
+        self.description = "A framework for the creation and generation of reports in multiple formats"
+        self.options.configure.args = " -DBUILD_EXAMPLES=ON"
+
+    def setDependencies(self):
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
+        self.buildDependencies["dev-util/python2"] = "default"
+        self.runtimeDependencies["libs/qt5/qtwebkit"] = "default"
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kcoreaddons"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kconfig"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kwidgetsaddons"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kguiaddons"] = "default"
+        self.runtimeDependencies["frameworks/tier1/breeze-icons"] = "default" # hard dependency for now
+        self.runtimeDependencies["extragear/kproperty"] = "default"
+        # TODO Windows/Mac: add marble libs (we only need marble widget), for now marble libs are disabled there
+        if not OsUtils.isWin() and not OsUtils.isMac():
+            self.runtimeDependencies["kde/applications/marble"] = "default"
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
