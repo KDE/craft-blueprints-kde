3fa6ce9feb5c64bdb4118017c813b75bb0fcd467
Hannah von Reth <vonreth@kde.org>
2018-02-15 20:08:06 +0100

    Fix lldb with mingw

--- win32libs/llvm-meta/lldb/lldb.py
+++ win32libs/llvm-meta/lldb/lldb.py
@@ -5,6 +5,8 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
+        if CraftCore.compiler.isMinGW():
+            self.patchToApply["5.0.1"] = [("0501-fixes-to-get-lldb-building.patch", 1)]#https://github.com/Alexpux/MINGW-packages/blob/5cbc0add24760165f557fd00bd9d915279ea4611/mingw-w64-clang/0501-fixes-to-get-lldb-building.patch
 
 
     def setDependencies(self):
