b6440abc4f7692c3453ba0734d3e70c7ecc5d49b
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2018-02-18 14:51:14 +0100

    Update aqbanking to 5.7.8
    Differential Revision: https://phabricator.kde.org/D10639

--- autotools/aqbanking/aqbanking.py
+++ autotools/aqbanking/aqbanking.py
@@ -4,15 +4,16 @@
 
 import info
 from Package.AutoToolsPackageBase import *
+from CraftOS.osutils import OsUtils
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets["5.7.6"] = "https://www.aquamaniac.de/sites/download/download.php?package=03&release=215&file=01&dummy=aqbanking-5.7.6beta.tar.gz"
-        self.targetDigests["5.7.6"] = (['f9420d8b34c2eee703a7d26dd71a849700fdb2d7372b7649a3488b5a69f55565'], CraftHash.HashAlgorithm.SHA256)
-        self.archiveNames["5.7.6"] = "aqbanking-5.7.6beta.tar.gz"
-        self.targetInstSrc["5.7.6"] = "aqbanking-5.7.6beta"
+        self.targets["5.7.8"] = "https://www.aquamaniac.de/sites/download/download.php?package=03&release=217&file=02&dummy=aqbanking-5.7.8.tar.gz"
+        self.targetDigests["5.7.8"] = (['16f86e4cc49a9eaaa8dfe3206607e627873208bce45a70030c3caea9b5afc768'], CraftHash.HashAlgorithm.SHA256)
+        self.archiveNames["5.7.8"] = "aqbanking-5.7.8.tar.gz"
+        self.targetInstSrc["5.7.8"] = "aqbanking-5.7.8"
 
-        self.defaultTarget = "5.7.6"
+        self.defaultTarget = "5.7.8"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
@@ -27,3 +28,23 @@ class Package(AutoToolsPackageBase):
         self.subinfo.options.configure.args += " --disable-static --enable-shared "
         # this prevents "cannot find the library libaqhbci.la or unhandled argument libaqhbci.la"
         self.subinfo.options.make.supportsMultijob = False
+
+    def install(self):
+        if OsUtils.isWin():
+            cmakes = [ os.path.join(self.buildDir(), "aqbanking-config.cmake") ]
+            for cmake in cmakes:
+                f = open(cmake, "r+")
+                cmakeFileContents = f.readlines()
+                for i in range(len(cmakeFileContents)):
+                    m = re.search('set_and_check\(prefix "(?P<root>[^"]*)"\)', cmakeFileContents[i])
+                    if m is not None:
+                        cmakeFileContents[i] = cmakeFileContents[i].replace(m.group('root'), CraftStandardDirs.craftRoot()[:-1])
+
+                    m2 = re.search("libaqbanking.so", cmakeFileContents[i])
+                    if m2 is not None:
+                        cmakeFileContents[i] = cmakeFileContents[i].replace("lib/libaqbanking.so", "bin/libaqbanking-35.dll")
+
+                f.seek(0)
+                f.write(''.join(cmakeFileContents))
+                f.close()
+        return AutoToolsPackageBase.install(self)
--- autotools/gwenhywfar/gwenhywfar.py
+++ autotools/gwenhywfar/gwenhywfar.py
@@ -2,17 +2,20 @@
 
 # Copyright (C) 2018 Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
 
+import re
 import info
 from Package.AutoToolsPackageBase import *
+from CraftOS.osutils import OsUtils
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets["4.18"] = "https://www.aquamaniac.de/sites/download/download.php?package=01&release=206&file=01&dummy=gwenhywfar-4.18.0.tar.gz"
-        self.targetDigests["4.18"] = (['6915bba42d8b7f0213cee186a944296e5e5e97cdbde5b539a924261af03086ca'], CraftHash.HashAlgorithm.SHA256)
-        self.archiveNames["4.18"] = "gwenhywfar-4.18.0.tar.gz"
-        self.targetInstSrc["4.18"] = "gwenhywfar-4.18.0"
-        self.patchToApply["4.18"] = [("gwenhywfar-qt-detection.diff", 1)]
-        self.defaultTarget = "4.18"
+        self.targets["4.19"] = "https://www.aquamaniac.de/sites/download/download.php?package=01&release=207&file=01&dummy=gwenhywfar-4.19.0.tar.gz"
+        self.targetDigests["4.19"] = (['c54a9a162dc63ab69e4d3fc946aae92b929383ca60a2690b539adcdc58de9495'], CraftHash.HashAlgorithm.SHA256)
+        self.archiveNames["4.19"] = "gwenhywfar-4.19.0.tar.gz"
+        self.targetInstSrc["4.19"] = "gwenhywfar-4.19.0"
+        self.patchToApply["4.19"] = [("gwenhywfar-4.19.0-20180218.diff", 1)]
+        # self.patchToApply["4.19"] = [("gwenhywfar-qt-detection.diff", 1)]
+        self.defaultTarget = "4.19"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
@@ -27,7 +30,7 @@ class subinfo(info.infoclass):
 class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.configure.args = " --disable-static --enable-shared --disable-binreloc --with-guis=cpp qt5 "
+        self.subinfo.options.configure.args += " --disable-static --enable-shared --disable-binreloc --with-guis='qt5 cpp'"
 
     def configure(self):
         _, includedir = CraftCore.cache.getCommandOutput("qmake", "-query QT_INSTALL_HEADERS")
@@ -36,8 +39,36 @@ class Package(AutoToolsPackageBase):
         guidir = self.shell.toNativePath(os.path.join(includedir , "QtGui"))
         coredir = self.shell.toNativePath(os.path.join(includedir , "QtCore"))
 
-        self.subinfo.options.configure.ldflags += '-lQt5Widgets -lQt5Gui -lQt5Core '
         self.subinfo.options.configure.cxxflags += f"-I{widgetsdir} -I{guidir} -I{coredir} -I{includedir} "
         return super().configure()
 
+    def install(self):
+        if OsUtils.isWin():
+            cmakes = [ os.path.join(self.buildDir(), "gui", "cpp", "gwengui-cpp-config.cmake"),
+                    os.path.join(self.buildDir(), "gui", "qt5", "gwengui-qt5-config.cmake"),
+                    os.path.join(self.buildDir(), "gwenhywfar-config.cmake")
+                    ]
+            for cmake in cmakes:
+                f = open(cmake, "r+")
+                cmakeFileContents = f.readlines()
+                for i in range(len(cmakeFileContents)):
+                    m = re.search('set_and_check\(prefix "(?P<root>[^"]*)"\)', cmakeFileContents[i])
+                    if m is not None:
+                        cmakeFileContents[i] = cmakeFileContents[i].replace(m.group('root'), CraftStandardDirs.craftRoot()[:-1])
 
+                    m2 = re.search("libgwenhywfar.so.(?P<number>[\d]*)", cmakeFileContents[i])
+                    if m2 is not None:
+                        cmakeFileContents[i] = cmakeFileContents[i].replace("lib/" + m2.group(0), "bin/libgwenhywfar-" + m2.group('number') +".dll")
+
+                    m3 = re.search("libgwengui-cpp.so", cmakeFileContents[i])
+                    if m3 is not None:
+                        cmakeFileContents[i] = cmakeFileContents[i].replace("lib/libgwengui-cpp.so", "bin/libgwengui-cpp-0.dll")
+
+                    m4 = re.search("libgwengui-qt5.so", cmakeFileContents[i])
+                    if m4 is not None:
+                        cmakeFileContents[i] = cmakeFileContents[i].replace("lib/libgwengui-qt5.so", "lib/libgwengui-qt5.a")
+
+                f.seek(0)
+                f.write(''.join(cmakeFileContents))
+                f.close()
+        return AutoToolsPackageBase.install(self)
