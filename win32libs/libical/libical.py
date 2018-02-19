c622d2370de3f1e16e38dd3f2c15f9c8040ea80d
Hannah von Reth <vonreth@kde.org>
2018-02-19 20:58:32 +0100

    Make ical find icu...

--- win32libs/libical/libical.py
+++ win32libs/libical/libical.py
@@ -27,4 +27,4 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args = " -DUSE_BUILTIN_TZDATA=ON -DICAL_UNIX_NEWLINE=OFF -DICAL_GLIB=OFF -DSHARED_ONLY=ON"
+        self.subinfo.options.configure.args = f" -DUSE_BUILTIN_TZDATA=ON -DICAL_UNIX_NEWLINE=OFF -DICAL_GLIB=OFF -DSHARED_ONLY=ON -DICU_BASE={CraftCore.standardDirs.craftRoot()}"
