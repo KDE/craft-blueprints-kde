ca5e9e04c356a894c7bf401393e956b656b815f9
Hannah von Reth <vonreth@kde.org>
2018-02-19 20:32:02 +0100

    Update ical

--- win32libs/libical/libical.py
+++ win32libs/libical/libical.py
@@ -3,7 +3,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2.0.0", "3.0.1"]:
+        for ver in ["2.0.0", "3.0.1", "3.0.2"]:
             self.targets[ver] = f"https://github.com/libical/libical/archive/v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"libical-{ver}"
             self.archiveNames[ver] = f"libical-{ver}.tar.gz"
@@ -11,9 +11,11 @@ class subinfo(info.infoclass):
         self.patchToApply["2.0.0"] = [("001-libical-2.0.0-search-snprintf.diff", 1)]
         self.targetDigests["2.0.0"] = (["20f4a98475052e1200d2691ba50b27969e4bedc6e50bffd5e2fa81f4ac90de9a"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["3.0.1"] = (['6405964d596aac64fc82c03c4486387fd6a9c09b1f7af1ff251238e66b9e66e1'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["3.0.2"] = (['f9699be8521e1aea190a7c87802805d3388e0a3f59a9b9faedec490f596807a0'],  CraftHash.HashAlgorithm.SHA256)
 
         self.description = "Reference implementation of the icalendar data type and serialization format"
-        self.defaultTarget = "3.0.1"
+        self.webpage = "http://libical.github.io/libical/"
+        self.defaultTarget = "3.0.2"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
