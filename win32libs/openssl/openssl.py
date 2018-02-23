fb8da38781b0a508897240ff72fa6fd44e361b28
Hannah von Reth <vonreth@kde.org>
2018-02-23 18:02:50 +0100

    Only install openssl dynamic

--- win32libs/openssl/openssl.py
+++ win32libs/openssl/openssl.py
@@ -23,7 +23,7 @@ class subinfo(info.infoclass):
         self.description = "The OpenSSL runtime environment"
 
         #set the default config for openssl 1.1
-        self.options.configure.args = "no-zlib threads no-rc5 no-idea no-ssl3-method no-weak-ssl-ciphers no-heartbeats"
+        self.options.configure.args = "shared no-zlib threads no-rc5 no-idea no-ssl3-method no-weak-ssl-ciphers no-heartbeats"
 
         self.defaultTarget = '1.0.2m'
 
@@ -55,70 +55,73 @@ class PackageCMake(CMakePackageBase):
     def configure( self, defines=""):
         if self.subinfo.opensslUseLegacyBuildSystem:
             return True
-        self.enterBuildDir()
-        prefix = OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())
-        return utils.system(["perl", os.path.join(self.sourceDir(), "Configure"), f"--prefix={prefix}", f"--openssldir={prefix}/ssl"]
-                            + self.subinfo.options.configure.args.split(" ")
-                            + ["-FS",
-                                f"-I{OsUtils.toUnixPath(os.path.join(CraftStandardDirs.craftRoot(), 'include'))}",
-                                "VC-WIN64A" if CraftCore.compiler.isX64() else "VC-WIN32"])
+        else:
+            self.enterBuildDir()
+            prefix = OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())
+            return utils.system(["perl", os.path.join(self.sourceDir(), "Configure"), f"--prefix={prefix}", f"--openssldir={prefix}/ssl"]
+                                + self.subinfo.options.configure.args.split(" ")
+                                + ["-FS",
+                                    f"-I{OsUtils.toUnixPath(os.path.join(CraftStandardDirs.craftRoot(), 'include'))}",
+                                    "VC-WIN64A" if CraftCore.compiler.isX64() else "VC-WIN32"])
 
 
     def compile(self):
         if not self.subinfo.opensslUseLegacyBuildSystem:
             return super().compile()
-        self.enterSourceDir()
-        cmd = ""
-        if CraftCore.compiler.isX64():
-            config = "VC-WIN64A"
         else:
-            config = "VC-WIN32"
+            self.enterSourceDir()
+            cmd = ""
+            if CraftCore.compiler.isX64():
+                config = "VC-WIN64A"
+            else:
+                config = "VC-WIN32"
 
-        if not utils.system("perl Configure %s" % config):
-            return False
-
-        if CraftCore.compiler.isX64():
-            if not utils.system("ms\do_win64a.bat"):
-                return False
-        else:
-            if not utils.system("ms\do_nasm.bat"):
+            if not utils.system("perl Configure %s" % config):
                 return False
 
-        if self.staticBuild:
-            cmd = r"nmake -f ms\nt.mak"
-        else:
-            cmd = r"nmake -f ms\ntdll.mak"
+            if CraftCore.compiler.isX64():
+                if not utils.system("ms\do_win64a.bat"):
+                    return False
+            else:
+                if not utils.system("ms\do_nasm.bat"):
+                    return False
 
-        return utils.system(cmd)
+            if self.staticBuild:
+                cmd = r"nmake -f ms\nt.mak"
+            else:
+                cmd = r"nmake -f ms\ntdll.mak"
+
+            return utils.system(cmd)
 
     def install(self):
         if not self.subinfo.opensslUseLegacyBuildSystem:
             return super().install()
-        src = self.sourceDir()
-        dst = self.imageDir()
-
-        if not os.path.isdir(dst):
-            os.mkdir(dst)
-        if not os.path.isdir(os.path.join(dst, "bin")):
-            os.mkdir(os.path.join(dst, "bin"))
-        if not os.path.isdir(os.path.join(dst, "lib")):
-            os.mkdir(os.path.join(dst, "lib"))
-        if not os.path.isdir(os.path.join(dst, "include")):
-            os.mkdir(os.path.join(dst, "include"))
-
-        if self.staticBuild:
-            outdir = "out32"
         else:
-            outdir = "out32dll"
+            src = self.sourceDir()
+            dst = self.imageDir()
 
-        if not self.staticBuild:
-            shutil.copy(os.path.join(src, outdir, "libeay32.dll"), os.path.join(dst, "bin"))
-            shutil.copy(os.path.join(src, outdir, "ssleay32.dll"), os.path.join(dst, "bin"))
-        shutil.copy(os.path.join(src, outdir, "libeay32.lib"), os.path.join(dst, "lib"))
-        shutil.copy(os.path.join(src, outdir, "ssleay32.lib"), os.path.join(dst, "lib"))
-        utils.copyDir(os.path.join(src, "inc32"), os.path.join(dst, "include"))
+            if not os.path.isdir(dst):
+                os.mkdir(dst)
+            if not os.path.isdir(os.path.join(dst, "bin")):
+                os.mkdir(os.path.join(dst, "bin"))
+            if not os.path.isdir(os.path.join(dst, "lib")):
+                os.mkdir(os.path.join(dst, "lib"))
+            if not os.path.isdir(os.path.join(dst, "include")):
+                os.mkdir(os.path.join(dst, "include"))
 
-        return True
+            if self.staticBuild:
+                outdir = "out32"
+            else:
+                outdir = "out32dll"
+
+            if not self.staticBuild:
+                shutil.copy(os.path.join(src, outdir, "libeay32.dll"), os.path.join(dst, "bin"))
+                shutil.copy(os.path.join(src, outdir, "ssleay32.dll"), os.path.join(dst, "bin"))
+            shutil.copy(os.path.join(src, outdir, "libeay32.lib"), os.path.join(dst, "lib"))
+            shutil.copy(os.path.join(src, outdir, "ssleay32.lib"), os.path.join(dst, "lib"))
+            utils.copyDir(os.path.join(src, "inc32"), os.path.join(dst, "include"))
+
+            return True
 
 
 from Package.AutoToolsPackageBase import *
@@ -146,24 +149,32 @@ class PackageMSys(AutoToolsPackageBase):
     def make(self, dummyBuildType=None):
         if not self.subinfo.opensslUseLegacyBuildSystem:
             return super().make()
-        return self.shell.execute(self.sourceDir(), self.makeProgram, "depend") and \
-               AutoToolsPackageBase.make(self, dummyBuildType)
+        else:
+            return self.shell.execute(self.sourceDir(), self.makeProgram, "depend") and \
+                AutoToolsPackageBase.make(self, dummyBuildType)
 
     def install(self):
         if not self.subinfo.opensslUseLegacyBuildSystem:
-            return super().install()
-        self.enterSourceDir()
-        self.shell.execute(self.sourceDir(), self.makeProgram,
-                           "INSTALLTOP=%s install_sw" % (self.shell.toNativePath(self.imageDir())))
-        if OsUtils.isWin():
-            self.shell.execute(os.path.join(self.imageDir(), "lib"), "chmod", "-R 664 .")
-            self.shell.execute(os.path.join(self.imageDir(), "lib", "engines"), "chmod", " -R 755 .")
-            self.shell.execute(os.path.join(self.imageDir(), "bin"), "chmod", " -R 755 .")
-            shutil.move(os.path.join(self.imageDir(), "lib", "libcrypto.dll.a"),
-                        os.path.join(self.imageDir(), "lib", "libeay32.dll.a"))
-            shutil.move(os.path.join(self.imageDir(), "lib", "libssl.dll.a"),
-                        os.path.join(self.imageDir(), "lib", "ssleay32.dll.a"))
-        return True
+            # TODO: don't install doc
+            if not super().install():
+                return False
+            # we don't want people to link to the static build but openssl doesn't provide an option to
+            # disable the static build
+            return (utils.deleteFile(os.path.join(self.installDir(), "lib", "libcrypto.a")) and
+                    utils.deleteFile(os.path.join(self.installDir(), "lib", "libssl.a")))
+        else:
+            self.enterSourceDir()
+            self.shell.execute(self.sourceDir(), self.makeProgram,
+                            "INSTALLTOP=%s install_sw" % (self.shell.toNativePath(self.imageDir())))
+            if OsUtils.isWin():
+                self.shell.execute(os.path.join(self.imageDir(), "lib"), "chmod", "-R 664 .")
+                self.shell.execute(os.path.join(self.imageDir(), "lib", "engines"), "chmod", " -R 755 .")
+                self.shell.execute(os.path.join(self.imageDir(), "bin"), "chmod", " -R 755 .")
+                shutil.move(os.path.join(self.imageDir(), "lib", "libcrypto.dll.a"),
+                            os.path.join(self.imageDir(), "lib", "libeay32.dll.a"))
+                shutil.move(os.path.join(self.imageDir(), "lib", "libssl.dll.a"),
+                            os.path.join(self.imageDir(), "lib", "ssleay32.dll.a"))
+            return True
 
 if CraftCore.compiler.isGCCLike():
     class Package(PackageMSys):
