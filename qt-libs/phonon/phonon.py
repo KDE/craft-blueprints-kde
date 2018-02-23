e77f65ffa70df971fb790f8be8f3b6bf432089a6
Hannah von Reth <vonreth@kde.org>
2018-02-23 19:58:26 +0100

    Update phonon to 4.10.0

--- qt-libs/phonon/phonon.py
+++ qt-libs/phonon/phonon.py
@@ -3,23 +3,20 @@ import info
 
 class subinfo(info.infoclass):
     def setDependencies(self):
-        self.runtimeDependencies["virtual/base"] = "default"
-        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
         # qtquick1 is optional
-        # self.runtimeDependencies['libs/qtquick1'] = 'default'
+        # self.runtimeDependencies["libs/qtquick1"] = "default"
 
     def setTargets(self):
-        self.svnTargets['master'] = 'git://anongit.kde.org/phonon'
-        for ver in ['4.9.0', '4.9.1']:
-            self.targets[ver] = 'http://download.kde.org/stable/phonon/%s/phonon-%s.tar.xz' % (ver, ver)
-            self.targetInstSrc[ver] = 'phonon-%s' % ver
-        self.targetDigests['4.9.0'] = (
-            ['bb74b40f18ade1d9ab89ffcd7aeb7555be797ca395f1224c488b394da6deb0e0'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests['4.9.1'] = (
-            ['67bee986f85ca8b575186c8ba58a85886cb3b1c3567c86a118d56129f221e69c'], CraftHash.HashAlgorithm.SHA256)
-
+        self.svnTargets["master"] = "git://anongit.kde.org/phonon"
+        for ver in ["4.10.0"]:
+            self.targets[ver] = f"https://download.kde.org/stable/phonon/{ver}/phonon-{ver}.tar.xz"
+            self.targetDigestUrls[ver] = f"https://download.kde.org/stable/phonon/{ver}/phonon-{ver}.tar.xz.sha256"
+            self.targetInstSrc[ver] = f"phonon-{ver}"
         self.description = "a Qt based multimedia framework"
-        self.defaultTarget = '4.9.1'
+        self.defaultTarget = "4.10.0"
 
 
 from Package.CMakePackageBase import *
@@ -29,6 +26,6 @@ class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
         self.subinfo.options.package.disableBinaryCache = True
-        self.subinfo.options.configure.args = " -DPHONON_BUILD_EXAMPLES=OFF -DPHONON_BUILD_TESTS=OFF -DPHONON_INSTALL_QT_EXTENSIONS_INTO_SYSTEM_QT=ON -DPHONON_BUILD_PHONON4QT5=ON"
+        self.subinfo.options.configure.args = " -DPHONON_INSTALL_QT_EXTENSIONS_INTO_SYSTEM_QT=ON -DPHONON_BUILD_PHONON4QT5=ON"
         if not self.subinfo.options.isActive("win32libs/dbus"):
             self.subinfo.options.configure.args += " -DPHONON_NO_DBUS=ON "
