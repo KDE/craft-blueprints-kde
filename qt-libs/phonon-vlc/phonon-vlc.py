8f1426e7de5a66aa8a084f4691888cef169bf024
Hannah von Reth <vonreth@kde.org>
2018-02-24 12:07:16 +0100

    Update phonon-vlc to 0.10.0

--- qt-libs/phonon-vlc/phonon-vlc.py
+++ qt-libs/phonon-vlc/phonon-vlc.py
@@ -6,29 +6,23 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setDependencies(self):
-        self.runtimeDependencies["qt-libs/phonon"] = "default"
-        self.runtimeDependencies["binary/vlc"] = "default"
+        self.runtimeDependencies["qt-libs/phonon"] = None
+        self.runtimeDependencies["binary/vlc"] = None
         if CraftCore.compiler.isMSVC() or CraftCore.compiler.isIntel():
-            self.runtimeDependencies["kdesupport/kdewin"] = "default"
+            self.runtimeDependencies["kdesupport/kdewin"] = None
 
     def setTargets(self):
-        for ver in ['0.9.0']:
-            self.targets[
-                ver] = "http://download.kde.org/stable/phonon/phonon-backend-vlc/%s/phonon-backend-vlc-%s.tar.xz" % (
-            ver, ver)
-            self.targetInstSrc[ver] = "phonon-vlc-%s" % ver
-
-        self.targetDigests['0.9.0'] = (
-            ['c0ced7ca571acc22211eecf5158241714fa9ccdb82d4fe0a970ad702860ccdbe'], CraftHash.HashAlgorithm.SHA256)
-
-        self.svnTargets['master'] = 'git://anongit.kde.org/phonon-vlc'
+        for ver in ["0.10.0"]:
+            self.targets[ver] = f"https://download.kde.org/stable/phonon/phonon-backend-vlc/{ver}/phonon-backend-vlc-{ver}.tar.xz"
+            self.targetDigestUrls[ver] = f"https://download.kde.org/stable/phonon/phonon-backend-vlc/{ver}/phonon-backend-vlc-{ver}.tar.xz.sha256"
+            self.targetInstSrc[ver] = f"phonon-vlc-{ver}"
+        self.svnTargets["master"] = "git://anongit.kde.org/phonon-vlc"
 
         self.description = "the vlc based phonon multimedia backend"
-        self.defaultTarget = '0.9.0'
+        self.defaultTarget = "0.10.0"
 
 
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args = ' -DCMAKE_CXX_FLAGS=-DWIN32  -DPHONON_BUILD_PHONON4QT5=ON -DPHONON_BUILDSYSTEM_DIR=\"%s\" ' % (
-        os.path.join(CraftStandardDirs.craftRoot(), 'share', 'phonon', 'buildsystem').replace('\\', '/'))
+        self.subinfo.options.configure.args = " -DPHONON_BUILD_PHONON4QT5=ON"
