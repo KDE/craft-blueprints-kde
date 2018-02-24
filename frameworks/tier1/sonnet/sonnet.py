2fb4f70b5462a1a23c708326cadfa6a96e323ab8
Hannah von Reth <vonreth@kde.org>
2018-02-24 12:27:33 +0100

    Backport patches to make sonnet detect dictionaries

--- frameworks/tier1/sonnet/sonnet.py
+++ frameworks/tier1/sonnet/sonnet.py
@@ -4,9 +4,9 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
-        self.patchToApply["5.41.0"] = [("0001-Don-t-cause-circular-linking-on-Windows.patch", 1),
-                                       ("0001-Remove-anchient-and-broken-workaround.patch", 1)]
-        self.patchLevel["5.41.0"] = 2
+        self.patchToApply["5.43.0"] = [("0001-Find-libhunspell-build-by-msvc.patch", 1),
+                                       ("0002-Use-Locale-name-instead-of-Locale-bcp47Name.patch", 1)]
+        self.patchLevel["5.43.0"] = 1
 
         self.description = "Spelling framework for Qt, plugin-based."
 
