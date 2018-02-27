82cd415edfb2adf59fd92aff4e0752c6d31f146d
Hannah von Reth <vonreth@kde.org>
2018-02-27 17:55:10 +0100

    Actually add baloo....

---
+++ frameworks/tier3/baloo/baloo.py
@@ -0,0 +1,27 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        self.description = "Baloo is a file indexing and searching framework."
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["frameworks/tier1/solid"] = None
+        self.runtimeDependencies["frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["frameworks/tier1/kdbusaddons"] = None
+        self.runtimeDependencies["frameworks/tier1/kidletime"] = None
+        self.runtimeDependencies["frameworks/tier2/kfilemetadata"] = None
+        self.runtimeDependencies["frameworks/tier3/kio"] = None
+        self.runtimeDependencies["win32libs/lmdb"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
