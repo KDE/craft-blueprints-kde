ec5b43bf632fda52ff8a759cb96a8aa8640f05d3
Hannah von Reth <vonreth@kde.org>
2018-03-08 15:26:10 +0100

    Configure dbus for the windows data dir

--- win32libs/dbus/dbus.py
+++ win32libs/dbus/dbus.py
@@ -1,4 +1,27 @@
 # -*- coding: utf-8 -*-
+# Copyright Hannah von Reth <vonreth@kde.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
 import info
 
 
@@ -16,7 +39,8 @@ class subinfo(info.infoclass):
         self.patchToApply["1.8.4"] = [("dont_include_afxres.diff", 1)]
         self.patchToApply["1.10.4"] = [("dont_include_afxres.diff", 1)]
         self.patchToApply["1.11.4"] = [("dbus-1.11.4-20160903.diff", 1)]
-        self.patchToApply["1.11.14"] = [("dbus-1.11.4-20160903.diff", 1)]
+        self.patchToApply["1.11.14"] = [("dbus-1.11.4-20160903.diff", 1),
+                                        ("dbus-fix_data_dir.diff", 1)]
         self.patchLevel["1.11.14"] = 1
 
         self.targetDigests["1.10.4"] = "ec1921a09199c81ea20b20448237146a414d51ae"
@@ -34,6 +58,7 @@ class subinfo(info.infoclass):
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
         self.runtimeDependencies["win32libs/expat"] = "default"
+        self.runtimeDependencies["win32libs/glib"] = "default"
 
 
 from Package.CMakePackageBase import *
@@ -61,23 +86,13 @@ class Package(CMakePackageBase):
         if OsUtils.isWin():
             # kde uses debugger output, so dbus should do too
             self.subinfo.options.configure.args += "-DDBUS_USE_OUTPUT_DEBUG_STRING=ON "
+            self.subinfo.options.configure.args += "-DCMAKE_INSTALL_DATADIR:STRING=bin/data "
 
         if OsUtils.isMac():
             self.subinfo.options.configure.args += "-DDBUS_BUILD_X11:BOOL=OFF "
 
     def install(self):
         if not CMakePackageBase.install(self): return False
-
-        #patch our config...
-        conf = os.path.join(self.imageDir(), "share", "dbus-1", "session.conf")
-        lines = []
-        with open(conf, "rt+") as f:
-            lines = f.readlines()
-        with open(conf, "wt+") as f:
-            for line in lines:
-                f.write(line.replace("<standard_session_servicedirs />", "<standard_session_servicedirs />\n"
-                                                                         "  <servicedir>../../bin/data/dbus-1/services</servicedir>"))
-
         # TODO: fix
         if self.buildType() == "Debug":
             imagedir = os.path.join(self.installDir(), "lib")
