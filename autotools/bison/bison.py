576f5bd7f232b88990c3fbd43d4948f55389a60e
Filipe Azevedo <filipe.azevedo@kdab.com>
2018-03-12 14:52:22 +0100

    Introduce bison package

---
+++ autotools/bison/bison.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import info
+from Package.AutoToolsPackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setDependencies(self):
+        self.buildDependencies["dev-util/msys"] = "default"
+        self.runtimeDependencies["virtual/base"] = "default"
+
+    def setTargets(self):
+        self.description = "Bison is a general-purpose parser generator that converts an annotated context-free grammar into a deterministic LR or generalized LR (GLR) parser employing LALR(1) parser tables"
+        self.svnTargets['master'] = 'git://git.savannah.gnu.org/bison.git'
+        for ver in ["3.0.4"]:
+            self.targets[ver] = f"http://ftp.gnu.org/gnu/bison/bison-{ver}.tar.xz"
+            self.targetInstSrc[ver] = f"bison-{ver}"
+        self.defaultTarget = '3.0.4'
+        self.patchToApply["3.0.4"] = [("vasnprintf-macos.diff", 1)]
+
+
+class Package(AutoToolsPackageBase):
+    def __init__( self, **args ):
+        AutoToolsPackageBase.__init__( self )
+
+    def install( self ):
+        if not AutoToolsPackageBase.install(self):
+            return False
+        return self.copyToMsvcImportLib()
