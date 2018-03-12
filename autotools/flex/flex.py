a089d810361f96c231018328ffe4c90d178637b7
Filipe Azevedo <filipe.azevedo@kdab.com>
2018-03-12 15:10:31 +0100

    Introduce flex package

---
+++ autotools/flex/flex.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import info
+from Package.AutoToolsPackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setDependencies(self):
+        self.buildDependencies["dev-util/msys"] = "default"
+        self.runtimeDependencies["virtual/base"] = "default"
+
+    def setTargets(self):
+        self.description = "Flex is a tool for generating scanners: programs which recognize lexical patterns in text."
+        self.svnTargets['master'] = 'https://github.com/westes/flex.git'
+        for ver in ["2.6.4"]:
+            self.targets[ver] = f"https://github.com/westes/flex/releases/download/v{ver}/flex-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"flex-{ver}"
+        self.defaultTarget = '2.6.4'
+
+
+class Package(AutoToolsPackageBase):
+    def __init__( self, **args ):
+        AutoToolsPackageBase.__init__( self )
+
+    def install( self ):
+        if not AutoToolsPackageBase.install(self):
+            return False
+        return self.copyToMsvcImportLib()
