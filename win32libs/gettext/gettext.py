c2d00aeb01a560d579fa2d1c678c64571b9a5a6a
Hannah von Reth <vonreth@kde.org>
2018-03-14 11:02:44 +0100

    Trigger rebuild

--- win32libs/gettext/gettext.py
+++ win32libs/gettext/gettext.py
@@ -9,7 +9,7 @@ class subinfo(info.infoclass):
         for ver in ['0.19.8.1']:
             self.targets[ver] = 'http://ftp.gnu.org/pub/gnu/gettext/gettext-%s.tar.gz' % ver
             self.targetInstSrc[ver] = "gettext-%s" % ver
-
+        self.patchLevel['0.19.8.1'] = 1
         self.targetDigests['0.19.8.1'] = (['ff942af0e438ced4a8b0ea4b0b6e0d6d657157c5e2364de57baa279c1c125c43'], CraftHash.HashAlgorithm.SHA256)
 
         #patch based on https://github.com/fanc999/gtk-msvc-projects/tree/master/gettext/0.19.8.1
