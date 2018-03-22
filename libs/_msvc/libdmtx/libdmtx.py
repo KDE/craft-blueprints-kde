c997459a1c683e616f561c89a6b15ac93861b434
Hannah von Reth <vonreth@kde.org>
2018-03-22 18:48:57 +0100

    Fix url

--- libs/_msvc/libdmtx/libdmtx.py
+++ libs/_msvc/libdmtx/libdmtx.py
@@ -3,7 +3,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.addCachedAutotoolsBuild(f"https://files.kde.org/craft/libs/2018.02/windows/mingw_{CraftCore.compiler.bits}/gcc/Release/", "libs/libdmtx-src")
+        self.addCachedAutotoolsBuild(f"https://files.kde.org/craft/autotools/2018.02/windows/mingw_{CraftCore.compiler.bits}/gcc/Release/", "libs/libdmtx-src")
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
