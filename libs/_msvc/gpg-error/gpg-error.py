af6ba68fa68bb118b1eb7867c1cd88f8ad729c40
Hannah von Reth <vonreth@kde.org>
2018-03-23 17:15:01 +0100

    Use new syntax for addCachedAutotoolsBuild

--- libs/_msvc/gpg-error/gpg-error.py
+++ libs/_msvc/gpg-error/gpg-error.py
@@ -3,7 +3,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.addCachedAutotoolsBuild(f"https://files.kde.org/craft/autotools/2018.02/windows/mingw_{CraftCore.compiler.bits}/gcc/Release/", "libs/_autotools/gpg-error")
+        self.addCachedAutotoolsBuild("libs/_autotools/gpg-error")
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
