752dc5cbb1ebf1996b48c64ef57b982ce7734c0f
Hannah von Reth <vonreth@kde.org>
2018-03-26 16:48:00 +0200

    Ignore icu on mac

--- libs/icu/icu.py
+++ libs/icu/icu.py
@@ -20,6 +20,7 @@ class subinfo(info.infoclass):
             self.patchToApply["58.2"] = ("0020-workaround-missing-locale.patch",
                                          2)  # https://raw.githubusercontent.com/Alexpux/MINGW-packages/master/mingw-w64-icu/0020-workaround-missing-locale.patch
 
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NotMacOS
         self.defaultTarget = "58.2"
 
     def setDependencies(self):
