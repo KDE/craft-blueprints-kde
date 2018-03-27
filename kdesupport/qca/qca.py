527ecbad6b54fc5d1b456e1014ac74017b438ef1
Hannah von Reth <vonreth@kde.org>
2018-03-27 17:59:44 +0200

    Try to install qca in relocatable way

--- kdesupport/qca/qca.py
+++ kdesupport/qca/qca.py
@@ -9,9 +9,16 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/cyrus-sasl"] = "default"
 
     def setTargets(self):
-        self.svnTargets['master'] = 'git://anongit.kde.org/qca.git'
+        self.svnTargets["master"] = "git://anongit.kde.org/qca.git"
+        self.patchLevel["master"] = 1
+
+        #for ver in []:
+            #self.targets[ver] = f"https://download.kde.org/stable/qca-qt5/{ver}/src/qca-qt5-{ver}.tar.xz"
+            #self.targetDigestUrls[ver] = f"https://download.kde.org/stable/qca-qt5/{ver}/src/qca-qt5-{ver}.tar.xz.sha256"
+            #self.targetInstSrc[ver] = f"qca-qt5-{ver}"
+
         self.description = "Qt Cryptographic Architecture (QCA)"
-        self.defaultTarget = 'master'
+        self.defaultTarget = "master"
 
 
 from Package.CMakePackageBase import *
@@ -20,3 +27,4 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.args += " -DUSE_RELATIVE_PATHS=ON"
