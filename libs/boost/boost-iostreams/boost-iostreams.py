b770a6cdc6f750d7b9e14db62fc2274ae169931a
Hannah von Reth <vonreth@kde.org>
2018-04-06 09:38:38 +0200

    Update used api

--- libs/boost/boost-iostreams/boost-iostreams.py
+++ libs/boost/boost-iostreams/boost-iostreams.py
@@ -3,7 +3,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.versionInfo.setDefaultValues("", tarballInstallSrc=self.package.package.name.replace("boost-", "").replace("-", "_"))
+        self.versionInfo.setDefaultValues("", tarballInstallSrc=self.parent.package.name.replace("boost-", "").replace("-", "_"))
 
         self.webpage = 'http://www.boost.org/'
 
