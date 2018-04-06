c2379cbe1757adf1de354282734ee0646c5a9786
Hannah von Reth <vonreth@kde.org>
2018-04-06 12:42:24 +0200

    Don't cache qca, its not relocatable

--- kdesupport/qca/qca.py
+++ kdesupport/qca/qca.py
@@ -10,7 +10,7 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets["master"] = "git://anongit.kde.org/qca.git"
-        self.patchLevel["master"] = 2
+        self.patchLevel["master"] = 3
 
         #for ver in []:
             #self.targets[ver] = f"https://download.kde.org/stable/qca-qt5/{ver}/src/qca-qt5-{ver}.tar.xz"
@@ -28,3 +28,5 @@ class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
         self.subinfo.options.configure.args += " -DUSE_RELATIVE_PATHS=ON"
+        # the cmake config is not relocatable
+        self.subinfo.options.package.disableBinaryCache = True
