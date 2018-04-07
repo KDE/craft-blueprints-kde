1d0443dc4a574a60f95d4296a756f1ea00a87dc4
Hannah von Reth <vonreth@kde.org>
2018-04-07 12:46:07 +0200

    Try to  apply the same patch to 6.0

--- libs/llvm-meta/lldb/lldb.py
+++ libs/llvm-meta/lldb/lldb.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
         self.versionInfo.setDefaultValues()
         if CraftCore.compiler.isMinGW():
             self.patchToApply["5.0.1"] = [("0501-fixes-to-get-lldb-building.patch", 1)]#https://github.com/Alexpux/MINGW-packages/blob/5cbc0add24760165f557fd00bd9d915279ea4611/mingw-w64-clang/0501-fixes-to-get-lldb-building.patch
+            self.patchToApply["6.0.0"] = [("0501-fixes-to-get-lldb-building.patch", 1)]#https://github.com/Alexpux/MINGW-packages/blob/5cbc0add24760165f557fd00bd9d915279ea4611/mingw-w64-clang/0501-fixes-to-get-lldb-building.patch
 
 
     def setDependencies(self):
