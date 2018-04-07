538e03c21898bc97a413daf16a7acbf60e7fc808
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2018-04-07 15:51:57 +0200

    KF5 branch is now master

--- extragear/rkward/rkward.py
+++ extragear/rkward/rkward.py
@@ -4,7 +4,7 @@ from CraftOS.osutils import OsUtils
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets['master'] = 'git://anongit.kde.org/rkward|frameworks'
+        self.svnTargets['master'] = 'git://anongit.kde.org/rkward'
         self.defaultTarget = 'master'
 
     def setDependencies(self):
