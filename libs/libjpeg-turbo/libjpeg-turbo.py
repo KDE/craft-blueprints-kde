62e8dc9c155c3657acbe123dfa984155f33f65d1
Hannah von Reth <vonreth@kde.org>
2018-04-10 09:14:44 +0200

    Use our nasm if provided

--- libs/libjpeg-turbo/libjpeg-turbo.py
+++ libs/libjpeg-turbo/libjpeg-turbo.py
@@ -57,7 +57,7 @@ class PackageAutotools(AutoToolsPackageBase):
         AutoToolsPackageBase.__init__(self)
         # Version 2 uses cmake
         self.subinfo.options.configure.bootstrap = True
-        self.subinfo.options.configure.args = "--enable-shared --disable-static --disable-doc-build"
+        self.subinfo.options.configure.args = f"--enable-shared --disable-static --disable-doc-build NASM={CraftCore.cache.findApplication('nasm')}"
 
 if CraftCore.compiler.isUnix:
     class Package(PackageAutotools):
