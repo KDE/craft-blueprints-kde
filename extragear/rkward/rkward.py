d97b14d5671602a6e7d9193be1678d184da2a66e
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2018-04-10 21:08:23 +0200

    For debugging a timeout while packaging on Mac (binary-factory): Try packaging without macdeployqt.

--- extragear/rkward/rkward.py
+++ extragear/rkward/rkward.py
@@ -107,4 +107,63 @@ class Package(CMakePackageBase):
         self.ignoredPackages.append("binary/mysql")
         self.ignoredPackages.append("data/hunspell-dictionaries")
 
-        return TypePackager.createPackage(self)
+        if OsUtils.isMac():
+            return self.debugCreatePackageMac()
+        else:
+            return TypePackager.createPackage(self)
+
+    # HACK: Remove me. This is a copy of MacDMGPackager, for the purpose of pinning down just what exactly causes
+    #       the .dmg detach to hang on the binary factory.
+    def debugCreatePackageMac(self):
+        """ create a package """
+        CraftCore.log.debug("packaging using the MacDMGPackager")
+
+        self.internalCreatePackage()
+        self.preArchive()
+
+        self._setDefaults()
+
+
+        archive = self.archiveDir()
+        appPath = self.defines['apppath']
+        if not appPath:
+            apps = glob.glob(os.path.join(archive, f"**/{self.defines['appname']}.app"), recursive=True)
+            if len(apps) != 1:
+                CraftCore.log.error(f"Failed to detect *.app for {self}, please provide self.defines['apppath']")
+                return False
+            appPath = apps[0]
+        appPath = os.path.join(archive, appPath)
+        CraftCore.log.info(f"Packaging {appPath}")
+        if os.path.exists(os.path.join(archive, "lib/plugins")):
+            utils.mergeTree(os.path.join(archive, "lib/plugins"), os.path.join(appPath, "Contents/PlugIns/"))
+        targetLibdir = os.path.join(appPath, "Contents/Frameworks/")
+        if not os.path.exists(targetLibdir):
+            os.makedirs(targetLibdir)
+        if os.path.exists(os.path.join(archive, "lib")):
+            utils.mergeTree(os.path.join(archive, "lib"), targetLibdir)
+        if os.path.exists(os.path.join(archive, "share")):
+            utils.mergeTree(os.path.join(archive, "share"), os.path.join(appPath, "Contents/Resources/"))
+        utils.mergeTree(os.path.join(archive, "bin"), os.path.join(appPath, "Contents/MacOS/"))
+
+        with utils.ScopedEnv({'DYLD_LIBRARY_PATH' : os.path.join(CraftStandardDirs.craftRoot(), "lib")}):
+            if not utils.system(["dylibbundler",
+                                 "--overwrite-files",
+                                 "--bundle-deps",
+                                 "--install-path", "@executable_path/../Frameworks",
+                                 "--dest-dir", targetLibdir,
+                                 "--fix-file", f"{appPath}/Contents/MacOS/{self.defines['appname']}"]):
+                return False
+
+#            if not utils.system(["macdeployqt", appPath,  "-always-overwrite", "-verbose=1"]):
+#                return False
+
+            name = self.binaryArchiveName(fileType="", includeRevision=True)
+            dmgDest = os.path.join(self.packageDestinationDir(), f"{name}.dmg")
+            if os.path.exists(dmgDest):
+                utils.deleteFile(dmgDest)
+            if not utils.system(["create-dmg", "--volname", name, dmgDest, appPath]):
+                return False
+
+            CraftHash.createDigestFiles(dmgDest)
+
+            return True
