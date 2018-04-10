a6dc103fa77ea03ff4a22169a711a5d034faa6c5
Hannah von Reth <vonreth@kde.org>
2018-04-10 08:26:05 +0200

    Use autotools for jpegturbo on unix

--- libs/libjpeg-turbo/libjpeg-turbo.py
+++ libs/libjpeg-turbo/libjpeg-turbo.py
@@ -1,4 +1,27 @@
 # -*- coding: utf-8 -*-
+# Copyright Hannah von Reth <vonreth@kde.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
 import info
 
 
@@ -26,6 +49,19 @@ class subinfo(info.infoclass):
 from Package.CMakePackageBase import *
 
 
-class Package(CMakePackageBase):
+from Package.AutoToolsPackageBase import *
+from Package.CMakePackageBase import *
+
+class PackageAutotools(AutoToolsPackageBase):
     def __init__(self, **args):
-        CMakePackageBase.__init__(self)
+        AutoToolsPackageBase.__init__(self)
+        self.subinfo.options.configure.args = "--enable-shared --disable-static --disable-doc-build"
+
+if CraftCore.compiler.isUnix:
+    class Package(PackageAutotools):
+        def __init__(self):
+            PackageAutotools.__init__(self)
+else:
+    class Package(CMakePackageBase):
+        def __init__(self):
+            CMakePackageBase.__init__(self)
