5bc31fcb4a9740b0e5ea8aa37d2543e0eed3e62a
Hannah von Reth <vonreth@kde.org>
2018-04-11 15:11:19 +0200

    Update liblzma, use default build system

--- libs/liblzma/liblzma.py
+++ libs/liblzma/liblzma.py
@@ -1,30 +1,66 @@
 # -*- coding: utf-8 -*-
+# Copyright Hannah von Reth <vonreth@kde.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
 
 import info
-
+import glob
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["5.0.5"]:
-            self.targets[ver] = f"http://tukaani.org/xz/xz-" + ver + ".tar.xz"
-            self.targetInstSrc[ver] = "xz-" + ver
-            self.patchToApply[ver] = [("xz-5.0.5.diff", 1), ("xz-cmake-5.0.5.diff", 1)]
-            self.targetDigests["5.0.5"] = "56f1d78117f0c32bbb1cfd40117aa7f55bee8765"
+        for ver in ["5.2.3"]:
+            self.targets[ver] = f"http://tukaani.org/xz/xz-{ver}.tar.xz"
+            self.targetInstSrc[ver] = f"xz-{ver}"
+
+        self.targetDigests["5.2.3"] = (['7876096b053ad598c31f6df35f7de5cd9ff2ba3162e5a5554e4fc198447e0347'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "free general-purpose data compression software with high compression ratio"
+        self.webpage = "https://tukaani.org/xz"
         self.defaultTarget = ver
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
 
 
-from Package.CMakePackageBase import *
+from Package.MSBuildPackageBase import *
 
 
-class PackageCMake(CMakePackageBase):
-    def __init__(self):
-        CMakePackageBase.__init__(self)
+class PackageMSBuild(MSBuildPackageBase):
+    def __init__(self, **args):
+        MSBuildPackageBase.__init__(self)
+        self.subinfo.options.configure.projectFile = os.path.join(self.sourceDir(), "windows", "xz_win.sln")
+        self.msbuildTargets = ["liblzma_dll"]
 
+
+    def install(self):
+        if not MSBuildPackageBase.install(self, installHeaders=False):
+            return False
+
+        headerDir = os.path.join(self.sourceDir(), "src", "liblzma", "api")
+        includeDir = os.path.join(self.installDir(), "include")
+        header = glob.glob(os.path.join(headerDir, f"**/*.h"), recursive=True)
+        for h in header:
+            utils.copyFile(h, h.replace(headerDir, includeDir), linkOnly=False)
+        return True
 
 from Package.AutoToolsPackageBase import *
 
@@ -34,10 +70,8 @@ class PackageAutotools(AutoToolsPackageBase):
         self.subinfo.options.configure.args += " --disable-static --enable-shared "
 
 
-
-
-if CraftCore.compiler.isWindows:
-    class Package(PackageCMake):
+if CraftCore.compiler.isMSVC():
+    class Package(PackageMSBuild):
         pass
 else:
     class Package(PackageAutotools):
