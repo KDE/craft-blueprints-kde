03aeb7ab214792e31d136b657377cec96c78acb6
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2018-04-12 18:56:21 +0200

    Make libofx compile on Mac OS
    Differential Revision: https://phabricator.kde.org/D12152

--- libs/libofx/libofx.py
+++ libs/libofx/libofx.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
 import info
 
 
@@ -14,6 +38,8 @@ class subinfo(info.infoclass):
         if CraftCore.compiler.isMinGW():
             self.patchToApply['0.9.12'] += [("libofx-0.9.12-20180127-mingw.diff", 1)]
 
+        self.patchToApply['0.9.12'] += [("libofx-0.9.12-20180412.diff", 1)]
+
         self.description = "a parser and an API for the OFX (Open Financial eXchange) specification"
         self.defaultTarget = '0.9.12'
 
@@ -22,10 +48,22 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/iconv"] = "default"
 
 
+from Package.AutoToolsPackageBase import *
 from Package.CMakePackageBase import *
 
+class PackageAutotools(AutoToolsPackageBase):
+    def __init__(self, **args):
+        AutoToolsPackageBase.__init__(self)
+        openSPIncludeDir = CraftStandardDirs.craftRoot() + "/include/OpenSP"
+        openSPLibDir = CraftStandardDirs.craftRoot() + "/lib"
+        self.subinfo.options.configure.args = "--enable-shared --disable-static --with-opensp-includes=" + openSPIncludeDir + " --with-opensp-libs=" + openSPLibDir
 
-class Package(CMakePackageBase):
-    def __init__(self):
-        # we use subinfo for now too
-        CMakePackageBase.__init__(self)
+if CraftCore.compiler.isGCCLike():
+    class Package(PackageAutotools):
+        def __init__(self):
+            PackageAutotools.__init__(self)
+else:
+    class Package(CMakePackageBase):
+        def __init__(self):
+            # we use subinfo for now too
+            CMakePackageBase.__init__(self)
