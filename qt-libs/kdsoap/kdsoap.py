05a3ebc9b755162e23919a9f51756178794d636d
Kevin Funk <kfunk@kde.org>
2018-04-17 08:24:28 +0200

    kdsoap: Update to new 1.7.0 release
    Works for me

--- qt-libs/kdsoap/kdsoap.py
+++ qt-libs/kdsoap/kdsoap.py
@@ -7,15 +7,15 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/KDAB/KDSoap.git"
-        for ver in ["1.6.0"]:
+        for ver in ["1.7.0"]:
             self.targets[ver] = f"https://github.com/KDAB/KDSoap/releases/download/kdsoap-{ver}/kdsoap-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"kdsoap-{ver}"
-        self.targetDigests['1.6.0'] = (
-            ['d6b6b01348d2e1453f7e12724d1848ee41c86a1b19168ca67ac98fedb0408668'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.7.0'] = (
+            ['c13cd01cc576e22d51f2cd336f1473894a3c78ce5300ceaa23b20420f99234bd'], CraftHash.HashAlgorithm.SHA256)
 
-        self.patchToApply['1.6.0'] = [("kdsoap-1.6.0-20171220.diff", 1)]
+        self.patchToApply['1.7.0'] = [("kdsoap-1.6.0-20171220.diff", 1)]
 
-        self.defaultTarget = "1.6.0"
+        self.defaultTarget = "1.7.0"
         self.description = "A Qt-based client-side and server-side SOAP component"
         self.webpage = "http://www.kdab.com/products/kd-soap"
 
