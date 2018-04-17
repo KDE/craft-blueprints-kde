d828b0514bd12d3b94e8cddb1fc2cb10a640b14d
Kevin Funk <kfunk@kde.org>
2018-04-17 10:47:04 +0200

    Add package for kdiff3

---
+++ kde/unreleased/kdiff3/kdiff3.py
@@ -0,0 +1,24 @@
+import info
+from CraftOS.osutils import OsUtils
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        self.description = "Compares and merges 2 or 3 files or directories"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = "default"
+        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = "default"
+        self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = "default"
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
