bfde741746d97ff52380a52cb7cad383bee6fdfd
Hannah von Reth <vonreth@kde.org>
2018-04-19 13:43:58 +0200

    Add boost 1.67 release

--- libs/boost/boost-headers/boost-headers.py
+++ libs/boost/boost-headers/boost-headers.py
@@ -3,9 +3,9 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.versionInfo.setDefaultValues("http://downloads.sourceforge.net/boost/boost_${VERSION}.tar.gz",
-                                          tarballInstallSrc="boost_${VERSION}")
+        self.versionInfo.setDefaultValues()
         self.targetDigests["1_65_1"] = (['a13de2c8fbad635e6ba9c8f8714a0e6b4264b60a29b964b940a22554705b6b60'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1_67_0"] = (['8aa4e330c870ef50a896634c931adf468b21f8a69b77007e45c444151229f665'], CraftHash.HashAlgorithm.SHA256)
 
         self.patchToApply['1_55_0'] = [('boost_1_54_0-spirit-20131114.diff', 1)]
         self.patchToApply['1_56_0'] = [
