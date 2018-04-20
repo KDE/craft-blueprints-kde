ed29d42c33776621d77543e7543a923d2a43c976
Hannah von Reth <vonreth@kde.org>
2018-04-20 09:47:52 +0200

    Add patches

--- libs/cyrus-sasl/cyrus-sasl.py
+++ libs/cyrus-sasl/cyrus-sasl.py
@@ -6,14 +6,18 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['2.1.26']:
-            self.targets[ver] = 'ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-' + ver + '.tar.gz'
-            self.targetInstSrc[ver] = 'cyrus-sasl-' + ver
+        for ver in ["2.1.26"]:
+            self.targets[ver] = "ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-" + ver + ".tar.gz"
+            self.targetInstSrc[ver] = "cyrus-sasl-" + ver
         if CraftCore.compiler.isWindows:
-            self.patchToApply['2.1.26'] = [('cyrus-sasl-2.1.26.patch', 1)]
-        self.targetDigests['2.1.26'] = 'd6669fb91434192529bd13ee95737a8a5040241c'
+            self.patchToApply["2.1.26"] = [("cyrus-sasl-2.1.26.patch", 1)]
+        else:
+            #http://www.linuxfromscratch.org/blfs/view/svn/postlfs/cyrus-sasl.html
+            self.patchToApply["2.1.26"] = [("cyrus-sasl-2.1.26-fixes-3.patch", 1)
+                                           ("cyrus-sasl-2.1.26-openssl-1.1.0-1.patch", 1)]
+        self.targetDigests["2.1.26"] = "d6669fb91434192529bd13ee95737a8a5040241c"
         self.description = "Cyrus SASL implementation"
-        self.defaultTarget = '2.1.26'
+        self.defaultTarget = "2.1.26"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
