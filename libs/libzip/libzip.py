88b313bfd486fc547a5b07d25a935b3017bc9363
Hannah von Reth <vonreth@kde.org>
2018-04-23 10:19:52 +0200

    Update libzip from 0.11.1 to 1.5.1

--- libs/libzip/libzip.py
+++ libs/libzip/libzip.py
@@ -7,21 +7,25 @@ class subinfo(info.infoclass):
         for ver in ['0.9', '0.9.3']:
             self.targets[ver] = "http://www.nih.at/libzip/libzip-" + ver + ".tar.bz2"
             self.targetInstSrc[ver] = "libzip-" + ver
-        for ver in ['0.11.1']:
+        for ver in ['0.11.1', '1.5.1']:
             self.targets[ver] = "http://www.nih.at/libzip/libzip-" + ver + ".tar.xz"
             self.targetInstSrc[ver] = "libzip-" + ver
         self.patchToApply['0.9.0'] = ('libzip-0.9.diff', 1)
         self.patchToApply['0.9.3'] = ('libzip-0.9.3-20101116.diff', 1)
         self.patchToApply['0.11.1'] = ('libzip-0.11.1-20130907.diff', 1)
+        self.patchToApply['1.5.1'] = ('libzip-1.5.1-20180423.diff', 1)
         self.targetDigests['0.9.3'] = '16e94bc0327f1a76a0296a28908cf6439b0a0a67'
         self.targetDigests['0.11.1'] = '3c82cdc0de51f06d5e1c60f098d3d9cc0d48f8a7'
+        self.targetDigests['1.5.1'] = (['04ea35b6956c7b3453f1ed3f3fe40e3ddae1f43931089124579e8384e79ed372'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "a library for handling zip archives"
-        self.defaultTarget = '0.11.1'
+        self.defaultTarget = '1.5.1'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
         self.runtimeDependencies["libs/zlib"] = "default"
+        self.runtimeDependencies["libs/nettle"] = "default"
+        self.runtimeDependencies["libs/gnutls"] = "default"
 
 
 from Package.CMakePackageBase import *
