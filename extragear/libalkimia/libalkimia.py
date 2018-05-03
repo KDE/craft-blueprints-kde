3c8f9db2f04b789726c8266fe8ca0032c9492499
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2018-05-03 11:59:51 +0200

    Use libgmp for KMyMoney on non-MSVC
    MPIR is needed only on MSVC and causes trouble on MacOS.

--- extragear/libalkimia/libalkimia.py
+++ extragear/libalkimia/libalkimia.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
 import info
 
 
@@ -8,8 +32,10 @@ class subinfo(info.infoclass):
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt5/qtbase"] = "default"
-        self.runtimeDependencies["libs/mpir"] = "default"
-        self.buildDependencies["libs/libgmp"] = "default"
+        if CraftCore.compiler.isMSVC():
+            self.runtimeDependencies["libs/mpir"] = "default"
+        else:
+            self.runtimeDependencies["libs/libgmp"] = "default"
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = "default"
         self.description = "A library with common classes and functionality used by finance applications for the KDE SC."
 
