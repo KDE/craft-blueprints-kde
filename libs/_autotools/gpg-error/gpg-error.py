9efa7bf7e117e79bf708db268e3f5e1bc0dcc36d
Hannah von Reth <vonreth@kde.org>
2018-05-03 13:43:28 +0200

    Call autoreconf

--- libs/_autotools/gpg-error/gpg-error.py
+++ libs/_autotools/gpg-error/gpg-error.py
@@ -22,6 +22,7 @@ from Package.AutoToolsPackageBase import *
 class Package(AutoToolsPackageBase):
     def __init__( self, **args ):
         AutoToolsPackageBase.__init__( self )
+        self.subinfo.options.configure.bootstrap = True
         self.subinfo.options.configure.args += " --disable-static --enable-shared "
 
     def install( self ):
