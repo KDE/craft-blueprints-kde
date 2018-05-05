0d8b2db7ff87517c106c86b43bfade3cc6c9d5bb
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2018-05-05 10:26:13 +0200

    Compile OpenSP with Autotools on MacOS only
    The only known way to compile OpenSP with Autotools on MinGW is
    involving lib only and that's what we do with CMake.

--- libs/libopensp/libopensp.py
+++ libs/libopensp/libopensp.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
+# Copyright 2018 Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -29,14 +29,18 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.targets['1.5.2'] = "http://downloads.sourceforge.net/project/openjade/opensp/1.5.2/OpenSP-1.5.2.tar.gz"
         self.targetInstSrc['1.5.2'] = "OpenSP-1.5.2"
-        if CraftCore.compiler.isMSVC():
+        if CraftCore.compiler.isWindows:
             self.patchToApply['1.5.2'] = ("OpenSP-1.5.2-20110111.diff", 1)
+        # elif CraftCore.compiler.isMinGW():
+        #     self.patchToApply['1.5.2'] = ("OpenSP-1.5.2-20180505.diff", 1)
         self.targetDigests['1.5.2'] = 'b4e903e980f8a8b3887396a24e067bef126e97d5'
         self.description = "a library for a SGML parser algorithm"
         self.defaultTarget = '1.5.2'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
+        # if CraftCore.compiler.isMinGW():
+        #     self.buildDependencies["dev-utils/msys"] = "default"
 
 from Package.AutoToolsPackageBase import *
 from Package.CMakePackageBase import *
@@ -44,9 +48,17 @@ from Package.CMakePackageBase import *
 class PackageAutotools(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.configure.args = "--enable-shared --disable-static --disable-doc-build"
+        self.subinfo.options.configure.args = "--enable-shared --disable-static --disable-doc-build "
+        if CraftCore.compiler.isMinGW():
+            self.subinfo.options.configure.noDataRootDir = True
 
-if CraftCore.compiler.isGCCLike():
+    def configure(self):
+        if CraftCore.compiler.isMinGW():
+            # to fix MessageReporter.cxx:126:49: error: cast from 'const OpenSP::MessageModule*' to 'long unsigned int' loses precision [-fpermissive]
+            self.subinfo.options.configure.cxxflags += "-fpermissive "
+        return super().configure()
+
+if CraftCore.compiler.isMacOS:
     class Package(PackageAutotools):
         def __init__(self):
             PackageAutotools.__init__(self)
