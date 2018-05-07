71b7e32587e426522ee8b42f9eb048e90ef12b23
Hannah von Reth <vonreth@kde.org>
2018-05-07 14:37:45 +0200

    Rebuild

--- libs/_autotools/gpg-error/gpg-error.py
+++ libs/_autotools/gpg-error/gpg-error.py
@@ -10,6 +10,7 @@ class subinfo(info.infoclass):
         self.targetDigests['1.27'] = (['4f93aac6fecb7da2b92871bb9ee33032be6a87b174f54abf8ddf0911a22d29d2'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['1.31'] =  (['40d0a823c9329478063903192a1f82496083b277265904878f4bc09e0db7a4ef'], CraftHash.HashAlgorithm.SHA256)
         self.description = "Small library with error codes and descriptions shared by most GnuPG related software"
+        self.patchLevel["1.31"] = 1
         self.defaultTarget = "1.31"
 
     def setDependencies( self ):
