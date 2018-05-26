c40883eda50fa44c1d7de46e68c9157f0cbe543f
Ben Cooksley <bcooksley@kde.org>
2018-05-26 20:06:12 +1200

    Upgrade to a newer version of libGLEW which better supports the newer version of macOS

--- libs/glew/glew.py
+++ libs/glew/glew.py
@@ -6,7 +6,7 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['1.5.4', '1.7.0', '1.9.0', '1.10.0']:
+        for ver in ['1.5.4', '1.7.0', '1.9.0', '1.10.0', '2.1.0']:
             self.targets[ver] = 'http://downloads.sourceforge.net/project/glew/glew/' + ver + '/glew-' + ver + '.zip'
             self.targetInstSrc[ver] = 'glew-' + ver
         self.patchToApply['1.5.4'] = [('glew-1.5.4-20100708.diff', 1)]
@@ -16,9 +16,12 @@ class subinfo(info.infoclass):
         self.targetDigests['1.7.0'] = '107c155ff5b69d97b9c530b40e4e8da571aaf729'
         self.targetDigests['1.9.0'] = '6c0dd8d6af14db93868bb5482b55e784b2dc1127'
         self.targetDigests['1.10.0'] = 'da45a883ca9b2a8e8fc1a642bd043f251ad69151'
+        self.targetDigests['2.1.0'] = '85ea9f4d1279b107019e48b9174c34e86c634830'
+
+        self.targetConfigurePath['2.1.0'] = 'build/cmake'
 
         self.description = "OpenGL Extension Wrangler Library"
-        self.defaultTarget = '1.10.0'
+        self.defaultTarget = '2.1.0'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
