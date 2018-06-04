396b9ed8b578d0a423c122c70dd5825927765585
Chris Rizzitello <sithlord48@gmail.com>
2018-06-04 10:37:09 -0400

    Include plugins for mac os
    Summary: Signed-off-by: Chris Rizzitello <sithlord48@gmail.com>
    
    Reviewers: vonreth
    
    Reviewed By: vonreth
    
    Subscribers: vonreth
    
    Differential Revision: https://phabricator.kde.org/D13333

--- extragear/atcore/atcore.py
+++ extragear/atcore/atcore.py
@@ -23,11 +23,21 @@ class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
         self.subinfo.options.configure.args = "-DBUILD_GUI=ON "
-        self.subinfo.options.configure.args += "-DDEPLOY_PLUGINS_WITH_BINARY=ON "
+        if self.buildTarget != "1.0":
+            self.subinfo.options.configure.args += "-DDEPLOY_PLUGINS_WITH_BINARY=ON "
+
         self.blacklist_file = [
             PackagerLists.runtimeBlacklist,
             os.path.join(self.packageDir(), 'blacklist.txt')
         ]
+    def preArchive(self):
+        if CraftCore.compiler.isMacOS:
+            if self.buildTarget != "1.0":
+                return utils.mergeTree(os.path.join(self.archiveDir(), "bin/plugins/"), os.path.join(self.archiveDir(), "AtCoreTest.app/Contents/MacOS/plugins"))
+            else:
+                return utils.mergeTree(os.path.join(self.archiveDir(), "bin/plugins/"), os.path.join(self.archiveDir(), "atcore-gui.app/Contents/MacOS/plugins"))
+        else:
+            return True
 
     def createPackage(self):
         self.defines["executable"] = "bin\\atcore-gui.exe"
@@ -36,4 +46,3 @@ class Package(CMakePackageBase):
         self.defines["icon"] = os.path.join(self.packageDir(), "atcore.ico")
 
         return TypePackager.createPackage(self)
-
