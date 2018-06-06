b8456cb7a7398fecb26d23bcfbeb0d15b6f0b227
Hannah von Reth <vonreth@kde.org>
2018-06-06 12:16:52 +0200

    Apply security patches also on Windows

--- libs/cyrus-sasl/cyrus-sasl.py
+++ libs/cyrus-sasl/cyrus-sasl.py
@@ -6,13 +6,13 @@ class subinfo(info.infoclass):
         for ver in ["2.1.26"]:
             self.targets[ver] = "ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-" + ver + ".tar.gz"
             self.targetInstSrc[ver] = "cyrus-sasl-" + ver
-        if CraftCore.compiler.isWindows:
-            self.patchToApply["2.1.26"] = [("cyrus-sasl-2.1.26.patch", 1)]
-        else:
             #http://www.linuxfromscratch.org/blfs/view/svn/postlfs/cyrus-sasl.html
             self.patchToApply["2.1.26"] = [("cyrus-sasl-2.1.26-fixes-3.patch", 1),
                                            ("cyrus-sasl-2.1.26-openssl-1.1.0-1.patch", 1)]
+        if CraftCore.compiler.isWindows:
+            self.patchToApply["2.1.26"] += [("cyrus-sasl-2.1.26.patch", 1)]
         self.targetDigests["2.1.26"] = "d6669fb91434192529bd13ee95737a8a5040241c"
+        self.patchLevel["2.1.26"] = 1
         self.description = "Cyrus SASL implementation"
         self.defaultTarget = "2.1.26"
 
