4e0f28fb42bcb729c7482f665264c8854be72edd
Hannah von Reth <vonreth@kde.org>
2018-06-08 16:44:40 +0200

    Fix

--- python-modules/doxyqml/doxyqml.py
+++ python-modules/doxyqml/doxyqml.py
@@ -6,12 +6,13 @@ from CraftOS.osutils import OsUtils
 
 class subinfo(info.infoclass):
     def setDependencies(self):
-        self.buildDependencies["dev-utils/python3"] = "default"
+        self.buildDependencies["dev-utils/python3"] = None
 
     def setTargets(self):
-        self.svnTargets['master'] = ''
+        self.svnTargets["master"] = ""
+        self.patchLevel["master"] = 1
         self.description = "Doxyqml is an input filter for Doxygen, a documentation system for C++ and a few other languages."
-        self.defaultTarget = 'master'
+        self.defaultTarget = "master"
 
 
 class Package(PipPackageBase):
@@ -23,5 +24,5 @@ class Package(PipPackageBase):
         if OsUtils.isWin():
             utils.createShim(os.path.join(self.imageDir(), "bin", "doxyqml.exe"),
                             os.path.join(self.imageDir(), "dev-utils", "bin", "python3.exe"),
-                            args=os.path.join(CraftCore.settings.get("Paths", "PYTHON27"), "Scripts", "doxyqml"))
+                            args=os.path.join(CraftCore.settings.get("Paths", "Python"), "Scripts", "doxyqml"))
         return PipBuildSystem.install(self)
