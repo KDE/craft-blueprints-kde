ebddb227a6bb9eb2eedec2288f955283700e8ebd
Chris Rizzitello <sithlord48@gmail.com>
2018-06-10 12:57:14 -0400

    Set patchLevel for each version
    Signed-off-by: Chris Rizzitello <sithlord48@gmail.com>

--- kdesupport/qwt/qwt.py
+++ kdesupport/qwt/qwt.py
@@ -7,7 +7,7 @@ class subinfo(info.infoclass):
         for ver in ["6.1.3"]:
             self.targets[ver] = f"http://downloads.sourceforge.net/sourceforge/qwt/qwt-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"qwt-{ver}"
-            self.PatchLevel = 1
+            self.patchLevel[ver] = 1
             self.patchToApply[ver] = [("qwt-6.1.3-20180429.diff", 1)]
         self.targetDigests["6.1.3"] = (['f3ecd34e72a9a2b08422fb6c8e909ca76f4ce5fa77acad7a2883b701f4309733'], CraftHash.HashAlgorithm.SHA256)
         self.description = "The Qwt library contains GUI Components and utility classes which are primarily useful for programs with a technical background"
