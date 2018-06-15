d51ddc1010ad2b47efac8ccb26ccc38b483eda3e
Hannah von Reth <vonreth@kde.org>
2018-06-15 08:38:19 +0200

    Depend on png2ico until the icoutils patch is upstream

--- kde/frameworks/extra-cmake-modules/extra-cmake-modules.py
+++ kde/frameworks/extra-cmake-modules/extra-cmake-modules.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright Hannah von Reth <vonreth@kde.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
 import info
 
 
@@ -12,14 +36,15 @@ class subinfo(info.infoclass):
               self.patchToApply[ver] = ('icotool.diff', 1)
 
     def setDependencies(self):
-        self.runtimeDependencies["virtual/base"] = "default"
-        self.buildDependencies["dev-utils/icoutils"] = "default"
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["dev-utils/icoutils"] = None
+        self.buildDependencies["dev-utils/png2ico"] = None# TODO: remove once the icoutils is upstream
         # needed for many kf5's
-        self.buildDependencies["dev-utils/flexbison"] = "default"
-        self.buildDependencies["libs/qt5/qttools"] = "default"
+        self.buildDependencies["dev-utils/flexbison"] = None
+        self.buildDependencies["libs/qt5/qttools"] = None
 
         if CraftCore.settings.getboolean("CMake", "KDE_L10N_AUTO_TRANSLATIONS", False):
-            self.buildDependencies["dev-utils/ruby"] = "default"
+            self.buildDependencies["dev-utils/ruby"] = None
 
 
 from Package.CMakePackageBase import *
