77f3172ae9e41c21136653dabdddd89d8870e21d
Hannah von Reth <vonreth@kde.org>
2018-07-10 09:57:52 +0200

    Add Charm 1.12.0

--- qt-apps/charm/charm.py
+++ qt-apps/charm/charm.py
@@ -7,7 +7,12 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/KDAB/Charm.git'
-        self.defaultTarget = 'master'
+        for ver in ["1.12.0"]:
+            self.targets[ver] = f"https://github.com/KDAB/Charm/archive/{ver}.tar.gz"
+            self.archiveNames[ver] = f"charm-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"Charm-{ver}"
+        self.defaultTarget = "1.12.0"
+
         self.description = "The Cross-Platform Time Tracker"
         self.displayName = "Charm"
 
@@ -21,6 +26,8 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
+        if self.subinfo.buildTarget != "master":
+           self.subinfo.options.configure.args = f"-DCharm_VERSION={self.subinfo.buildTarget}"
 
     def createPackage(self):
         self.blacklist_file.append(os.path.join(self.packageDir(), 'blacklist.txt'))
