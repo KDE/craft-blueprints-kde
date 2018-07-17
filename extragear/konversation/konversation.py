a4072b14bc02807de53ad4dd7d7665678e975060
Hannah von Reth <vonreth@kde.org>
2018-07-17 08:36:44 +0200

    Cleanup

--- extragear/konversation/konversation.py
+++ extragear/konversation/konversation.py
@@ -5,34 +5,34 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets['1.7'] = 'git://anongit.kde.org/konversation|1.7'
-        self.svnTargets['master'] = 'git://anongit.kde.org/konversation|master'
-        for ver in ['1.7.4', '1.7.5']:
-            self.targets[ver] = 'http://download.kde.org/stable/konversation/%s/src/konversation-%s.tar.xz' % (ver, ver)
-            self.targetInstSrc[ver] = 'konversation-%s' % ver
-        self.defaultTarget = '1.7.5'
+        self.svnTargets["1.7"] = "git://anongit.kde.org/konversation|1.7"
+        self.svnTargets["master"] = "git://anongit.kde.org/konversation|master"
+        for ver in ["1.7.4", "1.7.5"]:
+            self.targets[ver] = "http://download.kde.org/stable/konversation/%s/src/konversation-%s.tar.xz" % (ver, ver)
+            self.targetInstSrc[ver] = "konversation-%s" % ver
+        self.defaultTarget = "1.7.5"
 
         self.displayName = "Konversation"
         self.description = "a KDE based irc client"
 
     def setDependencies(self):
-        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier1/karchive"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier3/kbookmarks"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier3/kemoticons"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier1/kidletime"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier3/knotifyconfig"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier3/kio"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier3/kparts"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier1/solid"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier1/sonnet"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier3/kwallet"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = "default"
-        self.runtimeDependencies["kdesupport/qca"] = "default"
-        self.runtimeDependencies["qt-libs/phonon"] = "default"
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/karchive"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kbookmarks"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kemoticons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kidletime"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/knotifyconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kparts"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/solid"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/sonnet"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kwallet"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
+        self.runtimeDependencies["kdesupport/qca"] = None
+        self.runtimeDependencies["qt-libs/phonon"] = None
 
 
 class Package(CMakePackageBase):
@@ -40,12 +40,12 @@ class Package(CMakePackageBase):
         CMakePackageBase.__init__(self)
         self.blacklist_file = [
             PackagerLists.runtimeBlacklist,
-            os.path.join(os.path.dirname(__file__), 'blacklist.txt')
+            os.path.join(self.packageDir(), "blacklist.txt")
         ]
 
     def createPackage(self):
         self.defines["executable"] = "bin\\konversation.exe"
-        self.defines["icon"] = os.path.join(os.path.dirname(__file__), "konversation.ico")
+        self.defines["icon"] = os.path.join(self.packageDir(), "konversation.ico")
 
         self.ignoredPackages.append("binary/mysql")
 
