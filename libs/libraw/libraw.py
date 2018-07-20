99409ba4f3179327dea945f70c9dfa6107646bc2
Simon Depiets <sdepiets@gmail.com>
2018-07-20 13:10:50 +0800

    Switch to a more recent version of LibRaw for Libkdcraw
    Summary: Libkdcraw requires libraw >= 0.18 now, doesn't build with the new 0.19, this is the latest stable 0.18 package
    
    Reviewers: #craft, vonreth
    
    Reviewed By: #craft, vonreth
    
    Tags: #craft, #windows
    
    Differential Revision: https://phabricator.kde.org/D14239

--- libs/libraw/libraw.py
+++ libs/libraw/libraw.py
@@ -4,14 +4,14 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['0.17.2'] = "http://www.libraw.org/data/LibRaw-0.17.2.tar.gz"
-        self.archiveNames['0.17.2'] = "LibRaw-0.17.2.tar.gz"
-        self.targetInstSrc['0.17.2'] = "LibRaw-0.17.2"
-        self.patchToApply['0.17.2'] = [("LibRaw-0.17.2-20180327.diff", 1)]#https://github.com/LibRaw/LibRaw-cmake/
-        self.targetDigests['0.17.2'] = (['92b0c42c7666eca9307e5e1f97d6fefc196cf0b7ee089e22880259a76fafd15c'], CraftHash.HashAlgorithm.SHA256)
+        self.targets['0.18.13'] = "https://www.libraw.org/data/LibRaw-0.18.13.tar.gz"
+        self.archiveNames['0.18.13'] = "LibRaw-0.18.13.tar.gz"
+        self.targetInstSrc['0.18.13'] = "LibRaw-0.18.13"
+        self.patchToApply['0.18.13'] = [("LibRaw-0.18.13-20180720.diff", 1)]#https://github.com/LibRaw/LibRaw-cmake/
+        self.targetDigests['0.18.13'] = (['cb1f9d0d1fabc8967d501d95c05d2b53d97a2b917345c66553b1abbea06757ca'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "LibRaw is a library for reading RAW files obtained from digital photo cameras (CRW/CR2, NEF, RAF, DNG, and others)."
-        self.defaultTarget = '0.17.2'
+        self.defaultTarget = '0.18.13'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
