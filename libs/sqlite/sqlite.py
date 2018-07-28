cd9ea54e7e467f5db128d11a376bc7550e267c87
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2018-07-28 11:53:07 +0200

    Fix SQLite discovery on MacOS

--- libs/sqlite/sqlite.py
+++ libs/sqlite/sqlite.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
 import info
 from Package.CMakePackageBase import *
 
@@ -17,18 +41,18 @@ class subinfo(info.infoclass):
 
         self.patchToApply['3.8.1.0'] = [("sqlite_cmake_and_wince_20130124.diff", 1)]
         self.patchToApply['3.15.0'] = [("sqlite-amalgamation-3150000-20161021.diff", 1)]
+        self.patchToApply['3.15.0'] += [("sqlite-pkgconfig-file-20180728.diff", 1)]
+        self.patchLevel['3.15.0'] = 2
         self.patchToApply['3.24.0'] = [("sqlite-amalgamation-3150000-20161021.diff", 1)]
+        self.patchToApply['3.24.0'] += [("sqlite-pkgconfig-file-20180728.diff", 1)]
+        self.patchLevel['3.24.0'] = 2
 
         self.targetInstSrc['3.8.1.0'] = "sqlite-amalgamation-3080100"
         self.targetInstSrc['3.15.0'] = "sqlite-amalgamation-3150000"
         self.targetInstSrc['3.24.0'] = "sqlite-amalgamation-3240000"
 
         self.description = "a library providing a self-contained, serverless, zero-configuration, transactional SQL database engine"
-        if OsUtils.isMac():
-          #otherwise akonadi reports that "Sqlite 3.24.0 was found, but it is not compiled with -DSQLITE_ENABLE_UNLOCK_NOTIFY"
-          self.defaultTarget = '3.15.0'
-        else:
-          self.defaultTarget = '3.24.0'
+        self.defaultTarget = '3.24.0'
 
     def setDependencies(self):
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = "default"
@@ -40,10 +64,60 @@ class Package(CMakePackageBase):
         CMakePackageBase.__init__(self)
 
         if OsUtils.isMac():
+            pass
+
+            # /usr/lib/libsqlite3.dylib is version 9.0.0
+            # %CraftRoot%/lib/libsqlite3.dylib is version 3.0.0
+            # Akonadi tried to link to the first one,
+            # while the second one is packaged into the final package.
+            # Since 2018-07-28 SQLite can be found through PKGConfig,
+            # so let's see if below error will persist without static library.
+
             # Build sqlite as static library under macOS
             # Otherwise we might run into:
             # executing command: .../macdeployqt .../kdevelop.app -always-overwrite -verbose=1
             # dyld: Library not loaded: /usr/lib/libsqlite3.dylib
             #  Referenced from: /System/Library/Frameworks/Security.framework/Versions/A/Security
             #  Reason: Incompatible library version: Security requires version 9.0.0 or later, but libsqlite3.dylib provides version 3.0.0
-            self.subinfo.options.configure.args += " -DSTATIC_LIBRARY=TRUE"
+            # self.subinfo.options.configure.args += " -DSTATIC_LIBRARY=TRUE"
+
+    def configure(self):
+        CMakeListsFile = os.path.join(self.sourceDir(), "CMakeLists.txt")
+        with open(CMakeListsFile, "rt") as f:
+            content = f.read()
+
+        content = content.replace(r"set(SQLITE_VERSION_MINOR 15)", r"set(SQLITE_VERSION_MINOR %s)" % self.version.split(".")[1])
+
+        with open(CMakeListsFile, "wt") as f:
+            f.write(content)
+
+        return super().configure()
+
+    def install(self):
+
+        isInstalled = super().install()
+
+        if isInstalled and OsUtils.isMac():
+            # allow finding sqlite library by pkgconfig module
+            # otherwise e.g. Akonadi finds /usr/lib/libsqlite3.dylib
+            # instead of Craft's sqlite library
+            pkgConfigDir = os.path.join(self.installDir(), "lib", "pkgconfig")
+            pkgConfigFile = os.path.join(pkgConfigDir, "sqlite3.pc")
+            utils.createDir(pkgConfigDir)
+            utils.copyFile(os.path.join(self.sourceDir(), "sqlite.pc.in"), pkgConfigFile)
+
+            with open(pkgConfigFile, "rt") as f:
+                content = f.read()
+
+            content = content.replace(r"@prefix@", CraftCore.standardDirs.craftRoot())
+            content = content.replace(r"@exec_prefix@", r"${prefix}/bin")
+            content = content.replace(r"@libdir@", r"${prefix}/lib")
+            content = content.replace(r"@includedir@", r"${prefix}/include")
+            content = content.replace(r"@RELEASE@", self.version)
+
+            with open(pkgConfigFile, "wt") as f:
+                f.write(content)
+
+        return isInstalled
+
+
