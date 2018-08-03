3c1712dda98d1fa4f199f0bb4341bccf9bc74870
Hannah von Reth <vonreth@kde.org>
2018-08-03 12:34:22 +0200

    Add digests

--- dev-utils/_unix/bison/bison.py
+++ dev-utils/_unix/bison/bison.py
@@ -15,9 +15,10 @@ class subinfo(info.infoclass):
         for ver in ["3.0.4"]:
             self.targets[ver] = f"http://ftp.gnu.org/gnu/bison/bison-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"bison-{ver}"
-        self.defaultTarget = '3.0.4'
         self.patchToApply["3.0.4"] = [("vasnprintf-macos.diff", 1)]
+        self.targetDigests["3.0.4"] = (['a72428c7917bdf9fa93cb8181c971b6e22834125848cf1d03ce10b1bb0716fe1'], CraftHash.HashAlgorithm.SHA256)
 
+        self.defaultTarget = '3.0.4'
 
 class Package(AutoToolsPackageBase):
     def __init__(self, **args):
