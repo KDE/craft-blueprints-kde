4f83f556a8ace2149a6f317acc809bd160d4c60f
Hannah von Reth <vonreth@kde.org>
2018-08-10 09:13:45 +0200

    Allow to install pip with the embedded python

---
+++ python-modules/pip/pip.py
@@ -0,0 +1,35 @@
+import info
+from Package.MaybeVirtualPackageBase import *
+
+from pathlib import Path
+import sys
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.targets["master"] = f"https://bootstrap.pypa.io/get-pip.py"
+        self.defaultTarget = "master"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/bin-base"] = "default"
+
+
+from Package.BinaryPackageBase import *
+
+
+class PPackage(BinaryPackageBase):
+    def __init__(self):
+        BinaryPackageBase.__init__(self)
+        self.subinfo.options.unpack.runInstaller = True
+
+    def unpack(self):
+        get_pip = self.localFilePathe()[0]
+        return (utils.system([sys.executable, get_pip])
+                and utils.deleteFile(get_pip))
+
+
+class Package(MaybeVirtualPackageBase):
+    def __init__(self):
+        root = Path(CraftCore.standardDirs.craftRoot())
+        py = Path(sys.executable)
+        MaybeVirtualPackageBase.__init__(self, condition=root in py.parents, classA=PPackage)
+
