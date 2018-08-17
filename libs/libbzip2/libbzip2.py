a43efa99526e6f6862c94bafe4b8a86e0cd1c541
Simon Depiets <sdepiets@gmail.com>
2018-08-17 12:01:36 +0800

    Changed d/l link as the domain expired

--- libs/libbzip2/libbzip2.py
+++ libs/libbzip2/libbzip2.py
@@ -10,7 +10,8 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['1.0.6'] = 'http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz'
+        #self.targets['1.0.6'] = 'http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz'
+        self.targets['1.0.6'] = 'https://ftp.osuosl.org/pub/clfs/conglomeration/bzip2/bzip2-1.0.6.tar.gz'
         self.targetInstSrc['1.0.6'] = "bzip2-1.0.6"
         self.patchToApply['1.0.6'] = ("bzip.diff", 1)
         self.targetDigests['1.0.6'] = '3f89f861209ce81a6bab1fd1998c0ef311712002'
