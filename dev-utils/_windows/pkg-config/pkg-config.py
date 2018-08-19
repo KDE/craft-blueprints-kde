06600d200172ca504716f07c941f4a3c27cf5041
Hannah von Reth <vonreth@kde.org>
2018-08-19 22:21:50 +0200

    Update pkg-config

--- dev-utils/_windows/pkg-config/pkg-config.py
+++ dev-utils/_windows/pkg-config/pkg-config.py
@@ -4,14 +4,15 @@ from Package.BinaryPackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets["0.28"] = "http://downloads.sourceforge.net/msys2/mingw-w64-i686-pkgconf-0.9.5-1-any.pkg.tar.xz"
+        self.targets["0.29.2"] = "http://repo.msys2.org/mingw/x86_64/mingw-w64-x86_64-pkg-config-0.29.2-1-any.pkg.tar.xz"
         self.targetDigests["0.28"] = "8fe89c3ac4217beffbb942a2e9da3597d30226e8"
-        self.targetInstSrc["0.28"] = "mingw32"
-        self.targetInstallPath["0.28"] = "dev-utils"
-        self.defaultTarget = "0.28"
+        self.targetInstSrc["0.29.2"] = "mingw64"
+        self.targetInstallPath["0.29.2"] = "dev-utils"
+        self.defaultTarget = "0.29.2"
 
     def setDependencies(self):
         self.buildDependencies["virtual/bin-base"] = "default"
+        self.buildDependencies["libs/mingw-crt4msvc"] = "default"
 
 
 class Package(BinaryPackageBase):
