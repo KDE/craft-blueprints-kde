87022380a788ef3ad4f0175ffd3e7a36457f92cd
Dominik Schmidt <dev@dominik-schmidt.de>
2018-08-24 12:42:52 +0200

    Bump qtkeychain to 0.9.1

--- qt-libs/qtkeychain/qtkeychain.py
+++ qt-libs/qtkeychain/qtkeychain.py
@@ -11,7 +11,7 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/frankosterfeld/qtkeychain.git'
-        for ver in ["0.4.0", "0.5.0", "0.7.0", "0.8.0"]:
+        for ver in ["0.4.0", "0.5.0", "0.7.0", "0.8.0", "0.9.0", "0.9.1"]:
             self.targets[ver] = "https://github.com/frankosterfeld/qtkeychain/archive/v%s.tar.gz" % ver
             self.archiveNames[ver] = "qtkeychain-v%s.tar.gz" % ver
             self.targetInstSrc[ver] = 'qtkeychain-%s' % ver
@@ -22,7 +22,11 @@ class subinfo(info.infoclass):
             ['9f9afaae8e909391d7fc932bac95e38befaac5b2eb49f6623a8efec60a2e6a3a'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['0.8.0'] = (
             ['b492f603197538bc04b2714105b1ab2b327a9a98d400d53d9a7cb70edd2db12f'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = '0.8.0'
+        self.targetDigests['0.9.0'] = (
+            ['0935e5eb67fbed0b2db5e74ea8cbd667c6be6eed3767f212eac6ac318ab8a6fc'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['0.9.1'] = (
+            ['9c2762d9d0759a65cdb80106d547db83c6e9fdea66f1973c6e9014f867c6f28e'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = '0.9.1'
 
 
 class Package(CMakePackageBase):
