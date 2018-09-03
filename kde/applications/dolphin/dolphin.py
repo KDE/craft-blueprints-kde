196091872d66e2a40a42015617f8b0e6299168ba
Alex Richardson <arichardson.kde@gmail.com>
2018-09-03 17:43:24 +0100

    Build dolphin without kdelibs4support
    Summary:
    Without this change I was not able to build dolphin on MacOS.
    I'm not sure if dolphin still needs kdelibs4support but it seems to
    build just fine without.
    
    Test Plan: dolphin builds now
    
    Reviewers: vonreth, broulik
    
    Reviewed By: broulik
    
    Subscribers: broulik
    
    Differential Revision: https://phabricator.kde.org/D15244

--- kde/applications/dolphin/dolphin.py
+++ kde/applications/dolphin/dolphin.py
@@ -28,7 +28,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/ktexteditor"] = "default"
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = "default"
         self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = "default"
-        self.runtimeDependencies["kde/frameworks/tier4/kdelibs4support"] = "default"
 
         # While KDNSSD is nice, it doesn't work on macOS, so we cannot have kio-extras there
         if not CraftCore.compiler.isMacOS:
