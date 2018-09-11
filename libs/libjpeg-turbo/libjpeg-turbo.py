e6e8aa4d4e80f97e485a0d8c792d8419987fc6bd
Alex Richardson <arichardson.kde@gmail.com>
2018-09-11 09:18:50 +0100

    Add libjpeg-turbo 1.5.3
    1.5.1 doesn't compile on macOS 10.13 anymore due to nasm errors. 1.5.3
    appears to work fine. Next step is to update to 2.0.0 since that uses
    CMake for all platforms.

--- libs/libjpeg-turbo/libjpeg-turbo.py
+++ libs/libjpeg-turbo/libjpeg-turbo.py
@@ -29,17 +29,18 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/libjpeg-turbo/libjpeg-turbo.git"
 
-        for ver in ["1.5.1"]:
+        for ver in ["1.5.1", "1.5.3"]:
             self.targets[ver] = "https://github.com/libjpeg-turbo/libjpeg-turbo/archive/%s.tar.gz" % ver
             self.archiveNames[ver] = "libjpeg-turbo-%s.tar.gz" % ver
             self.targetInstSrc[ver] = 'libjpeg-turbo-%s' % ver
 
         self.targetDigests['1.5.1'] = (
             ['c15a9607892113946379ccea3ca8b85018301b200754f209453ab21674268e77'], CraftHash.HashAlgorithm.SHA256)
-
+        self.targetDigests['1.5.3'] = (
+            ['1a17020f859cb12711175a67eab5c71fc1904e04b587046218e36106e07eabde'], CraftHash.HashAlgorithm.SHA256)
         self.description = "libjpeg-turbo is a JPEG image codec that uses SIMD instructions (MMX, SSE2, NEON, AltiVec) to accelerate baseline JPEG compression and decompression on x86, x86-64, ARM, and PowerPC systems"
         self.webpage = "http://libjpeg-turbo.virtualgl.org/"
-        self.defaultTarget = '1.5.1'
+        self.defaultTarget = '1.5.3'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
