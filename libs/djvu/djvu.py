2b6289dc21b381ce1cb1decfb729fd6154536e1e
Alex Richardson <arichardson.kde@gmail.com>
2018-09-13 13:23:15 +0100

    Fix djvu build on macos
    Summary: Build using autotools on non-Windows systems since that is the supported way.
    
    Reviewers: vonreth
    
    Reviewed By: vonreth
    
    Differential Revision: https://phabricator.kde.org/D15446

--- libs/djvu/djvu.py
+++ libs/djvu/djvu.py
@@ -7,10 +7,11 @@ class subinfo(info.infoclass):
             self.targets[ver] = 'http://downloads.sourceforge.net/djvu/djvulibre-' + ver + '.tar.gz'
             self.targetInstSrc[ver] = 'djvulibre-' + ver
         self.targetInstSrc['3.5.25.3'] = 'djvulibre-3.5.25'
-        self.patchToApply['3.5.21'] = [("djvu-cmake.diff", 0)]
-        self.patchToApply['3.5.23'] = [("djvulibre-3.5.23-20101116.diff", 1)]
-        self.patchToApply['3.5.25.3'] = [("djvulibre-3.5.25.3-20130906.diff", 1)]
-        self.patchToApply['3.5.27'] = [("djvulibre-3.5.27-20151208.diff", 1)]
+        if OsUtils.isWin():
+            self.patchToApply['3.5.21'] = [("djvu-cmake.diff", 0)]
+            self.patchToApply['3.5.23'] = [("djvulibre-3.5.23-20101116.diff", 1)]
+            self.patchToApply['3.5.25.3'] = [("djvulibre-3.5.25.3-20130906.diff", 1)]
+            self.patchToApply['3.5.27'] = [("djvulibre-3.5.27-20151208.diff", 1)]
         self.targetDigests['3.5.23'] = 'b19f6b461515a52eb1048aec81e04dfd836d681f'
         self.targetDigests['3.5.25.3'] = 'ad35056aabb1950f385360ff59520a82a6f779ec'
         self.targetDigests['3.5.27'] = '99c4f2c621c063bf8c8a1626030539fe5a8675f9'
@@ -21,11 +22,28 @@ class subinfo(info.infoclass):
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
 
-
 from Package.CMakePackageBase import *
+from Package.AutoToolsPackageBase import *
 
 
-class Package(CMakePackageBase):
+class PackageAutoTools(AutoToolsPackageBase):
+    def __init__(self, **args):
+        AutoToolsPackageBase.__init__(self)
+        self.subinfo.options.configure.args = " --enable-shared "
+        if CraftCore.compiler.isMacOS:
+            # attempting to build and install the png files will fail on MacOS
+            self.subinfo.options.configure.args += " --disable-desktopfiles "
+
+
+
+class PackageCMake(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
         self.subinfo.options.configure.args = "-DBUILD_TOOLS=OFF"
+
+if CraftCore.compiler.isWindows:
+    class Package(PackageCMake):
+        pass
+else:
+    class Package(PackageAutoTools):
+        pass
