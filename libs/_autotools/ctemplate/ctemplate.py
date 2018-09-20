f88841661073824a7922315125d4b04dc4381fd7
Hannah von Reth <vonreth@kde.org>
2018-09-20 17:07:13 +0200

    Add ctemplate

---
+++ libs/_autotools/ctemplate/ctemplate.py
@@ -0,0 +1,29 @@
+
+
+import info
+import re
+import CraftCore
+
+
+class subinfo(info.infoclass):
+    def setTargets( self ):
+        for ver in ["2.3"]:
+            self.targets[ver] = f"https://github.com/OlafvdSpek/ctemplate/archive/ctemplate-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"ctemplate-ctemplate-{ver}"
+
+        self.targetDigests["2.3"] = (['99e5cb6d3f8407d5b1ffef96b1d59ce3981cda3492814e5ef820684ebb782556'], CraftHash.HashAlgorithm.SHA256)
+        self.patchToApply["2.3"] = [("ctemplate-2.3-20180920.diff", 1)]
+        self.description = "This library provides an easy to use and lightning fast text templating system to use with C++ programs."
+        self.defaultTarget = "2.3"
+
+    def setDependencies( self ):
+        self.buildDependencies["dev-utils/msys"] = "default"
+        self.runtimeDependencies["virtual/base"] = "default"
+
+from Package.AutoToolsPackageBase import *
+
+class Package(AutoToolsPackageBase):
+    def __init__( self, **args ):
+        AutoToolsPackageBase.__init__( self )
+        self.subinfo.options.useShadowBuild = False
+        self.subinfo.options.configure.args += f" --disable-static --enable-shared"
