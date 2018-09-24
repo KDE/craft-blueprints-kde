5f07905fce7cccb2d25078de062ae856f00cca9f
Simon Depiets <sdepiets@gmail.com>
2018-09-24 16:53:45 +0800

    Upgrade to libfftw 3.3.8 with native cmake support
    Summary: BUG: 386878
    
    Reviewers: #craft, vonreth
    
    Reviewed By: #craft, vonreth
    
    Tags: #craft
    
    Differential Revision: https://phabricator.kde.org/D15792

--- libs/libfftw/libfftw.py
+++ libs/libfftw/libfftw.py
@@ -4,14 +4,12 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['3.2.2'] = 'http://www.fftw.org/fftw-3.2.2.tar.gz'
-        self.targetDigests['3.2.2'] = 'd43b799eedfb9408f62f9f056f5e8a645618467b'
-        self.targetInstSrc['3.2.2'] = "fftw-3.2.2"
-        self.patchToApply['3.2.2'] = [('fftw-3.2.2-20111221.diff', 1),
-                                      ('fftw-3.2.2-20130818.diff', 1)]
+        self.targets['3.3.8'] = 'http://www.fftw.org/fftw-3.3.8.tar.gz'
+        self.targetDigests['3.3.8'] = '59831bd4b2705381ee395e54aa6e0069b10c3626'
+        self.targetInstSrc['3.3.8'] = "fftw-3.3.8"
         self.description = "a C subroutine library for computing the discrete Fourier transform (DFT)"
 
-        self.defaultTarget = '3.2.2'
+        self.defaultTarget = '3.3.8'
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = "default"
@@ -24,4 +22,3 @@ class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
         self.supportsNinja = False
-        self.subinfo.options.configure.args = "-DFFTW_SINGLE=ON -DFFTW_DOUBLE=OFF -DBUILD_BENCHMARKS=OFF"
