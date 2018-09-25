512937488587687cb076fe15a5816ad37ef903bc
Hannah von Reth <vonreth@kde.org>
2018-09-25 11:30:17 +0200

    Don't build ghostscrip with nmake on mac

--- libs/ghostscript/ghostscript.py
+++ libs/ghostscript/ghostscript.py
@@ -143,7 +143,7 @@ class PackageMSys(AutoToolsPackageBase):
         return True
 
 
-if CraftCore.compiler.isMinGW():
+if CraftCore.compiler.isGCCLike():
     class Package(PackageMSys):
         pass
 else:
