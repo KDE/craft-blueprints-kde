3dbbfae78cc24990dc6279186159fc1bf0c2078b
Hannah von Reth <vonreth@kde.org>
2018-09-25 16:21:38 +0200

    Make sure that projects use atleast cmake 3.0

--- kde/applications/libkexiv2/libkexiv2.py
+++ kde/applications/libkexiv2/libkexiv2.py
@@ -4,6 +4,7 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
+        self.patchToApply["18.08.1"] = [("libkexiv2-18.08.1-20180925.diff", 1)]
 
         self.description = "Libkexiv2 is a wrapper around Exiv2 library to manipulate pictures metadata as EXIF IPTC and XMP."
 
--- kde/frameworks/tier3/purpose/purpose.py
+++ kde/frameworks/tier3/purpose/purpose.py
@@ -4,6 +4,7 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
+        self.patchToApply["5.50.0"] = [("purpose-5.50.0-20180925.diff", 1)]
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
