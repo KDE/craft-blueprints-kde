bc65ae374b3aacd7cf10e5c064d1f7269fa5a41b
Simon Depiets <sdepiets@gmail.com>
2018-09-28 10:46:16 +0800

    Upgrade libsamplerate and fix compilation

--- libs/libsamplerate/libsamplerate.py
+++ libs/libsamplerate/libsamplerate.py
@@ -5,12 +5,12 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['0.1.7'] = "http://www.mega-nerd.com/SRC/libsamplerate-0.1.7.tar.gz"
-        self.targetInstSrc['0.1.7'] = 'libsamplerate-0.1.7'
-        self.targetDigests['0.1.7'] = 'f3f803ec5feae5a3fdb0fa3937277669e854386e'
-        self.patchToApply['0.1.7'] = ('libsamplerate-0.1.7-20091002.diff', 1)
+        self.targets['0.1.9'] = "http://www.mega-nerd.com/SRC/libsamplerate-0.1.9.tar.gz"
+        self.targetInstSrc['0.1.9'] = 'libsamplerate-0.1.9'
+        self.targetDigests['0.1.9'] = 'ed60f957a4ff87aa15cbb1f3dbd886fa7e5e9566'
+        self.patchToApply['0.1.9'] = ('libsamplerate-0.1.9-20180928.diff', 1)
         self.description = "an audio sample rate converter library"
-        self.defaultTarget = '0.1.7'
+        self.defaultTarget = '0.1.9'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
@@ -20,4 +20,4 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args = " -DBUILD_SHARED_LIB=ON -DBUILD_EXAMPLES=ON -DBUILD_TESTS=ON"
+        self.subinfo.options.configure.args = " -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=ON -DBUILD_TEST=ON"
