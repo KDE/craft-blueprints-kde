db70204015ec0e21536a190aa99dc07e3f4482b3
Hannah von Reth <vonreth@kde.org>
2018-10-04 10:17:22 +0200

    Add qtc 4.7.1

--- qt-apps/qtcreator/qtcreator.py
+++ qt-apps/qtcreator/qtcreator.py
@@ -7,14 +7,17 @@ class subinfo(info.infoclass):
         self.svnTargets["master"] = "git://code.qt.io/qt-creator/qt-creator.git"
         for branch in ["4.3", "4.4", "4.5"]:
             self.svnTargets[branch] = f"git://code.qt.io/qt-creator/qt-creator.git|{branch}"
-        self.targets[
-            "4.0.2"] = "http://download.qt-project.org/official_releases/qtcreator/4.0/4.0.2/qt-creator-opensource-src-4.0.2.tar.gz"
+
+        for ver in ["4.7.1"]:
+            majorVer = ".".join(ver.split(".")[:2])
+            self.targets[ver] = f"http://download.qt.io/official_releases/qtcreator/{majorVer}/{ver}/qt-creator-opensource-src-{ver}.tar.xz"
+            self.targetInstSrc[ver] = f"qt-creator-opensource-src-{ver}"
         self.targetDigests['4.0.2'] = 'ef7c5760d7dc72cb68ee1ddf84cb4245e41c39d5'
-        self.targetInstSrc["4.0.2"] = "qt-creator-opensource-src-4.0.2"
-        self.defaultTarget = "4.4"
+        self.defaultTarget = "4.7.1"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtscript"] = None
 
 
 from Package.Qt5CorePackageBase import *
