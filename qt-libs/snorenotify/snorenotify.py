505c6a615a606daafbd609902eba725c5b518202
Hannah von Reth <vonreth@kde.org>
2018-10-04 14:25:50 +0200

    Trigger rebuild of snorenotify

--- qt-libs/snorenotify/snorenotify.py
+++ qt-libs/snorenotify/snorenotify.py
@@ -25,6 +25,7 @@ class subinfo(info.infoclass):
             CraftHash.HashAlgorithm.SHA256)
 
         self.description = "Snorenotify is a multi platform Qt notification framework. Using a plugin system it is possible to create notifications with many different notification systems on Windows, Mac OS and Unix."
+        self.patchLevel["0.7"] = 1
         self.defaultTarget = '0.7'
 
 
