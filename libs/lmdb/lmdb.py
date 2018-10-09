d5e9f5ddf3a25f0ed8d9e8dcf58efaeac00d8a2b
Hannah von Reth <vonreth@kde.org>
2018-10-09 09:06:02 +0200

    Fix lib name

--- libs/lmdb/lmdb.py
+++ libs/lmdb/lmdb.py
@@ -7,8 +7,10 @@ class subinfo(info.infoclass):
         for ver in ['0.9.16']:
             self.targets[ver] = 'https://github.com/LMDB/lmdb/archive/LMDB_' + ver + '.tar.gz'
             self.targetInstSrc[ver] = 'lmdb-LMDB_' + ver + '/libraries/liblmdb'
-        self.patchToApply['0.9.16'] = [('lmdb-LMDB_0.9.16-20151004.diff', 3)]
+        self.patchToApply['0.9.16'] = [('lmdb-LMDB_0.9.16-20151004.diff', 3),
+                                       ("lmdb-0.9.16-20181009.diff", 3)]
         self.targetDigests['0.9.16'] = '367182e1d9dbc314db76459a71be719209f131b4'
+        self.patchLevel["0.9.16"] = 1
 
         self.description = 'in memory database from the openldap project'
         self.defaultTarget = '0.9.16'
@@ -23,4 +25,4 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args = "-DBUILD_TESTS=OFF -DBUILD_TOOLS=OFF"
+        self.subinfo.options.configure.args = "-DBUILD_TESTS=OFF -DBUILD_TOOLS=OFF -DBUILD_STATIC=OFF"
