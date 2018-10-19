d3142c12a88b72d4373cd6b461d59aaa6f8f0190
Łukasz Wojniłowicz <lukasz.wojnilowicz@gmail.com>
2018-10-19 18:49:25 +0200

    Fix KDiagram on MacOS, try 5

--- extragear/kdiagram/kdiagram.py
+++ extragear/kdiagram/kdiagram.py
@@ -7,7 +7,7 @@ class subinfo(info.infoclass):
         self.defaultTarget = "master"
         # otherwise undefined KGantt::Constraint while building tests
         if CraftCore.compiler.isMacOS:
-            self.patchToApply["master"] = [("kdiagram-20181018.patch", 1)]
+            self.patchToApply["master"] = [("kdiagram-20181019.patch", 1)]
 
         self.description = "Powerful libraries (KChart, KGantt) for creating business diagrams"
 
