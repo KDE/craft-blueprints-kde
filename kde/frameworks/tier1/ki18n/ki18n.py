66ad0797aacfa57ed1e5e6ed3afd1cb6f965e1d5
Hannah von Reth <vonreth@kde.org>
2018-10-19 21:46:38 +0200

    Apply to all tarballs

--- kde/frameworks/tier1/ki18n/ki18n.py
+++ kde/frameworks/tier1/ki18n/ki18n.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
         self.versionInfo.setDefaultValues()
         self.patchToApply["5.45.0"] = [("0001-Don-t-need-to-run-previous-iterations-commands-again.patch", 1),
                 ("0002-Keep-LibIntl-libraries-path.patch", 1)]
-        for ver in ['master', '5.42.0', '5.43.0', '5.44.0', '5.46.0', '5.47.0', '5.48.0', '5.49.0']:
+        for ver in ["master"] + self.versionInfo.tarballs():
             self.patchToApply[ver] = [("0002-Keep-LibIntl-libraries-path.patch", 1)]
         self.description = "Ki18n"
 
