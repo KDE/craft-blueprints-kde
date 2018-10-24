d96c5256d23e16f059ab84ee8299ee4596ff9bdc
Hannah von Reth <vonreth@kde.org>
2018-10-24 13:55:14 +0200

    Fix 32 bit digests by removing the 32bit url, we require a 64bit host anyway

--- dev-utils/_windows/putty/putty.py
+++ dev-utils/_windows/putty/putty.py
@@ -4,13 +4,11 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ["0.70"]:
-            self.targets[ver] = f"https://the.earth.li/~sgtatham/putty/{ver}/w{CraftCore.compiler.bits}/putty.zip"
+            self.targets[ver] = f"https://the.earth.li/~sgtatham/putty/{ver}/w64/putty.zip"
             self.archiveNames[ver] = f"putty-{ver}.zip"
             self.targetInstallPath[ver] = os.path.join("dev-utils", "bin")
 
-            self.targetDigestsX64['0.70'] = (['8422ad5fe060b7229fbf51512e3eb23c5bfe631eb660e9604343e5e81b69fad0'], CraftHash.HashAlgorithm.SHA256)
             self.targetDigests['0.70'] = (['8422ad5fe060b7229fbf51512e3eb23c5bfe631eb660e9604343e5e81b69fad0'], CraftHash.HashAlgorithm.SHA256)
-
         self.defaultTarget = "0.70"
 
     def setDependencies(self):
