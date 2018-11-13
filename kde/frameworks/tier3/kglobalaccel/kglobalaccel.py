c9b68270185ac226c67710e9ce7d50f55cc2fd03
Hannah von Reth <vonreth@kde.org>
2018-11-13 13:02:55 +0100

    Fix mac build of kglobalaccel

--- kde/frameworks/tier3/kglobalaccel/kglobalaccel.py
+++ kde/frameworks/tier3/kglobalaccel/kglobalaccel.py
@@ -4,6 +4,7 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
+        self.patchToApply["5.52.0"] = [("0001-Fix-CMake-condition.patch", 1)]
 
         self.description = "KGlobalAccel"
 
