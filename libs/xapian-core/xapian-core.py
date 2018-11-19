61dbc65074302b0e077c2a3250f3f21336eee830
Hannah von Reth <vonreth@kde.org>
2018-11-19 17:10:04 +0100

    Build xapian-core with msvc

--- libs/xapian-core/xapian-core.py
+++ libs/xapian-core/xapian-core.py
@@ -5,31 +5,44 @@ from Package.AutoToolsPackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.4.5"]:
+        self.svnTargets["master"] = "https://github.com/xapian/xapian.git"
+        self.targetInstSrc["master"] = f"xapian-core"
+        for ver in ["1.4.5", "1.4.9"]:
             self.targets[ver] = f"http://oligarchy.co.uk/xapian/{ver}/xapian-core-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"xapian-core-{ver}"
         self.targetDigests["1.4.5"] = (['85b5f952de9df925fd13e00f6e82484162fd506d38745613a50b0a2064c6b02b'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.4.9"] = (['cde9c39d014f04c09b59d9c21551db9794c10617dc69ab4c9826352a533df5cc'], CraftHash.HashAlgorithm.SHA256)
         if CraftCore.compiler.isWindows:
             self.patchToApply["1.4.5"] = [("xapian-core-1.4.5-20180515.diff", 1)]
+            self.patchToApply["1.4.9"] = [("xapian-core-1.4.5-20180515.diff", 1)]
         self.description = "Open Source Search Engine library"
         self.webpage = "https://xapian.org/"
-        self.defaultTarget = "1.4.5"
+        self.defaultTarget = "1.4.9"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/libxslt"] = None
         self.runtimeDependencies["libs/zlib"] = None
         self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["dev-utils/msys"] = None
 
 
 class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
         self.subinfo.options.configure.args += " --disable-static --enable-shared "
+        if CraftCore.compiler.isMSVC():
+            self.shell.useMSVCCompatEnv = True
+            self.subinfo.options.useShadowBuild = False
+            self.shell.environment["CC"] = f"{self.shell.toNativePath(os.path.join(self.sourceDir(), 'compile'))} {self.shell.environment['CC']}"
+            self.shell.environment["CXX"] = f"{self.shell.toNativePath(os.path.join(self.sourceDir(), 'compile'))} {self.shell.environment['CXX']}"
 
+    def configure(self):
+        if self.buildTarget == "master":
+            if not self.shell.execute(self.sourceDir(), "perl", "preautoreconf"):
+                return False
+        return super().configure()
 
     def postInstall(self):
-        if not self.copyToMsvcImportLib():
-            return False
         return (self.patchInstallPrefix([os.path.join(self.installDir(), "lib" , "cmake", "xapian", "xapian-config.cmake")],
                                         OsUtils.toMSysPath(self.subinfo.buildPrefix),
                                         OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())) and
