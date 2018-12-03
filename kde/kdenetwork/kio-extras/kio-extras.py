4dea48a064a33cde52d7e4d14b2c8f9179a0c1df
Hannah von Reth <vonreth@kde.org>
2018-12-03 10:17:40 +0100

    Fix dep

--- kde/kdenetwork/kio-extras/kio-extras.py
+++ kde/kdenetwork/kio-extras/kio-extras.py
@@ -12,7 +12,7 @@ class subinfo(info.infoclass):
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
-        self.runtimeDependencies["lib/libssh"] = None
+        self.runtimeDependencies["libs/libssh2"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kactivities"] = None
         self.runtimeDependencies["kde/frameworks/tier1/karchive"] = None
