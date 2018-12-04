c616cba5a009ecfd486c9b5aae0f9eda92facc77
Hannah von Reth <vonreth@kde.org>
2018-12-04 09:29:21 +0100

    Update libssh, 0.8.5

--- libs/libssh/libssh.py
+++ libs/libssh/libssh.py
@@ -1,22 +1,40 @@
 # -*- coding: utf-8 -*-
+# Copyright Hannah von Reth <vonreth@kde.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
 import info
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in [(195, '0.7.3')]:
-            # e.g. https://red.libssh.org/attachments/download/195/libssh-0.7.3.tar.xz
-            self.targets[ver[1]] = "https://red.libssh.org/attachments/download/{0}/libssh-{1}.tar.xz".format(ver[0],
-                                                                                                              ver[1])
-            self.targetInstSrc[ver[1]] = "libssh-" + ver[1]
-        self.targetDigests['0.7.3'] = '9de2a8fde51aa7b7855008fafd5bf47ebb01289f'
-
-        self.svnTargets['master'] = "git://git.libssh.org/projects/libssh.git"
+        self.svnTargets["master"] = "git://git.libssh.org/projects/libssh.git"
+        for ver in ["0.8.5"]:
+            self.targets[ver] = f"https://www.libssh.org/files/{ver[:3]}/libssh-{ver}.tar.xz"
+            self.targetInstSrc[ver] = f"libssh-{ver}"
+        self.targetDigests["0.8.5"] = (['07d2c431240fc88f6b06bcb36ae267f9afeedce2e32f6c42f8844b205ab5a335'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "a working SSH implementation by the mean of a library"
-        self.defaultTarget = '0.7.3'
-        # self.options.configure.args = "-DWITH_STATIC_LIB=ON"
-
+        self.defaultTarget = "0.8.5"
+        
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/zlib"] = None
