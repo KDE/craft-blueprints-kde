843f72d51a36e32cfd4991e2a17ad69a386ede58
Albert Vaca <albertvaka@gmail.com>
2018-12-11 20:58:10 +0100

    Update openal
    Previous version didn't compile with current CMake

--- libs/openal-soft/openal-soft.py
+++ libs/openal-soft/openal-soft.py
@@ -4,14 +4,14 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['1.15.1', '1.18.0']:
+        for ver in ['1.15.1', '1.18.0', '1.19.1']:
             self.targets[ver] = \
                 'http://kcat.strangesoft.net/openal-releases/openal-soft-' + ver + '.tar.bz2'
             self.targetInstSrc[ver] = 'openal-soft-' + ver
         self.patchToApply['1.15.1'] = ('openal-soft-1.15.1-20130411.diff', 1)
         self.targetDigests['1.15.1'] = 'a0e73a46740c52ccbde38a3912c5b0fd72679ec8'
         self.description = 'a library for audio support'
-        self.defaultTarget = '1.18.0'
+        self.defaultTarget = '1.19.1'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
