482bda20650e85ef1bc1293e3815d474d506a2b8
Patrick José Pereira <patrickelectric@gmail.com>
2018-12-11 18:45:58 -0200

    libbfd: Update lib to 2.31
    Summary: Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>
    
    Reviewers: vonreth
    
    Reviewed By: vonreth
    
    Subscribers: vonreth
    
    Differential Revision: https://phabricator.kde.org/D17512

--- libs/libbfd/libbfd.py
+++ libs/libbfd/libbfd.py
@@ -4,23 +4,25 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['2.21.1a'] = "http://ftp.gnu.org/gnu/binutils/binutils-2.21.1a.tar.bz2"
-        self.targetInstSrc['2.21.1a'] = 'binutils-2.21.1/bfd'
-        self.targetDigests['2.21.1a'] = '525255ca6874b872540c9967a1d26acfbc7c8230'
-        self.defaultTarget = '2.21.1a'
+        version = '2.31'
+        self.targets[version] = "http://ftp.gnu.org/gnu/binutils/binutils-%s.tar.bz2" % version
+        self.targetInstSrc[version] = 'binutils-%s/bfd' % version
+        self.targetDigests[version] = ('2c49536b1ca6b8900531b9e34f211a81caf9bf85b1a71f82b81ae32fcd8ffe19', CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = version
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/zlib"] = None
         self.buildDependencies["dev-utils/msys"] = None
 
 
 from Package.AutoToolsPackageBase import *
 from Package.VirtualPackageBase import *
 
-
 class PackageMinGW(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
+        self.subinfo.options.configure.args = "--with-system-zlib "
 
 
 if CraftCore.compiler.isMinGW():
