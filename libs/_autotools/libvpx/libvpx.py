16171538cacbdb5e039098a9d81ffd0da69df4a3
Vincent Pinon <vpinon@kde.org>
2018-12-22 22:04:13 +0100

    Fix building Kdenlive on Linux

--- libs/_autotools/libvpx/libvpx.py
+++ libs/_autotools/libvpx/libvpx.py
@@ -4,7 +4,7 @@ class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ["1.7.0"]:
             self.targets[ver] = f"https://chromium.googlesource.com/webm/libvpx/+archive/v{ver}.tar.gz"
-        self.targetDigests['1.7.0'] = (['542cb556dabff1f2caea1643aa6f528ded0d55261aef71702dad761af7fbeac0'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.7.0'] = (['9ec6e5265fdcdae07bb723768cf4fee87012bde98c188170849562d2722dc458'], CraftHash.HashAlgorithm.SHA256)
         self.description = "VP8 and VP9 video codec"
         self.defaultTarget = '1.7.0'
 
--- libs/libsndfile/libsndfile.py
+++ libs/libsndfile/libsndfile.py
@@ -1,17 +1,21 @@
 import info
 
 from Package.CMakePackageBase import *
+from Package.AutoToolsPackageBase import *
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['1.0.24'] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.24.tar.gz'
-        self.targetInstSrc['1.0.24'] = 'libsndfile-1.0.24'
-        self.patchToApply['1.0.24'] = [('libsndfile-1.0.24-20131003.diff', 1)]
+        for ver in ['1.0.24', '1.0.28']:
+            self.targets[ver] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-' + ver + '.tar.gz'
+            self.targetInstSrc[ver] = 'libsndfile-' + ver
+            if not CraftCore.compiler.isGCCLike():
+                self.patchToApply[ver] = [('libsndfile-1.0.24-20131003.diff', 1)]
         self.targetDigests['1.0.24'] = 'ade2dad272b52f61bb58aca3a4004b28549ee0f8'
+        self.targetDigests['1.0.28'] = (['1ff33929f042fa333aed1e8923aa628c3ee9e1eb85512686c55092d1e5a9dfa9'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "a C library for reading and writing files containing sampled sound"
-        self.defaultTarget = '1.0.24'
+        self.defaultTarget = '1.0.28'
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
@@ -19,6 +23,12 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/libvorbis"] = None
 
 
-class Package(CMakePackageBase):
-    def __init__(self):
-        CMakePackageBase.__init__(self)
+if CraftCore.compiler.isGCCLike():
+    class Package(AutoToolsPackageBase):
+        def __init__(self):
+            AutoToolsPackageBase.__init__(self)
+else:
+    class Package(CMakePackageBase):
+        def __init__(self):
+            CMakePackageBase.__init__(self)
+
