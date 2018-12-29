7b4968818c9c73856ed5ad0cc45455f17eba2f27
Vincent Pinon <vpinon@kde.org>
2018-12-29 16:13:06 +0100

    Fix lame
      * upgraded to latest release
      * needed to patch sym file

--- libs/_autotools/liblame/liblame.py
+++ libs/_autotools/liblame/liblame.py
@@ -4,14 +4,18 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['3.99.5'] = "http://downloads.sourceforge.net/sourceforge/lame/lame-3.99.5.tar.gz"
-        self.targetDigests['3.99.5'] = '03a0bfa85713adcc6b3383c12e2cc68a9cfbf4c4'
-        self.targetInstSrc['3.99.5'] = "lame-3.99.5"
-        self.defaultTarget = '3.99.5'
+        for ver in ['3.100']:
+            self.targets[ver] = f'https://sourceforge.net/projects/lame/files/lame/{ver}/lame-{ver}.tar.gz'
+            
+            self.targetInstSrc[ver] = 'lame-'+ver
+        self.patchToApply['3.100'] = [("lame_init_old-missing-symfile.patch", 1)]
+        self.targetDigests['3.100'] = (['ddfe36cab873794038ae2c1210557ad34857a4b6bdc515785d1da9e175b1da1e'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = '3.100'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["dev-utils/msys"] = None
+        self.buildDependencies["dev-utils/nasm"] = None
 
 
 from Package.AutoToolsPackageBase import *
@@ -21,4 +25,4 @@ class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
         self.subinfo.options.package.withCompiler = False
-        self.subinfo.options.configure.args = "--disable-static --enable-shared "
+        self.subinfo.options.configure.args = " --disable-static --disable-gtktest --disable-frontend --enable-nasm "
