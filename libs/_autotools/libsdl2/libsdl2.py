9808929be326e3352dd0cd76bfa63bac98b1e212
Vincent Pinon <vpinon@kde.org>
2018-12-29 22:39:16 +0100

    Fix SDL2
      * upgraded to latest release
      * autoreconf failed: libtool wrapping of windres missing

--- libs/_autotools/libsdl2/libsdl2.py
+++ libs/_autotools/libsdl2/libsdl2.py
@@ -4,11 +4,12 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "Simple DirectMedia Layer"
         self.webpage = "https://www.libsdl.org"
-        for ver in ['2.0.8']:
+        for ver in ['2.0.9']:
             self.targets[ver] = f"https://www.libsdl.org/release/SDL2-{ver}.tar.gz"
             self.targetInstSrc[ ver ] = f"SDL2-{ver}"
-        self.targetDigests['2.0.8'] = (['edc77c57308661d576e843344d8638e025a7818bff73f8fbfab09c3c5fd092ec'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = '2.0.8'
+            self.patchToApply[ ver ] = [("libtool_windres.patch", 1)]
+        self.targetDigests['2.0.9'] =(['255186dc676ecd0c1dbf10ec8a2cc5d6869b5079d8a38194c2aecdff54b324b1'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = '2.0.9'
 
     def setDependencies(self):
         self.runtimeDependencies["libs/libsamplerate"] = None
@@ -20,4 +21,5 @@ from Package.AutoToolsPackageBase import *
 class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.configure.args = "--enable-threads --enable-directx --enable-libsamplerate"
+        self.subinfo.options.configure.autoreconf = False
+        self.subinfo.options.configure.args = " --disable-static --enable-threads --enable-directx --enable-libsamplerate "
