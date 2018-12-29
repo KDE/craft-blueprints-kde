479bc22f1ee0377ca66e9f6775d28e233da877eb
Vincent Pinon <vpinon@kde.org>
2018-12-29 23:07:26 +0100

    Fix libvpx
      * archives from googlesource have changing checksums (timestamp?)
        => pick from github mirror

--- libs/_autotools/libvpx/libvpx.py
+++ libs/_autotools/libvpx/libvpx.py
@@ -3,8 +3,9 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ["1.7.0"]:
-            self.targets[ver] = f"https://chromium.googlesource.com/webm/libvpx/+archive/v{ver}.tar.gz"
-        self.targetDigests['1.7.0'] = (['9ec6e5265fdcdae07bb723768cf4fee87012bde98c188170849562d2722dc458'], CraftHash.HashAlgorithm.SHA256)
+            self.targets[ver] = f"https://github.com/webmproject/libvpx/archive/v{ver}.tar.gz"
+            self.targetInstSrc[ ver ] = "libvpx-" + ver
+        self.targetDigests['1.7.0'] = (['1fec931eb5c94279ad219a5b6e0202358e94a93a90cfb1603578c326abfc1238'], CraftHash.HashAlgorithm.SHA256)
         self.description = "VP8 and VP9 video codec"
         self.defaultTarget = '1.7.0'
 
