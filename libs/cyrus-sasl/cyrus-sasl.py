ec692a27390d80c807461f5d6127c0a86875163b
Hannah von Reth <vonreth@kde.org>
2018-12-31 11:54:03 +0100

    Use github mirror for cyrus sasl

--- libs/cyrus-sasl/cyrus-sasl.py
+++ libs/cyrus-sasl/cyrus-sasl.py
@@ -4,14 +4,14 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ["2.1.26"]:
-            self.targets[ver] = "ftp://ftp.cyrusimap.org/cyrus-sasl/cyrus-sasl-" + ver + ".tar.gz"
-            self.targetInstSrc[ver] = "cyrus-sasl-" + ver
+            self.targets[ver] = f"https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-{ver}/cyrus-sasl-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"cyrus-sasl-{ver}"
             #http://www.linuxfromscratch.org/blfs/view/svn/postlfs/cyrus-sasl.html
             self.patchToApply["2.1.26"] = [("cyrus-sasl-2.1.26-fixes-3.patch", 1),
                                            ("cyrus-sasl-2.1.26-openssl-1.1.0-1.patch", 1)]
         if CraftCore.compiler.isWindows:
             self.patchToApply["2.1.26"] += [("cyrus-sasl-2.1.26.patch", 1)]
-        self.targetDigests["2.1.26"] = "d6669fb91434192529bd13ee95737a8a5040241c"
+        self.targetDigests["2.1.26"] = (['8fbc5136512b59bb793657f36fadda6359cae3b08f01fd16b3d406f1345b7bc3'], CraftHash.HashAlgorithm.SHA256)
         self.patchLevel["2.1.26"] = 1
         self.description = "Cyrus SASL implementation"
         self.defaultTarget = "2.1.26"
