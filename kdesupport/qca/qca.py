807192a78538595fdcf8a3d58a5b6765d10c6f68
Hannah von Reth <vonreth@kde.org>
2019-01-02 12:14:54 +0100

    Apply D17913 to qca

--- kdesupport/qca/qca.py
+++ kdesupport/qca/qca.py
@@ -12,6 +12,8 @@ class subinfo(info.infoclass):
         self.svnTargets["master"] = "git://anongit.kde.org/qca.git"
         self.patchLevel["master"] = 4
 
+        self.patchToApply["master"] = [( "0001-Require-CMake-3.4-and-use-the-OpenSSL-imported-libs.patch",1)]
+
         #for ver in []:
             #self.targets[ver] = f"https://download.kde.org/stable/qca-qt5/{ver}/src/qca-qt5-{ver}.tar.xz"
             #self.targetDigestUrls[ver] = f"https://download.kde.org/stable/qca-qt5/{ver}/src/qca-qt5-{ver}.tar.xz.sha256"
