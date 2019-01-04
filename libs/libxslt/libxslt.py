dbe15a2269580ba7ba653085dfe67742b4c65326
Hannah von Reth <vonreth@kde.org>
2019-01-04 16:32:38 +0100

    BuiÃld libxslt withot python bindings

--- libs/libxslt/libxslt.py
+++ libs/libxslt/libxslt.py
@@ -51,8 +51,8 @@ class PackageMSVC(MakeFilePackageBase):
 class PackageMinGW(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.configure.args += " --disable-static --enable-shared "
-        
+        self.subinfo.options.configure.args += f" --disable-static --enable-shared --with-python=no"
+
 if CraftCore.compiler.isGCCLike():
     class Package(PackageMinGW):
         def __init__(self):
