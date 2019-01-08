224da9a5504c565a8acf8919cbfce7d3d4f67823
Hannah von Reth <vonreth@kde.org>
2019-01-08 20:11:40 +0100

    Update dbus to 1.12.12

--- libs/dbus/dbus.py
+++ libs/dbus/dbus.py
@@ -53,7 +53,7 @@ class subinfo(info.infoclass):
 
         self.description = "Freedesktop message bus system (daemon and clients)"
         self.webpage = "http://www.freedesktop.org/wiki/Software/dbus/"
-        self.defaultTarget = "1.11.14"
+        self.defaultTarget = "1.12.12"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
