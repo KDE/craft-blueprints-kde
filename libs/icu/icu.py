d66250a94acccaece72b2576d828c8d31673b17f
Hannah von Reth <vonreth@kde.org>
2019-01-08 20:12:12 +0100

    Update icu to 63.1

--- libs/icu/icu.py
+++ libs/icu/icu.py
@@ -27,10 +27,7 @@ class subinfo(info.infoclass):
             self.patchToApply["55.1"] = [("icu-20150414.diff", 2), ("icu-msys.diff", 2)]
             self.patchToApply["58.2"] = ("0020-workaround-missing-locale.patch",
                                          2)  # https://raw.githubusercontent.com/Alexpux/MINGW-packages/master/mingw-w64-icu/0020-workaround-missing-locale.patch
-        if CraftCore.compiler.isWindows:
-            self.defaultTarget = "58.2"
-        else:
-            self.defaultTarget = "62.1"
+        self.defaultTarget = "63.1"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
