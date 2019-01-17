5234cd730d092fbd61149d824e48cc5d356c8297
Hannah von Reth <vonreth@kde.org>
2019-01-17 10:38:40 +0100

    use ned dep

--- kde/frameworks/tier2/kdoctools/kdoctools.py
+++ kde/frameworks/tier2/kdoctools/kdoctools.py
@@ -33,6 +33,7 @@ class subinfo(info.infoclass):
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["dev-utils/perl"] = None
+        self.buildDependencies["perl-modules/xml-parser"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["kde/frameworks/tier1/karchive"] = None
