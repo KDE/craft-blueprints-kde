50405543d3aaa46fe4129836efd44bc768c71b2b
Ben Cooksley <bcooksley@kde.org>
2019-01-23 22:19:32 +1300

    Ensure we can find a more recent Exiv2

--- kde/applications/libkexiv2/libkexiv2.py
+++ kde/applications/libkexiv2/libkexiv2.py
@@ -6,6 +6,9 @@ class subinfo(info.infoclass):
         self.versionInfo.setDefaultValues()
         self.patchToApply["18.08.1"] = [("libkexiv2-18.08.1-20180925.diff", 1)]
 
+        self.patchLevel["18.12.1"] = 2
+        self.patchToApply["18.12.1"] = [("libkexiv2-18.12.1-20190123.diff", 1)]
+
         self.description = "Libkexiv2 is a wrapper around Exiv2 library to manipulate pictures metadata as EXIF IPTC and XMP."
 
     def setDependencies(self):
