78f8a47c9f4a4b0c4befc74ddbaac8fa2ee534b0
Hannah von Reth <vonreth@kde.org>
2019-01-27 15:14:44 +0100

    Update libdmtx

--- libs/_autotools/libdmtx/libdmtx.py
+++ libs/_autotools/libdmtx/libdmtx.py
@@ -4,16 +4,17 @@ import info
 class subinfo(info.infoclass):
     def setTargets( self ):
 
-        for ver in ["0.7.4"]:
-            self.targets[ver] = f"http://downloads.sourceforge.net/libdmtx/libdmtx-{ver}.tar.bz2"
+        for ver in ["0.7.5"]:
+            self.targets[ver] = f"https://github.com/dmtx/libdmtx/archive/v{ver}.zip"
+            self.archiveNames["0.7.5"] = f"libdmtx-{ver}"
             self.targetInstSrc[ver] = f"libdmtx-{ver}"
 
-        self.targetDigests["0.7.4"] = (['b62c586ac4fad393024dadcc48da8081b4f7d317aa392f9245c5335f0ee8dd76'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.7.5"] = (['7c67be07bd5a952733bc47fa17cbb637e0ed8a2bbae78323b48961da0267a772'], CraftHash.HashAlgorithm.SHA256)
 
-        self.patchToApply["0.7.4"] = [("libdmtx-0.7.4-20180820.diff", 1)]
+        self.patchToApply["0.7.5"] = [("libdmtx-0.7.4-20180820.diff", 1)]
 
         self.description = "libdmtx is open source software for reading and writing Data Matrix barcodes on Linux, Unix, OS X, Windows, and mobile devices. At its core libdmtx is a native shared library, allowing C/C++ programs to use its capabilities without extra restrictions or overhead."
-        self.defaultTarget = "0.7.4"
+        self.defaultTarget = "0.7.5"
 
     def setDependencies( self ):
         self.buildDependencies["dev-utils/msys"] = None
