4991c25aaa6b55328d33a585708b118bebc79dd3
Hannah von Reth <vonreth@kde.org>
2019-02-10 11:52:13 +0100

    Update prebuilts

--- dev-utils/_msvc/pkg-config/pkg-config.py
+++ dev-utils/_msvc/pkg-config/pkg-config.py
@@ -4,13 +4,7 @@ from Package.BinaryPackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        build = "56"
-        for ver in ["0.29.2"]:
-            self.targets[ ver ] = f"https://files.kde.org/craft/autotools/packages/pkg-config-{ver}-{build}-windows-mingw_{CraftCore.compiler.bits}-gcc.7z"
-            self.targetDigestUrls[ ver ] = f"https://files.kde.org/craft/autotools/packages/pkg-config-{ver}-{build}-windows-mingw_{CraftCore.compiler.bits}-gcc.7z.sha256"
-            self.targetInstallPath[ ver ] = "dev-utils/pkg-config"
-        self.patchLevel["0.29.2"] = 1
-        self.defaultTarget = "0.29.2"
+        self.addCachedAutotoolsBuild("dev-utils/_autotools/pkg-config", targetInstallPath=os.path.join("dev-utils", "bin"))
 
     def setDependencies(self):
         self.buildDependencies["virtual/bin-base"] = None
