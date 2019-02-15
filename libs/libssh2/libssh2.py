21d88b2cd355c5b056230f8a325c3a9a5a6007cb
Hannah von Reth <vonreth@kde.org>
2019-02-15 16:35:03 +0100

    fix libssh2

--- libs/libssh2/libssh2.py
+++ libs/libssh2/libssh2.py
@@ -3,11 +3,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        self.svnTargets['master'] = 'https://github.com/libssh2/libssh2.git||libssh2-1.8.0'
-        self.targets['1.8.0'] = "https://www.libssh2.org/download/libssh2-1.8.0.tar.gz"
-        self.targetInstSrc['1.8.0'] = "libssh2-1.8.0"
-        self.patchToApply['master'] = ('0001-Ensure-other-libraries-are-told-the-correct-linkage-.patch', 1)
-        self.defaultTarget = 'master'
+        self.svnTargets['master'] = 'https://github.com/libssh2/libssh2.git'
+        for ver in ["1.8.0"]:
+            self.targets['1.8.0'] = "https://www.libssh2.org/download/libssh2-1.8.0.tar.gz"
+            self.targetInstSrc['1.8.0'] = "libssh2-1.8.0"
+        if CraftCore.compiler.isMSVC():
+            self.patchToApply['1.8.0'] = ('0001-Ensure-other-libraries-are-told-the-correct-linkage-.patch', 1)
+        self.defaultTarget = '1.8.0'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -20,4 +22,3 @@ class Package(CMakePackageBase):
     def __init__( self, **args ):
         CMakePackageBase.__init__( self )
         self.subinfo.options.configure.defines = "-DENABLE_ZLIB_COMPRESSION=ON "
-
