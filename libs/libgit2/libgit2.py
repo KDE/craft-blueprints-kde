68566678a09c61f2002363d3ac19b72905e973ea
Hannah von Reth <vonreth@kde.org>
2019-02-15 16:58:09 +0100

    build shared

--- libs/libgit2/libgit2.py
+++ libs/libgit2/libgit2.py
@@ -6,8 +6,15 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/libgit2/libgit2.git'
 
+        for ver in ['0.28.1']:
+            self.targets[ver] = f"https://github.com/libgit2/libgit2/archive/v{ver}.tar.gz"
+            self.archiveNames[ver] = f"libgit2-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"libgit2-{ver}"
+        self.targetDigests["0.28.1"] = (['0ca11048795b0d6338f2e57717370208c2c97ad66c6d5eac0c97a8827d13936b'], CraftHash.HashAlgorithm.SHA256)
+        self.patchLevel["0.28.1"] = 1
+
         self.description = "a portable C library for accessing git repositories"
-        self.defaultTarget = 'master'
+        self.defaultTarget = '0.28.1'
 
     def setDependencies(self):
         self.buildDependencies['dev-utils/pkg-config'] = 'default'
@@ -23,6 +30,6 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.defines = "-DUSE_ICONV=ON -DBUILD_CLAR=OFF"
+        self.subinfo.options.configure.args += "-DUSE_ICONV=ON -DBUILD_CLAR=OFF"
 
 # self.subinfo.options.configure.args = "-DDBUS_REPLACE_LOCAL_DIR=ON "
