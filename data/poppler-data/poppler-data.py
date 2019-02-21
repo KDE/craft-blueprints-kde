248d00a4cd1e3da86872236cedd83fe8367e257b
Hannah von Reth <vonreth@kde.org>
2019-02-21 10:54:43 +0100

    Update poppler

--- data/poppler-data/poppler-data.py
+++ data/poppler-data/poppler-data.py
@@ -4,14 +4,15 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = "git://git.freedesktop.org/git/poppler/poppler-data"
-        for v in ['0.4.6', '0.4.7']:
+        for v in ['0.4.6', '0.4.7', '0.4.9']:
             self.targets[v] = 'http://poppler.freedesktop.org/poppler-data-' + v + '.tar.gz'
             self.targetInstSrc[v] = 'poppler-data-' + v
         self.patchToApply['0.4.7'] = [("poppler-data-0.4.7-20170810.diff", 1)]
         self.targetDigests['0.4.7'] = '556a5bebd0eb743e0d91819ba11fd79947d8c674'
+        self.targetDigests['0.4.9'] = (['1f9c7e7de9ecd0db6ab287349e31bf815ca108a5a175cf906a90163bdbe32012'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "the poppler CJK encoding data"
-        self.defaultTarget = '0.4.7'
+        self.defaultTarget = '0.4.9'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
--- qt-libs/poppler/poppler.py
+++ qt-libs/poppler/poppler.py
@@ -29,7 +29,7 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "git://git.freedesktop.org/git/poppler/poppler"
-        for ver in ["0.38.0", "0.57.0", "0.62.0", "0.68.0"]:
+        for ver in ["0.38.0", "0.57.0", "0.62.0", "0.68.0", "0.74.0"]:
             self.targets[ver] = f"https://poppler.freedesktop.org/poppler-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"poppler-{ver}"
         self.patchToApply["0.38.0"] = ("poppler-0.38.0-20151130.diff", 1)
@@ -39,9 +39,10 @@ class subinfo(info.infoclass):
         self.targetDigests['0.57.0'] = (['0ea37de71b7db78212ebc79df59f99b66409a29c2eac4d882dae9f2397fe44d8'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['0.62.0'] = (['5b9a73dfd4d6f61d165ada1e4f0abd2d420494bf9d0b1c15d0db3f7b83a729c6'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['0.68.0'] = (['f90d04f0fb8df6923ecb0f106ae866cf9f8761bb537ddac64dfb5322763d0e58'],  CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['0.74.0'] = (['92e09fd3302567fd36146b36bb707db43ce436e8841219025a82ea9fb0076b2f'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "PDF rendering library based on xpdf-3.0"
-        self.defaultTarget = "0.68.0"
+        self.defaultTarget = "0.74.0"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/pkg-config"] = None
@@ -66,4 +67,4 @@ class Package(CMakePackageBase):
         CMakePackageBase.__init__(self)
 
         self.subinfo.options.package.packageName = "poppler"
-        self.subinfo.options.configure.args = "-DENABLE_XPDF_HEADERS=ON -DENABLE_ZLIB=ON -DENABLE_LIBCURL=ON -DENABLE_UTILS=OFF -DENABLE_LIBOPENJPEG=openjpeg2"
+        self.subinfo.options.configure.args = "-DENABLE_XPDF_HEADERS=ON -DENABLE_ZLIB=ON -DENABLE_LIBCURL=ON -DENABLE_UTILS=OFF -DENABLE_LIBOPENJPEG=openjpeg2 -DENABLE_GLIB=OFF"
