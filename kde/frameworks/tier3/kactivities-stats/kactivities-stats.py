1bfdbe722a7861dfeda33edde146e950b0241285
Filipe Azevedo <filipe.azevedo@kdab.com>
2019-03-06 15:18:19 +0100

    Add kactivities-stats framework

---
+++ kde/frameworks/tier3/kactivities-stats/kactivities-stats.py
@@ -0,0 +1,23 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        self.description = "A library for accessing the usage data collected by the activities system"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kactivities"] = None
+        self.runtimeDependencies["libs/boost/boost-headers"] = None
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.args = "-DBUILD_TESTING=OFF "
