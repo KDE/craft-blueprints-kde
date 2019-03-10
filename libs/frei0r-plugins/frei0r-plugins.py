0d9e41c89d8b4a502969f5513b20ec0565b7871b
Vincent Pinon <vpinon@kde.org>
2019-03-10 15:00:46 +0100

    Frei0r: default to master fix bugs & Mac build

--- libs/frei0r-plugins/frei0r-plugins.py
+++ libs/frei0r-plugins/frei0r-plugins.py
@@ -7,11 +7,10 @@ class subinfo(info.infoclass):
         for ver in ['1.6.1']:
             self.targets[ ver ] = f"https://github.com/dyne/frei0r/archive/v{ver}.tar.gz"
             self.targetInstSrc[ ver ] = f"frei0r-{ver}"
-        self.targetDigests['1.6.1'] = '69a2330997c95d4afc9dc025d7970b11446ea686'
         self.targetDigests['1.6.1'] = (['dae0ca623c83173788ce4fc74cb67ac7e50cf33a4412ee3d33bed284da1a8437'], CraftHash.HashAlgorithm.SHA256)
 
         self.svnTargets['master'] = 'https://github.com/dyne/frei0r.git'
-        self.defaultTarget = '1.6.1'
+        self.defaultTarget = 'master'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
