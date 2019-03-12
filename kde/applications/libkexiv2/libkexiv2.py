393199344803eaecd82424d02c3a5da1835099c1
Hannah von Reth <vonreth@kde.org>
2019-03-12 14:25:28 +0100

    Apply libkexiv2 patch on all 18.12 releases

--- kde/applications/libkexiv2/libkexiv2.py
+++ kde/applications/libkexiv2/libkexiv2.py
@@ -7,7 +7,9 @@ class subinfo(info.infoclass):
         self.patchToApply["18.08.1"] = [("libkexiv2-18.08.1-20180925.diff", 1)]
 
         self.patchLevel["18.12.1"] = 2
-        self.patchToApply["18.12.1"] = [("libkexiv2-18.12.1-20190123.diff", 1)]
+        for ver in self.targets:
+            if CraftVersion(ver) % "18.12":
+                self.patchToApply[ver] = [("libkexiv2-18.12.1-20190123.diff", 1)]
 
         self.description = "Libkexiv2 is a wrapper around Exiv2 library to manipulate pictures metadata as EXIF IPTC and XMP."
 
