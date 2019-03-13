0ba023a5e082f207a4985076ccf61165f34b0f0c
Jaroslaw Staniek <staniek@kde.org>
2019-03-13 21:58:00 +0100

    breeze depends on libs/libfftw

--- kde/plasma/breeze/breeze.py
+++ kde/plasma/breeze/breeze.py
@@ -9,6 +9,7 @@ class subinfo(info.infoclass):
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/libfftw"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kguiaddons"] = None
