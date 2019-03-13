04182a6a2e825d08bce21e88011918c7bed579a6
Hannah von Reth <vonreth@kde.org>
2019-03-13 09:18:15 +0100

    add current branches

--- qt-apps/qtcreator/qtcreator/qtcreator.py
+++ qt-apps/qtcreator/qtcreator/qtcreator.py
@@ -4,9 +4,9 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets["master"] = "git://code.qt.io/qt-creator/qt-creator.git"
-        for branch in ["4.3", "4.4", "4.5"]:
-            self.svnTargets[branch] = f"git://code.qt.io/qt-creator/qt-creator.git|{branch}"
+        self.svnTargets["master"] = "https://code.qt.io/qt-creator/qt-creator.git"
+        for branch in ["4.3", "4.4", "4.5", "4.8", "4.9"]:
+            self.svnTargets[branch] = f"https://code.qt.io/qt-creator/qt-creator.git|{branch}"
 
         for ver in ["4.7.1", "4.8.0", "4.8.1", "4.8.2"]:
             majorVer = ".".join(ver.split(".")[:2])
