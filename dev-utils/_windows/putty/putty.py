14086a9c3771b42f6a56933b84557e68c4e1d0cb
Hannah von Reth <vonreth@kde.org>
2019-03-19 14:40:31 +0100

    Update putty

--- dev-utils/_windows/putty/putty.py
+++ dev-utils/_windows/putty/putty.py
@@ -3,13 +3,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["0.70"]:
+        for ver in ["0.71"]:
             self.targets[ver] = f"https://the.earth.li/~sgtatham/putty/{ver}/w64/putty.zip"
             self.archiveNames[ver] = f"putty-{ver}.zip"
             self.targetInstallPath[ver] = os.path.join("dev-utils", "bin")
 
-            self.targetDigests['0.70'] = (['8422ad5fe060b7229fbf51512e3eb23c5bfe631eb660e9604343e5e81b69fad0'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "0.70"
+            self.targetDigests['0.71'] = (['eaeb59b265ca07d1214c9b67fb307c639a9b8739af4279f8eba6fa166c0f17db'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "0.71"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/bin-base"] = None
