113cf475d6a879994bb76d1a91ad491e6b9fcacd
Hannah von Reth <vonreth@kde.org>
2019-04-03 10:39:02 +0200

    Build vsd static

--- dev-utils/_windows/vsd/vsd.py
+++ dev-utils/_windows/vsd/vsd.py
@@ -15,3 +15,8 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.args += " -DSTATIC_VSD=ON"
+
+    def createPackage(self):
+        self.ignoredPackages.append("libs/runtime")
+        return super().createPackage()
