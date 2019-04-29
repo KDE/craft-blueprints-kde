5d968e74a5570daa0ac22761d7eedd25cc8b56b9
Hannah von Reth <vonreth@kde.org>
2019-04-29 22:28:26 +0200

    Fix build with msvc2019

--- kde/frameworks/tier3/kjs/kjs.py
+++ kde/frameworks/tier3/kjs/kjs.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
             tarballUrl="http://download.kde.org/stable/frameworks/${VERSION_MAJOR}.${VERSION_MINOR}/portingAids/${PACKAGE_NAME}-${VERSION}.tar.xz",
             tarballDigestUrl="http://download.kde.org/stable/frameworks/${VERSION_MAJOR}.${VERSION_MINOR}/portingAids/${PACKAGE_NAME}-${VERSION}.tar.xz.sha1")
 
+        self.patchToApply["5.57.0"] = [("0001-Disable-broken-and-no-longer-needed-workaround-code-.patch", 1)]
         self.description = "KJS"
 
     def setDependencies(self):
