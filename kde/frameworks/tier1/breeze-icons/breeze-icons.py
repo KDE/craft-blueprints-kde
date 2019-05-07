1ca66fe517bea5469d8ceaa79f2bbda0a92e6113
Hannah von Reth <vonreth@kde.org>
2019-05-07 15:17:21 +0200

    Rebuild breeze-icons with fixed symlinks

--- kde/frameworks/tier1/breeze-icons/breeze-icons.py
+++ kde/frameworks/tier1/breeze-icons/breeze-icons.py
@@ -4,6 +4,7 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
+        self.patchLevel["5.57.0"] = 1
         self.description = "Breeze icon theme."
 
     def registerOptions(self):
