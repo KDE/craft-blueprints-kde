ece62680ca1ff3c07813b5ef8987136a603fa9ee
Hannah von Reth <vonreth@kde.org>
2019-05-22 23:13:11 +0200

    Build zlib with autotools (rpath etc)

--- libs/zlib/zlib.py
+++ libs/zlib/zlib.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
 import info
+from Package.AutoToolsPackageBase import AutoToolsPackageBase
 from Package.CMakePackageBase import *
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['1.2.5', '1.2.6', '1.2.7', '1.2.8', '1.2.11']:
+        for ver in ['1.2.11']:
             self.targets[ver] = 'http://downloads.sourceforge.net/sourceforge/libpng/zlib-%s.tar.gz' % ver
             self.targetInstSrc[ver] = "zlib-" + ver
-        self.patchToApply['1.2.5'] = [("zlib-1.2.5-20110629.diff", 1)]
-        self.patchToApply['1.2.6'] = [("zlib-1.2.6-20120421.diff", 1)]
-        self.patchToApply['1.2.7'] = [("zlib-1.2.7-20130123.diff", 1)]
-        self.patchToApply['1.2.8'] = [("zlib-1.2.8-20130901.diff", 1)]
-        self.patchToApply['1.2.11'] = [("zlib-1.2.11-20180203.diff", 1),
-                                       ("zlib-1.2.11-20190509.diff", 1)  # its a cmake define, don't change it
-                                       ]
+
+        if CraftCore.compiler.isWindows:
+            self.patchToApply['1.2.11'] = [("zlib-1.2.11-20180203.diff", 1),
+                                           ("zlib-1.2.11-20190509.diff", 1)  # its a cmake define, don't change it
+                                           ]
         self.patchLevel["1.2.11"] = 2
-        self.targetDigests['1.2.5'] = '8e8b93fa5eb80df1afe5422309dca42964562d7e'
-        self.targetDigests['1.2.7'] = '4aa358a95d1e5774603e6fa149c926a80df43559'
-        self.targetDigests['1.2.8'] = 'a4d316c404ff54ca545ea71a27af7dbc29817088'
         self.targetDigests['1.2.11'] = (['c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = 'The zlib compression and decompression library'
@@ -28,6 +24,11 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
 
 
-class Package(CMakePackageBase):
-    def __init__(self, **args):
-        CMakePackageBase.__init__(self)
+if CraftCore.compiler.isWindows:
+    class Package(CMakePackageBase):
+        def __init__(self, **args):
+            CMakePackageBase.__init__(self)
+else:
+    class Package(AutoToolsPackageBase):
+        def __init__(self, **args):
+            AutoToolsPackageBase.__init__(self)
