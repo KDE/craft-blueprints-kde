7caae8929d9f85d51dab8026ab6836392f09f840
Hannah von Reth <vonreth@kde.org>
2019-05-24 09:25:03 +0200

    Fix zlib for linux 32bit

--- libs/zlib/zlib.py
+++ libs/zlib/zlib.py
@@ -32,3 +32,5 @@ else:
     class Package(AutoToolsPackageBase):
         def __init__(self, **args):
             AutoToolsPackageBase.__init__(self)
+            self.subinfo.options.configure.autoreconf = False
+            self.platform = ""
