9cf65a039e02fb404505b5bfd1949f7e6a3dd444
Hannah von Reth <vonreth@kde.org>
2019-06-05 13:09:10 +0200

    We will ship snoretoast soon so move it to the root

--- dev-utils/_mingw/snoretoast/snoretoast.py
+++ dev-utils/_mingw/snoretoast/snoretoast.py
@@ -6,7 +6,6 @@ class subinfo(info.infoclass):
         for ver in ["0.5.2"]:
             self.targets[ver] = "https://download.kde.org/stable/snoretoast/0.5.2/bin/snoretoast_v0.5.2-x64.7z"
             self.targetInstSrc[ver] = "snoretoast_v0.5.2-x64"
-            self.targetInstallPath[ver] = "dev-utils"
         self.targetDigests["0.5.2"] = (["94209bbf777265bfbd5b77fde4e0ff5801509db043e0575ee00ba5736d2e946f"], CraftHash.HashAlgorithm.SHA256)
         self.description = "A command line application capable of creating Windows Toast notifications."
         self.webpage = "https://phabricator.kde.org/source/snoretoast/"
