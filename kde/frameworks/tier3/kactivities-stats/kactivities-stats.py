99030a2f175c73dd548eb191aa2f897a4031904d
Hannah von Reth <vonreth@kde.org>
2019-06-11 10:22:08 +0200

    We still need this patch

--- kde/frameworks/tier3/kactivities-stats/kactivities-stats.py
+++ kde/frameworks/tier3/kactivities-stats/kactivities-stats.py
@@ -8,6 +8,8 @@ class subinfo(info.infoclass):
             self.patchToApply["5.55.0"] = [("kactivities-stats-5.55.0-20190320.diff", 1)]
             self.patchToApply["5.56.0"] = [("kactivities-stats-5.55.0-20190320.diff", 1)]
             self.patchToApply["5.57.0"] = [("kactivities-stats-5.55.0-20190320.diff", 1)]
+            self.patchToApply["5.58.0"] = [("kactivities-stats-5.55.0-20190320.diff", 1)]
+            self.patchToApply["5.59.0"] = [("kactivities-stats-5.55.0-20190320.diff", 1)]
 
 
         self.description = "A library for accessing the usage data collected by the activities system"
