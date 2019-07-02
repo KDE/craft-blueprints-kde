813d2d9ecfceb374847fce1260cbbe209ea6d74e
Hannah von Reth <vonreth@kde.org>
2019-07-02 13:31:38 +0200

    Update snoretoast to 0.6.0

--- dev-utils/_mingw/snoretoast/snoretoast.py
+++ dev-utils/_mingw/snoretoast/snoretoast.py
@@ -3,14 +3,18 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["0.5.2"]:
-            self.targets[ver] = "https://download.kde.org/stable/snoretoast/0.5.2/bin/snoretoast_v0.5.2-x64.7z"
-            self.targetInstSrc[ver] = "snoretoast_v0.5.2-x64"
+        for ver in ["0.5.2", "0.6.0"]:
+            self.targets[ver] = f"https://download.kde.org/stable/snoretoast/{ver}/bin/snoretoast_v{ver}-{CraftCore.compiler.architecture}.7z"
+            if ver != "0.6.0":
+                self.targetInstSrc[ver] = f"snoretoast_v{ver}-{CraftCore.compiler.architecture}"
         self.targetDigests["0.5.2"] = (["94209bbf777265bfbd5b77fde4e0ff5801509db043e0575ee00ba5736d2e946f"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigestsX64["0.6.0"] = (["170443d2d87318fcaa8d8a051f29c6c65aaca4f79f1d462aa0b4aa5493f4981e"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.6.0"] = (["824eb0272cb7ef05856809c9faea0615a480c7c1cac51d9b22e5f90751a91888"], CraftHash.HashAlgorithm.SHA256)
+
         self.description = "A command line application capable of creating Windows Toast notifications."
         self.webpage = "https://phabricator.kde.org/source/snoretoast/"
         self.displayName = "SnoreToast"
-        self.defaultTarget = "0.5.2"
+        self.defaultTarget = "0.6.0"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/bin-base"] = None
--- dev-utils/_msvc/snoretoast/snoretoast.py
+++ dev-utils/_msvc/snoretoast/snoretoast.py
@@ -4,15 +4,15 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = 'git://anongit.kde.org/snoretoast|master'
-        for ver in ["0.5.2"]:
-            self.targets[ver] = "https://download.kde.org/stable/snoretoast/0.5.2/src/snoretoast-0.5.2.tar.xz"
-            self.targetInstSrc[ver] = "snoretoast-0.5.2"
-            self.targetInstallPath[ver] = "dev-utils"
+        for ver in ["0.5.2", "0.6.0"]:
+            self.targets[ver] = f"https://download.kde.org/stable/snoretoast/{ver}/src/snoretoast-{ver}.tar.xz"
+            self.targetInstSrc[ver] = f"snoretoast-{ver}"
         self.targetDigests["0.5.2"] = (["e25ad5e21c2c748e234a2b114373bbb115dec09ad15abc7ee1bb6387046472a0"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.6.0"] =  (['56b93c1f3aa06a9b170802385492c058fb4834608b1a3c0e773aa1bb5b3ec7ec'], CraftHash.HashAlgorithm.SHA256)
         self.description = "A command line application capable of creating Windows Toast notifications."
         self.webpage = "https://phabricator.kde.org/source/snoretoast/"
         self.displayName = "SnoreToast"
-        self.defaultTarget = "0.5.2"
+        self.defaultTarget = "0.6.0"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
