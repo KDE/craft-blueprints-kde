a8a11cc176e6216fc4a28c8a93a7299fda848f65
Hannah von Reth <vonreth@kde.org>
2019-08-07 12:09:37 +0200

    procexp was updated

--- dev-utils/_windows/procexp/procexp.py
+++ dev-utils/_windows/procexp/procexp.py
@@ -3,11 +3,11 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["16.22"]:
+        for ver in ["16.26"]:
             self.targets[ver] = 'http://download.sysinternals.com/files/ProcessExplorer.zip'
             self.targetInstallPath[ver] = os.path.join("dev-utils", "bin")
-        self.targetDigests['16.22'] = (['d393f062091c4fcf720ce0cad56520a920ffcc9412cdc7941150e3bb3fc4fefa'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "16.22"
+        self.targetDigests['16.26'] = (['dc422a6ca2872207e8d0ef55ba4291998c8923efe847ba9e025c610090ad20be'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "16.26"
 
 
 from Package.BinaryPackageBase import *
