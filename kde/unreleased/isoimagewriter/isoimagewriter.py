501b141a662ec134710b13492678d11918e9ff12
Farid Boudedja <farid.boudedja@gmail.com>
2019-08-13 16:02:33 +0200

    Add blueprint for KDE ISO Image Writer
    Summary: Add blueprint to build KDE ISO Image Writer
    
    Reviewers: #craft, vonreth
    
    Reviewed By: #craft, vonreth
    
    Differential Revision: https://phabricator.kde.org/D23096

---
+++ kde/unreleased/isoimagewriter/isoimagewriter.py
@@ -0,0 +1,25 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+        self.svnTargets['master'] = 'https://invent.kde.org/faridb/isoimagewriter.git'
+        self.defaultTarget = "master"
+
+        self.displayName = "KDE ISO Image Writer"
+        self.description = "A tool to write ISO images to USB flash drives"
+
+    def setDependencies(self):
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
+        self.runtimeDependencies['libs/gpgme'] = None
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
