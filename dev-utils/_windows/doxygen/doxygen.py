0d5aa414ebb6a59011ae211233619ebad8bad2f4
Hannah von Reth <vonreth@kde.org>
2019-09-11 10:34:10 +0200

    Update doxygen

--- dev-utils/_windows/doxygen/doxygen.py
+++ dev-utils/_windows/doxygen/doxygen.py
@@ -3,14 +3,15 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.8.14"]:
+        for ver in ["1.8.16"]:
             self.targets[ver] = f"http://doxygen.nl/files/doxygen-{ver}.windows.x64.bin.zip"
             self.targetInstallPath[ver] = "dev-utils/bin"
 
         self.targetDigests['1.8.14'] = (['e2d635a05fb0516311071cfcc41a3859fa22a912b484ed2c2ddec70248b75845'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.8.16'] = (['c84890ec003841c1ee6ff35dd3b17fdfd5ae4c2914d1c8df11dcc3719d294ac5'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = 'Automated C, C++, and Java Documentation Generator'
-        self.defaultTarget = '1.8.14'
+        self.defaultTarget = '1.8.16'
 
 
 from Package.BinaryPackageBase import *
