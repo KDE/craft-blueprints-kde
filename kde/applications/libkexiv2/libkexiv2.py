0f7a4307654079ee985fa40debfeba257790a3ba
Christoph Cullmann <cullmann@kde.org>
2019-09-16 21:09:22 +0200

    remove patches for no longer build versions

--- kde/applications/libkexiv2/libkexiv2.py
+++ kde/applications/libkexiv2/libkexiv2.py
@@ -4,12 +4,6 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
-        self.patchToApply["18.08.1"] = [("libkexiv2-18.08.1-20180925.diff", 1)]
-
-        self.patchLevel["18.12.1"] = 2
-        for ver in self.targets:
-            if CraftVersion(ver) % "18.12":
-                self.patchToApply[ver] = [("libkexiv2-18.12.1-20190123.diff", 1)]
 
         self.description = "Libkexiv2 is a wrapper around Exiv2 library to manipulate pictures metadata as EXIF IPTC and XMP."
 
