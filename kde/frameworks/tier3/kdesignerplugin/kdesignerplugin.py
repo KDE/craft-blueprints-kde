44946024dc554065e628f9e5ea5a94ec7eb2ebfc
Hannah von Reth <vonreth@kde.org>
2019-09-16 10:09:07 +0200

    kdesignerplugin was moved to porting aids

--- kde/frameworks/tier3/kdesignerplugin/kdesignerplugin.py
+++ kde/frameworks/tier3/kdesignerplugin/kdesignerplugin.py
@@ -5,6 +5,11 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
+        for ver in filter(lambda x: x >= CraftVersion("5.62"), self.targets):
+            self.targets[ver] = self.versionInfo.format("http://download.kde.org/stable/frameworks/${VERSION_MAJOR}.${VERSION_MINOR}/portingAids/${PACKAGE_NAME}-${VERSION}.tar.xz", ver)
+            self.targetDigestUrls[ver] = self.versionInfo.format("http://download.kde.org/stable/frameworks/${VERSION_MAJOR}.${VERSION_MINOR}/portingAids/${PACKAGE_NAME}-${VERSION}.tar.xz.sha256", ver)
+
+
         self.description = "Integration of our widgets in Qt Designer/Creator"
 
     def setDependencies(self):
