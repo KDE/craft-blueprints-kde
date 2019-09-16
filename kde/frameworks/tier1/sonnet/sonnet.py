092af7ada7c351590cda601836830b60fd45d829
Christoph Cullmann <cullmann@kde.org>
2019-09-16 21:38:06 +0200

    remove old patches no longer needed

--- kde/frameworks/tier1/sonnet/sonnet.py
+++ kde/frameworks/tier1/sonnet/sonnet.py
@@ -4,9 +4,6 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
-        self.patchToApply["5.43.0"] = [("0001-Find-libhunspell-build-by-msvc.patch", 1),
-                                       ("0002-Use-Locale-name-instead-of-Locale-bcp47Name.patch", 1)]
-        self.patchLevel["5.43.0"] = 1
 
         self.description = "Spelling framework for Qt, plugin-based."
 
