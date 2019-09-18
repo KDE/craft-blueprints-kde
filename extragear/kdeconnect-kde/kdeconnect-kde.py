da617d813afe35c26d691efafebf6c57f2b4e928
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2019-09-19 04:56:38 +0530

    add auto start to KDE Connect Blueprint
    Summary: adds auto start support to KDE Connect `.Appx` version.
    
    Reviewers: vonreth, sredman
    
    Tags: #kde_connect, #craft
    
    Differential Revision: https://phabricator.kde.org/D24032

--- extragear/kdeconnect-kde/kdeconnect-kde.py
+++ extragear/kdeconnect-kde/kdeconnect-kde.py
@@ -45,11 +45,11 @@ class Package(CMakePackageBase):
                 Section "@{productname}"
                     SectionIn 1
                     !insertmacro MUI_STARTMENU_WRITE_BEGIN Application
-                        !insertmacro SnoreShortcut "$SMPROGRAMS\@{productname}.lnk" "$INSTDIR\bin\@{appname}.exe" "${MyApp_AppUserModelId}"
-                        CreateShortCut "$SMPROGRAMS\Startup\@{productname}.lnk" "$INSTDIR\bin\@{appname}.exe"
-                        CreateShortCut "$DESKTOP\@{productname}.lnk" "$INSTDIR\bin\@{appname}.exe"
-                        CreateShortCut "$SENDTO\Send to remote device via @{productname}.lnk" "$INSTDIR\bin\kdeconnect-handler.exe"  "" "$INSTDIR\bin\@{appname}.exe" 0
-                        CreateShortCut "$SENDTO\Open on remote device via @{productname}.lnk" "$INSTDIR\bin\kdeconnect-handler.exe"  "--open" "$INSTDIR\bin\@{appname}.exe" 0
+                        !insertmacro SnoreShortcut "$SMPROGRAMS\@{productname}.lnk" "$INSTDIR\bin\@{exename}.exe" "@{AppUserModelID}"
+                        CreateShortCut "$SMPROGRAMS\Startup\@{productname}.lnk" "$INSTDIR\bin\@{exename}.exe"
+                        CreateShortCut "$DESKTOP\@{productname}.lnk" "$INSTDIR\bin\@{exename}.exe"
+                        CreateShortCut "$SENDTO\Send to remote device via @{productname}.lnk" "$INSTDIR\bin\kdeconnect-handler.exe"  "" "$INSTDIR\bin\@{exename}.exe" 0
+                        CreateShortCut "$SENDTO\Open on remote device via @{productname}.lnk" "$INSTDIR\bin\kdeconnect-handler.exe"  "--open" "$INSTDIR\bin\@{exename}.exe" 0
                     !insertmacro MUI_STARTMENU_WRITE_END
                 SectionEnd
                 """
@@ -62,8 +62,9 @@ class Package(CMakePackageBase):
                     Delete "$SENDTO\Open on remote device via @{productname}.lnk"
                 SectionEnd
                 """
-        else:
-            self.defines["shortcuts"] = [{"name" : self.subinfo.displayName , "target" : f"bin/{self.defines['appname']}{CraftCore.compiler.executableSuffix}", "description" : self.subinfo.description}]
+        elif isinstance(self, AppxPackager):
+            self.defines["startup_task"] = r"bin/@{exename}.exe"
+            self.defines["shortcuts"] = [{"name" : self.subinfo.displayName , "target" : f"bin/{self.defines['exename']}{CraftCore.compiler.executableSuffix}", "description" : self.subinfo.description}]
             self.defines["icon_png"] = os.path.join(self.packageDir(), ".assets", "Square150x150Logo.scale-100.png")
             self.defines["icon_png_44"] = os.path.join(self.packageDir(), ".assets", "Square44x44Logo.scale-100.png")
             self.defines["icon_png_310x150"] = os.path.join(self.packageDir(), ".assets", "Wide310x150Logo.scale-100.png")
