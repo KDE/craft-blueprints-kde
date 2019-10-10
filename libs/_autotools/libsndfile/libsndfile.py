fee8b2baaf0bece564fd44be76d88b492883699f
Hannah von Reth <vonreth@kde.org>
2019-10-10 22:14:30 +0200

    kibsndfile is autotools only, we should provide a binary

--- libs/_autotools/libsndfile/libsndfile.py
+++ libs/_autotools/libsndfile/libsndfile.py
@@ -1,6 +1,5 @@
 import info
 
-from Package.CMakePackageBase import *
 from Package.AutoToolsPackageBase import *
 
 
@@ -9,8 +8,6 @@ class subinfo(info.infoclass):
         for ver in ['1.0.24', '1.0.28']:
             self.targets[ver] = 'http://www.mega-nerd.com/libsndfile/files/libsndfile-' + ver + '.tar.gz'
             self.targetInstSrc[ver] = 'libsndfile-' + ver
-            if not CraftCore.compiler.isGCCLike():
-                self.patchToApply[ver] = [('libsndfile-1.0.24-20131003.diff', 1)]
         self.targetDigests['1.0.24'] = 'ade2dad272b52f61bb58aca3a4004b28549ee0f8'
         self.targetDigests['1.0.28'] = (['1ff33929f042fa333aed1e8923aa628c3ee9e1eb85512686c55092d1e5a9dfa9'], CraftHash.HashAlgorithm.SHA256)
 
@@ -23,12 +20,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/libvorbis"] = None
 
 
-if CraftCore.compiler.isGCCLike():
-    class Package(AutoToolsPackageBase):
-        def __init__(self):
-            AutoToolsPackageBase.__init__(self)
-else:
-    class Package(CMakePackageBase):
-        def __init__(self):
-            CMakePackageBase.__init__(self)
-
+class Package(AutoToolsPackageBase):
+    def __init__(self):
+        AutoToolsPackageBase.__init__(self)
