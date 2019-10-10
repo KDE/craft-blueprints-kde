7ebb13c43f90e6f0245f41fbb2c581b3a03e6049
Hannah von Reth <vonreth@kde.org>
2019-10-10 22:21:37 +0200

    Move libvorbis and libogg to autotools, we will use prebuilts for msvc

--- libs/_autotools/libogg/libogg.py
+++ libs/_autotools/libogg/libogg.py
@@ -1,6 +1,5 @@
 import info
 
-from Package.CMakePackageBase import *
 from Package.AutoToolsPackageBase import *
 
 
@@ -9,7 +8,6 @@ class subinfo(info.infoclass):
         for ver in ['1.2.0', '1.3.0', '1.3.1', '1.3.3']:
             self.targets[ver] = 'http://downloads.xiph.org/releases/ogg/libogg-' + ver + '.tar.gz'
             self.targetInstSrc[ver] = 'libogg-' + ver
-            self.patchToApply[ver] = ('libogg-1.2.0-20100707.diff', 1)
         self.targetDigests['1.2.0'] = '135fb812282e08833295c91e005bd0258fff9098'
         self.targetDigests['1.3.0'] = 'a900af21b6d7db1c7aa74eb0c39589ed9db991b8'
         self.targetDigests['1.3.1'] = '270685c2a3d9dc6c98372627af99868aa4b4db53'
@@ -22,12 +20,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
 
 
-if CraftCore.compiler.isGCCLike():
-    class Package(AutoToolsPackageBase):
-        def __init__(self):
-            AutoToolsPackageBase.__init__(self)
-else:
-    class Package(CMakePackageBase):
-        def __init__(self):
-            CMakePackageBase.__init__(self)
+class Package(AutoToolsPackageBase):
+    def __init__(self):
+        AutoToolsPackageBase.__init__(self)
 
