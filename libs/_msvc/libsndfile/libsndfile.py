5ad37960df489b78f82d67a862859672f1d9531a
Hannah von Reth <vonreth@kde.org>
2019-10-11 11:24:01 +0200

    Use new binaries for libogg, libvorbis, libsndfile

---
+++ libs/_msvc/libsndfile/libsndfile.py
@@ -0,0 +1,19 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.addCachedAutotoolsBuild(f"libs/_autotools/{self.parent.package.name}")
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/libogg"] = None
+        self.runtimeDependencies["libs/libvorbis"] = None
+        self.runtimeDependencies["libs/mingw-crt4msvc"] = None
+
+
+from Package.BinaryPackageBase import *
+
+class Package(BinaryPackageBase):
+    def __init__(self, **args):
+        BinaryPackageBase.__init__(self)
