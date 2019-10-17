13a157150e6729f35fd29e1ab9baa6bf27b7271e
Thomas Branch <kde@twig.hk>
2019-10-17 18:29:00 +0100

    Blueprint for KSudoku
    Summary:
    Added blueprint for kde/kdegames/ksudoku.
    
    Seems to compile and run fine on windows with mingw compiler.
    
    Reviewers: vonreth
    
    Reviewed By: vonreth
    
    Subscribers: vonreth
    
    Tags: #craft, #kde_games
    
    Differential Revision: https://phabricator.kde.org/D24547

---
+++ kde/kdegames/ksudoku/ksudoku.py
@@ -0,0 +1,29 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        self.description = "KSudoku"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kcrash"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kdbusaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/knewstuff"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
+        self.runtimeDependencies["kde/kdegames/libkdegames"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
