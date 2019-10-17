ffccf0aed5cd86055901859d3b216c9f3ebdff5e
Hannah von Reth <vonreth@kde.org>
2019-10-17 14:01:31 +0200

    Fix qtcreator build with clang 9

--- qt-apps/qtcreator/qtcreator/qtcreator.py
+++ qt-apps/qtcreator/qtcreator/qtcreator.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import info
-
+from Blueprints.CraftVersion import CraftVersion
+from Blueprints.CraftPackageObject import CraftPackageObject
 
 class subinfo(info.infoclass):
     def setTargets(self):
@@ -11,19 +12,22 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"http://download.qt.io/official_releases/qtcreator/{majorVer}/{ver}/qt-creator-opensource-src-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"qt-creator-opensource-src-{ver}"
             self.targetDigestUrls[ver] = f"https://download.qt.io/official_releases/qtcreator/{majorVer}/{ver}/qt-creator-opensource-src-{ver}.tar.xz.sha256"
+
+        if CraftPackageObject.get("libs/llvm-meta/llvm").version == "9.0.0":
+            self.patchToApply["4.10.1"] = [("qtcreator-4.10.1-20191017.diff", 1)]
         self.defaultTarget = "4.10.1"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/qt5/qtscript"] = None
         self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
-        self.runtimeDependencies["libs/llvm-meta/llvm"] = None if self.buildTarget < CraftVersion("4.9") else "8.0"
+        self.runtimeDependencies["libs/llvm-meta/llvm"] = None
         self.runtimeDependencies["kde/frameworks/tier1/syntax-highlighting"] = None
         self.runtimeDependencies["binary/python"] = None
         self.runtimeDependencies["dev-utils/clazy"] = None
 
 
-from Package.CMakePackageBase import *
+from Package.CMakePackageBase import CMakePackageBase
 
 
 class Package(CMakePackageBase):
