4d286dbebcbf54226045b4ac03ca62d3d366763a
Hannah von Reth <vonreth@kde.org>
2019-10-19 23:16:26 +0200

    An other try to build openal with mingw

--- libs/openal-soft/openal-soft.py
+++ libs/openal-soft/openal-soft.py
@@ -9,7 +9,7 @@ class subinfo(info.infoclass):
                 f'https://github.com/kcat/openal-soft/archive/openal-soft-{ver}.tar.gz'
             self.targetInstSrc[ver] = f'openal-soft-openal-soft-{ver}'
         self.targetDigests["1.19.1"] = (['9f3536ab2bb7781dbafabc6a61e0b34b17edd16bd6c2eaf2ae71bc63078f98c7'], CraftHash.HashAlgorithm.SHA256)
-        self.patchToApply["1.19.1"] = [("openal-soft-1.19.1-20191017.diff", 1)]
+        self.patchToApply["1.19.1"] = [("openal-soft-1.19.1-20191017.diff", 1),  ("openal-soft-1.19.1-20191019.diff", 1)]
         self.description = 'a library for audio support'
         self.defaultTarget = '1.19.1'
 
