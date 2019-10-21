2413a544b01f1239bde2b506e16182ee1c6c1898
Hannah von Reth <vonreth@kde.org>
2019-10-21 09:28:49 +0200

    Make msys upgrades work
    The package won't be reinstalled but updated

--- dev-utils/_windows/msys/msys.py
+++ dev-utils/_windows/msys/msys.py
@@ -26,7 +26,42 @@ class subinfo(info.infoclass):
     def msysInstallShim(self, installDir):
         return utils.createShim(os.path.join(installDir, "dev-utils", "bin", "msys.exe"),
                                 os.path.join(installDir, "dev-utils", "bin", "python3.exe"),
-                                args=os.path.join(CraftStandardDirs.craftBin(), "shells.py"))
+                                args=[os.path.join(CraftStandardDirs.craftBin(), "shells.py")])
+
+    def updateMsys(self):
+        msysDir = CraftCore.settings.get("Paths", "Msys", os.path.join(CraftStandardDirs.craftRoot(), "msys"))
+        shell = shells.BashShell()
+
+        def stopProcesses():
+            return OsUtils.killProcess("*", msysDir)
+
+        def queryForUpdate():
+            out = io.BytesIO()
+            if not shell.execute(".", "pacman", "-Sy --noconfirm --force"):
+                raise Exception()
+            shell.execute(".", "pacman", "-Qu --noconfirm", stdout=out, stderr=subprocess.PIPE)
+            out = out.getvalue()
+            return out != b""
+
+        # start and restart msys before first use
+        if not (shell.execute(".", "echo", "Firstrun") and
+                stopProcesses() and
+                shell.execute(".", "pacman-key", "--init") and
+                shell.execute(".", "pacman-key", "--populate")):
+            return False
+
+        try:
+            while queryForUpdate():
+                if not (shell.execute(".", "pacman", "-Su --noconfirm --force --ask 20") and
+                        stopProcesses()):
+                    return False
+        except Exception as e:
+            CraftCore.log.error(e, exc_info=e)
+            return False
+        if not (shell.execute(".", "pacman", "-S base-devel msys/binutils --noconfirm --force --needed") and
+                stopProcesses()):
+            return False
+        return utils.system("autorebase.bat", cwd=msysDir)
 
 
 from Package.BinaryPackageBase import *
@@ -35,45 +70,12 @@ from Package.BinaryPackageBase import *
 class MsysPackage(BinaryPackageBase):
     def __init__(self):
         BinaryPackageBase.__init__(self)
-        self.shell = shells.BashShell()
 
     def postInstall(self):
         return self.subinfo.msysInstallShim(self.imageDir())
 
     def postQmerge(self):
-        msysDir = os.path.join(CraftStandardDirs.craftRoot(), "msys")
-
-        def stopProcesses():
-            return OsUtils.killProcess("*", msysDir)
-
-        def queryForUpdate():
-            out = io.BytesIO()
-            if not self.shell.execute(".", "pacman", "-Sy --noconfirm --force"):
-                raise Exception()
-            self.shell.execute(".", "pacman", "-Qu --noconfirm", stdout=out, stderr=subprocess.PIPE)
-            out = out.getvalue()
-            return out != b""
-
-        # start and restart msys before first use
-        if not (self.shell.execute(".", "echo", "Firstrun") and
-                stopProcesses() and
-                self.shell.execute(".", "pacman-key", "--init") and
-                self.shell.execute(".", "pacman-key", "--populate")):
-            return False
-
-        try:
-            while queryForUpdate():
-                if not (self.shell.execute(".", "pacman", "-Su --noconfirm --force --ask 20") and
-                        stopProcesses()):
-                    return False
-        except Exception as e:
-            print(e)
-            return False
-        if not (self.shell.execute(".", "pacman", "-S base-devel msys/binutils --noconfirm --force --needed") and
-                stopProcesses()):
-            return False
-        return utils.system("autorebase.bat", cwd=msysDir)
-
+        return self.subinfo.updateMsys()
 
 class VirtualPackage(VirtualPackageBase):
     def __init__(self):
@@ -82,15 +84,19 @@ class VirtualPackage(VirtualPackageBase):
     def install(self):
         if not VirtualPackageBase.install(self):
             return False
-        return self.subinfo.msysInstallShim(self.imageDir())
+        return self.subinfo.msysInstallShim(self.imageDir()) and self.subinfo.updateMsys()
 
+    def qmerge(self):
+        if self.package.isInstalled:
+            return True
+        return super().qmerge()
 
 class Package(MaybeVirtualPackageBase):
     def __init__(self):
-        self.skipCondition = ("Paths", "Msys") not in CraftCore.settings
+        useExternalMsys = ("Paths", "Msys") not in CraftCore.settings
+        self.skipCondition = useExternalMsys and not CraftPackageObject.get("dev-utils/msys").isInstalled
         MaybeVirtualPackageBase.__init__(self, condition=self.skipCondition, classA=MsysPackage, classB=VirtualPackage)
-
-        if not self.skipCondition:
+        if not useExternalMsys:
             # override the install method
             def install():
                 CraftCore.log.info(f"Using manually installed msys {CraftStandardDirs.msysDir()}")
