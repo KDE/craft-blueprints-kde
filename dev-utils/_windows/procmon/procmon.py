94887acdf8e6dd33b3fb78a1e5b28609b4106228
Hannah von Reth <vonreth@kde.org>
2019-10-29 16:35:33 +0100

    Update procmon

--- dev-utils/_windows/procmon/procmon.py
+++ dev-utils/_windows/procmon/procmon.py
@@ -3,11 +3,10 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['2.8'] = 'http://download.sysinternals.com/Files/ProcessMonitor.zip'
-        self.defaultTarget = '2.8'
-        # the zip file does not have a bin dir, so we have to create it
-        # This attribute is in prelimary state
-        self.targetInstallPath['2.8'] = os.path.join("dev-utils", "bin")
+        self.targets["3.52"] = 'https://download.sysinternals.com/files/ProcessMonitor.zip'
+        self.defaultTarget = '3.52'
+        self.targetDigests["3.52"] =  (['3a0ce29f1654468a470d8b4e0f5f163a428ddab7bf0ea37b0cef504362cb94dd'], CraftHash.HashAlgorithm.SHA256)
+        self.targetInstallPath['3.52'] = os.path.join("dev-utils", "bin")
 
 
 from Package.BinaryPackageBase import *
