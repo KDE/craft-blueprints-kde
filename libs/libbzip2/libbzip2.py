fbb45d260a59d9f34ee5e000925f6b31a2a231bd
Hannah von Reth <vonreth@kde.org>
2019-10-30 18:09:47 +0100

    Cleanup

--- libs/libbzip2/libbzip2.py
+++ libs/libbzip2/libbzip2.py
@@ -1,13 +1,6 @@
 import info
 from Package.CMakePackageBase import *
 
-
-##########################################################################
-############################# ATTENTION ##################################
-##########################################################################
-######### There is no binary build for Windows Ce, just the lib! #########
-##########################################################################
-
 class subinfo(info.infoclass):
     def setTargets(self):
         #self.targets['1.0.6'] = 'http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz'
