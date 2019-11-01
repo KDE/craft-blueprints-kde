46e69947149dbfb2c07b6859ac2ad64ad276457b
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2019-11-01 08:24:07 +0100

    no tests in package

--- kde/applications/labplot/labplot.py
+++ kde/applications/labplot/labplot.py
@@ -45,7 +45,7 @@ class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
         # hdf5 and lz4 (needed for ROOT) not available on craft
-        self.subinfo.options.configure.args += "-DENABLE_HDF5=OFF -DENABLE_NETCDF=OFF -DENABLE_ROOT=OFF"
+        self.subinfo.options.configure.args += "-DENABLE_TESTS=OFF -DENABLE_HDF5=OFF -DENABLE_NETCDF=OFF -DENABLE_ROOT=OFF"
 
     def createPackage(self):
         self.blacklist_file.append(os.path.join(self.packageDir(), 'blacklist.txt'))
