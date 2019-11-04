91141b36291601bc6d7424847bab553dd19546bb
Hannah von Reth <vonreth@kde.org>
2019-11-04 16:21:29 +0100

    Pull in ccache if is enabled

---
+++ virtual/craft-blueprints-kde-base/craft-blueprints-kde-base.py
@@ -0,0 +1,20 @@
+import info
+from CraftCore import CraftCore
+from Package.VirtualPackageBase import VirtualPackageBase
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.targets['0.1'] = ""
+        self.defaultTarget = '0.1'
+
+    def setDependencies(self):
+        # The order is important
+        self.runtimeDependencies["virtual/base"] = None
+        if CraftCore.settings.getboolean("Compile", "UseCCache", False):
+            self.buildDependencies["dev-utils/ccache"] = None
+
+
+class Package(VirtualPackageBase):
+    def __init__(self):
+        VirtualPackageBase.__init__(self)
