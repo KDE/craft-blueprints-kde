171c33b3493247efe6b2ded489767a5b7c0c2231
Hannah von Reth <vonreth@kde.org>
2019-11-19 22:47:08 +0100

    Fix gpg-error with awk5 https://dev.gnupg.org/rE7865041c77f4f7005282f10f9b6666b19072fbdf

--- libs/_autotools/gpg-error/gpg-error.py
+++ libs/_autotools/gpg-error/gpg-error.py
@@ -3,12 +3,14 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets( self ):
-        for ver in ["1.27", "1.31"]:
+        for ver in ["1.27", "1.31", "1.36"]:
             self.targets[ver] = f"https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"libgpg-error-{ver}"
-        self.patchToApply["1.31"] = [("libgpg-error-1.31.diff", 1)]
+        self.patchToApply["1.31"] = [("libgpg-error-1.31.diff", 1),
+                                     ("gpg-error-1.31-20191119.diff", 1)]  # https://dev.gnupg.org/rE7865041c77f4f7005282f10f9b6666b19072fbdf
         self.targetDigests['1.27'] = (['4f93aac6fecb7da2b92871bb9ee33032be6a87b174f54abf8ddf0911a22d29d2'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests['1.31'] =  (['40d0a823c9329478063903192a1f82496083b277265904878f4bc09e0db7a4ef'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.31'] = (['40d0a823c9329478063903192a1f82496083b277265904878f4bc09e0db7a4ef'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.36'] = (['babd98437208c163175c29453f8681094bcaf92968a15cafb1a276076b33c97c'], CraftHash.HashAlgorithm.SHA256)
         self.description = "Small library with error codes and descriptions shared by most GnuPG related software"
         self.patchLevel["1.31"] = 2
         self.defaultTarget = "1.31"
