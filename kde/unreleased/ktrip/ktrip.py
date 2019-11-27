b0669e17e4c4d76be48887c8aa5cbcb786f44938
Nicolas Fella <nicolas.fella@gmx.de>
2019-11-27 20:27:04 +0100

    Add ktrip blueprint
    Reviewers: vonreth
    
    Reviewed By: vonreth
    
    Differential Revision: https://phabricator.kde.org/D25574

---
+++ kde/unreleased/ktrip/ktrip.py
@@ -0,0 +1,27 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['master'] = 'git://anongit.kde.org/ktrip.git'
+        self.defaultTarget = 'master'
+
+        self.description = "TODO"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/unreleased/kpublictransport"] = None
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
