d12d56d2f239524dd1df3fb26b7da3c249b2b0a5
Gilles Caulier <caulier.gilles@gmail.com>
2019-12-02 21:42:37 +0100

    add deps

--- extragear/digikam/digikam.py
+++ extragear/digikam/digikam.py
@@ -6,10 +6,18 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = 'git://anongit.kde.org/digikam.git'
-
         self.defaultTarget = 'master'
 
+        self.displayName = "digiKam"
+        self.webpage = "https://www.digikam.org"
+        self.description = "Professional Photo Management with the Power of Open Source"
+
     def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.buildDependencies["dev-utils/flexbison"] = None
+        self.runtimeDependencies["libs/sqlite"] = None
+        self.runtimeDependencies["binary/mysql"] = None
         self.runtimeDependencies['libs/x265'] = None
         self.runtimeDependencies['libs/tiff'] = None
         self.runtimeDependencies['libs/boost'] = None
@@ -23,6 +31,8 @@ class subinfo(info.infoclass):
         self.runtimeDependencies['libs/libpng'] = None
         self.runtimeDependencies['libs/libxslt'] = None
         self.runtimeDependencies['libs/libxml2'] = None
+        self.runtimeDependencies['libs/openal-soft'] = None
+        self.runtimeDependencies['libs/pthreads'] = None
         self.runtimeDependencies['libs/libjpeg-turbo'] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/qt5/qtsvg"] = None
@@ -42,8 +52,16 @@ class subinfo(info.infoclass):
         self.runtimeDependencies['kde/pim/kcalcore'] = None
         self.runtimeDependencies["kde/applications/marble"] = None
 
-        self.description = "Professional Photo Management with the Power of Open Source"
-
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
+
+    def createPackage(self):
+        self.defines["productname"] = "digiKam"
+        self.defines["website"] = "https://www.digikam.org"
+        self.defines["executable"] = "bin\\digikam.exe"
+        self.defines["icon"] = os.path.join(self.packageDir(), "digikam.ico")
+
+        self.ignoredPackages.append("binary/mysql")
+
+        return TypePackager.createPackage(self)
