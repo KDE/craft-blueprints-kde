1cb070313158489a9e313eecf5d290f9808411f7
Mladen Milinkovic <maxrd2@smoothware.net>
2019-12-02 18:41:45 +0100

    extragear/subtitlecomposer: added recipe
    Summary:
    Added Subtitle Composer recipe
    
    Reviewers: #craft, vonreth, bcooksley
    
    Reviewed By: #craft, vonreth
    
    Differential Revision: https://phabricator.kde.org/D25689

---
+++ extragear/subtitlecomposer/subtitlecomposer.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import info
+from Blueprints.CraftVersion import CraftVersion
+
+import subprocess
+import sys
+from info import DependencyRequirementType
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+        self.description = "Text-based video subtitle editor that supports basic and advanced editing operations"
+        self.webpage = "https://invent.kde.org/kde/subtitlecomposer"
+        self.displayName = "Subtitle Composer"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.buildDependencies["dev-utils/_autotools/pkg-config"] = None
+        self.runtimeDependencies["data/hicolor-icon-theme"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcodecs"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/sonnet"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kauth"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kross"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/ktextwidgets"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
+        self.runtimeDependencies["libs/ffmpeg"] = None
+        self.runtimeDependencies["libs/qtav"] = None
+        self.runtimeDependencies["libs/gettext"] = None
+        self.runtimeDependencies["libs/icu"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/sphinxbase"] = None
+        self.runtimeDependencies["libs/pocketsphinx"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+
