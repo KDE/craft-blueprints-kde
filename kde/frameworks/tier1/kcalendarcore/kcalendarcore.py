aa4b9b54359514290907b4beb3df8dd9d92a662e
Alexander Potashev <aspotashev@gmail.com>
2019-12-14 01:51:17 +0300

    Add blueprint for KF5::KCalendarCore
    Blueprint in kde/pim/kcalcore is still present for older versions of the
    library.
    
    CCMAIL: vkrause@kde.org

---
+++ kde/frameworks/tier1/kcalendarcore/kcalendarcore.py
@@ -0,0 +1,22 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        self.description = "The KDE calendar access library"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/libical"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
