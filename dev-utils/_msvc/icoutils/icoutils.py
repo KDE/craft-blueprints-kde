3e08b58a185895a4a8d0ecfb84b3def2c37cacbe
Hannah von Reth <vonreth@kde.org>
2019-12-13 16:40:20 +0100

    Update prebuilts

--- dev-utils/_msvc/icoutils/icoutils.py
+++ dev-utils/_msvc/icoutils/icoutils.py
@@ -3,7 +3,7 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.addCachedAutotoolsBuild("dev-utils/_autotools/icoutils", targetInstallPath=os.path.join("dev-utils", "bin"))
+        self.addCachedBuild("https://files.kde.org/craft/prebuilt/packages/", packageName="dev-utils/_autotools/icoutils", targetInstallPath="dev-utils")
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
@@ -18,5 +18,5 @@ class Package(BinaryPackageBase):
     def install(self):
         if not super().install():
             return False
-        return (utils.createShim(os.path.join(self.imageDir(), "bin", "icotool.exe"), os.path.join(self.installDir(), "bin", "icotool.exe")) and
-                utils.createShim(os.path.join(self.imageDir(), "bin", "wrestool.exe"), os.path.join(self.installDir(), "bin", "wrestool.exe")) )
+        return (utils.createShim(os.path.join(self.imageDir(), "dev-utils", "bin", "icotool.exe"), os.path.join(self.installDir(), "bin", "icotool.exe")) and
+                utils.createShim(os.path.join(self.imageDir(), "dev-utils", "bin", "wrestool.exe"), os.path.join(self.installDir(), "bin", "wrestool.exe")) )
--- dev-utils/_msvc/pkg-config/pkg-config.py
+++ dev-utils/_msvc/pkg-config/pkg-config.py
@@ -4,7 +4,7 @@ from Package.BinaryPackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.addCachedAutotoolsBuild("dev-utils/_autotools/pkg-config", targetInstallPath=os.path.join("dev-utils", "bin"))
+        self.addCachedBuild("https://files.kde.org/craft/prebuilt/packages/", packageName="dev-utils/_autotools/pkg-config", targetInstallPath="dev-utils")
 
     def setDependencies(self):
         self.buildDependencies["virtual/bin-base"] = None
@@ -14,4 +14,5 @@ class Package(BinaryPackageBase):
         BinaryPackageBase.__init__(self)
 
     def postInstall(self):
-        return utils.createShim(os.path.join(self.imageDir(), "bin", "pkg-config.exe"), os.path.join(self.installDir(), "bin", "pkg-config.exe"))
+        return utils.createShim(os.path.join(self.imageDir(), "dev-utils", "bin", "pkg-config.exe"), os.path.join(self.installDir(), "bin", "pkg-config.exe"))
+
