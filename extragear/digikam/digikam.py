3b6babd70a4d690d4f4ddd9d853b65cc750696c6
Ben Cooksley <bcooksley@kde.org>
2019-12-22 21:12:03 +1300

    We need the tier in there

--- extragear/digikam/digikam.py
+++ extragear/digikam/digikam.py
@@ -49,7 +49,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies['kde/frameworks/tier1/kcoreaddons'] = None
         self.runtimeDependencies['kde/frameworks/tier1/threadweaver'] = None
         self.runtimeDependencies['kde/frameworks/tier3/kiconthemes'] = None
-        self.runtimeDependencies['kde/frameworks/kcalendarcore'] = None
+        self.runtimeDependencies['kde/frameworks/tier1/kcalendarcore'] = None
         self.runtimeDependencies["kde/applications/marble"] = None
 
 class Package(CMakePackageBase):
