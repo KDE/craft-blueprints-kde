d321dd8354602e31fba63d43d1bfbd49613c8a6b
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2019-12-31 13:43:12 +0100

    enable building shared libs

--- libs/hdf5/hdf5.py
+++ libs/hdf5/hdf5.py
@@ -19,4 +19,4 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        #self.subinfo.options.configure.args = "-DBUILD_SHARED_LIBS=OFF"
+        self.subinfo.options.configure.args = "-DBUILD_SHARED_LIBS=ON"
