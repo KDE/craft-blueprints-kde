39798d5ff9974fc7f5cb3208457cb4e3e09552e9
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2020-01-05 22:08:24 +0100

    Add older version and disable building tests

--- libs/netcdf/netcdf.py
+++ libs/netcdf/netcdf.py
@@ -4,14 +4,16 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = "[git]https://github.com/Unidata/netcdf-c.git"
-        for ver in ["4.7.3"]:
+        for ver in ["4.6.0", "4.7.3"]:
             self.targets[ver] = f"https://github.com/Unidata/netcdf-c/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"netcdf-c-{ver}"
+        self.targetDigests['4.6.0'] = (
+            ['6d740356399aac12290650325a05aec2fe92c1905df10761b2b0100994197725'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['4.7.3'] = (
             ['05d064a2d55147b83feff3747bea13deb77bef390cb562df4f9f9f1ce147840d'], CraftHash.HashAlgorithm.SHA256)
         self.description = 'A set of software libraries and self-describing, machine-independent data formats that support the creation, access, and sharing of array-oriented scientific data'
-        self.defaultTarget = '4.7.3'
+        self.defaultTarget = '4.6.0'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
@@ -21,6 +23,6 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        #self.subinfo.options.configure.args = "-DHDF5_DIR=${CPP}/cmake/hdf5"
         hdf5dir = os.path.join(CraftStandardDirs.craftRoot(), "cmake", "hdf5")
-        self.subinfo.options.configure.args = f"-DHDF5_DIR={hdf5dir}"
+        # -DENABLE_PARALLEL_TESTS=OFF -DENABLE_UNIT_TESTS=OFF
+        self.subinfo.options.configure.args = f"-DHDF5_DIR={hdf5dir} -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DBUILD_TESTSETS=OFF"
