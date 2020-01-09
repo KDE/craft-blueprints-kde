44d0af76c183e54985976e7acd6970ac17b81448
Hannah von Reth <vonreth@kde.org>
2020-01-09 22:59:37 +0100

    Use configure also on msvc

--- libs/icu/icu.py
+++ libs/icu/icu.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import info
-
+from Package.AutoToolsPackageBase import *
 
 class subinfo(info.infoclass):
     def registerOptions(self):
@@ -32,9 +32,6 @@ class subinfo(info.infoclass):
         self.patchToApply["63.1"] += [("icu-63.1-20181212.diff", 1),
                                      ("icu-63.1-20181215.diff", 2), # backport https://github.com/unicode-org/icu/pull/228
                                      ]
-        self.patchToApply["65.1"] = [
-                                     ("icu-65.1-20200109.diff", 2),
-                                     ]
         self.defaultTarget = "63.1"
 
     def setDependencies(self):
@@ -43,71 +40,30 @@ class subinfo(info.infoclass):
         if CraftCore.compiler.isMinGW():
             self.buildDependencies["dev-utils/msys"] = None
 
-from Package.MSBuildPackageBase import *
 
-
-class PackageCMake(MSBuildPackageBase):
-    def __init__(self, **args):
-        MSBuildPackageBase.__init__(self)
-        self.subinfo.options.configure.projectFile = os.path.join(self.sourceDir(), "allinone", "allinone.sln")
-
-    def install(self):
-        self.cleanImage()
-        if not MSBuildPackageBase.install(self, installHeaders=False,
-                                          buildDirs=[os.path.join(self.sourceDir(), "..", dir) for dir in
-                                                     ["bin", "bin64", "lib", "lib64"]]):
-            return False
-        utils.copyDir(os.path.join(self.sourceDir(), "..", "include"), os.path.join(self.imageDir(), "include"))
-
-        if CraftCore.compiler.isMSVC() and self.buildType() == "Debug":
-            imagedir = os.path.join(self.installDir(), "lib")
-            filelist = os.listdir(imagedir)
-            for f in filelist:
-                if f.endswith("d.lib"):
-                    utils.copyFile(os.path.join(imagedir, f), os.path.join(imagedir, f.replace("d.lib", ".lib")))
-
-        return True
-
-
-from Package.AutoToolsPackageBase import *
-
-
-class PackageMSys(AutoToolsPackageBase):
+class Package(AutoToolsPackageBase):
     def __init__(self):
         AutoToolsPackageBase.__init__(self)
+        self.shell.useMSVCCompatEnv = True
         self.subinfo.options.configure.autoreconf = False
+        self.subinfo.options.configure.args += " --enable-samples --enable-release=yes --enable-debug=no "
+        if CraftCore.compiler.isWindows:
+            self.subinfo.options.configure.args += " --with-data-packaging=dll"
+            if CraftCore.compiler.isMSVC():
+                self.subinfo.options.configure.args += " --enable-extras=no"
 
     def make(self):
-        datafile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "icudt55l.dat")
-        if os.path.exists(datafile):
-            datafileDestination = os.path.join(self.sourceDir(), "data", "in", "icudt55l.dat")
-            if os.path.exists(datafileDestination):
-                os.remove(datafileDestination)
-            utils.copyFile(datafile, datafileDestination)
-
-        return AutoToolsPackageBase.make(self)
+        utils.createDir(Path(self.buildDir()) / "data/out/tmp/")
+        return super().make()
 
     def install(self):
-        if not AutoToolsPackageBase.install(self):
+        if not super().install():
             return False
         files = os.listdir(os.path.join(self.installDir(), "lib"))
         for dll in files:
             if dll.endswith(".dll"):
-                utils.copyFile(os.path.join(self.installDir(), "lib", dll), os.path.join(self.installDir(), "bin", dll))
+                utils.moveFile(os.path.join(self.installDir(), "lib", dll), os.path.join(self.installDir(), "bin", dll))
         return True
-
-
-if CraftCore.compiler.isGCCLike():
-    class PackageSuper(PackageMSys):
-        pass
-else:
-    class PackageSuper(PackageCMake):
-        pass
-
-
-class Package(PackageSuper):
-    def __init__(self, **args):
-        PackageSuper.__init__(self)
 
     def postInstall(self):
         res = True
