8dd3cade6d310c4960e498e7453c3aadacf3d394
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2020-01-26 14:38:06 +0100

    HDF5 1.10.6

--- libs/hdf5/hdf5.py
+++ libs/hdf5/hdf5.py
@@ -4,14 +4,16 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = "[git]https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git"
-        for ver in ["1.10.5"]:
+        for ver in ["1.10.5", "1.10.6"]:
             self.targets[ver] = f"https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-{ver}/src/hdf5-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"hdf5-{ver}"
             self.archiveNames[ver] = f"hdf5-{ver}.tar.gz"
         self.targetDigests['1.10.5'] = (
             ['6d4ce8bf902a97b050f6f491f4268634e252a63dadd6656a1a9be5b7b7726fa8'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.10.6'] = (
+            ['5f9a3ee85db4ea1d3b1fa9159352aebc2af72732fc2f58c96a3f0768dba0e9aa'], CraftHash.HashAlgorithm.SHA256)
         self.description = 'A data model, library, and file format for storing and managing data'
-        self.defaultTarget = '1.10.5'
+        self.defaultTarget = '1.10.6'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
