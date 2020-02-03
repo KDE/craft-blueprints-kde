d4657dc4e879a008e041541c2ccb8d4787f8c68f
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2020-02-04 00:01:31 +0100

    Disable failing backends

--- kde/applications/cantor/cantor.py
+++ kde/applications/cantor/cantor.py
@@ -15,12 +15,13 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.buildDependencies["dev-utils/png2ico"] = None
-        #self.runtimeDependencies["dev-utils/python3"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["qt-libs/poppler"] = None
         # R backend fails compiling with MSVC
         if not CraftCore.compiler.isMSVC():
             self.runtimeDependencies["binary/r-base"] = None
+        # we use Crafts python
+        #self.runtimeDependencies["dev-utils/python3"] = None
         #self.runtimeDependencies["binary/python-libs"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kcrash"] = None
@@ -38,20 +39,23 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
         self.runtimeDependencies["kde/applications/analitza"] = None
 
-
 from Package.CMakePackageBase import *
 
 
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        if CraftCore.compiler.isMSVC():
+        if CraftCore.compiler.isWindows:
             self.subinfo.options.make.supportsMultijob = False
 
-        #if CraftCore.compiler.isMSVC():
-        #    self.r_dir = os.path.join(CraftCore.standardDirs.craftRoot(), "lib", "R", "bin", "x64")
-        #    self.subinfo.options.configure.args = "-DR_EXECUTABLE=" + OsUtils.toUnixPath(os.path.join(self.r_dir, "R.exe"))
-        #    self.subinfo.options.configure.args += " -DR_R_LIBRARY=" + OsUtils.toUnixPath(os.path.join(self.r_dir, "R.dll"))
+            # R backend fail compiling on Windows
+            #self.r_dir = os.path.join(CraftCore.standardDirs.craftRoot(), "lib", "R", "bin", "x64")
+            #self.subinfo.options.configure.args = "-DR_EXECUTABLE=" + OsUtils.toUnixPath(os.path.join(self.r_dir, "R.exe"))
+            #self.subinfo.options.configure.args += " -DR_R_LIBRARY=" + OsUtils.toUnixPath(os.path.join(self.r_dir, "R.dll"))
 
-        pythonPath = CraftCore.settings.get("Paths", "PYTHON")
-        self.subinfo.options.configure.args += f" -DPYTHONLIBS3_LIBRARY={pythonPath}"
+            # Python (src/backends/python/pythonserver.cpp) backend fail compiling with MSVC
+            if not CraftCore.compiler.isMSVC():
+                pythonPath = CraftCore.settings.get("Paths", "PYTHON")
+                self.subinfo.options.configure.args += f" -DPYTHONLIBS3_LIBRARY={pythonPath}/python36.dll -DPYTHONLIBS3_INCLUDE_DIR={pythonPath}/include"
+                python27Path = CraftCore.settings.get("Paths", "PYTHON27")
+                self.subinfo.options.configure.args += f" -DPYTHON_LIBRARIES_DIR={python27Path}/libs -DPYTHON_INCLUDE_DIR={python27Path}/include"
