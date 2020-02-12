5e88f879f7976157686d135dfdd558c9ec8fa7b0
Christoph Cullmann <cullmann@kde.org>
2020-02-12 18:03:10 +0100

    add new kirigami dep

--- kde/frameworks/tier3/purpose/purpose.py
+++ kde/frameworks/tier3/purpose/purpose.py
@@ -11,6 +11,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
 
 
 from Package.CMakePackageBase import *
