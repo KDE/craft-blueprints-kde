4e27ec7e10d5c836d49911cf988a365b1ed66366
Hannah von Reth <vonreth@kde.org>
2020-02-12 22:18:30 +0100

    Add qqc2-desktop-style

---
+++ kde/frameworks/tier3/qqc2-desktop-style/qqc2-desktop-style.py
@@ -0,0 +1,24 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        self.description = "QtQuickControls 2 style that integrates with the desktop"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kitemviews"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
+        
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
