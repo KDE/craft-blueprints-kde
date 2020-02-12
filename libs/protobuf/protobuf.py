5db55be53ef43b264e087fd98466beb5ea19082f
Hannah von Reth <hannah.vonreth@owncloud.com>
2020-02-12 17:43:20 +0100

    Build protobuf shared on windows, this should fix opencv

--- libs/protobuf/protobuf.py
+++ libs/protobuf/protobuf.py
@@ -11,6 +11,7 @@ class subinfo(info.infoclass):
             self.targetInstSrc[ver] = f"protobuf-{ver}"
             self.targetConfigurePath[ver] = "cmake"
         self.targetDigests["3.11.2"] = (['e8c7601439dbd4489fe5069c33d374804990a56c2f710e00227ee5d8fd650e67'], CraftHash.HashAlgorithm.SHA256)
+        self.patchLevel["3.11.2"] = 1
         self.defaultTarget = "3.11.2"
 
     def setDependencies(self):
@@ -22,6 +23,4 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args += " -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF"
-        if not CraftCore.compiler.isWindows:
-            self.subinfo.options.configure.args += " -DBUILD_SHARED_LIBS=ON"
+        self.subinfo.options.configure.args += " -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF -DBUILD_SHARED_LIBS=ON"
