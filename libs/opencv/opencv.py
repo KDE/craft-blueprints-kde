cdc604e914d5e91b745f6d92a1ecf0a90af8f1be
Gilles Caulier <caulier.gilles@gmail.com>
2020-02-15 04:48:27 -0500

    back to 4.2.0, this do not solve digiKam linking stage error

--- libs/opencv/opencv.py
+++ libs/opencv/opencv.py
@@ -13,7 +13,7 @@ class subinfo(info.infoclass):
 
         self.targetDigests['4.1.2'] = (['385dd0a9c25e67ef0dd60e022d2a2d7b17e2f36819cf3cb46aa8cdff5c5282c9'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['4.2.0'] = (['9ccb2192d7e8c03c58fee07051364d94ed7599363f3b0dce1c5e6cc11c1bb0ec'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = '4.1.2'
+        self.defaultTarget = '4.2.0'
 
         self.options.configure.args = "-DBUILD_NEW_PYTHON_SUPPORT=OFF -DBUILD_ZLIB=OFF -DBUILD_TIFF=OFF -DBUILD_JPEG=OFF -DBUILD_PNG=OFF -DBUILD_WEBP=OFF -DBUILD_JASPER=OFF -DWITH_JASPER=OFF -DBUILD_PROTOBUF=OFF -DPROTOBUF_UPDATE_FILES=ON -DWITH_QT=5"
 
