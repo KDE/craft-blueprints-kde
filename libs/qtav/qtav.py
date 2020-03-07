03c6e294172eeb6d99c4248bf0f2b8dc23d567e6
Mladen Milinkovic <maxrd2@smoothware.net>
2020-03-07 20:42:51 +0100

    libs/qtav: added 1.13.0 recipe
    Summary: Added QtAV recipe
    
    Reviewers: #craft, vonreth, bcooksley
    
    Reviewed By: #craft, vonreth
    
    Differential Revision: https://phabricator.kde.org/D27920

---
+++ libs/qtav/qtav.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import info
+from Blueprints.CraftVersion import CraftVersion
+
+import subprocess
+import sys
+from info import DependencyRequirementType
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+        self.description = "A cross-platform multimedia framework based on Qt and FFmpeg."
+        self.webpage = "https://github.com/wang-bin/QtAV"
+        self.displayName = "QtAV"
+        self.patchToApply['1.13.0'] = [
+            ("0001-Include-QSGMaterial.patch", 1),
+            ("0002-Fix-install-prefix.patch", 1),
+        ]
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/ffmpeg"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+
+
+from Package.QMakePackageBase import *
+
+
+class Package(QMakePackageBase):
+    def __init__(self):
+        QMakePackageBase.__init__(self)
+
+    def configureOptions(self, defines=""):
+        return super().configureOptions(defines + ' "CONFIG += no-examples no-tests"')
