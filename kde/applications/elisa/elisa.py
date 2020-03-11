7cc6465434486bfb4ef4210613810bbe711f801e
Matthieu Gallien <matthieu_gallien@yahoo.fr>
2020-03-11 07:36:12 +0100

    use a 44x44 icon as default icon for Elisa after a failed submission
    the following validation error has been reported:
    
    Error Found: The app resources validation test detected the following errors:
    
        Image reference "Assets\150-apps-elisa.png": The image "Assets\150-apps-elisa.png" failed the size restrictions of 50 X 50.

--- kde/applications/elisa/elisa.py
+++ kde/applications/elisa/elisa.py
@@ -55,7 +55,7 @@ class Package( CMakePackageBase ):
         self.defines["executable"] = "bin\\elisa.exe"
 
         self.defines["icon"] = os.path.join(self.packageDir(), "elisa.ico")
-        self.defines["icon_png"] = os.path.join(self.packageDir(), ".assets", "150-apps-elisa.png")
+        self.defines["icon_png"] = os.path.join(self.packageDir(), ".assets", "44-apps-elisa.png")
         self.defines["icon_png_44"] = os.path.join(self.packageDir(), ".assets", "44-apps-elisa.png")
 
         self.defines["mimetypes"] = ["audio/mpeg", "audio/mp4"]
