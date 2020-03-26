8f2b05836914b875a46f7e6a04764dc28192c70d
Dawid Wróbel <me@dawidwrobel.com>
2020-03-26 15:53:01 -0400

    AqBanking: Update to 6.1.3

--- libs/_autotools/aqbanking/aqbanking.py
+++ libs/_autotools/aqbanking/aqbanking.py
@@ -28,10 +28,10 @@ from CraftOS.osutils import OsUtils
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets["6.1.2"] = "https://www.aquamaniac.de/rdm/attachments/download/269/aqbanking-6.1.2.tar.gz"
-        self.targetDigests["6.1.2"] = (['0b54c70a8ab1bd5537d8b26d05a9e44db0600b64d8cbb63c34018dee11f67190'], CraftHash.HashAlgorithm.SHA256)
-        self.targetInstSrc["6.1.2"] = "aqbanking-6.1.2"
-        self.defaultTarget = "6.1.2"
+        self.targets["6.1.3"] = "https://www.aquamaniac.de/rdm/attachments/download/279/aqbanking-6.1.3.tar.gz"
+        self.targetDigests["6.1.3"] = (['dd35fd0161e8f71f6fb7a76efc375c61e44e98c5d1203e4401b792ceac683615'], CraftHash.HashAlgorithm.SHA256)
+        self.targetInstSrc["6.1.3"] = "aqbanking-6.1.3"
+        self.defaultTarget = "6.1.3"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
