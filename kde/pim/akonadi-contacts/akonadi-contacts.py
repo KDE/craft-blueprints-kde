6b68787712b21151bf1216e93dab3747ec4805c1
Patrick Spendrin <patrick.spendrin@kdab.com>
2020-04-21 14:13:57 +0200

    akonadi-contacts: Use another patch

--- kde/pim/akonadi-contacts/akonadi-contacts.py
+++ kde/pim/akonadi-contacts/akonadi-contacts.py
@@ -11,6 +11,7 @@ class subinfo(info.infoclass):
         self.patchToApply['17.04.1'] = [("akonadi-contacts-17.04.1-fix-compile.diff", 1)]
         self.patchToApply['17.04.2'] = [("akonadi-contacts-17.04.1-fix-compile.diff", 1)]
         self.patchToApply['18.12.1'] = [("akonadi-contacts-18.12.1-20190124.diff", 1)]
+        self.patchToApply['19.12.3'] = [("0001-Revert-Fix-windows-build.patch", 1)]
 
     def setDependencies(self):
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
