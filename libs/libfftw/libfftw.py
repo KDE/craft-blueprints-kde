31f6d83665897036110811a58c1482dfdf393dd0
Vincent Pinon <vpinon@kde.org>
2020-04-22 14:44:11 -0700

    Try using upstream libfftw win build options (rubberband crash)

--- libs/libfftw/libfftw.py
+++ libs/libfftw/libfftw.py
@@ -5,7 +5,8 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.targets['3.3.8'] = 'http://www.fftw.org/fftw-3.3.8.tar.gz'
-        self.patchToApply['3.3.8'] = ('libfftw-win32-aligned-malloc.patch', 0)
+        if CraftCore.compiler.isWindows:
+            self.patchToApply['3.3.8'] = ('libfftw-win32-aligned-malloc.patch', 0)
         self.targetDigests['3.3.8'] = '59831bd4b2705381ee395e54aa6e0069b10c3626'
         self.targetInstSrc['3.3.8'] = "fftw-3.3.8"
         self.description = "a C subroutine library for computing the discrete Fourier transform (DFT)"
@@ -23,8 +24,10 @@ class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
         self.supportsNinja = False
-        self.subinfo.options.configure.args += ' -DBUILD_TESTS=OFF'
-        # -DENABLE_THREADS=ON -DENABLE_OPENMP=ON'
-        self.subinfo.options.configure.args += ' -DENABLE_SSE2=ON -DENABLE_AVX=ON'
+        self.subinfo.options.configure.args += \
+                ' -DENABLE_SSE2=ON -DENABLE_AVX=OFF ' \
+                ' -DENABLE_THREADS=ON -DWITH_COMBINED_THREADS=ON '
         if CraftCore.compiler.isWindows:
-            self.subinfo.options.configure.args += ' -DWITH_OUR_MALLOC=1'
+            self.subinfo.options.configure.args += \
+                ' -DWITH_OUR_MALLOC=ON -DFFTW_ENABLE_ALLOCA=OFF ' \
+                ' -DWINDOWS_F77_MANGLING=ON '
