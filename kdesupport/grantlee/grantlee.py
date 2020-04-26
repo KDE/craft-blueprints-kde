b374d7fd7952883b137fade95a7ffac879eca085
Dawid Wróbel <me@dawidwrobel.com>
2020-04-26 18:32:16 -0400

    grantlee: change default target to 5.2.0 release

--- kdesupport/grantlee/grantlee.py
+++ kdesupport/grantlee/grantlee.py
@@ -5,7 +5,10 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = "https://github.com/steveire/grantlee.git"
-        self.defaultTarget = 'master'
+        self.targets["5.2.0"] = "https://github.com/steveire/grantlee/archive/v5.2.0.tar.gz"
+        self.targetDigests["5.2.0"] = (['139acee5746b957bdf1327ec0d97c604d4c0b9be42aec5d584297cb5ed6a990a'], CraftHash.HashAlgorithm.SHA256)
+        self.targetInstSrc["5.2.0"] = "grantlee-5.2.0"
+        self.defaultTarget = "5.2.0"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
