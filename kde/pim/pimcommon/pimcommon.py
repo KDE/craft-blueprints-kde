40190f6d9819454d0d28f1c70f0c8851ed23cac7
Laurent Montel <montel@kde.org>
2020-05-04 13:08:27 +0200

    Add patchLevel line

--- kde/pim/pimcommon/pimcommon.py
+++ kde/pim/pimcommon/pimcommon.py
@@ -43,6 +43,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/pim/libkdepim"] = None
         self.runtimeDependencies["kdesupport/grantlee"] = None
         self.runtimeDependencies["libs/libxslt"] = None
+        self.patchLevel["20.04.0"] = 1
 
 
 from Package.CMakePackageBase import *
