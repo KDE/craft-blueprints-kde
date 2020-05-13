f635510b37cab389d3c4811e95b2bcc4e3c9dc82
Hannah von Reth <hannah.vonreth@owncloud.com>
2020-05-13 12:38:16 +0200

    Jpeg does not support ccache

--- libs/libpng/libpng.py
+++ libs/libpng/libpng.py
@@ -28,5 +28,5 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
+        self.supportsCCACHE = False
         self.subinfo.options.configure.args = "-DPNG_TESTS=OFF -DPNG_STATIC=OFF -DPNG_NO_STDIO=OFF"
-        self.subinfo.options.package.packageName = 'libpng'
