9d0b546127fb3334961e34967e111f3f4a6096b1
Piyush Aggarwal <piyushaggarwal002@gmail.com>
2020-05-26 23:49:40 +0530

    Add libssh 0.9.4 to fix kio-extras on windows
    Reviewers: vonreth, #craft
    
    Reviewed By: vonreth, #craft
    
    Tags: #craft
    
    Differential Revision: https://phabricator.kde.org/D29835

--- libs/libssh/libssh.py
+++ libs/libssh/libssh.py
@@ -27,14 +27,15 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "git://git.libssh.org/projects/libssh.git"
-        for ver in ["0.8.5"]:
+        for ver in ["0.8.5", "0.9.4"]:
             self.targets[ver] = f"https://www.libssh.org/files/{ver[:3]}/libssh-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"libssh-{ver}"
         self.targetDigests["0.8.5"] = (['07d2c431240fc88f6b06bcb36ae267f9afeedce2e32f6c42f8844b205ab5a335'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.9.4"] = (['150897a569852ac05aac831dc417a7ba8e610c86ca2e0154a99c6ade2486226b'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "a working SSH implementation by the mean of a library"
-        self.defaultTarget = "0.8.5"
-        
+        self.defaultTarget = "0.9.4"
+
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/zlib"] = None
