d5555443eae58b3f99e1a07a293c6507cc45d21b
Hannah von Reth <hannah.vonreth@owncloud.com>
2020-05-31 11:34:36 +0200

    Remove checksum to chaning binary

--- dev-utils/_windows/procexp/procexp.py
+++ dev-utils/_windows/procexp/procexp.py
@@ -6,7 +6,6 @@ class subinfo(info.infoclass):
         for ver in ["16.26"]:
             self.targets[ver] = 'http://download.sysinternals.com/files/ProcessExplorer.zip'
             self.targetInstallPath[ver] = os.path.join("dev-utils", "bin")
-        self.targetDigests['16.26'] = (['dc422a6ca2872207e8d0ef55ba4291998c8923efe847ba9e025c610090ad20be'], CraftHash.HashAlgorithm.SHA256)
         self.defaultTarget = "16.26"
 
 
