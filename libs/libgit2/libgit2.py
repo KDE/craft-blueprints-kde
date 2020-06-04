00c63763f0c033c41613534880327a099b0f9534
Hannah von Reth <hannah.vonreth@owncloud.com>
2020-06-04 23:57:50 +0200

    update libgit2 1.0.1

--- libs/libgit2/libgit2.py
+++ libs/libgit2/libgit2.py
@@ -6,16 +6,17 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/libgit2/libgit2.git'
 
-        for ver in ['0.28.1']:
+        for ver in ['0.28.1', "1.0.1"]:
             self.targets[ver] = f"https://github.com/libgit2/libgit2/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"libgit2-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"libgit2-{ver}"
         self.patchToApply["0.28.1"] = [ ("libgit2-0.28.1-20190216.diff", 1)]
         self.targetDigests["0.28.1"] = (['0ca11048795b0d6338f2e57717370208c2c97ad66c6d5eac0c97a8827d13936b'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.0.1"] = (['1775427a6098f441ddbaa5bd4e9b8a043c7401e450ed761e69a415530fea81d2'], CraftHash.HashAlgorithm.SHA256)
         self.patchLevel["0.28.1"] = 3
 
         self.description = "a portable C library for accessing git repositories"
-        self.defaultTarget = '0.28.1'
+        self.defaultTarget = '1.0.1'
 
     def setDependencies(self):
         self.buildDependencies['dev-utils/pkg-config'] = 'default'
