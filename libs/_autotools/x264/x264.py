09a4c00d7c66cb98baca56d0465f33c81a9555da
Hannah von Reth <vonreth@kde.org>
2020-06-28 16:18:52 +0200

    x264: Don't depend on system ffmepg

--- libs/_autotools/x264/x264.py
+++ libs/_autotools/x264/x264.py
@@ -8,6 +8,7 @@ class subinfo(info.infoclass):
             self.targetInstSrc[ ver ] = f"x264-snapshot-{ver}"
         self.targetDigests['20180806-2245-stable'] = (['958e78e7563f0018285ebdbff563fb22db89b0abf3649d7e914abd9d50785fc6'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['20180119-2245'] = (['c9162e612f989c8d97c7a6bb3924a04f43d14221dcc983c69fb9ab12613c3669'], CraftHash.HashAlgorithm.SHA256)
+        self.patchLevel["20180119-2245"] = 1
         self.defaultTarget = '20180119-2245'
 
     def setDependencies(self):
@@ -19,7 +20,7 @@ from Package.AutoToolsPackageBase import *
 class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.configure.args = "--enable-shared"
+        self.subinfo.options.configure.args = "--enable-shared --disable-cli --disable-avs--disable-lavf --disable-swscale --disable-ffms --disable-gpac --enable-pic"
 
 
     def install(self):
