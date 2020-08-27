3d31fe30c5c7ebbe48907d74f12bfa3106259a39
Michael Reeves <reeves.87@gmail.com>
2020-08-27 01:39:38 +0000

    Update kdiff3.py

--- extragear/kdiff3/kdiff3.py
+++ extragear/kdiff3/kdiff3.py
@@ -37,8 +37,8 @@ class Package(CMakePackageBase):
         #Only attempt to install shell extention in standalone mode 
         if not isinstance(self, AppxPackager):
             #Windows app store has special requirements for the version format let craft deal with that.
-            if self.subinfo.buildTarget == "1.8":
-                self.defines["version"] = "1.8.4"
+            #if self.subinfo.buildTarget == "1.8":
+            #    self.defines["version"] = "1.8.4"
             
             self.defines["registry_hook"]=(r"""
     !define DIFF_EXT_CLSID "{34471FFB-4002-438b-8952-E4588D0C0FE9}"
