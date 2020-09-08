162d3fe1b03901658c0ca56a2ee7d14430f3f255
Mladen Milinkovic <maxrd2@smoothware.net>
2020-09-08 15:32:11 +0200

    libs/lapack: fixed windows mingw build
    Reviewers: #craft, vonreth
    
    Reviewed By: #craft, vonreth
    
    Subscribers: thiagosueto
    
    Differential Revision: https://phabricator.kde.org/D28545

--- libs/lapack/lapack.py
+++ libs/lapack/lapack.py
@@ -9,6 +9,7 @@ class subinfo(info.infoclass):
         self.displayName = "lapack"
         self.patchToApply['3.9.0'] = [
             ("0001-Restore-Missing-Prototypes.patch", 1), # https://github.com/Reference-LAPACK/lapack/commit/87536aa3.patch
+            ("0002-Fix-mingw-build.patch", 1),
         ]
 
     def setDependencies(self):
@@ -20,4 +21,8 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args = "-DCMAKE_SKIP_RPATH=ON -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DLAPACKE_WITH_TMG=ON -DCBLAS=ON -DBUILD_DEPRECATED=ON"
+        self.subinfo.options.configure.args = "-DCMAKE_SKIP_RPATH=ON -DBUILD_TESTING=OFF -DLAPACKE_WITH_TMG=ON -DCBLAS=ON -DBUILD_DEPRECATED=ON"
+        if OsUtils.isWin(): # mingw-w64 (gfortran) 8.1 includes only static libgfortran and fails to build dll
+            self.subinfo.options.configure.args = " -DBUILD_SHARED_LIBS=OFF"
+        else:
+            self.subinfo.options.configure.args = " -DBUILD_SHARED_LIBS=ON"
--- libs/sphinxbase/sphinxbase.py
+++ libs/sphinxbase/sphinxbase.py
@@ -7,6 +7,9 @@ class subinfo(info.infoclass):
         self.description = "Common library for sphinx speech recognition."
         self.webpage = "http://cmusphinx.sourceforge.net/"
         self.displayName = "SphinxBase"
+        self.patchToApply['057d423ddddea2351678abd0d29b5f1463a343c7'] = [
+            ("0001-fix-static-lapack-link.patch", 1),
+        ]
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
