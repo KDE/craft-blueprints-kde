0f23693b092e0e538132b7c718e545bac10f1af3
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2020-09-09 14:18:14 +0200

    Always import the most recent translation for RKWard, instead of cached translation import
    Merge request: https://invent.kde.org/packaging/craft-blueprints-kde/-/merge_requests/15 (squashed)

--- extragear/rkward/rkward.py
+++ extragear/rkward/rkward.py
@@ -9,7 +9,6 @@ class subinfo(info.infoclass):
         self.displayName = "RKWard"
 
     def setDependencies(self):
-        self.buildDependencies["extragear/rkward/rkward-translations"] = None
         self.runtimeDependencies["binary/r-base"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier3/ktexteditor"] = None
@@ -31,7 +30,6 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        self.translations = CraftPackageObject.get("extragear/rkward/rkward-translations").instance
 
         if OsUtils.isWin():
             if CraftCore.compiler.isX64():
@@ -42,17 +40,11 @@ class Package(CMakePackageBase):
         elif OsUtils.isMac():
             self.subinfo.options.configure.args = " -DR_EXECUTABLE=" + os.path.join(CraftCore.standardDirs.craftRoot(), "lib", "R", "R.framework", "Resources", "R")
 
-        if self.subinfo.hasSvnTarget:
-            self.subinfo.options.configure.args += f" -DTRANSLATION_SRC_DIR={OsUtils.toUnixPath(self.translations.sourceDir())}"
-
     def fetch(self):
-        # Temporary workaround for failure to pull due to local modification of ver.R. Remove the line below around June, 2018.
-        utils.deleteFile(os.path.join(self.checkoutDir(), "rkward", "rbackend", "rpackages", "rkward", "R", "ver.R"))
-
         if not CMakePackageBase.fetch(self):
             return False
         if self.subinfo.hasSvnTarget:
-            return self.translations.fetch(noop=False)
+            utils.system([sys.executable, os.path.join(self.checkoutDir(), "scripts", "import_translations.py")])
         return True
 
     def install(self):
