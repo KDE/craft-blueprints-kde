2568e4745f87a84dedd44eca7677c79a23277405
Hannah von Reth <vonreth@kde.org>
2020-09-24 10:02:15 +0200

    Clazy: 1.7

--- dev-utils/clazy/clazy.py
+++ dev-utils/clazy/clazy.py
@@ -13,12 +13,14 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/llvm-meta/clang"] = None
 
     def setTargets(self):
-        self.svnTargets['master'] = 'https://anongit.kde.org/clazy'
-        for ver in ["1.3", "1.4", "1.5", "1.6"]:
+        self.svnTargets["master"] = "https://invent.kde.org/sdk/clazy.git"
+        self.targetUpdatedRepoUrl["master"] = (["https://anongit.kde.org/clazy"], "https://invent.kde.org/sdk/clazy.git")
+
+        for ver in ["1.3", "1.4", "1.5", "1.6", "1.7"]:
             self.targets[ver] = f"https://download.kde.org/stable/clazy/{ver}/src/clazy-{ver}.tar.xz"
             self.targetDigestUrls[ver] = f"https://download.kde.org/stable/clazy/{ver}/src/clazy-{ver}.tar.xz.sha256"
             self.targetInstSrc[ver] = f"clazy-{ver}"
-        self.defaultTarget = '1.6'
+        self.defaultTarget = '1.7'
 
 
 class Package(CMakePackageBase):
