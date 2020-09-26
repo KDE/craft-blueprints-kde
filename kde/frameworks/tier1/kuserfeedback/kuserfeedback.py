375a02e290614ba62772411653c329560e2f9a3f
Christoph Cullmann <cullmann@kde.org>
2020-09-26 20:45:30 +0200

    try to package KUserFeedback

---
+++ kde/frameworks/tier1/kuserfeedback/kuserfeedback.py
@@ -0,0 +1,21 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        self.description = "KUserFeedback"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
