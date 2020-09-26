2a8936ac4276f414f811b4239338b56389731efc
Christoph Cullmann <cullmann@kde.org>
2020-09-26 20:56:35 +0200

    kuserfeedback not yet part of the normal releases

--- kde/frameworks/tier1/kuserfeedback/kuserfeedback.py
+++ kde/frameworks/tier1/kuserfeedback/kuserfeedback.py
@@ -3,7 +3,11 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.versionInfo.setDefaultValues()
+        self.targets['1.0.0'] = 'https://download.kde.org/stable/kuserfeedback/kuserfeedback-1.0.0.tar.xz'
+        self.targetDigests['1.0.0'] = (
+            ['5a2f53ebb4b99a280757ca32bd9b686a7764a726e7e4d8bafee33acbb44b9db7'], CraftHash.HashAlgorithm.SHA256)
+        self.targetInstSrc['1.0.0'] = 'kuserFeedback'
+        self.defaultTarget = '1.0.0'
 
         self.description = "KUserFeedback"
 
