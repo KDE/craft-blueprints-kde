dd929ed7305391d7f11693b43e5180460fd5c62c
Michael Reeves <reeves.87@gmail.com>
2020-09-27 23:50:48 -0400

    Turn on shellext

--- extragear/kdiff3/kdiff3.py
+++ extragear/kdiff3/kdiff3.py
@@ -37,16 +37,16 @@ class Package(CMakePackageBase):
         #Only attempt to install shell extention in standalone mode 
         if not isinstance(self, AppxPackager):
             if self.subinfo.buildTarget == "1.8":
-                self.defines["version"] = "1.8.4"
-            if 0 and self.subinfo.buildTarget == "master":
-                self.defines["registry_hook"]=(r"""
+                self.defines["version"] = "1.8.5"
+            
+            self.defines["registry_hook"]=(r"""
         !define DIFF_EXT_CLSID "{34471FFB-4002-438b-8952-E4588D0C0FE9}"
         !define DIFF_EXT_ID "Diff-ext for KDiff3"
         !define DIFF_EXT_DLL "kdiff3ext.dll"
         
         SetRegView 64
         WriteRegStr SHCTX "Software\Classes\CLSID\${DIFF_EXT_CLSID}" "" "${DIFF_EXT_ID}"
-        WriteRegStr SHCTX "Software\Classes\CLSID\${DIFF_EXT_CLSID}\InProcServer32" "" "$INSTDIR\bin\${DIFF_EXT_DLL}"
+        WriteRegStr SHCTX "Software\Classes\CLSID\${DIFF_EXT_CLSID}\InProcServer32" "" "$INSTDIR\bin\kf5\${DIFF_EXT_DLL}"
         WriteRegStr SHCTX "Software\Classes\CLSID\${DIFF_EXT_CLSID}\InProcServer32" "ThreadingModel" "Apartment"
         WriteRegStr SHCTX "Software\Classes\*\shellex\ContextMenuHandlers\${DIFF_EXT_ID}" "" "${DIFF_EXT_CLSID}"
         WriteRegStr SHCTX "Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Approved" "${DIFF_EXT_CLSID}" "${DIFF_EXT_ID}"
@@ -58,26 +58,26 @@ class Package(CMakePackageBase):
         WriteRegStr HKCU "${regkey}\diff-ext" "InstallDir" "$INSTDIR\bin"
         WriteRegStr HKCU "${regkey}\diff-ext" "diffcommand" "$INSTDIR\bin\kdiff3.exe"
                 """)
-                self.defines["un_sections"] = r"""
-                        Section "Un.Cleanup Regsistry"
-                            SetRegView 64
-                            DeleteRegKey SHCTX "Software\Classes\CLSID\${DIFF_EXT_CLSID}"
-                            DeleteRegKey SHCTX "Software\Classes\*\shellex\ContextMenuHandlers\${DIFF_EXT_ID}"
-                            DeleteRegKey SHCTX "Software\Classes\Folder\shellex\ContextMenuHandlers\${DIFF_EXT_ID}"
-                            DeleteRegKey SHCTX "Software\Classes\Directory\shellex\ContextMenuHandlers\${DIFF_EXT_ID}"
-                            DeleteRegValue SHCTX "Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Approved" "${DIFF_EXT_CLSID}"
-                            SetRegView 32
-                            ;remove old diff-ext settings
-                            DeleteRegKey HKCU  "Software\KDiff3"
-                            ;Maybe left behind due to a bug in previous installers.
-                            DeleteRegKey SHCTX  "Software\KDE\KDiff3"
-                            DeleteRegKey /ifempty SHCTX  "Software\KDE\"
-                        SectionEnd
-                        """
+            self.defines["un_sections"] = r"""
+                    Section "Un.Cleanup Regsistry"
+                        SetRegView 64
+                        DeleteRegKey SHCTX "Software\Classes\CLSID\${DIFF_EXT_CLSID}"
+                        DeleteRegKey SHCTX "Software\Classes\*\shellex\ContextMenuHandlers\${DIFF_EXT_ID}"
+                        DeleteRegKey SHCTX "Software\Classes\Folder\shellex\ContextMenuHandlers\${DIFF_EXT_ID}"
+                        DeleteRegKey SHCTX "Software\Classes\Directory\shellex\ContextMenuHandlers\${DIFF_EXT_ID}"
+                        DeleteRegValue SHCTX "Software\Microsoft\Windows\CurrentVersion\Shell Extensions\Approved" "${DIFF_EXT_CLSID}"
+                        SetRegView 32
+                        ;remove old diff-ext settings
+                        DeleteRegKey HKCU  "Software\KDiff3"
+                        ;Maybe left behind due to a bug in previous installers.
+                        DeleteRegKey SHCTX  "Software\KDE\KDiff3"
+                        DeleteRegKey /ifempty SHCTX  "Software\KDE\"
+                    SectionEnd
+                    """
         else:
 	        #Windows app store has special requirements for the version format
             #Craft attempts to alter the second and third number so we have to adjust to craft's logic as well.
-            self.defines["version"] = "1.0.84"
+            self.defines["version"] = "1.0.85"
                         
             self.defines["un_sections"] = r"""
             Section "Un.Cleanup Regsistry"
