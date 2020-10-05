667819041e73a295ec0178abbe998ae0ed67e5b9
Vincent Pinon <vpinon@kde.org>
2020-10-05 14:55:22 -0700

    Kdenlive: add cairo for frei0r plugins

--- libs/frei0r-plugins/frei0r-plugins.py
+++ libs/frei0r-plugins/frei0r-plugins.py
@@ -12,7 +12,7 @@ class subinfo(info.infoclass):
             self.targetInstSrc[ ver ] = f"frei0r-{ver}"
         self.targetDigests['1.6.1'] = (['dae0ca623c83173788ce4fc74cb67ac7e50cf33a4412ee3d33bed284da1a8437'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['1.7.0'] = (['6f7cf95ea2257687cc31db0ed9c9bc0eec152e953d515f346eabec048ed2b29d'], CraftHash.HashAlgorithm.SHA256)
-        self.patchLevel['master'] = 2
+        self.patchLevel['master'] = 20201005
 
         self.svnTargets['master'] = 'https://github.com/dyne/frei0r.git'
         self.defaultTarget = 'master'
@@ -21,7 +21,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/gavl"] = None
         self.runtimeDependencies["libs/opencv/opencv"] = None
-        #self.runtimeDependencies["libs/cairo"] = None
+        self.runtimeDependencies["libs/cairo"] = None
 
 from Package.CMakePackageBase import *
 
