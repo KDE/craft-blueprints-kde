9d0352d7fc456edfd54d394277e5986645357685
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2020-10-09 20:04:49 +0200

    Add qtmqtt

---
+++ qt-libs/qtmqtt/qtmqtt.py
@@ -0,0 +1,20 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        for ver in ['5.12.9','5.13.2','5.14.0','5.14.1','5.14.2','5.15.0','5.15.1']:
+            self.svnTargets[ver] = f"https://github.com/qt/qtmqtt|{ver}"
+
+        self.defaultTarget = CraftPackageObject.get("libs/qt5/qtdoc").version
+        self.description = "QtMQTT"
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+
+from Package.Qt5CorePackageBase import *
+
+class Package(Qt5CorePackageBase):
+    def __init__(self, **args):
+        Qt5CorePackageBase.__init__(self)
