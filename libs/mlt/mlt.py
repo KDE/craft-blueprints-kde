44f6a76531273a0d7d33dc00f46f5f781308fb2a
Vincent Pinon <vpinon@kde.org>
2020-10-09 04:49:09 -0700

    MLT: make cmake to find ladspa.h

--- libs/mlt/mlt.py
+++ libs/mlt/mlt.py
@@ -38,4 +38,5 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__( self, **args ):
         CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.args = f"-DCMAKE_C_FLAGS=\"-isystem {CraftCore.standardDirs.craftRoot()}/include\""
 
