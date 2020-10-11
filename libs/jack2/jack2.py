5b0171e35389dbb640ba006494bb9772a48ced3c
Vincent Pinon <vpinon@kde.org>
2020-10-11 09:07:31 +0200

    jack2: patchlevel++

--- libs/jack2/jack2.py
+++ libs/jack2/jack2.py
@@ -5,11 +5,14 @@ class subinfo(info.infoclass):
         self.description = 'JACK Audio Connection Kit'
         self.webpage = 'https://jackaudio.org/'
         self.svnTargets['master'] = 'https://github.com/jackaudio/jack2'
+        self.targetInstSrc['master'] = 'common'
+        self.patchToApply['master'] = ('jack2-cmake.patch', 0)
         for ver in ['1.9.14']:
             self.targets[ver] = f"https://github.com/jackaudio/jack2/archive/v{ver}.tar.gz"
             self.targetInstSrc[ver] = "jack2-" + ver + '/common'
             self.patchToApply[ver] = ('jack2-cmake.patch', 1)
         self.targetDigests['1.9.14'] = (['a20a32366780c0061fd58fbb5f09e514ea9b7ce6e53b080a44b11a558a83217c'], CraftHash.HashAlgorithm.SHA256)
+        self.patchLevel['1.9.14'] = 1
         self.defaultTarget = '1.9.14'
 
     def setDependencies( self ):
