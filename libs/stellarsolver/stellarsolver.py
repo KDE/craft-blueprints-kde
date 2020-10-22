8c9b788eba08799d3b591274539971b12176773b
Jasem Mutlaq <mutlaqja@ikarustech.com>
2020-10-22 17:11:56 +0300

    Disable configure options for Mac since this causes an issue on binary factory

--- libs/stellarsolver/stellarsolver.py
+++ libs/stellarsolver/stellarsolver.py
@@ -25,6 +25,6 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        root = CraftCore.standardDirs.craftRoot()
-        craftLibDir = os.path.join(root,  'lib')
-        self.subinfo.options.configure.args += f" -DCMAKE_MACOSX_RPATH=1 -DCMAKE_INSTALL_RPATH={craftLibDir}"
+        #root = CraftCore.standardDirs.craftRoot()
+        #craftLibDir = os.path.join(root,  'lib')
+        #self.subinfo.options.configure.args += f" -DCMAKE_MACOSX_RPATH=1 -DCMAKE_INSTALL_RPATH={craftLibDir}"
