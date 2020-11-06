528b10cdb7f3edb520cdf587542849cb05d1905d
Laurent Montel <montel@kde.org>
2020-11-06 16:32:12 +0100

    Add kuserfeedback here too

--- kde/pim/akregator/akregator.py
+++ kde/pim/akregator/akregator.py
@@ -29,6 +29,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/pim/syndication"] = None
         self.runtimeDependencies["kde/pim/akonadi-mime"] = None
         self.runtimeDependencies["kde/pim/pimcommon"] = None
+        self.runtimeDependencies["kde/unreleased/kuserfeedback"] = None
 
 
 from Package.CMakePackageBase import *
