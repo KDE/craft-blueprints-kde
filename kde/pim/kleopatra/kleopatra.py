fcc607b3d362a0b2eadb5c078819871fdb1534ae
Laurent Montel <montel@kde.org>
2020-11-08 14:19:43 +0100

    Add build dep

--- kde/pim/kleopatra/kleopatra.py
+++ kde/pim/kleopatra/kleopatra.py
@@ -25,7 +25,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/pim/libkleo"] = None
         self.runtimeDependencies["kde/pim/kmime"] = None
         self.runtimeDependencies["libs/gpgme/gpgmepp"] = None
-        self.runtimeDependencies["libs/assuan2"] = None
+        self.buildDependencies["libs/assuan2"] = None
 
 
 
