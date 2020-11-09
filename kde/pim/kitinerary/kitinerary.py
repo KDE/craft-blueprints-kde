0f61cec766181a4de81a759c881a3a7c47267f2d
Laurent Montel <montel@kde.org>
2020-11-09 13:27:58 +0100

    Add libs/zxing-cpp dep

--- kde/pim/kitinerary/kitinerary.py
+++ kde/pim/kitinerary/kitinerary.py
@@ -19,6 +19,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["qt-libs/poppler"] = None
         self.runtimeDependencies["libs/libxml2"] = None
         self.runtimeDependencies["libs/zlib"] = None
+        self.runtimeDependencies["libs/zxing-cpp"] = None
 
 
 from Package.CMakePackageBase import *
