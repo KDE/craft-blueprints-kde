7dd75243ae3bdb0d797fb0ee7bee73022133c606
Laurent Montel <montel@kde.org>
2020-11-13 19:45:33 +0100

    it doesn"t depend against kwallet now

--- kde/pim/akonadi-import-wizard/akonadi-import-wizard.py
+++ kde/pim/akonadi-import-wizard/akonadi-import-wizard.py
@@ -11,7 +11,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kwallet"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kauth"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
