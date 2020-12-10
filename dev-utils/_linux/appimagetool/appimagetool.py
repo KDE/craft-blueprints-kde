21bd91853119c9f9e498e36a490978a33835cbbf
Milian Wolff <mail@milianw.de>
2020-12-10 12:25:39 +0100

    Add blueprint for appimagetool
    Sometimes it's useful to run the appimage tool separately from
    linuxdeploy, e.g. to cleanup the AppDir that gets created by
    linuxdeploy before creating the appimage itself.

---
+++ dev-utils/_linux/appimagetool/appimagetool.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import stat
+
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.targets["continous"] = ["https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"]
+        self.targetInstallPath["continous"] = "dev-utils/bin"
+        self.description = "Converts an AppDir into a self-mounting filesystem image"
+        self.webpage = "https://github.com/AppImage/AppImageKit"
+        self.defaultTarget = "continous"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+
+
+from Package.BinaryPackageBase import *
+
+
+class Package(BinaryPackageBase):
+    def __init__(self, **args):
+        BinaryPackageBase.__init__(self)
+
+
+    def unpack(self):
+        return True
+
+    def install(self):
+        utils.createDir(self.installDir())
+        for f in self.localFilePath():
+            src = Path(f)
+            dest = Path(self.installDir()) / src.name
+            # we move the files so that on a reinstall the continous target gets redownloaded
+            if not utils.moveFile(src, dest):
+                return False
+            dest.chmod(dest.stat().st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+        return True
