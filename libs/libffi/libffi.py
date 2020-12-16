46b69305ef6b41dc8689d46fb176d8871f46e7d5
Hannah von Reth <vonreth@kde.org>
2020-12-16 11:16:06 +0100

    ffi 3.3

--- libs/libffi/libffi.py
+++ libs/libffi/libffi.py
@@ -1,61 +1,26 @@
 import info
 
-from Package.MSBuildPackageBase import MSBuildPackageBase
+from Package.CMakePackageBase import CMakePackageBase
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["3.2.1"]:
-            if OsUtils.isWin():
-                self.targets[ver] = f"https://github.com/winlibs/libffi/archive/libffi-{ver}.tar.gz"
-                self.archiveNames[ver] = f"libffi-libffi-{ver}.tar.gz"
-                self.targetInstSrc[ver] = f"libffi-libffi-{ver}"
-            else:
-                self.targets[ver] = f"ftp://sourceware.org/pub/libffi/libffi-{ver}.tar.gz"
-                self.targetInstSrc[ver] = f"libffi-{ver}"
+        for ver in ["3.3"]:
+            self.targets[ver] = f"https://github.com/libffi/libffi/releases/download/v{ver}/libffi-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"libffi-{ver}"
+        if not CraftCore.compiler.isGCCLike():
+            self.patchToApply["3.3"] = [("535.patch", 1)] # https://github.com/libffi/libffi/pull/535
 
-        self.patchLevel["3.2.1"] = 2
-        if OsUtils.isWin():
-            self.targetDigests['3.2.1'] = (
-                ['9f8e1133c6b9f72b73943103414707a1970e2e9b1d332c3df0d35dac1d9917e5'], CraftHash.HashAlgorithm.SHA256)
-        else:
-            self.targetDigests['3.2.1'] = (
-                ['d06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722ca37'], CraftHash.HashAlgorithm.SHA256)
-
-
-        self.defaultTarget = "3.2.1"
+        self.defaultTarget = "3.3"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
-        if CraftCore.compiler.isMinGW():
-            self.buildDependencies["dev-utils/msys"] = None
+        self.buildDependencies["dev-utils/msys"] = None
 
 
-class PackageCMake(MSBuildPackageBase):
+class PackageCMake(CMakePackageBase):
     def __init__(self, **args):
-        MSBuildPackageBase.__init__(self)
-
-        folderSuffix = "x64"
-        if CraftCore.compiler.isX86():
-            folderSuffix = "x86"
-        self.subinfo.options.configure.projectFile = os.path.join(self.sourceDir(), "win32", f"vc14_{folderSuffix}",
-                                                                  "libffi-msvc.sln")
-
-    def install(self):
-        if not MSBuildPackageBase.install(self, installHeaders=False):
-            return False
-        utils.copyFile(os.path.join(self.sourceDir(), "include", "ffi.h"),
-                       os.path.join(self.imageDir(), "include", "ffi.h"), False)
-        utils.copyFile(os.path.join(self.sourceDir(), "include", "ffi_common.h"),
-                       os.path.join(self.imageDir(), "include", "ffi_common.h"), False)
-        if CraftCore.compiler.isX86():
-            utils.copyFile(os.path.join(self.sourceDir(), "src", "x86", "ffitarget.h"),
-                           os.path.join(self.imageDir(), "include", "ffitarget.h"), False)
-        else:
-            utils.copyFile(os.path.join(self.sourceDir(), "src", "ia64", "ffitarget.h"),
-                           os.path.join(self.imageDir(), "include", "ffitarget.h"), False)
-        return True
-
+        CMakePackageBase.__init__(self)
 
 from Package.AutoToolsPackageBase import *
 
@@ -63,7 +28,7 @@ from Package.AutoToolsPackageBase import *
 class PackageAutoTools(AutoToolsPackageBase):
     def __init__(self):
         AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.configure.args = "--enable-shared --disable-static"
+        self.subinfo.options.configure.args += ["--enable-shared", "--disable-static"]
 
 
 if CraftCore.compiler.isGCCLike():
