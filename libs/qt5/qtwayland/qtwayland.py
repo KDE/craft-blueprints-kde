4492780b11c5d53e472b3a03e75d36f0d505ecab
Milian Wolff <mail@milianw.de>
2020-12-22 21:13:36 +0100

    Add blueprint for qtwayland

---
+++ libs/qt5/qtwayland/qtwayland.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import info
+
+
+class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.Linux
+
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+
+
+from Package.Qt5CorePackageBase import *
+
+
+class QtPackage(Qt5CorePackageBase):
+    def __init__(self, **args):
+        Qt5CorePackageBase.__init__(self)
+
+
+class Package(Qt5CoreSdkPackageBase):
+    def __init__(self):
+        Qt5CoreSdkPackageBase.__init__(self, classA=QtPackage)
