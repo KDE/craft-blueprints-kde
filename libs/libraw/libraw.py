b8bdd630e6a265bea9bf0a46547fbd7a3eddcea3
Jasem Mutlaq <mutlaqja@ikarustech.com>
2021-01-01 11:09:59 +0300

    Add 0.20.2 release

--- libs/libraw/libraw.py
+++ libs/libraw/libraw.py
@@ -10,6 +10,12 @@ class subinfo(info.infoclass):
         self.patchToApply['0.18.13'] = [("LibRaw-0.18.13-20180720.diff", 1)]#https://github.com/LibRaw/LibRaw-cmake/
         self.targetDigests['0.18.13'] = (['cb1f9d0d1fabc8967d501d95c05d2b53d97a2b917345c66553b1abbea06757ca'], CraftHash.HashAlgorithm.SHA256)
         self.patchLevel["0.18.13"] = 1
+        
+        # 0.20.2
+        self.targets['0.20.2'] = "https://www.libraw.org/data/LibRaw-0.20.2.tar.gz"
+        self.archiveNames['0.20.2'] = "LibRaw-0.20.2.tar.gz"
+        self.targetInstSrc['0.20.2'] = "LibRaw-0.20.2"        
+        self.targetDigests['0.20.2'] = (['dc1b486c2003435733043e4e05273477326e51c3ea554c6864a4eafaff1004a6'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "LibRaw is a library for reading RAW files obtained from digital photo cameras (CRW/CR2, NEF, RAF, DNG, and others)."
 
