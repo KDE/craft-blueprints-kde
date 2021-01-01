73d99d0815f2e9855eedfe35f5089007786a6e74
Jasem Mutlaq <mutlaqja@ikarustech.com>
2021-01-01 11:10:13 +0300

    Make KStars depend on libraw 0.20.2

--- kde/applications/kstars/kstars.py
+++ kde/applications/kstars/kstars.py
@@ -37,7 +37,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/cfitsio"] = None
         self.runtimeDependencies["libs/wcslib"] = None
         self.runtimeDependencies["libs/indiclient"] = None
-        self.runtimeDependencies["libs/libraw"] = None
+        self.runtimeDependencies["libs/libraw"] = "0.20.2"
         self.runtimeDependencies["libs/gsl"] = None
         self.runtimeDependencies["libs/stellarsolver"] = None
         self.runtimeDependencies["qt-libs/qtkeychain"] = None
