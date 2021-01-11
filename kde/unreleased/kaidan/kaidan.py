b7a2127c7e4e607cc27f6dcb631d3abb1234fbda
Jonah Brüchert <jbb.prv@gmx.de>
2021-01-11 20:28:23 +0100

    Kaidan: Add breeze dep

--- kde/unreleased/kaidan/kaidan.py
+++ kde/unreleased/kaidan/kaidan.py
@@ -28,6 +28,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
         self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
         self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+        self.runtimeDependencies["kde/plasma/breeze"] = None
 
 from Package.CMakePackageBase import *
 
