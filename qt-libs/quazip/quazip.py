53c38e6172b714dcba8fb181fd34df31aa9bf523
Jonah Brüchert <jbb.prv@gmx.de>
2021-01-11 23:09:07 +0100

    Package quazip

---
+++ qt-libs/quazip/quazip.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/zlib"] = None
+
+    def setTargets(self):
+        self.svnTargets["master"] = "https://github.com/stachenov/quazip"
+        for ver in ["1.1"]:
+            self.targets[ver] = f"https://github.com/stachenov/quazip/archive/v{ver}.tar.gz"
+            self.archiveNames[ver] = f"quazip-v{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"quazip-{ver}"
+        self.targetDigests["1.1"] = (
+            ["54edce9c11371762bd4f0003c2937b5d8806a2752dd9c0fd9085e90792612ad0"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.1"
+
+        self.description = "Qt/C++ wrapper over minizip"
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **args):
+        CMakePackageBase.__init__(self)
