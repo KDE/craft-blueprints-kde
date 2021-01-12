2a0819032a597b48c484248d8c9549b8fde256fe
Christoph Cullmann <cullmann@kde.org>
2021-01-12 19:48:19 +0100

    try to fix url

--- data/poppler-data/poppler-data.py
+++ data/poppler-data/poppler-data.py
@@ -9,7 +9,7 @@ class subinfo(info.infoclass):
         # use poppler data matching the latest poppler release used in poppler.py
         v = '0.4.10'
         self.defaultTarget = v
-        self.targets[v] = 'http://poppler.freedesktop.org/poppler-data-' + v + '.tar.gz'
+        self.targets[v] = 'https://poppler.freedesktop.org/poppler-data-' + v + '.tar.gz'
         self.targetInstSrc[v] = 'poppler-data-' + v
         self.targetDigests[v] = (['6e2fcef66ec8c44625f94292ccf8af9f1d918b410d5aa69c274ce67387967b30'], CraftHash.HashAlgorithm.SHA256)
 
