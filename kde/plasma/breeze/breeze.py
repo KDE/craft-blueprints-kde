5f2d32c119715bddedb43bff13aa9cac947a45f7
Nicolas Fella <nicolas.fella@gmx.de>
2021-01-26 23:10:35 +0100

    [breeze] Add missing kpackage dep

--- kde/plasma/breeze/breeze.py
+++ kde/plasma/breeze/breeze.py
@@ -16,6 +16,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kguiaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kpackage"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
         if not OsUtils.isWin():
             self.runtimeDependencies["kde/frameworks/tier4/frameworkintegration"] = None
