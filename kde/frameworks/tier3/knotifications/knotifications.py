e11c41ec3218549c406ba68de18de6d867691760
Nicolas Fella <nicolas.fella@gmx.de>
2021-01-26 22:42:49 +0100

    [knotifications] Remove unused dependencies

--- kde/frameworks/tier3/knotifications/knotifications.py
+++ kde/frameworks/tier3/knotifications/knotifications.py
@@ -10,10 +10,7 @@ class subinfo(info.infoclass):
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt5/qtspeech"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kservice"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
-        self.runtimeDependencies["kde/frameworks/tier1/kcodecs"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["qt-libs/phonon"] = None
         if OsUtils.isMac():
