ad82facb9bace0b1c7918ebc3a92ad71ba0e7a24
Nicolas Fella <nicolas.fella@gmx.de>
2021-01-26 16:05:05 +0100

    [neochat] Add qqc2-desktop-style, breeze and breeze-icons

--- extragear/neochat/neochat.py
+++ extragear/neochat/neochat.py
@@ -25,6 +25,9 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["qt-libs/libquotient"] = None
         self.runtimeDependencies["libs/cmark"] = None
         self.runtimeDependencies["kde/libs/kquickimageeditor"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+        self.runtimeDependencies["kde/plasma/breeze"] = None
 
 
 class Package(CMakePackageBase):
