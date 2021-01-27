d28dd91c6bde631f56afab30379ae2c74d100769
gitdev mod <djdomics@gmail.com>
2021-01-27 21:25:10 +0000

    Add Ktechlab

---
+++ extragear/ktechlab/ktechlab.py
@@ -0,0 +1,29 @@
+import info
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['master'] = "https://invent.kde.org/sdk/ktechlab.git"
+        self.description = "IDE for microcontrollers and electronics."
+        self.displayName = "ktechlab"
+        self.webpage = "https://userbase.kde.org/KTechlab"
+        self.defaultTarget = 'master'
+
+    def setDependencies(self):
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtserialport"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kparts"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/ktexteditor"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/ktextwidgets"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
