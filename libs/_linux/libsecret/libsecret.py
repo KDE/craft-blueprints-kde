ab0c699b10e1a5e42ced9cbdfb716a34e5269345
Hannah von Reth <vonreth@kde.org>
2021-01-28 15:42:29 +0100

    Add libsecret

---
+++ libs/_linux/libsecret/libsecret.py
@@ -0,0 +1,25 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ["0.20.4"]:
+            self.targets[ver] = f"https://gitlab.gnome.org/GNOME/libsecret/-/archive/{ver}/libsecret-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"libsecret-{ver}"
+        self.targetDigests['0.20.4'] = (['ca34e69b210df221ae5da6692c2cb15ef169bb4daf42e204442f24fdb0520d4b'], CraftHash.HashAlgorithm.SHA256)
+
+        self.description = "A GObject-based library for accessing the Secret Service API of the freedesktop.org project, a cross-desktop effort to access passwords, tokens and other types of secrets."
+        
+        self.defaultTarget = "0.20.4"
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/glib"] = None
+        self.runtimeDependencies["libs/gcrypt"] = None
+
+from Package.MesonPackageBase import *
+
+class Package(MesonPackageBase):
+    def __init__(self):
+        MesonPackageBase.__init__(self)
+        self.subinfo.options.configure.args += ["-Dvapi=false", "-Dgtk_doc=false", "-Dintrospection=false"]
+        
