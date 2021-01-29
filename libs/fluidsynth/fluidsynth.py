fe51913e3967d66976083a3a48057cbe38907465
Hannah von Reth <vonreth@kde.org>
2021-01-29 10:28:59 +0100

    Update fluidsynth to 2.1.6

--- libs/fluidsynth/fluidsynth.py
+++ libs/fluidsynth/fluidsynth.py
@@ -3,21 +3,21 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['1.1.6']:
-            self.targets[ver] = "http://downloads.sourceforge.net/sourceforge/fluidsynth/fluidsynth-%s.tar.bz2" % ver
-            self.targetInstSrc[ver] = 'fluidsynth-%s' % ver
-        self.targetDigests['1.1.6'] = (
-            ['d28b47dfbf7f8e426902ae7fa2981d821fbf84f41da9e1b85be933d2d748f601'], CraftHash.HashAlgorithm.SHA256)
-        self.patchToApply['1.1.6'] = [('fluidsynth-1.1.6-20160908.diff', 1)]
+        for ver in ["2.1.6"]:
+            self.targets[ver] = f"https://github.com/FluidSynth/fluidsynth/archive/v{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"fluidsynth-{ver}"
+        self.targetDigests['2.1.6'] = (['328fc290b5358544d8dea573f81cb1e97806bdf49e8507db067621242f3f0b8a'], CraftHash.HashAlgorithm.SHA256)
+        self.patchToApply["2.1.6"] = [("fluidsynth-2.1.6-20210129.diff", 1)]
 
         self.description = "FluidSynth is a real-time software synthesizer based on the SoundFont 2 specifications and has reached widespread distribution."
         self.webpage = "http://www.fluidsynth.org/"
-        self.defaultTarget = '1.1.6'
+        self.defaultTarget = '2.1.6'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["dev-utils/pkg-config"] = None
         self.runtimeDependencies["libs/glib"] = None
+        self.runtimeDependencies["libs/libsndfile"] = None
 
 
 from Package.CMakePackageBase import *
@@ -26,5 +26,5 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        # dbus support needs a small patch on windows but I have no idea why you would wan't dbus here
+        # dbus support needs a small patch on windows but I have no idea why you would want dbus here
         self.subinfo.options.configure.args = "-DLIB_SUFFIX='' -Denable-dbus=OFF"
