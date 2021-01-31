0f189c562986a925426bcc7e422bb4d46e7fc390
Nicolas Fella <nicolas.fella@gmx.de>
2021-01-31 20:22:51 +0100

    Update libquotient

--- qt-libs/libquotient/libquotient.py
+++ qt-libs/libquotient/libquotient.py
@@ -5,14 +5,14 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/quotient-im/libQuotient.git'
-        self.svnTargets['2021.01.26'] = 'https://github.com/quotient-im/libQuotient.git||ba1d3bda99c04f8783d1a5266f0fa7700ef3eb18'
+        self.svnTargets['2021.01.31'] = 'https://github.com/quotient-im/libQuotient.git||3fbd9ca775caeca4a28aeb48f5c8db013659c263'
 
         for ver in ['0.6.4']:
             self.targets[ver] = 'https://github.com/quotient-im/libQuotient/archive/%s.tar.gz' % ver
             self.archiveNames[ver] = 'libQuotient-%s.tar.gz' % ver
             self.targetInstSrc[ver] = 'libQuotient-%s' % ver
 
-        self.defaultTarget = '2021.01.26'
+        self.defaultTarget = '2021.01.31'
         self.description = "A Qt5 library to write cross-platform clients for Matrix"
 
     def setDependencies(self):
