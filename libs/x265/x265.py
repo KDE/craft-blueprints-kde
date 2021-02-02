7e14c8df8abaee6c09cdaa3992ac7852952ce821
Hannah von Reth <vonreth@kde.org>
2021-02-02 10:28:46 +0100

    Fix url

--- libs/x265/x265.py
+++ libs/x265/x265.py
@@ -3,7 +3,7 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ['2.9']:
-            self.targets[ver] = f"https://bitbucket.org/multicoreware/x265/downloads/x265_{ver}.tar.gz"
+            self.targets[ver] = f"https://bitbucket.org/multicoreware/x265_git/downloads/x265_{ver}.tar.gz"
             self.targetInstSrc[ver] = f"x265_{ver}/source"
         self.targetDigests['2.9'] = (['ebae687c84a39f54b995417c52a2fdde65a4e2e7ebac5730d251471304b91024'], CraftHash.HashAlgorithm.SHA256)
         self.patchToApply['2.9'] = [('mingw-no-pdb.patch', 1)]
