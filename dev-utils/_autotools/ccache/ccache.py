13d2a37f36ccaecd61835dbecad7322bccbf3ef2
Hannah von Reth <vonreth@kde.org>
2021-02-02 10:24:27 +0100

    ccache v4.2

--- dev-utils/_autotools/ccache/ccache.py
+++ dev-utils/_autotools/ccache/ccache.py
@@ -7,16 +7,17 @@ class subinfo(info.infoclass):
         self.svnTargets['master'] = "https://github.com/ccache/ccache.git"
         self.targetInstallPath["master"] = "dev-utils"
 
-        for ver in ["4.0", "4.1"]:
+        for ver in ["4.0", "4.1", "4.2"]:
             self.targets[ver] = f"https://github.com/ccache/ccache/releases/download/v{ver}/ccache-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"ccache-{ver}"
             self.targetInstallPath[ver] = "dev-utils"
         self.targetDigests["4.0"] = (['ac97af86679028ebc8555c99318352588ff50f515fc3a7f8ed21a8ad367e3d45'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["4.1"] = (['cdeefb827b3eef3b42b5454858123881a4a90abbd46cc72cf8c20b3bd039deb7'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["4.2"] = (['dbf139ff32031b54cb47f2d7983269f328df14b5a427882f89f7721e5c411b7e'], CraftHash.HashAlgorithm.SHA256)
         self.patchLevel["4.1"] = 1
 
         self.webpage = "https://ccache.dev/"
-        self.defaultTarget = "4.1"
+        self.defaultTarget = "4.2"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
