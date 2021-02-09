174df69706af142dc9150374b274f9197f153601
Vincent Pinon <vpinon@kde.org>
2021-02-09 14:56:59 -0800

    Fix TRE failure (skip po)

--- libs/_autotools/tre/tre.py
+++ libs/_autotools/tre/tre.py
@@ -4,10 +4,13 @@ class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ["0.8.0"]:
             self.targets[ver] = f"https://laurikari.net/tre/tre-{ver}.tar.bz2"
-            self.targetInstSrc[ ver ] = "tre-" + ver
-        #self.targetDigests['0.8.0'] = ([''], CraftHash.HashAlgorithm.SHA256)
+            self.targetInstSrc[ver] = "tre-" + ver
+            self.patchToApply[ver] = ('tre-skip-po.patch', 1)
+        self.targetDigests['0.8.0'] = (['8dc642c2cde02b2dac6802cdbe2cda201daf79c4ebcbb3ea133915edf1636658'], CraftHash.HashAlgorithm.SHA256)
+        self.svnTargets["master"] = "https://github.com/laurikari/tre.git"
+        self.patchToApply['master'] = ('tre-skip-po.patch', 1)
         self.description = "portable regex matching library"
-        self.defaultTarget = '0.8.0'
+        self.defaultTarget = 'master'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
