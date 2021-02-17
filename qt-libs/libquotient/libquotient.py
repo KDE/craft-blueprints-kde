67b4a5cfde9093d7af931e93ac5356814adacc20
Tobias Fella <fella@posteo.de>
2021-02-17 23:48:11 +0100

    LibQuotient: Build 0.6.4 by default
    NeoChat doesn't need master anymore

--- qt-libs/libquotient/libquotient.py
+++ qt-libs/libquotient/libquotient.py
@@ -5,14 +5,15 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/quotient-im/libQuotient.git'
-        self.svnTargets['2021.01.31'] = 'https://github.com/quotient-im/libQuotient.git||3fbd9ca775caeca4a28aeb48f5c8db013659c263'
 
         for ver in ['0.6.4']:
             self.targets[ver] = 'https://github.com/quotient-im/libQuotient/archive/%s.tar.gz' % ver
             self.archiveNames[ver] = 'libQuotient-%s.tar.gz' % ver
             self.targetInstSrc[ver] = 'libQuotient-%s' % ver
 
-        self.defaultTarget = '2021.01.31'
+        self.targetDigests['0.6.4'] = (['4c43a68c144d41e3826f2e1ce006760129694b79d8d7f302dea81328c6a663ce'], CraftHash.HashAlgorithm.SHA256)
+
+        self.defaultTarget = '0.6.4'
         self.description = "A Qt5 library to write cross-platform clients for Matrix"
 
     def setDependencies(self):
