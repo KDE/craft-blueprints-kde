fd4d018ec329ca26ac290ceb7de1a4758343cea1
Tobias Fella <fella@posteo.de>
2021-03-01 20:57:55 +0100

    LibQuotient: update to 0.6.5

--- qt-libs/libquotient/libquotient.py
+++ qt-libs/libquotient/libquotient.py
@@ -6,14 +6,14 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/quotient-im/libQuotient.git'
 
-        for ver in ['0.6.4']:
+        for ver in ['0.6.5']:
             self.targets[ver] = 'https://github.com/quotient-im/libQuotient/archive/%s.tar.gz' % ver
             self.archiveNames[ver] = 'libQuotient-%s.tar.gz' % ver
             self.targetInstSrc[ver] = 'libQuotient-%s' % ver
 
-        self.targetDigests['0.6.4'] = (['4c43a68c144d41e3826f2e1ce006760129694b79d8d7f302dea81328c6a663ce'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['0.6.5'] = (['a22738fd6fd90c662174ca31348934c75f29fdbfcf7d86f471d2fcbf628c36ea'], CraftHash.HashAlgorithm.SHA256)
 
-        self.defaultTarget = '0.6.4'
+        self.defaultTarget = '0.6.5'
         self.description = "A Qt5 library to write cross-platform clients for Matrix"
 
     def setDependencies(self):
