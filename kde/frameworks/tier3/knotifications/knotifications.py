a1b916940499332ae0e3b8d0b327496b2aa4fb2c
Volker Krause <vkrause@kde.org>
2021-04-02 21:00:53 +0200

    Allow to build KNotifications on Android

--- kde/frameworks/tier3/knotifications/knotifications.py
+++ kde/frameworks/tier3/knotifications/knotifications.py
@@ -8,11 +8,12 @@ class subinfo(info.infoclass):
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
-        self.runtimeDependencies["libs/qt5/qtspeech"] = None
-        self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
-        self.runtimeDependencies["qt-libs/phonon"] = None
+        if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["libs/qt5/qtspeech"] = None
+            self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
+            self.runtimeDependencies["qt-libs/phonon"] = None
         if OsUtils.isMac():
             self.runtimeDependencies["libs/qt5/qtmacextras"] = None
         if OsUtils.isWin():
