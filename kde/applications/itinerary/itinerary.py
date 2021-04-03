721bea1e6c434f0eae49cf4d69affbaa3cffe6ad
Volker Krause <vkrause@kde.org>
2021-04-03 12:10:47 +0200

    Add KDE Itinerary

---
+++ kde/applications/itinerary/itinerary.py
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2021 Volker Krause <vkrause@kde.org>
+
+import info
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues(gitUrl = "https://invent.kde.org/pim/itinerary.git")
+        self.description = "Digital travel assistant app"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/openssl"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt5/qtlocation"] = None
+        self.runtimeDependencies["libs/qt5/qtandroidextras"] = None
+        self.runtimeDependencies["libs/qt5/qtsvg"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/prison"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
+        self.runtimeDependencies["kde/libs/kosmindoormap"] = None
+        self.runtimeDependencies["kde/libs/kpublictransport"] = None
+        self.runtimeDependencies["kde/pim/kitinerary"] = None
+        self.runtimeDependencies["kde/pim/kpkpass"] = None
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
