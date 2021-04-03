5763aea0c3b8bf550b7a96a2ce19cbd6d874bd98
Volker Krause <vkrause@kde.org>
2021-04-03 12:10:19 +0200

    Add KOpeningHours
    Will be released as part of 21.04.

---
+++ kde/libs/kopeninghours/kopeninghours.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2021 Volker Krause <vkrause@kde.org>
+
+import info
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.description = "KOpeningHours"
+        self.versionInfo.setDefaultValues(gitUrl = "https://invent.kde.org/libraries/kopeninghours.git")
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kholidays"] = None
+
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
