a96d8643626433e63eb201c1e4d5d5da4ebe43bd
Volker Krause <vkrause@kde.org>
2021-04-03 10:17:11 +0200

    Add libintl-lite
    This is a gettext replacement we use on Android.

---
+++ libs/libintl-lite/libintl-lite.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2021 Volker Krause <vkrause@kde.org>
+
+import info
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['master'] = 'https://github.com/j-jorge/libintl-lite.git'
+        self.defaultTarget = 'master'
+        self.description = "libintl lite - gettext replacement for Android"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
