90c56010b36dbb0d54dc27cc8269f82eddd0cae7
Hannah von Reth <hannah.vonreth@owncloud.com>
2021-04-06 20:41:26 +0200

    libtasn1 v4.16

--- libs/_autotools/libtasn1/libtasn1.py
+++ libs/_autotools/libtasn1/libtasn1.py
@@ -7,10 +7,12 @@ from Package.VirtualPackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets["4.13"] = "https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.13.tar.gz"
+        for ver in ["4.13", "4.16.0"]:
+            self.targets[ver] = f"http://ftp.gnu.org/gnu/libtasn1/libtasn1-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"libtasn1-{ver}"
         self.targetDigests["4.13"] = (['7e528e8c317ddd156230c4e31d082cd13e7ddeb7a54824be82632209550c8cca'], CraftHash.HashAlgorithm.SHA256)
-        self.targetInstSrc["4.13"] = "libtasn1-4.13"
-        self.defaultTarget = "4.13"
+        self.targetDigests["4.16.0"] = (['0e0fb0903839117cb6e3b56e68222771bebf22ad7fc2295a0ed7d576e8d4329d'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "4.16.0"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
@@ -21,7 +23,3 @@ class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
         self.subinfo.options.configure.args += " --disable-static --enable-shared "
-        # Don't include the system aclocal files since it breaks gnulib:
-        # https://lists.gnu.org/archive/html/bug-gnulib/2016-12/msg00140.html
-        self.subinfo.options.configure.useDefaultAutoreconfIncludes = False
-
