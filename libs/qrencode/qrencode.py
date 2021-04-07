40aaf3ba52864317de1575b631a3b0e700000af6
Volker Krause <vkrause@kde.org>
2021-04-07 15:40:28 +0200

    Don't explicitly set BUILD_SHARED_LIBS
    This is done by the basic CMake support, and overwriting this breaks
    support for static builds.

--- libs/qrencode/qrencode.py
+++ libs/qrencode/qrencode.py
@@ -22,4 +22,4 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args = "-DWITH_TOOLS=OFF -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF"
+        self.subinfo.options.configure.args = "-DWITH_TOOLS=OFF -DBUILD_TESTING=OFF"
