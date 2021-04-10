3cdbc519b8ea1a2a6a5df9634be7f78214d9b591
Nicolas Fella <nicolas.fella@gmx.de>
2021-04-10 18:12:02 +0200

    Only build KXmlGui with globalaccel on Linux

--- kde/frameworks/tier3/kxmlgui/kxmlgui.py
+++ kde/frameworks/tier3/kxmlgui/kxmlgui.py
@@ -12,12 +12,14 @@ class subinfo(info.infoclass):
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kitemviews"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kglobalaccel"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/attica"] = None
+
+        if CraftCore.compiler.isLinux or CraftCore.compiler.isFreeBSD:
+            self.runtimeDependencies["kde/frameworks/tier3/kglobalaccel"] = None
 
 
 from Package.CMakePackageBase import *
@@ -26,3 +28,6 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
+
+        if not CraftCore.compiler.isLinux and not CraftCore.compiler.isFreeBSD:
+            self.subinfo.options.configure.args += "-DFORCE_DISABLE_KGLOBALACCEL=ON" # TODO only needed for KXmlGui < 5.82
