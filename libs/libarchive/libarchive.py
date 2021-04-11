5e994fff247865ceeca02a6d8db7f343f7544146
Vincent Pinon <vpinon@kde.org>
2021-04-11 22:16:45 +0200

    upgrade libarchive

--- libs/libarchive/libarchive.py
+++ libs/libarchive/libarchive.py
@@ -3,18 +3,14 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for v in ['2.7.0', '2.8.4', '3.3.1', "3.3.2"]:
+        for v in ['3.3.1', '3.3.2', '3.4.3']:
             self.targets[v] = 'https://github.com/libarchive/libarchive/archive/v' + v + '.tar.gz'
             self.targetInstSrc[v] = 'libarchive-' + v
-        self.targetDigests['2.8.4'] = 'b9cc3bbd20bd71f996be9ec738f19fda8653f7af'
-        self.targetDigests['2.8.4'] = (['ff138120fe7fca1bd02bed6f06d6869c7497658904a2f8916947f9a3f3257377'], CraftHash.HashAlgorithm.SHA256)
-        self.patchToApply['2.8.4'] = ("libarchive-2.8.4-20101205.diff", 1)
         self.patchToApply['3.3.1'] = ("libarchive-no-fatal-warnings-in-debug-mode.diff", 1)
-
         self.patchLevel["3.3.1"] = 1
-
+        self.targetDigests['3.4.3'] = (['19556c1c67aacdff547fd719729630444dbc7161c63eca661a310676a022bb01'], CraftHash.HashAlgorithm.SHA256)
         self.description = "C library and command-line tools for reading and writing tar, cpio, zip, ISO, and other archive formats"
-        self.defaultTarget = '3.3.1'
+        self.defaultTarget = '3.4.3'
 
     def setDependencies(self):
         self.buildDependencies["libs/liblzma"] = None
