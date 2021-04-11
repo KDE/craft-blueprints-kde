2d31bad7c07360ef766c71a7dcfe8161ae03ff2c
Vincent Pinon <vpinon@kde.org>
2021-04-11 22:34:08 +0200

    upgrade libvpx

--- libs/_autotools/libvpx/libvpx.py
+++ libs/_autotools/libvpx/libvpx.py
@@ -2,13 +2,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.7.0","1.8.0"]:
+        for ver in ['1.8.0', '1.9.0']:
             self.targets[ver] = f"https://github.com/webmproject/libvpx/archive/v{ver}.tar.gz"
             self.targetInstSrc[ ver ] = "libvpx-" + ver
-        self.targetDigests['1.7.0'] = (['1fec931eb5c94279ad219a5b6e0202358e94a93a90cfb1603578c326abfc1238'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['1.8.0'] = (['86df18c694e1c06cc8f83d2d816e9270747a0ce6abe316e93a4f4095689373f6'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.9.0'] = (['d279c10e4b9316bf11a570ba16c3d55791e1ad6faa4404c67422eb631782c80a'], CraftHash.HashAlgorithm.SHA256)
         self.description = "VP8 and VP9 video codec"
-        self.defaultTarget = '1.8.0'
+        self.defaultTarget = '1.9.0'
 
     def setDependencies(self):
         self.runtimeDependencies["libs/pthreads"] = None
