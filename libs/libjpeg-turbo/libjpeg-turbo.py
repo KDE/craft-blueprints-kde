544f2924933387b314fef3810db1420972405b58
Vincent Pinon <vpinon@kde.org>
2021-04-11 22:03:38 +0200

    upgrade libjpeg-turbo

--- libs/libjpeg-turbo/libjpeg-turbo.py
+++ libs/libjpeg-turbo/libjpeg-turbo.py
@@ -29,52 +29,28 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/libjpeg-turbo/libjpeg-turbo.git"
 
-        for ver in ["1.5.1", "1.5.3"]:
+        for ver in ['2.0.6']:
             self.targets[ver] = "https://github.com/libjpeg-turbo/libjpeg-turbo/archive/%s.tar.gz" % ver
             self.archiveNames[ver] = "libjpeg-turbo-%s.tar.gz" % ver
             self.targetInstSrc[ver] = 'libjpeg-turbo-%s' % ver
 
-        self.targetDigests['1.5.1'] = (
-            ['c15a9607892113946379ccea3ca8b85018301b200754f209453ab21674268e77'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests['1.5.3'] = (
-            ['1a17020f859cb12711175a67eab5c71fc1904e04b587046218e36106e07eabde'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['2.0.6'] = (
+            ['005aee2fcdca252cee42271f7f90574dda64ca6505d9f8b86ae61abc2b426371'], CraftHash.HashAlgorithm.SHA256)
         self.description = "libjpeg-turbo is a JPEG image codec that uses SIMD instructions (MMX, SSE2, NEON, AltiVec) to accelerate baseline JPEG compression and decompression on x86, x86-64, ARM, and PowerPC systems"
         self.webpage = "http://libjpeg-turbo.virtualgl.org/"
-        self.defaultTarget = '1.5.3'
+        self.defaultTarget = '2.0.6'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["dev-utils/nasm"] = None
 
-
 from Package.CMakePackageBase import *
 
-
-from Package.AutoToolsPackageBase import *
-from Package.CMakePackageBase import *
-
-class PackageAutotools(AutoToolsPackageBase):
-    def __init__(self, **args):
-        AutoToolsPackageBase.__init__(self)
-        # Version 2 uses cmake
-        self.subinfo.options.configure.bootstrap = True
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
         if CraftCore.compiler.isMacOS:
-            self.subinfo.options.configure.args = "--disable-shared --enable-static "
+            self.subinfo.options.configure.args = " -DENABLE_SHARED=OFF -DENABLE_STATIC=ON "
         else:
-            self.subinfo.options.configure.args = "--enable-shared --disable-static "
-        self.subinfo.options.configure.args += "--disable-doc-build"
+            self.subinfo.options.configure.args = " -DENABLE_SHARED=ON -DENABLE_STATIC=OFF "
 
-    def configure(self):
-        nasm = CraftCore.cache.findApplication("nasm")
-        if nasm:
-            self.subinfo.options.configure.args += f" NASM={nasm}"
-        return super().configure()
-
-if CraftCore.compiler.isUnix:
-    class Package(PackageAutotools):
-        def __init__(self):
-            PackageAutotools.__init__(self)
-else:
-    class Package(CMakePackageBase):
-        def __init__(self):
-            CMakePackageBase.__init__(self)
