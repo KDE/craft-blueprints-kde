cd360b48e8dff1e622f147dd5a23a1ba3a439764
Hannah von Reth <vonreth@kde.org>
2021-04-13 18:05:07 +0200

    Fix patch

--- libs/ffmpeg/ffmpeg.py
+++ libs/ffmpeg/ffmpeg.py
@@ -12,7 +12,7 @@ class subinfo(info.infoclass):
 
         if CraftCore.compiler.isMSVC():
             self.patchToApply["4.2"] = [("ffmpeg-4.1-20190507.diff", 1)]
-            self.patchToApply["4.4"] = [("ffmpeg-4.1-20190507.diff", 1)]
+            self.patchToApply["4.4"] = [("ffmpeg-4.4-20210413.diff", 1)]
 
         self.description = "A complete, cross-platform solution to record, convert and stream audio and video."
         self.webpage = "https://ffmpeg.org/"
