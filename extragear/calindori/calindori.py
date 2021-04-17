06d4b1af531a8d3eda97e3cb07f78fb56b114602
Tobias Fella <fella@posteo.de>
2021-04-17 19:00:11 +0200

    Add blueprint for Calindori

---
+++ extragear/calindori/calindori.py
@@ -0,0 +1,40 @@
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["master"] = "https://invent.kde.org/plasma-mobile/calindori.git"
+        self.defaultTarget = "master"
+
+        self.displayName = "Calindori"
+        self.description = "Calendar for Plasma Mobile"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcalendarcore"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kservice"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kpeople"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+        self.runtimeDependencies["kde/plasma/breeze"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+
+    def createPackage(self):
+        self.defines["executable"] = r"bin\calindori.exe"
+        self.addExecutableFilter(r"(bin|libexec)/(?!(calindori|update-mime-database|snoretoast)).*")
+        self.ignoredPackages.append("binary/mysql")
+        if not CraftCore.compiler.isLinux:
+            self.ignoredPackages.append("libs/dbus")
+        return super().createPackage()
