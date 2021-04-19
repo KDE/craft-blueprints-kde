24cb34494b9460d602a42f44f1adc2f8247f614f
Hannah von Reth <vonreth@kde.org>
2021-04-19 15:24:25 +0200

    Enforce BUILD_SHARED_LIBS=OFF on Windows

--- libs/llvm-meta/llvm/llvm.py
+++ libs/llvm-meta/llvm/llvm.py
@@ -45,21 +45,27 @@ class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
         self.supportsClang = False
-        self.subinfo.options.configure.args += (" -DLLVM_BUILD_TESTS=OFF  -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_GO_TESTS=OFF"
-                                                " -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_BUILD_EXAMPLES=OFF"
-                                                " -DLLVM_TARGETS_TO_BUILD=X86"
-                                                " -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_EH=ON -DLLVM_INCLUDE_DOCS=OFF -DLLVM_INSTALL_UTILS=ON"
-                                                " -DLLVM_OPTIMIZED_TABLEGEN=ON"
+        self.subinfo.options.configure.args += ["-DLLVM_BUILD_TESTS=OFF",
+                                                "-DLLVM_INCLUDE_TESTS=OFF",
+                                                "-DLLVM_INCLUDE_GO_TESTS=OFF",
+                                                "-DLLVM_INCLUDE_EXAMPLES=OFF",
+                                                "-DLLVM_BUILD_EXAMPLES=OFF",
+                                                "-DLLVM_TARGETS_TO_BUILD=X86",
+                                                "-DLLVM_ENABLE_RTTI=ON",
+                                                "-DLLVM_ENABLE_EH=ON",
+                                                "-DLLVM_INCLUDE_DOCS=OFF",
+                                                "-DLLVM_INSTALL_UTILS=ON",
+                                                "-DLLVM_OPTIMIZED_TABLEGEN=ON",
                                                 # Limit the maximum number of concurrent link jobs to 1. This should fix low amount of memory issue for link.
-                                                " -DLLVM_PARALLEL_LINK_JOBS=1")
+                                                "-DLLVM_PARALLEL_LINK_JOBS=1"]
         # allow gcc < 5
-        self.subinfo.options.configure.args += " -DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON"
+        self.subinfo.options.configure.args += ["-DLLVM_TEMPORARILY_ALLOW_OLD_TOOLCHAIN=ON"]
 
         # BEGIN: sub-package handling
         self.subPackages = []
         def maybeAddSubPackage(pkg, cmakeDefine):
             if not pkg.isIgnored():
-                self.subinfo.options.configure.args += f" -D{cmakeDefine}=\"{OsUtils.toUnixPath(pkg.instance.sourceDir())}\""
+                self.subinfo.options.configure.args += [f"-D{cmakeDefine}={OsUtils.toUnixPath(pkg.instance.sourceDir())}"]
                 self.subPackages.append(pkg.instance)
 
         maybeAddSubPackage(CraftPackageObject.get('libs/llvm-meta/clang'),
@@ -72,15 +78,19 @@ class Package(CMakePackageBase):
         # END: sub-package handling
 
         if CraftCore.compiler.isMSVC():
-            self.subinfo.options.configure.args += " -DLLVM_EXPORT_SYMBOLS_FOR_PLUGINS=ON"
+            self.subinfo.options.configure.args += ["-DLLVM_EXPORT_SYMBOLS_FOR_PLUGINS=ON"]
         elif CraftCore.compiler.isMacOS:
-            self.subinfo.options.configure.args += " -DLLVM_BUILD_LLVM_DYLIB=ON"
+            self.subinfo.options.configure.args += ["-DLLVM_BUILD_LLVM_DYLIB=ON"]
         else:
-            self.subinfo.options.configure.args += " -DBUILD_SHARED_LIBS=ON"
+            self.subinfo.options.configure.args += ["-DBUILD_SHARED_LIBS=ON"]
+
+        if CraftCore.compiler.isWindows:
+            #CMake Error at CMakeLists.txt:555 (message): BUILD_SHARED_LIBS options is not supported on Windows
+            self.subinfo.options.configure.args += ["-DBUILD_SHARED_LIBS=OFF"]
 
         # lldb by default needs SWIG for the Python integration
         # disable this support until we have a swig package in Craft
-        self.subinfo.options.configure.args += " -DLLDB_DISABLE_PYTHON=ON"
+        self.subinfo.options.configure.args += ["-DLLDB_DISABLE_PYTHON=ON"]
 
     def fetch(self):
         if not CMakePackageBase.fetch(self):
