6f5a635e9a42418a3ec1e0ed0572f1160beec2a9
Hannah von Reth <vonreth@kde.org>
2021-04-19 17:53:27 +0200

    Fix 0.2.1 build

--- libs/libsamplerate/libsamplerate.py
+++ libs/libsamplerate/libsamplerate.py
@@ -9,8 +9,8 @@ class subinfo(info.infoclass):
         for v in ['0.1.9', '0.2.1']:
             self.targets[v] = 'https://github.com/libsndfile/libsamplerate/archive/' + v + '.tar.gz'
             self.targetInstSrc[v] = 'libsamplerate-' + v
-            if not CraftCore.compiler.isGCCLike():
-                self.patchToApply['v'] = ('libsamplerate-0.1.9-20180928.diff', 1)
+        if not CraftCore.compiler.isGCCLike():
+            self.patchToApply["0.1.9"] = ('libsamplerate-0.1.9-20180928.diff', 1)
         self.targetDigests['0.1.9'] = 'ed60f957a4ff87aa15cbb1f3dbd886fa7e5e9566'
         self.targetDigests['0.2.1'] = (['c28dc9fd587f250114d35aab180e41551935e37c84e7641c31d225cda5eb5fab'], CraftHash.HashAlgorithm.SHA256)
         self.description = "an audio sample rate converter library"
@@ -24,9 +24,10 @@ if CraftCore.compiler.isGCCLike():
     class Package(AutoToolsPackageBase):
         def __init__(self):
             AutoToolsPackageBase.__init__(self)
-            self.subinfo.options.configure.args = " --disable-static --disable-sndfile "
+            self.subinfo.options.configure.args += ["--disable-static", "--disable-sndfile"]
 else:
     class Package(CMakePackageBase):
         def __init__(self):
             CMakePackageBase.__init__(self)
-            self.subinfo.options.configure.args = " -DBUILD_SHARED_LIB=ON -DBUILD_EXAMPLES=OFF -DBUILD_TEST=OFF"
+            self.subinfo.options.dynamic.buildTests = False
+            self.subinfo.options.configure.args += ["-DLIBSAMPLERATE_EXAMPLES=OFF"]
