7ae275606e2933492d97f1c2d5c07c73ab812404
Hannah von Reth <hannah.vonreth@owncloud.com>
2021-04-22 15:18:52 +0200

    Fix dynamic mac builds

--- libs/libbzip2/libbzip2.py
+++ libs/libbzip2/libbzip2.py
@@ -6,9 +6,11 @@ class subinfo(info.infoclass):
         #self.targets['1.0.6'] = 'http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz'
         self.targets['1.0.6'] = 'https://ftp.osuosl.org/pub/clfs/conglomeration/bzip2/bzip2-1.0.6.tar.gz'
         self.targetInstSrc['1.0.6'] = "bzip2-1.0.6"
-        self.patchToApply['1.0.6'] = ("bzip.diff", 1)
+        self.patchToApply['1.0.6'] = [("bzip.diff", 1),
+                                      ("libbzip2-1.0.6-20210422.diff", 1)]
         self.targetDigests['1.0.6'] = '3f89f861209ce81a6bab1fd1998c0ef311712002'
         self.description = "shared libraries for handling bzip2 archives (runtime)"
+        self.patchLevel["1.0.6"] = 1
         self.defaultTarget = '1.0.6'
 
     def setDependencies(self):
@@ -18,8 +20,3 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        self.supportsNinja = False
-        self.subinfo.options.package.packageName = 'libbzip2'
-        if CraftCore.compiler.isClang() or CraftCore.compiler.isGCC():
-            self.subinfo.options.configure.args += ' -DCMAKE_C_FLAGS="-fPIC" '
-        # self.supportsNinja = False
