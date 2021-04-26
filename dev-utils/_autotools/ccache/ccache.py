9a992ce6b830a192529137395ad4ddc3575ccd05
Hannah von Reth <vonreth@kde.org>
2021-04-26 15:21:57 +0200

    Update ccache

--- dev-utils/_autotools/ccache/ccache.py
+++ dev-utils/_autotools/ccache/ccache.py
@@ -7,17 +7,18 @@ class subinfo(info.infoclass):
         self.svnTargets['master'] = "https://github.com/ccache/ccache.git"
         self.targetInstallPath["master"] = "dev-utils"
 
-        for ver in ["4.0", "4.1", "4.2"]:
+        for ver in ["4.0", "4.1", "4.2", "4.2.1"]:
             self.targets[ver] = f"https://github.com/ccache/ccache/releases/download/v{ver}/ccache-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"ccache-{ver}"
             self.targetInstallPath[ver] = "dev-utils"
         self.targetDigests["4.0"] = (['ac97af86679028ebc8555c99318352588ff50f515fc3a7f8ed21a8ad367e3d45'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["4.1"] = (['cdeefb827b3eef3b42b5454858123881a4a90abbd46cc72cf8c20b3bd039deb7'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["4.2"] = (['dbf139ff32031b54cb47f2d7983269f328df14b5a427882f89f7721e5c411b7e'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["4.2.1"] = (['320d2b17d2f76393e5d4bb28c8dee5ca783248e9cd23dff0654694d60f8a4b62'], CraftHash.HashAlgorithm.SHA256)
         self.patchLevel["4.1"] = 1
 
         self.webpage = "https://ccache.dev/"
-        self.defaultTarget = "4.2"
+        self.defaultTarget = "4.2.1"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
