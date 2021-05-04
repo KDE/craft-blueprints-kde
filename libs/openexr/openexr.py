bd533dd4b847acaf3d5c0723aec5da40ff534087
Hannah von Reth <vonreth@kde.org>
2021-05-04 15:43:57 +0200

    Fix OpenEXR blueprint

--- libs/openexr/openexr.py
+++ libs/openexr/openexr.py
@@ -5,8 +5,9 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ["2.5.5"]:
-            self.targets[ver] = 'https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v' + ver + '.tar.gz'
-            self.targetInstSrc[ver] = 'libpng-' + ver
+            self.targets[ver] = f"https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"openexr-{ver}"
+        self.targetDigests["2.5.5"] = (['59e98361cb31456a9634378d0f653a2b9554b8900f233450f2396ff495ea76b3'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = 'The OpenEXR project provides the specification and reference implementation of the EXR file format, the professional-grade image storage format of the motion picture industry.'
         self.defaultTarget = '2.5.5'
