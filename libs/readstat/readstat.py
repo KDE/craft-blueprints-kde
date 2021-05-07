7fc6cd4a8cff9edf3c0d4bedc864b6403c72b7a7
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2021-05-07 14:13:20 +0200

    Add readstat

---
+++ libs/readstat/readstat.py
@@ -0,0 +1,25 @@
+import info
+from Package.AutoToolsPackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['master'] = "[git]https://github.com/WizardMac/ReadStat.git"
+        for ver in ["1.1.6"]:
+            self.targets[ver] = f"https://github.com/WizardMac/ReadStat/releases/download/v{ver}/readstat-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"readstat-{ver}"
+        self.targetDigests['1.1.6'] = (
+            ['4b80558ab966ec0e0841b7359ee0b91a647a74f02b1b8816313c6f17ce663511'], CraftHash.HashAlgorithm.SHA256)
+        self.description = 'A command-line tool and MIT-licensed C library for reading files from popular stats packages'
+
+        for ver in ["1.1.6"]:
+            if CraftCore.compiler.isMSVC():
+                self.patchToApply[ver] = [('readstat-1.1.6-compiler-flags.diff', 1)]
+        self.defaultTarget = '1.1.6'
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        # TODO: iconv
+
+class Package(AutoToolsPackageBase):
+    def __init__(self, **args):
+        AutoToolsPackageBase.__init__(self)
