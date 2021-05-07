cd4733c7aebbbdbd460924eb2d4044df2a53cd45
Hannah von Reth <vonreth@kde.org>
2021-05-07 16:24:25 +0200

    Fix 32bit nasm, afaik we can't use the 64bit version with 32bit

--- dev-utils/nasm/nasm.py
+++ dev-utils/nasm/nasm.py
@@ -21,7 +21,8 @@ class subinfo(info.infoclass):
 
         if CraftCore.compiler.isMSVC():
             self.targetDigestsX64['2.14.02'] =  (['18918ac906e29417b936466e7a2517068206c8db8c04b9762a5befa18bfea5f0'], CraftHash.HashAlgorithm.SHA256)
-            self.targetDigests['2.15.05'] = (['f5c93c146f52b4f1664fa3ce6579f961a910e869ab0dae431bd871bdd2584ef2'], CraftHash.HashAlgorithm.SHA256)
+            self.targetDigestsX64['2.15.05'] = (['f5c93c146f52b4f1664fa3ce6579f961a910e869ab0dae431bd871bdd2584ef2'], CraftHash.HashAlgorithm.SHA256)
+            self.targetDigests['2.15.05'] = (['258c7d1076e435511cf2fdf94e2281eadbdb9e3003fd57f356f446e2bce3119e'], CraftHash.HashAlgorithm.SHA256)
         else:
             self.targetDigests['2.15.05'] = (['3c4b8339e5ab54b1bcb2316101f8985a5da50a3f9e504d43fa6f35668bee2fd0'], CraftHash.HashAlgorithm.SHA256)
 
