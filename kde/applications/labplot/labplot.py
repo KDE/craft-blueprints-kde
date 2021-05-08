17e7c76f2cb320947ac1ab410e0d94ac31307dee
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2021-05-08 10:32:04 +0200

    labplot: disable netcdf for MSVC again

--- kde/applications/labplot/labplot.py
+++ kde/applications/labplot/labplot.py
@@ -26,7 +26,9 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/libfftw"] = None
         self.runtimeDependencies["libs/liblz4"] = None
         self.runtimeDependencies["libs/hdf5"] = None
-        self.runtimeDependencies["libs/netcdf"] = None
+        # netcdf disabled for MSVC until build is stable on Binary Factory
+        if not CraftCore.compiler.isMSVC():
+            self.runtimeDependencies["libs/netcdf"] = None
         self.runtimeDependencies["kde/applications/cantor"] = None
         self.runtimeDependencies["qt-libs/qtmqtt"] = None
         self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
