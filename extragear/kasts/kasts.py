d4f25a4d061404245052fabd0cd11f1672506f41
Tobias Fella <fella@posteo.de>
2021-05-11 20:19:30 +0200

    Kasts: Add QtMultimedia dependency

--- extragear/kasts/kasts.py
+++ extragear/kasts/kasts.py
@@ -14,6 +14,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
         self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
+        self.runtimeDependencies["libs/qt5/qtmultimedia"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
