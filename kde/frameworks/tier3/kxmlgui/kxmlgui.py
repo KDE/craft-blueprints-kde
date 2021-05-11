d39b2d860774bf1159d218824eb4018b8799ecbd
Nicolas Fella <nicolas.fella@gmx.de>
2021-05-11 22:27:59 +0200

    kxmlgui: remove ktextwidgets
    It's only used for tests

--- kde/frameworks/tier3/kxmlgui/kxmlgui.py
+++ kde/frameworks/tier3/kxmlgui/kxmlgui.py
@@ -16,7 +16,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/ktextwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/attica"] = None
 
