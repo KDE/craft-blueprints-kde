1f01134792bd95ed69c24d64867799c0bbabe1c2
Nicolas Fella <nicolas.fella@gmx.de>
2021-05-11 23:15:45 +0200

    kiconthemes: Don't depend on breeze-icons on Android
    On Android we have a different way of bundling icons

--- kde/frameworks/tier3/kiconthemes/kiconthemes.py
+++ kde/frameworks/tier3/kiconthemes/kiconthemes.py
@@ -23,8 +23,9 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kitemviews"] = None
-        self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
 
+        if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
 
 from Package.CMakePackageBase import *
 
