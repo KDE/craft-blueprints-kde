28dba0f4b8c2fddd13b8ff31c5bafd89cb176999
Nicolas Fella <nicolas.fella@gmx.de>
2021-05-11 19:27:01 +0200

    neochat: remove qqc1

--- extragear/neochat/neochat.py
+++ extragear/neochat/neochat.py
@@ -14,7 +14,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
-        self.runtimeDependencies["libs/qt5/qtquickcontrols"] = None
         self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
         self.runtimeDependencies["libs/qt5/qtmultimedia"] = None
         self.runtimeDependencies["qt-libs/qtkeychain"] = None
