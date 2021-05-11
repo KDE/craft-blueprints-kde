bef64c0b2dcbc46960167d5d29cdfb576007bae2
Nicolas Fella <nicolas.fella@gmx.de>
2021-05-11 18:59:41 +0200

    Fix qqc2-desktop-style dependencies
    Change quickcontrols to quickcontrols2
    
    Remove itemviews

--- kde/frameworks/tier3/qqc2-desktop-style/qqc2-desktop-style.py
+++ kde/frameworks/tier3/qqc2-desktop-style/qqc2-desktop-style.py
@@ -10,11 +10,10 @@ class subinfo(info.infoclass):
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
-        self.runtimeDependencies["libs/qt5/qtquickcontrols"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
-        self.runtimeDependencies["kde/frameworks/tier1/kitemviews"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
         
 from Package.CMakePackageBase import *
