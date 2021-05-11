fbb534eafec2434dff3a8f25bc9e00db99882662
Tobias Fella <fella@posteo.de>
2021-05-11 20:06:35 +0200

    Alligator: Don't build breeze-icons for android

--- extragear/alligator/alligator.py
+++ extragear/alligator/alligator.py
@@ -19,9 +19,9 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
-        self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
         self.runtimeDependencies["kde/frameworks/tier2/syndication"] = None
         if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
             self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
             self.runtimeDependencies["kde/plasma/breeze"] = None
 
