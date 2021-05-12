a1b83fcfbc5ea67f9d29549415ec118b485eae9c
Nicolas Fella <nicolas.fella@gmx.de>
2021-05-12 14:43:25 +0200

    calindori: add qtquickcontrols
    It's used for QtQuick.Dialogs

--- extragear/calindori/calindori.py
+++ extragear/calindori/calindori.py
@@ -14,6 +14,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols"] = None
         self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
