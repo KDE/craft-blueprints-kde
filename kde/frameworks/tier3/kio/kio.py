f5d0a270521233697846a973f8724274ceec5951
Hannah von Reth <vonreth@kde.org>
2021-05-13 14:13:26 +0200

    Add new dep

--- kde/frameworks/tier3/kio/kio.py
+++ kde/frameworks/tier3/kio/kio.py
@@ -28,6 +28,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/solid"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/ktextwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kwallet"] = None
 
