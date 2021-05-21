a9219d3ccefc925e696cf36db4b6f011a05747c5
Jasem Mutlaq <mutlaqja@ikarustech.com>
2021-05-21 22:05:08 +0300

    Start using INDI master until v1.9.1 is officially released

--- libs/indiclient/indiclient.py
+++ libs/indiclient/indiclient.py
@@ -6,9 +6,11 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets['1.9.0'] = "https://github.com/indilib/indi.git||stable-1.9.0"
+        #self.svnTargets['1.9.0'] = "https://github.com/indilib/indi.git||stable-1.9.0"
+        #self.defaultTarget = '1.9.0'
+        self.svnTargets['master'] = "https://github.com/indilib/indi.git"
+        self.defaultTarget = 'master'
         self.description = 'INDI Library'
-        self.defaultTarget = '1.9.0'
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/grep"] = None
