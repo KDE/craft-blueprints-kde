4ff2559006050de77049bf0aa290a6f772f8a99f
George Florea Bănuș <georgefb899@gmail.com>
2021-05-26 13:46:07 +0000

    Add ffmpegthumbs blueprint

---
+++ kde/kdemultimedia/ffmpegthumbs/ffmpegthumbs.py
@@ -0,0 +1,27 @@
+import info
+from CraftConfig import *
+from CraftOS.osutils import OsUtils
+
+class subinfo( info.infoclass ):
+    def setTargets( self ):
+        self.versionInfo.setDefaultValues()
+
+        self.displayName = "FFmpeg Thumbnailer"
+        self.description = "FFmpeg-based thumbnail creator for video files"
+
+    def setDependencies( self ):
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/ffmpeg"] = None
+        self.runtimeDependencies["libs/taglib"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
+
+from Package.CMakePackageBase import *
+
+class Package( CMakePackageBase ):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+
