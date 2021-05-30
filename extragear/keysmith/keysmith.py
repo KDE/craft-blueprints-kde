4556d79692f247e23802be26c6e27a0aeaf5fdca
Tobias Fella <fella@posteo.de>
2021-05-31 00:52:37 +0200

    Add blueprint for keysmith

---
+++ extragear/keysmith/keysmith.py
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: 2021 Tobias Fella <fella@posteo.de>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import info
+from Package.CMakePackageBase import *
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["master"] = "https://invent.kde.org/utilities/keysmith.git"
+        self.defaultTarget = "master"
+
+        for ver in ["0.2"]:
+            self.targets[ver] = f"https://download.kde.org/stable/keysmith/{ver}/keysmith-v{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"keysmith-v{ver}"
+
+        self.defaultTarget = "0.2"
+
+        self.displayName = "Keysmith"
+        self.description = "OTP client for Plasma Mobile and Desktop"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["libs/libsodium"] = None
+        if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+            self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+            self.runtimeDependencies["kde/plasma/breeze"] = None
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+
+    def createPackage(self):
+        self.defines["executable"] = r"bin\keysmith.exe"
+        self.addExecutableFilter(r"(bin|libexec)/(?!(keysmith|update-mime-database)).*")
+        self.ignoredPackages.append("binary/mysql")
+        if not CraftCore.compiler.isLinux:
+            self.ignoredPackages.append("libs/dbus")
+        return super().createPackage()
