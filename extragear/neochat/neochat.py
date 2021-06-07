db0d3971bdd9def5fca93afece5fe052b17812cd
Tobias Fella <fella@posteo.de>
2021-06-07 21:52:56 +0200

    NeoChat: Add missing KIO and sonnet dependencies

--- extragear/neochat/neochat.py
+++ extragear/neochat/neochat.py
@@ -20,8 +20,10 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/sonnet"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kitemmodels"] = None
         self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
         self.runtimeDependencies["qt-libs/libquotient"] = None
         self.runtimeDependencies["libs/cmark"] = None
         self.runtimeDependencies["kde/libs/kquickimageeditor"] = None
