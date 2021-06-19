1db17f0ca12a54894915698d737850f60819c9d3
Christoph Cullmann <cullmann@kde.org>
2021-06-19 21:23:11 +0200

    blacklist for MSVC like 'mlt' that uses it
    needs ladspa-sdk that has issues with MSVC due to POSIX
    API usage

--- libs/ladspa-cmt/ladspa-cmt.py
+++ libs/ladspa-cmt/ladspa-cmt.py
@@ -1,6 +1,9 @@
 import info
 
 class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Compiler.NoCompiler if CraftCore.compiler.isMSVC() else CraftCore.compiler.Compiler.All
+
     def setTargets( self ):
         self.description = 'The Computer Music Toolkit LADSPA plugin collection'
         self.webpage = 'http://ladspa.org/'
