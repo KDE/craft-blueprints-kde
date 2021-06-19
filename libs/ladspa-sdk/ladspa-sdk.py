b260103b1df25399c2198c7ace4959f44229efeb
Christoph Cullmann <cullmann@kde.org>
2021-06-19 21:08:46 +0200

    blacklist for MSVC like 'mlt' that uses it

--- libs/ladspa-sdk/ladspa-sdk.py
+++ libs/ladspa-sdk/ladspa-sdk.py
@@ -1,6 +1,9 @@
 import info
 
 class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Compiler.NoCompiler if CraftCore.compiler.isMSVC() else CraftCore.compiler.Compiler.All
+
     def setTargets( self ):
         self.description = 'The Linux Audio Developers Plugin API'
         self.webpage = 'http://plugin.org.uk/'
