cf66127fed7c45d4a7ec4717bdfd1ad1d6458548
Christoph Cullmann <cullmann@kde.org>
2021-06-19 21:54:35 +0200

    use CMake option to avoid -Werror in debug

--- libs/libarchive/libarchive.py
+++ libs/libarchive/libarchive.py
@@ -3,11 +3,9 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for v in ['3.3.1', '3.3.2', '3.4.3']:
+        for v in ['3.4.3']:
             self.targets[v] = 'https://github.com/libarchive/libarchive/archive/v' + v + '.tar.gz'
             self.targetInstSrc[v] = 'libarchive-' + v
-            self.patchToApply[v] = ("libarchive-no-fatal-warnings-in-debug-mode.diff", 1)
-            self.patchLevel[v] = 2
 
         self.targetDigests['3.4.3'] = (['19556c1c67aacdff547fd719729630444dbc7161c63eca661a310676a022bb01'], CraftHash.HashAlgorithm.SHA256)
         self.description = "C library and command-line tools for reading and writing tar, cpio, zip, ISO, and other archive formats"
@@ -31,4 +29,4 @@ class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
         # use openssl for encryption
-        self.subinfo.options.configure.args += "-DENABLE_OPENSSL=ON -DENABLE_CNG=OFF -DENABLE_NETTLE=OFF"
+        self.subinfo.options.configure.args += "-DENABLE_OPENSSL=ON -DENABLE_CNG=OFF -DENABLE_NETTLE=OFF -DENABLE_WERROR=OFF"
