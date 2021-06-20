4520b513f755454b73c9ce324de62582da585bc5
Christoph Cullmann <cullmann@kde.org>
2021-06-20 17:01:57 +0200

    try to use latest libgit2
    avoid use of OLD pcre1, use pcre2

--- libs/libgit2/libgit2.py
+++ libs/libgit2/libgit2.py
@@ -4,27 +4,23 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
+        self.description = "a portable C library for accessing git repositories"
         self.svnTargets['master'] = 'https://github.com/libgit2/libgit2.git'
 
-        for ver in ['0.28.1', "1.0.1"]:
-            self.targets[ver] = f"https://github.com/libgit2/libgit2/archive/v{ver}.tar.gz"
-            self.archiveNames[ver] = f"libgit2-{ver}.tar.gz"
-            self.targetInstSrc[ver] = f"libgit2-{ver}"
-        self.patchToApply["0.28.1"] = [("libgit2-0.28.1-20190216.diff", 1)]
-        self.patchToApply["1.0.1"] = [("libgit2-0.28.1-20190216.diff", 1)]
-        self.targetDigests["0.28.1"] = (['0ca11048795b0d6338f2e57717370208c2c97ad66c6d5eac0c97a8827d13936b'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["1.0.1"] = (['1775427a6098f441ddbaa5bd4e9b8a043c7401e450ed761e69a415530fea81d2'], CraftHash.HashAlgorithm.SHA256)
-        self.patchLevel["0.28.1"] = 3
-
-        self.description = "a portable C library for accessing git repositories"
-        self.defaultTarget = '1.0.1'
+        # try to use latest stable libgit2
+        ver = '1.1.0'
+        self.targets[ver] = f"https://github.com/libgit2/libgit2/archive/v{ver}.tar.gz"
+        self.archiveNames[ver] = f"libgit2-{ver}.tar.gz"
+        self.targetInstSrc[ver] = f"libgit2-{ver}"
+        self.targetDigests[ver] = (['41a6d5d740fd608674c7db8685685f45535323e73e784062cf000a633d420d1e'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = ver
 
     def setDependencies(self):
         self.buildDependencies['dev-utils/pkg-config'] = 'default'
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies['libs/iconv'] = 'default'
         self.runtimeDependencies['libs/libssh2'] = 'default'
-        self.runtimeDependencies['libs/pcre'] = 'default'
+        self.runtimeDependencies['libs/pcre2'] = 'default'
         self.runtimeDependencies['libs/openssl'] = 'default'
 
 
