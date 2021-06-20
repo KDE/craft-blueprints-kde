843997f09cce2366a9f9c6b7cdd7b1844474acd3
Christoph Cullmann <cullmann@kde.org>
2021-06-20 09:33:50 +0200

    blacklist ladspa for macos
    has compile issues with non-GCC compilers
    
    packaging/craft-blueprints-kde#2

--- libs/ladspa-sdk/ladspa-sdk.py
+++ libs/ladspa-sdk/ladspa-sdk.py
@@ -2,7 +2,7 @@ import info
 
 class subinfo(info.infoclass):
     def registerOptions(self):
-        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Compiler.NoCompiler if CraftCore.compiler.isMSVC() else CraftCore.compiler.Compiler.All
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Compiler.NoCompiler if CraftCore.compiler.isMSVC() else CraftCore.compiler.Platforms.NotMacOS
 
     def setTargets( self ):
         self.description = 'The Linux Audio Developers Plugin API'
