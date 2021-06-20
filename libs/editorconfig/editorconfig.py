ee6bf803202e573275a31bb1561ba9590edb33c8
Christoph Cullmann <cullmann@kde.org>
2021-06-20 17:26:27 +0200

    use version that support PCRE2

--- libs/editorconfig/editorconfig.py
+++ libs/editorconfig/editorconfig.py
@@ -3,17 +3,18 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['0.12.1']:
-            self.targets[ver] = 'https://github.com/editorconfig/editorconfig-core-c/archive/v%s.tar.gz' % ver
-            self.targetInstSrc[ver] = 'editorconfig-core-c-%s' % ver
-        self.targetDigests['0.12.1'] = 'c6e91fd86f93974d88abbf1ddba080dab07ff239'
-
         self.description = "EditorConfig core library written in C"
-        self.defaultTarget = '0.12.1'
+
+        # use latest stable version
+        ver = '0.12.5'
+        self.targets[ver] = 'https://github.com/editorconfig/editorconfig-core-c/archive/v%s.tar.gz' % ver
+        self.targetInstSrc[ver] = 'editorconfig-core-c-%s' % ver
+        self.targetDigests[ver] = (['b2b212e52e7ea6245e21eaf818ee458ba1c16117811a41e4998f3f2a1df298d2'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = ver
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
-        self.runtimeDependencies["libs/pcre"] = None
+        self.runtimeDependencies["libs/pcre2"] = None
 
 
 from Package.CMakePackageBase import *
