e1b6debac8a82b8ce76a575de1a45a99a140a942
Christoph Cullmann <cullmann@kde.org>
2021-06-22 16:54:54 +0200

    one version is enough, remove patch for old one

--- qt-apps/sqlitebrowser/sqlitebrowser.py
+++ qt-apps/sqlitebrowser/sqlitebrowser.py
@@ -6,17 +6,13 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["3.8.0", "3.10.1", "3.12.0"]:
-            self.targets[ver] = f"https://github.com/sqlitebrowser/sqlitebrowser/archive/v{ver}.tar.gz"
-            self.archiveNames[ver] = f"sqlitebrowser-{ver}.tar.gz"
-            self.targetInstSrc[ver] = f"sqlitebrowser-{ver}"
-        self.patchToApply["3.10.1"] = [("sqlitebrowser-3.10.1.diff", 1)]
-        self.targetDigests["3.8.0"] = (["f638a751bccde4bf0305a75685e2a72d26fc3e3a69d7e15fd84573f88c1a4d92"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["3.10.1"] = (['36eb53bc75192c687dce298c79f1532c410ce4ecbeeacfb07b9d02a307f16bef'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["3.12.0"] = (['3f1a1453ed0f4b5b72b0468bf8ee56887eb23d71c2518a449f4eb179471d73d1'], CraftHash.HashAlgorithm.SHA256)
+        self.description = "DB Browser for SQLite"
 
         self.defaultTarget = "3.12.0"
-        self.description = "DB Browser for SQLite"
+        self.targets[self.defaultTarget] = f"https://github.com/sqlitebrowser/sqlitebrowser/archive/v{self.defaultTarget}.tar.gz"
+        self.archiveNames[self.defaultTarget] = f"sqlitebrowser-{self.defaultTarget}.tar.gz"
+        self.targetInstSrc[self.defaultTarget] = f"sqlitebrowser-{self.defaultTarget}"
+        self.targetDigests[self.defaultTarget] = (['3f1a1453ed0f4b5b72b0468bf8ee56887eb23d71c2518a449f4eb179471d73d1'], CraftHash.HashAlgorithm.SHA256)
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
