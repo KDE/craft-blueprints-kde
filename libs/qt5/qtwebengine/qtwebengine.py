e5b6412b44b86d701693741cb06e45a148281bfb
Hannah von Reth <vonreth@kde.org>
2021-06-24 00:09:48 +0200

    Add patch for icu

--- libs/qt5/qtwebengine/qtwebengine.py
+++ libs/qt5/qtwebengine/qtwebengine.py
@@ -32,7 +32,8 @@ class subinfo(info.infoclass):
                 (".qt-5.15.0", 1)
         ]
 
-        self.patchToApply["5.15.2"] = [("qtwebengine-5.15.2-disable-fatal-warnings.patch", 1)]
+        self.patchToApply["5.15.2"] = [("qtwebengine-5.15.2-disable-fatal-warnings.patch", 1),
+                                        ("qtwebengine-5.15.2_p20210406-icu69.patch", 1)]
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/gperf"] = None
