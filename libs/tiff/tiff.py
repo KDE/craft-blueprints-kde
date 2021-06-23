cfdb31d8ef7c8c830b3217bb5f1b9fbf680a310e
Christoph Cullmann <cullmann@kde.org>
2021-06-23 16:27:21 +0200

    keep only latest libtiff

--- libs/tiff/tiff.py
+++ libs/tiff/tiff.py
@@ -4,20 +4,12 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['3.9.2', '4.0.3', '4.0.4', '4.0.9', '4.0.10']:
-            self.targets[ver] = "http://download.osgeo.org/libtiff/tiff-" + ver + ".tar.gz"
-            self.targetInstSrc[ver] = "tiff-" + ver
-        self.patchToApply['3.9.2'] = [('tiff-3.9.2-20100418.diff', 1)]
-        self.patchToApply['4.0.3'] = [('tiff-4.0.3-rename-test.diff', 1), ('tiff-4.0.3-20130124.diff', 1)]
-        self.patchToApply['4.0.4'] = [('tiff-4.0.4-20130124.diff', 1)]
-        self.targetDigests['3.9.2'] = '5c054d31e350e53102221b7760c3700cf70b4327'
-        self.targetDigests['4.0.3'] = '652e97b78f1444237a82cbcfe014310e776eb6f0'
-        self.targetDigests['4.0.9'] = (['6e7bdeec2c310734e734d19aae3a71ebe37a4d842e0e23dbb1b8921c0026cfcd'], CraftHash.HashAlgorithm.SHA256)
-        self.patchLevel["4.0.9"] = 2
-
         self.description = "a library to manipulate TIFF image files"
         self.webpage = "http://www.simplesystems.org/libtiff/"
+
         self.defaultTarget = '4.0.10'
+        self.targets[self.defaultTarget] = "http://download.osgeo.org/libtiff/tiff-" + self.defaultTarget + ".tar.gz"
+        self.targetInstSrc[self.defaultTarget] = "tiff-" + self.defaultTarget
 
     def setDependencies(self):
         self.runtimeDependencies["libs/zlib"] = None
