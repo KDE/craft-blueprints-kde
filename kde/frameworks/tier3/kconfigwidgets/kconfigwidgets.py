60b99872d6e28bac7b6ac62a70f4125db31064cc
Nicolas Fella <nicolas.fella@gmx.de>
2021-06-24 12:34:40 +0200

    kconfigwidgets: Don't use KDocTools on Android
    KDocTools doesn't build for Android since it's more of a host thing

--- kde/frameworks/tier3/kconfigwidgets/kconfigwidgets.py
+++ kde/frameworks/tier3/kconfigwidgets/kconfigwidgets.py
@@ -22,13 +22,13 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcodecs"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
-        self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kguiaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
 
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/frameworks/tier2/kauth"] = None
+            self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
 
 
 from Package.CMakePackageBase import *
