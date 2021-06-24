3fb1bbfc1174a5c63f9da418ede793f4d8cef808
Nicolas Fella <nicolas.fella@gmx.de>
2021-06-24 12:44:34 +0200

    kongress: remove obsolete patches
    They are for 21.05, but we ship 21.06 now

--- kde/plasma-mobile/kongress/kongress.py
+++ kde/plasma-mobile/kongress/kongress.py
@@ -8,11 +8,6 @@ class subinfo(info.infoclass):
         self.displayName = "Kongress"
         self.description = "Conference companion app"
 
-        self.patchToApply["21.05"] = [("0001-Build-without-DBus-on-Android.patch", 1),
-                                      ("0002-Use-QGuiApplication-on-Android.patch", 1),
-                                      ("0003-Use-QGuiApplication-instead-of-QApplication-where-po.patch", 1)]
-        self.patchLevel["21.05"] = 1
-
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
