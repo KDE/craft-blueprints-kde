e6bcc1c9ed3f3d44f26658a5e730689f2f4bce73
Nicolas Fella <nicolas.fella@gmx.de>
2021-06-24 13:23:41 +0200

    Update Plasma

--- kde/plasma/breeze/breeze.py
+++ kde/plasma/breeze/breeze.py
@@ -4,7 +4,6 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
-        self.patchLevel["5.20.5"] = 1
 
         for ver in self.versionInfo.tarballs():
             self.patchToApply[ver] = ('breeze-noWinDrag.diff', 0)
