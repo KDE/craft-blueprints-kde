71c5a7f232fa46fadb3ae668041973806105246d
Nicolas Fella <nicolas.fella@gmx.de>
2021-06-25 16:01:46 +0200

    kalgebra: don't use kdoctools on Android

--- kde/applications/kalgebra/kalgebra.py
+++ kde/applications/kalgebra/kalgebra.py
@@ -14,12 +14,12 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
-        self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/applications/analitza"] = None
 
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["libs/qt5/qtwebengine"] = None
+            self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
 
 
 from Package.CMakePackageBase import *
