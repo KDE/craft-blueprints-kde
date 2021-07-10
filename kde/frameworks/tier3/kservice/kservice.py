9f3df8f80bd01e0ba5e90ba9f8a8d8a8bde07aa1
Volker Krause <vkrause@kde.org>
2021-07-10 19:11:11 +0200

    Clean up KService dependencies on Android

--- kde/frameworks/tier3/kservice/kservice.py
+++ kde/frameworks/tier3/kservice/kservice.py
@@ -13,10 +13,11 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
-        self.runtimeDependencies["kde/frameworks/tier2/kcrash"] = None
-        self.runtimeDependencies["kde/frameworks/tier1/kdbusaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
-        self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
+        if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["kde/frameworks/tier1/kdbusaddons"] = None
+            self.runtimeDependencies["kde/frameworks/tier2/kcrash"] = None
+            self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
 
 
 from Package.CMakePackageBase import *
