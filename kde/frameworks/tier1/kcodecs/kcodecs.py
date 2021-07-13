f3d1eb3ed8ad11685ca9a24833a6dcd7feb26509
Nicolas Fella <nicolas.fella@gmx.de>
2021-07-13 21:45:42 +0200

    Update frameworks to 5.84

--- kde/frameworks/tier1/kcodecs/kcodecs.py
+++ kde/frameworks/tier1/kcodecs/kcodecs.py
@@ -4,8 +4,6 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
-        self.patchToApply["5.78.0"] = [("0001-Remove-the-usage-of-non-UTF-8-string-literals.patch", 1)]
-        self.patchLevel["5.78.0"] = 1
 
         self.description = "Plugins allowing Qt applications to access further types of images"
 
