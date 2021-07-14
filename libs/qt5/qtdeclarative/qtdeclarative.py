6cc5cf88984dad1a377fbf70aec580b116adc7b4
Volker Krause <vkrause@kde.org>
2021-07-14 17:03:23 +0200

    Disable QML debug plugins when building in MinSizeRel mode
    This further aligns binary factory's Android settings with the defaults.

--- libs/qt5/qtdeclarative/qtdeclarative.py
+++ libs/qt5/qtdeclarative/qtdeclarative.py
@@ -21,6 +21,8 @@ from Package.Qt5CorePackageBase import *
 class QtPackage(Qt5CorePackageBase):
     def __init__(self, **args):
         Qt5CorePackageBase.__init__(self)
+        if self.buildType() == "MinSizeRel":
+            self.subinfo.options.dynamic.featureArguments += ["-no-feature-qml-debug"]
 
 
 class Package(Qt5CoreSdkPackageBase):
