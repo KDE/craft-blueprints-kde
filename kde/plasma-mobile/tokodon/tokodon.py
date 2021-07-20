ab33001dab0b19e5457a3c52c031855bcfe6c5ea
Nicolas Fella <nicolas.fella@gmx.de>
2021-07-20 17:36:20 +0200

    tokodon: Add KNotifications

--- kde/plasma-mobile/tokodon/tokodon.py
+++ kde/plasma-mobile/tokodon/tokodon.py
@@ -19,6 +19,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/knotification"] = None
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
             self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
