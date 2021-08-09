323b59c557b05910d68128e8a0069614e9999a9a
Nicolas Fella <nicolas.fella@gmx.de>
2021-08-09 15:31:07 +0200

    kalgebra: Add quickcontrols2

--- kde/applications/kalgebra/kalgebra.py
+++ kde/applications/kalgebra/kalgebra.py
@@ -13,6 +13,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
