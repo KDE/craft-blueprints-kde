00c90d8a0af0e8fa88284174b132dd9bae6c26db
Volker Krause <vkrause@kde.org>
2021-08-11 17:03:34 +0200

    Update PCRE to 8.45 and make it build on Android

--- libs/pcre/pcre.py
+++ libs/pcre/pcre.py
@@ -4,15 +4,18 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["8.41"]:
+        for ver in ["8.45", "8.41"]:
             self.targets[ver] = f"https://ftp.pcre.org/pub/pcre/pcre-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"pcre-{ver}"
         self.patchToApply["8.41"] = [("pcre-8.10-20101125.diff", 1)]
+        self.patchToApply["8.45"] = [("pcre-8.10-20101125.diff", 1)]
         self.targetDigests['8.41'] = (
             ['e62c7eac5ae7c0e7286db61ff82912e1c0b7a0c13706616e94a7dd729321b530'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['8.45'] = (
+            ['4dae6fdcd2bb0bb6c37b5f97c33c2be954da743985369cddac3546e3218bffb8'], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "Perl-Compatible Regular Expressions"
-        self.defaultTarget = "8.41"
+        self.defaultTarget = "8.45"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
@@ -28,4 +31,6 @@ class Package(CMakePackageBase):
         defines += "-DPCRE_SUPPORT_UNICODE_PROPERTIES=ON "
         defines += "-DPCRE_SUPPORT_UTF8=ON "
         defines += "-DPCRE_EBCDIC=OFF "
+        if CraftCore.compiler.isAndroid:
+            defines += "-DHAVE_STRTOQ=FALSE "
         self.subinfo.options.configure.args = defines
