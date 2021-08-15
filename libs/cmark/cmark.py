355b40c64272ffe0484d2bd48290084c191edb7c
Nicolas Fella <nicolas.fella@gmx.de>
2021-08-15 13:10:17 +0200

    Update cmark

--- libs/cmark/cmark.py
+++ libs/cmark/cmark.py
@@ -7,7 +7,7 @@ class subinfo(info.infoclass):
         self.svnTargets['master'] = 'https://github.com/commonmark/cmark'
 
         # use latest stable version
-        self.defaultTarget = '0.30.0'
+        self.defaultTarget = '0.30.1'
         self.targets[self.defaultTarget] = "https://github.com/commonmark/cmark/archive/%s.tar.gz" % self.defaultTarget
         self.archiveNames[self.defaultTarget] = 'cmark-%s.tar.gz' % self.defaultTarget
         self.targetInstSrc[self.defaultTarget] = 'cmark-%s' % self.defaultTarget
