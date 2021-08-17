e885e670421a8dae623290a268bde54424beb517
Milian Wolff <mail@milianw.de>
2021-08-17 13:26:17 +0200

    Add elfutils v0.185

--- libs/elfutils/elfutils.py
+++ libs/elfutils/elfutils.py
@@ -6,15 +6,16 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['0.181']:
+        for ver in ['0.181', '0.185']:
             self.targets[ver] = 'https://sourceware.org/elfutils/ftp/%s/elfutils-%s.tar.bz2' % (ver, ver)
             self.targetInstSrc[ver] = "elfutils-" + ver
+            self.patchLevel[ver] = 0
 
-        self.patchLevel["0.181"] = 0
         self.targetDigests['0.181'] = (['d565541d5817f409dc89ebb1ee593366f69c371a1531308eeb67ff934b14a0fab0c9009fd7c23240efbaa1b4e04edac5c425e47d80e3e66ba03dcaf000afea36'], CraftHash.HashAlgorithm.SHA512)
+        self.targetDigests['0.185'] = (['34de0de1355b11740e036e0fc64f2fc063587c8eb121b19216ee5548d3f0f268d8fc3995176c47190466b9d881007cfa11a9d01e9a50e38af6119492bf8bb47f'], CraftHash.HashAlgorithm.SHA512)
 
         self.description = 'elfutils is a collection of utilities and libraries to read, create and modify ELF binary files, find and handle DWARF debug data, symbols, thread state and stacktraces for processes and core files on GNU/Linux.'
-        self.defaultTarget = '0.181'
+        self.defaultTarget = '0.185'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
