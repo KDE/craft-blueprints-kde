b9b6577ed858eb8eeabdc786619b8c76c35c9673
Gleb Popov <6yearold@gmail.com>
2021-08-18 23:09:57 +0300

    Add kde/frameworks/tier1/kwayland blueprint.

---
+++ kde/frameworks/tier1/kwayland/kwayland.py
@@ -0,0 +1,22 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NotWindows & CraftCore.compiler.Platforms.NotMacOS
+
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
