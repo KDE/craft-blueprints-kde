8750530ef786e1f0b735ccaf84cf8ae35b00b62c
Gleb Popov <6yearold@gmail.com>
2021-08-18 23:13:19 +0300

    Add kde/plasma/kwayland-server blueprint.

---
+++ kde/plasma/kwayland-server/kwayland-server.py
@@ -0,0 +1,21 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NotWindows & CraftCore.compiler.Platforms.NotMacOS
+
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kwayland"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
