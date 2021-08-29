287f3d2dce0e3412a1212d19de7fc3a7d33d934a
Jean-Baptiste Mardelle <jb@kdenlive.org>
2021-08-29 15:43:54 +0000

    Check if python scripts are stripped by exe filter

--- kde/kdemultimedia/kdenlive/kdenlive.py
+++ kde/kdemultimedia/kdenlive/kdenlive.py
@@ -63,7 +63,7 @@ class Package(CMakePackageBase):
 
     def createPackage(self):
         self.blacklist_file.append(os.path.join(self.packageDir(), 'exclude.list'))
-        self.addExecutableFilter(r"bin/(?!(dbus-daemon|ff|kdenlive|kioslave|melt|update-mime-database)).*")
+        self.addExecutableFilter(r"bin/(?!(dbus-daemon|ff|kdenlive|kioslave|melt|update-mime-database|checkvosk)).*")
         self.ignoredPackages.append("libs/llvm-meta")
         self.ignoredPackages.append("data/hunspell-dictionaries")
         self.ignoredPackages.append("binary/mysql")
