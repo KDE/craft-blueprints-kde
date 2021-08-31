93744bf1aca472168820ead61f5b94a3fb764726
Gleb Popov <6yearold@gmail.com>
2021-08-31 11:56:53 +0300

    Do not force KAuth to build Fake backend only.

--- kde/frameworks/tier2/kauth/kauth.py
+++ kde/frameworks/tier2/kauth/kauth.py
@@ -20,4 +20,3 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args += "-DKAUTH_BACKEND_NAME=\"Fake\""
