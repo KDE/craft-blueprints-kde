830fa0e31c6fefd8616e1037485d6c87e1ea621d
Vincent Pinon <vincent.pinon@asygn.com>
2021-09-03 18:32:34 +0200

    MLT: bump bugfix

--- libs/mlt/mlt.py
+++ libs/mlt/mlt.py
@@ -17,7 +17,7 @@ class subinfo(info.infoclass):
         self.svnTargets["v6"] = "https://github.com/mltframework/mlt.git|v6"
         self.patchLevel['v6'] = 20210425
         self.svnTargets["master"] = "https://github.com/mltframework/mlt.git"
-        self.patchLevel['master'] = 20210822
+        self.patchLevel['master'] = 20210903
         self.defaultTarget = "master"
 
     def setDependencies( self ):
