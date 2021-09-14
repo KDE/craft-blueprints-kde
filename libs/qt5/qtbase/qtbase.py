feca22a30f8d3a2122fd9b2097351fcb2da28543
Hannah von Reth <vonreth@kde.org>
2021-09-14 12:19:54 +0200

    Backport https://bugreports.qt.io/browse/QTBUG-91059

--- libs/qt5/qtbase/qtbase.py
+++ libs/qt5/qtbase/qtbase.py
@@ -82,9 +82,10 @@ class subinfo(info.infoclass):
         self.patchLevel["5.12.3"] = 1
         self.patchLevel["5.12.9"] = 1
         self.patchLevel["5.12.10"] = 2
+        self.patchLevel["5.12.11"] = 1
         self.patchLevel["5.15.0"] = 4
         self.patchLevel["5.15.1"] = 4
-        self.patchLevel["5.15.2"] = 3
+        self.patchLevel["5.15.2"] = 4
         self.description = "a cross-platform application framework"
 
     def setDependencies(self):
