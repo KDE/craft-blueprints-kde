de0c7db0688eab48ce9bc4c8076528d82d10f699
Jonah Brüchert <jbb.prv@gmx.de>
2021-10-03 08:13:04 -0700

    Update Kaidan to 0.8

--- kde/unreleased/kaidan/kaidan.py
+++ kde/unreleased/kaidan/kaidan.py
@@ -7,14 +7,13 @@ class subinfo(info.infoclass):
         self.displayName = "Kaidan"
 
         self.svnTargets['master'] = 'https://anongit.kde.org/kaidan.git'
-        for ver in ["0.7.0"]:
+        for ver in ["0.8.0"]:
             self.targets[ver] = f"https://download.kde.org/unstable/kaidan/{ver}/kaidan-{ver}.tar.xz"
             self.archiveNames[ver] = f"kaidan-v{ver}.tar.xz"
             self.targetInstSrc[ver] = f"kaidan-{ver}"
-            self.patchToApply[ver] = [("704a72a9f0f5c27dbd36ac04e20273c9480dde2a.patch", 1)]
 
-        self.targetDigests['0.7.0'] = (
-            ['d56b3c927505adbbe343aa0858599bdbb5322a3ac2bb61dbadffa19428b2304f'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['0.8.0'] = (
+            ['a7e772dc7abab565fdf9a7bdaf575a6229bdd509de0891079a83bd32766bb1a4'], CraftHash.HashAlgorithm.SHA256)
         self.defaultTarget = 'master'
 
     def setDependencies(self):
