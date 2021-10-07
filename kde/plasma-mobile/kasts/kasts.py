fbe9e339686e6b558055d28f5ba8e2cb502f5583
Tobias Fella <fella@posteo.de>
2021-10-07 14:10:52 +0200

    Add taglib dependency to neochat

--- kde/plasma-mobile/kasts/kasts.py
+++ kde/plasma-mobile/kasts/kasts.py
@@ -13,6 +13,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt5/qtdeclarative"] = None
         self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
         self.runtimeDependencies["libs/qt5/qtmultimedia"] = None
+        self.runtimeDependencies["libs/taglib"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
