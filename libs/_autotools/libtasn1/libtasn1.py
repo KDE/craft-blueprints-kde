0c28440a70f9df4f54d27d48499d25d44faef23a
Hannah von Reth <vonreth@kde.org>
2021-10-08 12:01:50 +0200

    Disable gtk-doc on windows

--- libs/_autotools/libtasn1/libtasn1.py
+++ libs/_autotools/libtasn1/libtasn1.py
@@ -24,4 +24,6 @@ class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
         self.subinfo.options.configure.args += " --disable-static --enable-shared "
+        # gtk-doc is missing
+        self.subinfo.options.configure.autoreconf = not CraftCore.compiler.isWindows
 
