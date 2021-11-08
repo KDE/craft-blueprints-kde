12ca6a2d69a38fa20fa08b4072d4562dc2b393e7
Hannah von Reth <vonreth@kde.org>
2021-11-08 08:58:09 +0100

    qtkeychain 0.13.0

--- qt-libs/qtkeychain/qtkeychain.py
+++ qt-libs/qtkeychain/qtkeychain.py
@@ -14,19 +14,20 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/frankosterfeld/qtkeychain.git'
-        for ver in ["0.10.0", "0.11.0", "0.12.0"]:
+        for ver in ["0.10.0", "0.11.0", "0.12.0", "0.13.0"]:
             self.targets[ver] = "https://github.com/frankosterfeld/qtkeychain/archive/v%s.tar.gz" % ver
             self.archiveNames[ver] = "qtkeychain-v%s.tar.gz" % ver
             self.targetInstSrc[ver] = 'qtkeychain-%s' % ver
         self.targetDigests['0.10.0'] = (['5f916cd97843de550467db32d2e10f218b904af5b21cfdfcc7c6425d7dfc3ec2'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['0.12.0'] = (['cc547d58c1402f6724d3ff89e4ca83389d9e2bdcfd9ae3d695fcdffa50a625a8'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['0.13.0'] = (['5c04ad1c2f1142197544ddbce249c73b45c724bd683e6dc89bdefbc8e6be36e7'], CraftHash.HashAlgorithm.SHA256)
 
         self.patchToApply["0.10.0"] = [("0001-Don-t-use-absolute-path-to-frameworks.patch", 1)]
         self.patchLevel["0.10.0"] = 1
         self.patchToApply["0.12.0"] = [("qtkeychain-0.12.0-20210128.diff", 1), ("0001-Don-t-find-QtDBus-on-Android.patch", 1), ("0001-Make-QtAndroidExtras-dependency-private.patch", 1)]
         self.patchLevel["0.12.0"] = 2
 
-        self.defaultTarget = '0.12.0'
+        self.defaultTarget = '0.13.0'
 
 
 class Package(CMakePackageBase):
