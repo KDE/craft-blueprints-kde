413ebac100ce16f14b4ee33864038f2441dc7417
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2021-11-14 08:56:20 +0100

    cantor: update dependency

--- kde/applications/cantor/cantor.py
+++ kde/applications/cantor/cantor.py
@@ -10,8 +10,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
-        if not CraftCore.compiler.isMacOS:
-            self.runtimeDependencies['libs/qt5/qtwebengine'] = None
+        self.runtimeDependencies['libs/qt5/qtwebengine'] = None
         self.runtimeDependencies["qt-libs/poppler"] = None
         # R backend fails compiling with MSVC
         # libR.dylib fails packaging on macOS (lapack.so)
