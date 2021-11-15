11f11c3a12bcc6daf3f58964adaa5b551c7b05cd
Weixuan Xiao <veyx.shaw@gmail.com>
2021-11-15 21:39:44 +0000

    Add MauiKit framework blueprints

---
+++ kde/maui/mauikit-filebrowsing/mauikit-filebrowsing.py
@@ -0,0 +1,30 @@
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['master'] = 'https://invent.kde.org/maui/mauikit-filebrowsing.git'
+
+        for ver in ['2.1.0']:
+            self.targets[ver] = 'https://download.kde.org/stable/maui/mauikit-filebrowsing/%s/mauikit-filebrowsing-%s.tar.xz' % (ver, ver)
+            self.archiveNames[ver] = 'mauikit-filebrowsing-%s.tar.gz' % ver
+            self.targetInstSrc[ver] = 'mauikit-filebrowsing-%s' % ver
+
+        self.description = "MauiKit File Browsing utilities and controls"
+        self.defaultTarget = '2.1.0'
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["kde/maui/mauikit"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **args):
+        CMakePackageBase.__init__(self)
---
+++ kde/maui/mauikit/mauikit.py
@@ -0,0 +1,30 @@
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['master'] = 'https://invent.kde.org/maui/mauikit.git'
+
+        for ver in ['2.1.0']:
+            self.targets[ver] = 'https://download.kde.org/stable/maui/mauikit/%s/mauikit-%s.tar.xz' % (ver, ver)
+            self.archiveNames[ver] = 'mauikit-%s.tar.gz' % ver
+            self.targetInstSrc[ver] = 'mauikit-%s' % ver
+
+        self.description = "Templated convergent controls and multi-platform utilities for Maui applications"
+        self.defaultTarget = '2.1.0'
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **args):
+        CMakePackageBase.__init__(self)
