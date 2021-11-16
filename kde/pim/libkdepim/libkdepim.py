c51bbd84620f187d4302d3fe3241bf055065fb72
Nicolas Fella <nicolas.fella@gmx.de>
2021-11-16 20:47:58 +0100

    libkdepim: Remove kwallet dep

--- kde/pim/libkdepim/libkdepim.py
+++ kde/pim/libkdepim/libkdepim.py
@@ -27,7 +27,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kitemviews"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kjobwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kcmutils"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kwallet"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/pim/akonadi"] = None
         self.runtimeDependencies["kde/pim/akonadi-contacts"] = None
