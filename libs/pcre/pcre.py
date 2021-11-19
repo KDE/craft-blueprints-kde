bc64e3221c241d5921020a64ca8fe62ffa869c17
Hannah von Reth <vonreth@kde.org>
2021-11-19 08:45:23 +0100

    Update url

--- libs/pcre/pcre.py
+++ libs/pcre/pcre.py
@@ -4,13 +4,10 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["8.45", "8.41"]:
-            self.targets[ver] = f"https://ftp.pcre.org/pub/pcre/pcre-{ver}.tar.bz2"
+        for ver in ["8.45"]:
+            self.targets[ver] = f"https://downloads.sourceforge.net/sourceforge/pcre/pcre-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"pcre-{ver}"
-        self.patchToApply["8.41"] = [("pcre-8.10-20101125.diff", 1)]
         self.patchToApply["8.45"] = [("pcre-8.10-20101125.diff", 1)]
-        self.targetDigests['8.41'] = (
-            ['e62c7eac5ae7c0e7286db61ff82912e1c0b7a0c13706616e94a7dd729321b530'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['8.45'] = (
             ['4dae6fdcd2bb0bb6c37b5f97c33c2be954da743985369cddac3546e3218bffb8'], CraftHash.HashAlgorithm.SHA256)
 
