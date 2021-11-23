570258039de06c782add53ac5097c7b8bd3894f0
Nicolas Fella <nicolas.fella@gmx.de>
2021-11-23 11:59:17 +0100

    Update qcoro

--- qt-libs/qcoro/qcoro.py
+++ qt-libs/qcoro/qcoro.py
@@ -6,16 +6,14 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/danvratil/qcoro.git'
 
-        for ver in ['0.2.0']:
+        for ver in ['0.3.0']:
             self.targets[ver] = 'https://github.com/danvratil/qcoro/archive/refs/tags/v%s.tar.gz' % ver
             self.archiveNames[ver] = 'qcoro-%s.tar.gz' % ver
             self.targetInstSrc[ver] = 'qcoro-%s' % ver
 
-        self.targetDigests['0.2.0'] = (['daae3ffd9a470e13e8ad7c98cdc58714ea9f08593b8367ffc56c4edc2e52de74'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['0.3.0'] = (['610fcf621de0cf04597e2e308d1d22a7fce2a39eafa2d4827d1ff6c8b0e724c0'], CraftHash.HashAlgorithm.SHA256)
 
-        self.patchToApply["0.2.0"] = [("0001-Disable-QDBus-integration-by-default-on-Windows-Mac-.patch", 1)]
-
-        self.defaultTarget = '0.2.0'
+        self.defaultTarget = '0.3.0'
         self.description = "C++ Coroutines for Qt"
 
     def setDependencies(self):
