b40a447acd8186046a58f9ab9fac0909bc7fa560
Hannah von Reth <vonreth@kde.org>
2021-11-29 22:41:32 +0100

    Patch lvl

--- libs/_linux/wayland-protocols/wayland-protocols.py
+++ libs/_linux/wayland-protocols/wayland-protocols.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://wayland.freedesktop.org/releases/wayland-protocols-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"wayland-protocols-{ver}"
         self.targetDigests['1.24'] = (['bff0d8cffeeceb35159d6f4aa6bab18c807b80642c9d50f66cba52ecf7338bc2'], CraftHash.HashAlgorithm.SHA256)
+        self.patchLevel["1.24"] = 1
 
         self.description = "wayland-protocols contains Wayland protocols that add functionality not available in the Wayland core protocol."
 
