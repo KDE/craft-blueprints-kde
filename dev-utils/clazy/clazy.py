1b0da5a99a4f21dcd1782aa609537b8204ed06a3
Hannah von Reth <vonreth@kde.org>
2021-11-30 08:41:53 +0100

    Clazy 1.10

--- dev-utils/clazy/clazy.py
+++ dev-utils/clazy/clazy.py
@@ -17,11 +17,11 @@ class subinfo(info.infoclass):
         self.svnTargets["master"] = "https://invent.kde.org/sdk/clazy.git"
         self.targetUpdatedRepoUrl["master"] = (["https://anongit.kde.org/clazy"], "https://invent.kde.org/sdk/clazy.git")
 
-        for ver in ["1.3", "1.4", "1.5", "1.6", "1.7"]:
+        for ver in ["1.10"]:
             self.targets[ver] = f"https://download.kde.org/stable/clazy/{ver}/src/clazy-{ver}.tar.xz"
             self.targetDigestUrls[ver] = f"https://download.kde.org/stable/clazy/{ver}/src/clazy-{ver}.tar.xz.sha256"
             self.targetInstSrc[ver] = f"clazy-{ver}"
-        self.defaultTarget = '1.7'
+        self.defaultTarget = '1.10'
 
 
 class Package(CMakePackageBase):
