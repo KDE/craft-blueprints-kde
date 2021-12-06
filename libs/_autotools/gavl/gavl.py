8e01134c9e1099b87eb4e76b0e6e341bf05310cc
Julius Künzel <jk.kdedev@smartlab.uber.space>
2021-12-06 23:22:52 +0000

    [gavl] Update and fix macOS build

--- libs/_autotools/gavl/gavl.py
+++ libs/_autotools/gavl/gavl.py
@@ -8,13 +8,22 @@ class subinfo(info.infoclass):
             self.targetInstSrc[ver] = f"gavl-{ver}"
         self.targetDigests['1.4.0'] = (['51aaac41391a915bd9bad07710957424b046410a276e7deaff24a870929d33ce'], CraftHash.HashAlgorithm.SHA256)
         self.patchToApply['1.4.0'] = ('FixCputest.patch', 1)
+        for ver in ['r6283']:
+            self.targets[ver] = f"https://sourceforge.net/code-snapshots/svn/g/gm/gmerlin/code/gmerlin-code-{ver}-trunk-gavl.zip"
+            self.targetInstSrc[ver] = f"gmerlin-code-{ver}-trunk-gavl"
+        self.targetDigests['r6283'] = (['589b44f12274c5febf766891f17d68eb0d7d866f5eb4066a6775a688bff4bce4'], CraftHash.HashAlgorithm.SHA256)
+        if CraftCore.compiler.isMacOS:
+            self.patchToApply['r6283'] = ('FixMacOS.patch', 1)
 
         self.description = "Low level library, upon which multimedia APIs can be built"
         self.webpage = "https://gmerlin.sourceforge.net"
-        self.defaultTarget = '1.4.0'
+        self.defaultTarget = 'r6283'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
+        if self.buildTarget in ['r6283']:
+            self.runtimeDependencies["libs/gnutls"] = None
+
 
 from Package.AutoToolsPackageBase import *
 
