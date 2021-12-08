c31a08f82faeb567030ce86f0e7e54e966f932fb
Julius Künzel <jk.kdedev@smartlab.uber.space>
2021-12-08 16:31:31 +0000

    [gavl] Use own mirror to avoid sourceforge hell

--- libs/_autotools/gavl/gavl.py
+++ libs/_autotools/gavl/gavl.py
@@ -8,20 +8,19 @@ class subinfo(info.infoclass):
             self.targetInstSrc[ver] = f"gavl-{ver}"
         self.targetDigests['1.4.0'] = (['51aaac41391a915bd9bad07710957424b046410a276e7deaff24a870929d33ce'], CraftHash.HashAlgorithm.SHA256)
         self.patchToApply['1.4.0'] = ('FixCputest.patch', 1)
-        for ver in ['r6283']:
-            self.targets[ver] = f"https://sourceforge.net/code-snapshots/svn/g/gm/gmerlin/code/gmerlin-code-{ver}-trunk-gavl.zip"
-            self.targetInstSrc[ver] = f"gmerlin-code-{ver}-trunk-gavl"
-        self.targetDigests['r6283'] = (['589b44f12274c5febf766891f17d68eb0d7d866f5eb4066a6775a688bff4bce4'], CraftHash.HashAlgorithm.SHA256)
+        self.svnTargets['master'] = "https://github.com/jlskuz/gavl.git"
+        self.patchLevel['master'] = 20211208
+        self.defaultTarget = "master"
         if CraftCore.compiler.isMacOS:
-            self.patchToApply['r6283'] = ('FixMacOS.patch', 1)
+            self.patchToApply['master'] = ('FixMacOS.patch', 1)
 
         self.description = "Low level library, upon which multimedia APIs can be built"
         self.webpage = "https://gmerlin.sourceforge.net"
-        self.defaultTarget = 'r6283'
+        self.defaultTarget = 'master'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
-        if self.buildTarget in ['r6283']:
+        if self.buildTarget in ['master']:
             self.runtimeDependencies["libs/gnutls"] = None
 
 
