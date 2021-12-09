2eaa5391bfd1323d156ede49840f2fb12c895471
Nicolas Fella <nicolas.fella@gmx.de>
2021-12-09 19:41:40 +0100

    Update KDSoap to 2.0.0

--- qt-libs/kdsoap/kdsoap.py
+++ qt-libs/kdsoap/kdsoap.py
@@ -7,19 +7,13 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/KDAB/KDSoap.git"
-        for ver in ["1.7.0", "1.9.0", "1.9.1"]:
+        for ver in ["2.0.0"]:
             self.targets[ver] = f"https://github.com/KDAB/KDSoap/releases/download/kdsoap-{ver}/kdsoap-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"kdsoap-{ver}"
-        self.targetDigests['1.7.0'] = (
-            ['c13cd01cc576e22d51f2cd336f1473894a3c78ce5300ceaa23b20420f99234bd'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests['1.9.0'] = (
-            ['e3b9626d5cb08f41a709fa35031ce17bfdd075b7387baf14ecf8a9ca10994828'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests['1.9.1'] = (
-            ['a020ea26e91a2bcdbfa7bc631870ed07be2c583ae29114cfe72a5a94e0e93d27'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['2.0.0'] = (
+            ['d18963104fa6f7d02b044631cddbe78f18f70e06c607af680c7ace04d6cf04ee'], CraftHash.HashAlgorithm.SHA256)
 
-        self.patchToApply['1.7.0'] = [("kdsoap-1.6.0-20171220.diff", 1)]
-
-        self.defaultTarget = "1.9.1"
+        self.defaultTarget = "2.0.0"
         self.description = "A Qt-based client-side and server-side SOAP component"
         self.webpage = "http://www.kdab.com/products/kd-soap"
         self.displayName = "KDSoap"
