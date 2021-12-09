5ffb2897339774c2bc03dde4e60562b764bfc6e6
Nicolas Fella <nicolas.fella@gmx.de>
2021-12-09 19:45:05 +0100

    Update KDReports to 2.0.0

--- qt-libs/kdreports/kdreports.py
+++ qt-libs/kdreports/kdreports.py
@@ -9,19 +9,14 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets["master"] = "[git]https://github.com/KDAB/KDReports.git"
-        for ver in ["1.7.1", "1.9.0"]:
+        for ver in ["2.0.0"]:
             self.targets[ver] = f"https://github.com/KDAB/KDReports/archive/kdreports-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"KDReports-kdreports-{ver}"
             self.archiveNames[ver] = f"kdreports-{ver}.tar.gz"
-        self.targetDigests["1.7.1"] = (
-            ["d75f4bf3399bea51837b7a931be8640823168ba19d6dfd346db3e2270a26ca23"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["1.9.0"] = (
-            ["7bf970e2836b14cfd143e60ede2d2a4fcc23904d23079adb0ae63ea5604c010e"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["2.0.0"] = (
+            ["59c215a424d5520690eaf27dbe8b02f1aeebd9fac822890692ff42de5d1e50f6"], CraftHash.HashAlgorithm.SHA256)
 
-        self.patchToApply['1.7.1'] = [("KDReports-kdreports-1.7.1-20171220.diff", 1),
-                                      ("0001-unittests-CMakeLists.txt-don-t-call-qt_wrap_cpp-sinc.patch", 1)]
-
-        self.defaultTarget = "1.9.0"
+        self.defaultTarget = "2.0.0"
 
         self.description = "Qt library for generating printable and exportable reports from code and from XML descriptions."
         self.webpage = "http://www.kdab.com/kd-reports/"
