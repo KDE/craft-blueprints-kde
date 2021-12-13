64e9eb46f8f5d93f4cd2a1f2677ace671bf1c671
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-12-13 22:51:20 +0100

    Add QtQuickControls dependency for KNewStuff
    This is for example used in the dialogs.
    
    BUG: 446950

--- kde/frameworks/tier3/knewstuff/knewstuff.py
+++ kde/frameworks/tier3/knewstuff/knewstuff.py
@@ -11,6 +11,7 @@ class subinfo(info.infoclass):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtquickcontrols"] = None
         self.runtimeDependencies["kde/frameworks/tier1/karchive"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kcompletion"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
