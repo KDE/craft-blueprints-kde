2784388756e928b5d9ac05ba63dccb00587022d1
Nicolas Fella <nicolas.fella@gmx.de>
2021-12-13 23:20:20 +0100

    [kwordquiz] Remove kdelibs4support

--- kde/applications/kwordquiz/kwordquiz.py
+++ kde/applications/kwordquiz/kwordquiz.py
@@ -23,7 +23,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/knotifyconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
-        self.runtimeDependencies["kde/frameworks/tier4/kdelibs4support"] = None
         self.runtimeDependencies["kde/applications/libkeduvocdocument"] = None
 
 
