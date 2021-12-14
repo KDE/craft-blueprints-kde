7137bb24ac3fa7e63c77a241237be5856a44e259
Volker Krause <vkrause@kde.org>
2021-12-14 18:19:36 +0100

    Remove opensslUseLegacyBuildSystem option
    This was for pre-1.1 OpenSSL which we don't use anymore, and it
    significantly simplifies the code.

--- libs/openssl/openssl.py
+++ libs/openssl/openssl.py
@@ -55,11 +55,6 @@ class subinfo(info.infoclass):
         elif CraftCore.compiler.isMSVC():
             self.buildDependencies["dev-utils/nasm"] = None
 
-    @property
-    def opensslUseLegacyBuildSystem(self):
-        return CraftVersion(self.buildTarget) < CraftVersion("1.1")
-
-
 
 from Package.CMakePackageBase import *
 
@@ -70,8 +65,7 @@ class PackageCMake(CMakePackageBase):
         self.staticBuild = False
         self.supportsNinja = False
         self.subinfo.options.make.supportsMultijob = False
-        if not self.subinfo.opensslUseLegacyBuildSystem:
-            self.subinfo.options.install.args += ["install_sw"]
+        self.subinfo.options.install.args += ["install_sw"]
 
         self.env = {}
         if CraftCore.compiler.isAndroid:
@@ -82,78 +76,20 @@ class PackageCMake(CMakePackageBase):
             self.subinfo.options.install.args += ["SHLIB_VERSION_NUMBER=", "SHLIB_EXT=_1_1.so", f"DESTDIR={self.installDir()}"]
 
     def configure( self, defines=""):
-        if self.subinfo.opensslUseLegacyBuildSystem:
-            return True
-        else:
-            self.enterBuildDir()
-            prefix = OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())
-            args = ["perl", os.path.join(self.sourceDir(), "Configure"), f"--prefix={prefix}"] + self.subinfo.options.configure.args.split(" ")
-            if not CraftCore.compiler.isAndroid:
-                args += ["-FS",
-                         f"-I{OsUtils.toUnixPath(os.path.join(CraftStandardDirs.craftRoot(), 'include'))}",
-                         "VC-WIN64A" if CraftCore.compiler.isX64() else "VC-WIN32"]
-            with utils.ScopedEnv(self.env):
-                return utils.system(args)
+        self.enterBuildDir()
+        prefix = OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())
+        args = ["perl", os.path.join(self.sourceDir(), "Configure"), f"--prefix={prefix}"] + self.subinfo.options.configure.args.split(" ")
+        if not CraftCore.compiler.isAndroid:
+            args += ["-FS",
+                        f"-I{OsUtils.toUnixPath(os.path.join(CraftStandardDirs.craftRoot(), 'include'))}",
+                        "VC-WIN64A" if CraftCore.compiler.isX64() else "VC-WIN32"]
+        with utils.ScopedEnv(self.env):
+            return utils.system(args)
 
 
     def compile(self):
-        if not self.subinfo.opensslUseLegacyBuildSystem:
-            with utils.ScopedEnv(self.env):
-                return super().compile()
-        else:
-            self.enterSourceDir()
-            cmd = ""
-            if CraftCore.compiler.isX64():
-                config = "VC-WIN64A"
-            else:
-                config = "VC-WIN32"
-
-            if not utils.system("perl Configure %s" % config):
-                return False
-
-            if CraftCore.compiler.isX64():
-                if not utils.system("ms\do_win64a.bat"):
-                    return False
-            else:
-                if not utils.system("ms\do_nasm.bat"):
-                    return False
-
-            if self.staticBuild:
-                cmd = r"nmake -f ms\nt.mak"
-            else:
-                cmd = r"nmake -f ms\ntdll.mak"
-
-            return utils.system(cmd)
-
-    def install(self):
-        if not self.subinfo.opensslUseLegacyBuildSystem:
-            return super().install()
-        else:
-            src = self.sourceDir()
-            dst = self.imageDir()
-
-            if not os.path.isdir(dst):
-                os.mkdir(dst)
-            if not os.path.isdir(os.path.join(dst, "bin")):
-                os.mkdir(os.path.join(dst, "bin"))
-            if not os.path.isdir(os.path.join(dst, "lib")):
-                os.mkdir(os.path.join(dst, "lib"))
-            if not os.path.isdir(os.path.join(dst, "include")):
-                os.mkdir(os.path.join(dst, "include"))
-
-            if self.staticBuild:
-                outdir = "out32"
-            else:
-                outdir = "out32dll"
-
-            if not self.staticBuild:
-                shutil.copy(os.path.join(src, outdir, "libeay32.dll"), os.path.join(dst, "bin"))
-                shutil.copy(os.path.join(src, outdir, "ssleay32.dll"), os.path.join(dst, "bin"))
-            shutil.copy(os.path.join(src, outdir, "libeay32.lib"), os.path.join(dst, "lib"))
-            shutil.copy(os.path.join(src, outdir, "ssleay32.lib"), os.path.join(dst, "lib"))
-            utils.copyDir(os.path.join(src, "inc32"), os.path.join(dst, "include"))
-
-            return True
+        with utils.ScopedEnv(self.env):
+            return super().compile()
 
     def postInstall(self):
         # remove API docs here as there is no build option for that
@@ -178,49 +114,21 @@ class PackageMSys(AutoToolsPackageBase):
             self.platform = ""
         self.supportsCCACHE = False
         self.subinfo.options.configure.noDataRootDir = True
-        if not self.subinfo.opensslUseLegacyBuildSystem:
-            self.subinfo.options.install.args = "install_sw"
-        else:
-            self.subinfo.options.make.supportsMultijob = False
-            self.subinfo.options.useShadowBuild = False
-
-            # target install needs perl with native path on configure time
-            self.subinfo.options.configure.args = " shared zlib-dynamic enable-camellia enable-idea enable-mdc2 enable-tlsext enable-rfc3779"
+        self.subinfo.options.install.args = "install_sw"
 
         if CraftCore.compiler.isGCC() and not CraftCore.compiler.isNative() and CraftCore.compiler.isX86():
             self.subinfo.options.configure.args += " linux-x86"
             self.subinfo.options.configure.projectFile = "Configure"
 
-    def make(self, dummyBuildType=None):
-        if not self.subinfo.opensslUseLegacyBuildSystem:
-            return super().make()
-        else:
-            return self.shell.execute(self.sourceDir(), self.makeProgram, "depend") and \
-                AutoToolsPackageBase.make(self, dummyBuildType)
-
     def install(self):
         self.subinfo.options.make.supportsMultijob = False
-        if not self.subinfo.opensslUseLegacyBuildSystem:
-            # TODO: don't install doc
-            if not super().install():
-                return False
-            # we don't want people to link to the static build but openssl doesn't provide an option to
-            # disable the static build
-            return (utils.deleteFile(os.path.join(self.installDir(), "lib", "libcrypto.a")) and
-                    utils.deleteFile(os.path.join(self.installDir(), "lib", "libssl.a")))
-        else:
-            self.enterSourceDir()
-            self.shell.execute(self.sourceDir(), self.makeProgram,
-                            "INSTALLTOP=%s install_sw" % (self.shell.toNativePath(self.imageDir())))
-            if OsUtils.isWin():
-                self.shell.execute(os.path.join(self.imageDir(), "lib"), "chmod", "-R 664 .")
-                self.shell.execute(os.path.join(self.imageDir(), "lib", "engines"), "chmod", " -R 755 .")
-                self.shell.execute(os.path.join(self.imageDir(), "bin"), "chmod", " -R 755 .")
-                shutil.move(os.path.join(self.imageDir(), "lib", "libcrypto.dll.a"),
-                            os.path.join(self.imageDir(), "lib", "libeay32.dll.a"))
-                shutil.move(os.path.join(self.imageDir(), "lib", "libssl.dll.a"),
-                            os.path.join(self.imageDir(), "lib", "ssleay32.dll.a"))
-            return True
+        # TODO: don't install doc
+        if not super().install():
+            return False
+        # we don't want people to link to the static build but openssl doesn't provide an option to
+        # disable the static build
+        return (utils.deleteFile(os.path.join(self.installDir(), "lib", "libcrypto.a")) and
+                utils.deleteFile(os.path.join(self.installDir(), "lib", "libssl.a")))
 
 if CraftCore.compiler.isGCCLike() and not CraftCore.compiler.isMSVC() and not CraftCore.compiler.isAndroid:
     class Package(PackageMSys):
