e6d7cb7866107e45eb28edb0a1f10b180d4f6269
Volker Krause <vkrause@kde.org>
2021-12-20 17:07:53 +0100

    KTuberling also needs KConfig on Android

--- kde/kdegames/ktuberling/ktuberling.py
+++ kde/kdegames/ktuberling/ktuberling.py
@@ -13,9 +13,9 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/qt5/qtmultimedia"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
-            self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
             self.runtimeDependencies["kde/frameworks/tier1/kdbusaddons"] = None
             self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
             self.runtimeDependencies["kde/frameworks/tier2/kcompletion"] = None
