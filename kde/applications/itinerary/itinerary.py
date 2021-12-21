90b22d6170f177a6ba31498fdd67f0ae303c0528
Volker Krause <vkrause@kde.org>
2021-12-21 08:16:57 +0100

    Add KItemModels as a dependency for Itinerary

--- kde/applications/itinerary/itinerary.py
+++ kde/applications/itinerary/itinerary.py
@@ -21,6 +21,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kitemmodels"] = None
         self.runtimeDependencies["kde/frameworks/tier1/prison"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kunitconversion"] = None
         self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
