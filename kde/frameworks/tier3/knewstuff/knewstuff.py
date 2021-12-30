9af17fe7e861553e8298e36d5880cab26079a866
Julius Künzel <jk.kdedev@smartlab.uber.space>
2021-12-30 12:55:38 +0000

    After last commit: add patchLevel

--- kde/frameworks/tier3/knewstuff/knewstuff.py
+++ kde/frameworks/tier3/knewstuff/knewstuff.py
@@ -8,6 +8,7 @@ class subinfo(info.infoclass):
         self.description = "Support for downloading application assets from the network"
 
         self.patchToApply["5.89.0"] = ('FixWindowsTargets.patch', 1)
+        self.patchLevel["5.89.0"] = 1
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
