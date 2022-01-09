0935f91a2a2d3dcc1bf7fe08ab18377aa0a0ab36
Robert Lancaster <rlancaste@gmail.com>
2022-01-09 19:57:32 +0000

    Adding my custom MacOS recipes for building dependencies for INDI and KStars...
    Adding my custom MacOS recipes for building dependencies for INDI and KStars that do not yet exist in craft

---
+++ libs/_mac/libgphoto2/libgphoto2.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ['2.5.27']:
+            self.targets[ver] = 'https://downloads.sourceforge.net/project/gphoto/libgphoto/' + ver + '/libgphoto2-' + ver +  '.tar.bz2'
+            self.archiveNames[ver] = "libgphoto2-%s.tar.gz" % ver
+            self.targetInstSrc[ver] = 'libgphoto2-' + ver
+        self.description = 'Gphoto2 digital camera library'
+        self.defaultTarget = '2.5.27'
+
+    def setDependencies(self):
+        self.buildDependencies["libs/gettext"] = None
+        self.buildDependencies["dev-utils/pkg-config"] = None
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/_mac/libusb-compat"] = None
+        #gd and libexif might be needed too
+
+from Package.AutoToolsPackageBase import *
+
+class Package(AutoToolsPackageBase):
+    def fixLibraryFolder(self, folder):
+        root = str(CraftCore.standardDirs.craftRoot())
+        craftLibDir = os.path.join(root,  'lib')
+        for library in utils.filterDirectoryContent(str(folder)):
+            for path in utils.getLibraryDeps(str(library)):
+                if path.startswith(craftLibDir):
+                    utils.system(["install_name_tool", "-change", path, os.path.join("@rpath", os.path.basename(path)), library])
+            if library.endswith(".dylib"):
+                utils.system(["install_name_tool", "-id", os.path.join("@rpath", os.path.basename(path)), library])
+            utils.system(["install_name_tool", "-add_rpath", craftLibDir, library])
+
+    def __init__( self, **args ):
+        AutoToolsPackageBase.__init__( self )
+        prefix = str(self.shell.toNativePath(CraftCore.standardDirs.craftRoot()))
+        self.subinfo.options.configure.autoreconf = False
+        self.subinfo.options.useShadowBuild = False
+        self.subinfo.options.configure.cflags += " -Wno-implicit-function-declaration"
+        self.subinfo.options.configure.args += " --disable-dependency-tracking" \
+        " --disable-silent-rules" \
+        " --prefix=" + prefix
+
+    def install(self):
+        ret = AutoToolsPackageBase.install(self)
+        if OsUtils.isMac():
+            self.fixLibraryFolder(os.path.join(self.imageDir(), "lib"))
+            self.fixLibraryFolder(os.path.join(self.imageDir(), "lib", "libgphoto2", "2.5.27"))
+            self.fixLibraryFolder(os.path.join(self.imageDir(), "lib", "libgphoto2_port", "0.12.0"))
+        return ret
+
+
+
---
+++ libs/_mac/limesuite/limesuite.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ['20.10.0']:
+            self.targets[ver] = 'https://github.com/myriadrf/LimeSuite/archive/v%s.tar.gz' % ver
+            self.archiveNames[ver] = "limesuite-%s.tar.bz2" % ver
+            self.targetInstSrc[ver] = 'LimeSuite-%s' % ver
+        self.description = 'Lime suite device drivers, GUI, and SDR support'
+        self.defaultTarget = '20.10.0'
+
+    def setDependencies(self):
+        self.buildDependencies["dev-utils/pkg-config"] = None
+        self.buildDependencies["libs/swig"] = None
+        self.buildDependencies["libs/_mac/libusb"] = None
+        self.runtimeDependencies["virtual/base"] = None
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.useShadowBuild = False
+        root = str(CraftCore.standardDirs.craftRoot())
+        self.subinfo.options.configure.args = "-DENABLE_STREAM=ON -DENABLE_GUI=OFF -DENABLE_NOVENARF7=OFF -DENABLE_SOAPY_LMS7=OFF -DLIME_SUITE_EXTVER=release -DLIME_SUITE_ROOT=" + root
+
+
+
+
+
+
+
+
+
