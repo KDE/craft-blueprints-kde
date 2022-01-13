37c793dfa266ae61abec6faae2628b0e0f861f57
Albert Astals Cid <aacid@kde.org>
2022-01-13 23:50:47 +0100

    Add gyp-next
    Will need it for upcoming NSS

---
+++ python-modules/gyp-next/gyp-next.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import info
+from Package.PipPackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.description = "GYP can Generate Your Projects."
+        # The version in pip is super old so install directly from release zip
+        for ver in ['0.10.1']:
+            self.targets[ver] = f"https://github.com/nodejs/gyp-next/archive/refs/tags/v{ver}.zip"
+            self.targetInstSrc[ver] = 'gyp-next-' + ver
+        self.defaultTarget = '0.10.1'
+
+class Package(PipPackageBase):
+    def __init__(self, **args):
+        PipPackageBase.__init__(self)
