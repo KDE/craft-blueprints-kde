d049495a45c90d7f3aa53e3a6df868b0ff31f7d3
Jasem Mutlaq <mutlaqja@ikarustech.com>
2022-01-17 15:32:16 +0300

    KStars 3.5.7 release

--- kde/applications/kstars/kstars.py
+++ kde/applications/kstars/kstars.py
@@ -7,9 +7,9 @@ class subinfo(info.infoclass):
         self.versionInfo.setDefaultValues()
         
         self.description = 'a desktop planetarium'
-        self.svnTargets['3.5.6'] = 'https://invent.kde.org/education/kstars.git|stable-3.5.6'
+        self.svnTargets['3.5.7'] = 'https://invent.kde.org/education/kstars.git|stable-3.5.7'
         self.svnTargets['master'] = "https://github.com/KDE/kstars.git"
-        self.defaultTarget = 'master'
+        self.defaultTarget = '3.5.7'
         self.displayName = "KStars Desktop Planetarium"
 
     def setDependencies(self):
