ce13fd5ef09b09de6675637013546bf9e165ee14
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2022-01-18 23:45:05 +0100

    hdf5: add 1.12.1

--- libs/hdf5/hdf5.py
+++ libs/hdf5/hdf5.py
@@ -8,6 +8,9 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-{ver}/src/hdf5-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"hdf5-{ver}"
             self.archiveNames[ver] = f"hdf5-{ver}.tar.gz"
+        for ver in ["1.12.1"]:
+            self.targets[ver] = f"https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-{ver}/src/hdf5-{ver}.tar.bz2"
+            self.targetInstSrc[ver] = f"hdf5-{ver}"
         self.targetDigests['1.10.5'] = (
             ['6d4ce8bf902a97b050f6f491f4268634e252a63dadd6656a1a9be5b7b7726fa8'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['1.10.6'] = (
@@ -16,6 +19,8 @@ class subinfo(info.infoclass):
             ['7a1a0a54371275ce2dfc5cd093775bb025c365846512961e7e5ceaecb437ef15'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['1.10.8'] = (
             ['d341b80d380dd763753a0ebe22915e11e87aac4e44a084a850646ff934d19c80'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.12.1'] = (
+            ['aaf9f532b3eda83d3d3adc9f8b40a9b763152218fa45349c3bc77502ca1f8f1c'], CraftHash.HashAlgorithm.SHA256)
         self.description = 'A data model, library, and file format for storing and managing data'
 
         self.patchToApply['1.10.7'] = [('hdf5-1.10.7-file-locking.diff', 1)]
@@ -29,3 +34,6 @@ class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
         self.subinfo.options.configure.args = "-DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DHDF5_ENABLE_Z_LIB_SUPPORT=ON"
+        # build with the 1.10 APIs
+        if self.buildTarget == "1.12.1":
+            self.subinfo.options.configure.args += " -DDEFAULT_API_VERSION:STRING=v110"
