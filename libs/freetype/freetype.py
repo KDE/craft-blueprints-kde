ca7b3f4cd1b1e9c6238b95f09d1dcb20da15fde6
Albert Astals Cid <aacid@kde.org>
2022-01-20 15:53:35 +0100

    freetype: Update to 2.11.1
    We don't need any patches in it as far as i can see, that is nice :)

--- libs/freetype/freetype.py
+++ libs/freetype/freetype.py
@@ -8,6 +8,9 @@ class subinfo(info.infoclass):
         for ver in ['2.3.12', '2.5.0.1', '2.9.1']:
             self.targets[ver] = "http://downloads.sourceforge.net/freetype/freetype-" + ver + ".tar.bz2"
             self.targetInstSrc[ver] = "freetype-" + ver
+        for ver in ['2.11.1']:
+            self.targets[ver] = "http://downloads.sourceforge.net/freetype/freetype-" + ver + ".tar.xz"
+            self.targetInstSrc[ver] = "freetype-" + ver
         self.patchToApply['2.3.12'] = ('freetype-2.3.12.diff', 1)
         self.patchToApply['2.5.0.1'] = ('freetype-2.5.0.1.diff', 1)
         self.patchToApply['2.9.1'] = [("freetype-2.9.1-20180925.diff", 1),
@@ -18,7 +21,7 @@ class subinfo(info.infoclass):
         self.targetDigests['2.5.0.1'] = '4bbd8357b4b723e1ff38414a9eaf50bf99dacb84'
         self.targetDigests['2.9.1'] = (['db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d'], CraftHash.HashAlgorithm.SHA256)
         self.patchLevel['2.9.1'] = 5
-        self.defaultTarget = '2.9.1'
+        self.defaultTarget = '2.11.1'
         self.description = "A Free, High-Quality, and Portable Font Engine"
 
     def setDependencies(self):
