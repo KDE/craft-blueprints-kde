568679fdaab80d538cbd2401ae0f7857f336775a
Albert Astals Cid <aacid@kde.org>
2022-02-02 00:06:21 +0100

    Update poppler

--- qt-libs/poppler/poppler.py
+++ qt-libs/poppler/poppler.py
@@ -32,7 +32,7 @@ class subinfo(info.infoclass):
         self.svnTargets["master"] = "git://git.freedesktop.org/git/poppler/poppler"
 
         # always try to use latest libpoppler with all security fixes
-        ver = "22.01.0"
+        ver = "22.02.0"
         self.targets[ver] = f"https://poppler.freedesktop.org/poppler-{ver}.tar.xz"
         self.targetInstSrc[ver] = f"poppler-{ver}"
         self.targetDigests[ver] = (['7d3493056b5b86413e5c693c2cae02c5c06cd8e618d14c2c31e2c84b67b2313e'], CraftHash.HashAlgorithm.SHA256)
