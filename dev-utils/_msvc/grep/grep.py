65c653a25e6515e986c84f979dc3025638de866b
Hannah von Reth <vonreth@kde.org>
2022-02-07 11:06:25 +0100

    Use our 3.7 build of grep instead of a 10 years old 2.5.4

--- dev-utils/_msvc/grep/grep.py
+++ dev-utils/_msvc/grep/grep.py
@@ -1,34 +1,17 @@
 import info
+from Package.BinaryPackageBase import *
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for t in ('2.5.1a', '2.5.4'):
-            self.targets[t] = ["http://downloads.sourceforge.net/sourceforge/gnuwin32/grep-%s-bin.zip" % t,
-                               "http://downloads.sourceforge.net/sourceforge/gnuwin32/grep-%s-dep.zip" % t]
-            self.targetInstallPath[t] = "dev-utils"
-        self.targetDigests['2.5.4'] = ['56f41d351b3ed8ac671df4dd3bbd4c4d3b9190a2',
-                                       '6dc3a0d1a1751c731fb680a01650a1396c76648c']
-        self.defaultTarget = '2.5.4'
+        self.addCachedBuild("https://files.kde.org/craft/prebuilt/packages/", packageName="dev-utils/_autotools/grep", targetInstallPath="dev-utils")
 
     def setDependencies(self):
-        self.runtimeDependencies["virtual/bin-base"] = None
-
-
-from Package.BinaryPackageBase import *
-
+        self.buildDependencies["virtual/bin-base"] = None
 
 class Package(BinaryPackageBase):
     def __init__(self):
         BinaryPackageBase.__init__(self)
 
-    def install(self):
-        if not BinaryPackageBase.install(self):
-            return False
-
-        manifestDir = os.path.join(self.imageDir(), "manifest")
-        if os.path.exists(manifestDir):
-            for file in os.listdir(manifestDir):
-                if file.endswith('.mft'):
-                    os.remove(os.path.join(manifestDir, file))
-        return True
+    def postInstall(self):
+        return utils.createShim(os.path.join(self.imageDir(), "dev-utils", "bin", "grep.exe"), os.path.join(self.installDir(), "bin", "grep.exe"))
