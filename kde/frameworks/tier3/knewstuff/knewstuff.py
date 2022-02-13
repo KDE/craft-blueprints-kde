7ac079f43a7ce39b71a43505d159e0948ea5db3e
Julius Künzel <jk.kdedev@smartlab.uber.space>
2022-02-13 23:18:05 +0000

    [KNewStuff] Add missing qqc2-desktop-style dep

--- kde/frameworks/tier3/knewstuff/knewstuff.py
+++ kde/frameworks/tier3/knewstuff/knewstuff.py
@@ -22,6 +22,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/kdeclarative"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies['kde/frameworks/tier1/kirigami'] = None
+        self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kitemviews"] = None
