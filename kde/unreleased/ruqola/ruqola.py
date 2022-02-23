49fb11d7839d8c7c301fbede7b1a3d28cd68d565
Nicolas Fella <nicolas.fella@gmx.de>
2022-02-24 00:43:04 +0100

    Ruqola 1.6.3

--- kde/unreleased/ruqola/ruqola.py
+++ kde/unreleased/ruqola/ruqola.py
@@ -28,11 +28,11 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
 
-        self.targets['1.6.2'] = 'https://download.kde.org/stable/ruqola/ruqola-1.6.2.tar.xz'
-        self.targetDigests['1.6.2'] = (['b8e801745c0eb340e081e3763004d1d628453abbf8b6dc5cea41915832a81df2'], CraftHash.HashAlgorithm.SHA256)
-        self.targetInstSrc['1.6.2'] = 'ruqola-1.6.2'
+        self.targets['1.6.3'] = 'https://download.kde.org/stable/ruqola/ruqola-1.6.3.tar.xz'
+        self.targetDigests['1.6.3'] = (['70a422793b186c544139c61ff2ee708ce266e931eacb0cdb9f37f125a531c541'], CraftHash.HashAlgorithm.SHA256)
+        self.targetInstSrc['1.6.3'] = 'ruqola-1.6.3'
         self.svnTargets["master"] = "https://invent.kde.org/network/ruqola.git"
-        self.defaultTarget = "1.6.2"
+        self.defaultTarget = "1.6.3"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
