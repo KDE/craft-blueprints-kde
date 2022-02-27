731b0ff4bfd949bda5222079f1e573fbeb13cfc1
Michael Reeves <reeves.87@gmail.com>
2022-02-27 15:04:55 -0500

    [KDiff3] Add 1.9.5 tag

--- extragear/kdiff3/kdiff3.py
+++ extragear/kdiff3/kdiff3.py
@@ -76,8 +76,8 @@ class Package(CMakePackageBase):
         else:
             #Windows app store has special requirements for the version format
             #Craft attempts to alter the second and third number so we have to adjust to craft's logic as well.
-            self.defines["version"] = "1.0.94"
                         
+            self.defines["version"] = "1.0.95"
             self.defines["un_sections"] = r"""
         Section "Un.Cleanup Regsistry"
         ;Maybe left behind due to a bug in previous installers.
