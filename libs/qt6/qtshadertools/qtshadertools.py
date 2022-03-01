df34a8ea44c2762b4b453d822003aa785b6d67cf
Hannah von Reth <vonreth@kde.org>
2022-03-01 20:51:54 +0100

    Add more Qt6 modules

---
+++ libs/qt6/qtshadertools/qtshadertools.py
@@ -0,0 +1,24 @@
+from ctypes import util
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt6/qtbase"] = None
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.args += ["-DINSTALL_PUBLICBINDIR=bin"]
+
+    def install(self):
+        if not super().install():
+            return False
+        return self.isntallQt6()
