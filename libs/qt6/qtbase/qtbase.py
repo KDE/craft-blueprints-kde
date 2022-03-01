ac4720ce34d7b4b08fe2986be58fb9ed4a5b1a4c
Hannah von Reth <vonreth@kde.org>
2022-03-01 17:57:24 +0100

    make qt6 coinstallable on Windows

--- libs/qt6/qtbase/qtbase.py
+++ libs/qt6/qtbase/qtbase.py
@@ -1,3 +1,4 @@
+from ctypes import util
 import info
 
 
@@ -29,6 +30,8 @@ class subinfo(info.infoclass):
             self.runtimeDependencies["libs/libjpeg-turbo"] = None
             self.runtimeDependencies["libs/sqlite"] = None
             self.runtimeDependencies["libs/pcre2"] = None
+            self.runtimeDependencies["libs/harfbuzz"] = None
+            self.runtimeDependencies["libs/freetype"] = None
             if CraftCore.compiler.isUnix and self.options.dynamic.withGlib:
                 self.runtimeDependencies["libs/glib"] = None
 
@@ -39,15 +42,54 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
+
+        dataDir = CraftCore.standardDirs.locations.data.relative_to(CraftCore.standardDirs.craftRoot())
         self.subinfo.options.configure.args += [
+            #"-DINSTALL_PUBLICBINDIR=bin",
+            "-DINSTALL_BINDIR=lib/qt6/bin",
+            "-DINSTALL_LIBEXECDIR=lib/qt6",
+            "-DINSTALL_ARCHDATADIR=lib/qt6",
+            "-DINSTALL_INCLUDEDIR=include/qt6",
+            "-DINSTALL_MKSPECSDIR=lib/qt6/mkspecs",
+            f"-DINSTALL_DOCDIR=doc/qt6",
+            f"-DINSTALL_DATADIR={dataDir}/qt6",
+
             "-DFEATURE_pkg_config=ON",
             
             "-DFEATURE_system_sqlite=ON",
             "-DFEATURE_system_pcre2=ON",
             "-DFEATURE_system_zlib=ON",
+            "-DFEATURE_system_harfbuzz=ON",
+            "-DFEATURE_system_freetype=ON",
 
             "-DQT_BUILD_EXAMPLES=OFF"
         ]
 
         if self.subinfo.options.dynamic.useLtcg:
             self.subinfo.options.configure.args += ["-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON"]
+
+    def make(self):
+        env = {}
+        if CraftCore.compiler.isWindows:
+            env["PATH"] = f"{self.buildDir() / 'lib/qt6/bin'};{os.environ['PATH']}"
+        with utils.ScopedEnv(env):
+            return super().make()
+
+    def install(self):
+        if not super().install():
+            return False
+        if CraftCore.compiler.isWindows:
+            # TODO: there must be a more elegant way
+            dll = utils.filterDirectoryContent(self.installDir() / "lib/qt6/bin",
+                                                    whitelist=lambda x, root: x.name.endswith(".dll") or x.name.endswith("6.exe"),
+                                                    blacklist=lambda x, root: True)
+            utils.createDir(self.installDir() / "bin")
+            for d in dll:
+                src = Path(d)
+                if not utils.copyFile(src, self.installDir() / "bin" / src.name):
+                    return False
+                src = src.with_suffix(".pdb")
+                if src.exists():
+                    if not utils.copyFile(src, self.installDir() / "bin" / src.name):
+                        return False
+        return True
