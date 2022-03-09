293f22d11e580f0446eca6d920e9e3ee62b3577e
Jasem Mutlaq <mutlaqja@ikarustech.com>
2022-03-10 01:36:36 +0300

    Use INDI v1.9.5

--- libs/indiclient/indiclient.py
+++ libs/indiclient/indiclient.py
@@ -6,9 +6,9 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets['1.9.1'] = "https://github.com/indilib/indi.git||v1.9.1"
-        self.defaultTarget = '1.9.1'
-        self.patchLevel['1.9.1'] = 1
+        self.svnTargets['1.9.5'] = "https://github.com/indilib/indi.git||v1.9.5"
+        self.defaultTarget = '1.9.5'
+        self.patchLevel['1.9.5'] = 1
         self.description = 'INDI Library'
 
     def setDependencies(self):
