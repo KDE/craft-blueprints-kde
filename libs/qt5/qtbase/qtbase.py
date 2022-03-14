c566deef146234df365e407447193aea9f369875
Hannah von Reth <vonreth@kde.org>
2022-03-14 15:14:46 +0100

    Add https://www.qt.io/blog/security-advisory-qprocess for 5.12.11

--- libs/qt5/qtbase/qtbase.py
+++ libs/qt5/qtbase/qtbase.py
@@ -77,7 +77,7 @@ class subinfo(info.infoclass):
         self.patchLevel["5.12.3"] = 1
         self.patchLevel["5.12.9"] = 1
         self.patchLevel["5.12.10"] = 2
-        self.patchLevel["5.12.11"] = 1
+        self.patchLevel["5.12.11"] = 2
         self.patchLevel["5.15.2"] = 7
         self.description = "a cross-platform application framework"
 
