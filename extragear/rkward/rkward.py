b811fa1f7ec8fc63bdfb721cb0045ab10e8e2d84
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2022-03-15 20:41:00 +0100

    Go back to qtwebengine on Mac

--- extragear/rkward/rkward.py
+++ extragear/rkward/rkward.py
@@ -14,9 +14,8 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/ktexteditor"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
         self.runtimeDependencies["libs/qt5/qtscript"] = None
-        if CraftCore.compiler.isMinGW() or OsUtils.isMac():
+        if CraftCore.compiler.isMinGW():
             # MinGW has not qtwebengine, but we can fall back to kdewebkit
-            # Mac is temporarily out of qtwebengine, too.
             self.runtimeDependencies["kde/frameworks/tier3/kdewebkit"] = None
         else:
             self.runtimeDependencies['libs/qt5/qtwebengine'] = None
