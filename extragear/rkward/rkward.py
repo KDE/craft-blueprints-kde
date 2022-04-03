6266cdc5c807b72aa152c5ea14355de717312044
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2022-04-03 19:24:59 +0200

    RKWard: QWebEngine does not appear to work well on Mac. Give WebKit another try.

--- extragear/rkward/rkward.py
+++ extragear/rkward/rkward.py
@@ -21,8 +21,8 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/ktexteditor"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
         self.runtimeDependencies["libs/qt5/qtscript"] = None
-        if CraftCore.compiler.isMinGW():
-            # MinGW has not qtwebengine, but we can fall back to kdewebkit
+        if CraftCore.compiler.isMinGW() or OsUtils.isMac():
+            # MinGW has no qtwebengine, but we can fall back to kdewebkit
             self.runtimeDependencies["kde/frameworks/tier3/kdewebkit"] = None
         else:
             self.runtimeDependencies['libs/qt5/qtwebengine'] = None
