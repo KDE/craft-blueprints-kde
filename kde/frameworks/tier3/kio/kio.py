6457f1a698240893aab6ae2a01ca2a126f771bfc
Nicolas Fella <nicolas.fella@gmx.de>
2022-04-05 19:04:04 +0200

    [kio] ktextwidgets isn't used on Android

--- kde/frameworks/tier3/kio/kio.py
+++ kde/frameworks/tier3/kio/kio.py
@@ -36,13 +36,13 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/solid"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/ktextwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/frameworks/tier2/kcrash"] = None
             self.runtimeDependencies["kde/frameworks/tier1/kdbusaddons"] = None
             self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
             self.runtimeDependencies["kde/frameworks/tier3/kwallet"] = None
+            self.runtimeDependencies["kde/frameworks/tier3/ktextwidgets"] = None
 
         if CraftCore.compiler.isLinux or CraftCore.compiler.isFreeBSD:
             self.runtimeDependencies["kde/frameworks/tier3/kded"] = None
