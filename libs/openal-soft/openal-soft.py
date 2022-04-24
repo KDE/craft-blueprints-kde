43050e7fffd3744195f153500e3afd7730247ae0
Julius Künzel <jk.kdedev@smartlab.uber.space>
2022-04-24 18:27:17 +0000

    [openal-soft] fix source location

--- libs/openal-soft/openal-soft.py
+++ libs/openal-soft/openal-soft.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ['1.21.1']:
             self.targets[ver] = f'https://github.com/kcat/openal-soft/archive/refs/tags/{ver}.tar.gz'
-            self.targetInstSrc[ver] = f'openal-soft-openal-soft-{ver}'
+            self.targetInstSrc[ver] = f'openal-soft-{ver}'
         self.targetDigests["1.21.1"] = (['8ac17e4e3b32c1af3d5508acfffb838640669b4274606b7892aa796ca9d7467f'], CraftHash.HashAlgorithm.SHA256)
         self.description = 'a library for audio support'
         self.defaultTarget = '1.21.1'
