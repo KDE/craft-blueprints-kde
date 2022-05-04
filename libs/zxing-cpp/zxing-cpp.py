5ec795ed9f49478a9bde053aceeb79ede994444c
Volker Krause <vkrause@kde.org>
2022-05-04 18:00:09 +0200

    Update ZXing to 1.3.0

--- libs/zxing-cpp/zxing-cpp.py
+++ libs/zxing-cpp/zxing-cpp.py
@@ -8,15 +8,15 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/nu-book/zxing-cpp.git'
-        self.defaultTarget = '1.2.0'
+        self.defaultTarget = '1.3.0'
         self.targets[self.defaultTarget] = f"https://github.com/nu-book/zxing-cpp/archive/v{self.defaultTarget}.tar.gz"
         self.archiveNames[self.defaultTarget] = f"zxing-cpp-v{self.defaultTarget}.tar.gz"
         self.targetInstSrc[self.defaultTarget] = f"zxing-cpp-{self.defaultTarget}"
         self.targetDigests[self.defaultTarget] = (
-            ['653d9e44195d86cf64a36af9ff3a1978ec5599df3882439fefa56e7064f55e8a'], CraftHash.HashAlgorithm.SHA256)
+            ['bfd8fc706def30e34f96088b5a7afdbe0917831e57a774d34e3ee864b01c6891'], CraftHash.HashAlgorithm.SHA256)
 
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
 
-        self.subinfo.options.configure.args += "-DBUILD_BLACKBOX_TESTS=OFF -DBUILD_EXAMPLES=OFF"
+        self.subinfo.options.configure.args += "-DBUILD_DEPENDENCIES=LOCAL -DBUILD_EXAMPLES=OFF -DBUILD_UNIT_TESTS=OFF -DBUILD_BLACKBOX_TESTS=OFF"
