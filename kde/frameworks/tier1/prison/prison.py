fd75ecce801a252beb2e4a1a28b3cffd63c2e329
Volker Krause <vkrause@kde.org>
2022-05-15 17:53:37 +0200

    Add Qt Multimedia as dependency of Prison
    Starting with 5.94 this can be used for Prison's barcode scanner module.

--- kde/frameworks/tier1/prison/prison.py
+++ kde/frameworks/tier1/prison/prison.py
@@ -14,6 +14,7 @@ class subinfo(info.infoclass):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
+        self.runtimeDependencies["libs/qt5/qtmultimedia"] = None
         self.runtimeDependencies["libs/qrencode"] = None
         self.runtimeDependencies["libs/libdmtx"] = None
         self.runtimeDependencies["libs/zxing-cpp"] = None
