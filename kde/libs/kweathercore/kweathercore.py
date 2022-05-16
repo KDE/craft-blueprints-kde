2342bdec2c1066bd7e42762dacf3942dc0c09879
Volker Krause <vkrause@kde.org>
2022-05-16 17:44:14 +0200

    KWeatherCore depends on KHolidays for sunset computations now

--- kde/libs/kweathercore/kweathercore.py
+++ kde/libs/kweathercore/kweathercore.py
@@ -24,6 +24,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/qt5/qtlocation"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kholidays"] = None
 
 class Package(CMakePackageBase):
     def __init__(self):
