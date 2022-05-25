56053605081c23abc10bf35a5140e0d89d0a32f1
Hannah von Reth <vonreth@kde.org>
2022-05-25 09:29:21 +0200

    qtmultimedia: don't link to system gstreamer, mixing system glib and ours will cause issues

--- libs/qt5/qtmultimedia/qtmultimedia.py
+++ libs/qt5/qtmultimedia/qtmultimedia.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
         self.versionInfo.setDefaultValues()
         if CraftCore.compiler.isMinGW():
             self.patchToApply["5.15.2"] = [("0001-fix-wmf-plugin.patch", 1)]
+        self.patchLevel["5.15.2"] = 1
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
@@ -18,6 +19,7 @@ from Package.Qt5CorePackageBase import *
 class QtPackage(Qt5CorePackageBase):
     def __init__(self, **args):
         Qt5CorePackageBase.__init__(self)
+        self.subinfo.options.dynamic.featureArguments += ["-no-gstreamer"]
 
 
 class Package(Qt5CoreSdkPackageBase):
