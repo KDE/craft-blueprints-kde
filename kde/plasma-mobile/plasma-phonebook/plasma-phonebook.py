be6b4dd6e8ddc0be1b0be67e29a1edc0fcedda45
Nicolas Fella <nicolas.fella@gmx.de>
2022-06-01 20:11:13 +0200

    Add kirigami-addons to plasma-phonebook

--- kde/plasma-mobile/plasma-phonebook/plasma-phonebook.py
+++ kde/plasma-mobile/plasma-phonebook/plasma-phonebook.py
@@ -20,6 +20,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier2/kcontacts"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kpeople"] = None
         self.runtimeDependencies["kde/pim/kpeoplevcard"] = None
+        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
 
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/plasma/breeze"] = None
