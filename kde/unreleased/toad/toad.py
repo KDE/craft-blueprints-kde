d008567e6b940812f56f6dcabd311bcb9647d2df
Nicolas Fella <nicolas.fella@gmx.de>
2022-06-03 14:11:56 +0200

    [toad] Add kitemmodels

--- kde/unreleased/toad/toad.py
+++ kde/unreleased/toad/toad.py
@@ -19,6 +19,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kitemmodels"] = None
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/frameworks/tier1/kdbusaddons"] = None
             self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
