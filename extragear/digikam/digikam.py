3764d459436e6e09f3f2976071a43c492839d7e7
Gilles Caulier <caulier.gilles@gmail.com>
2022-06-15 06:25:57 +0200

    try to add Marble deps at run time

--- extragear/digikam/digikam.py
+++ extragear/digikam/digikam.py
@@ -87,7 +87,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier2/kfilemetadata"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcalendarcore"] = None
         self.runtimeDependencies['kde/frameworks/tier3/kio'] = None
-#        self.packagingDependencies["extragear/digikam/libmarble"] = None        # install libmarble only for geolocation
+        self.runtimeDependencies["extragear/digikam/marble"] = None        # install libmarble, plugins and data for geolocation
 
 from Package.CMakePackageBase import *
 
