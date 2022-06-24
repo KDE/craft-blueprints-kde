e9ef6aeeaaf8101c6b2db60ba226115f8818f266
Gilles Caulier <caulier.gilles@gmail.com>
2022-06-24 03:41:17 +0200

    try to drop kdepim deps

--- extragear/digikam/digikam.py
+++ extragear/digikam/digikam.py
@@ -128,11 +128,11 @@ class subinfo(info.infoclass):
 
         # Required at run-time even if option is disabled in digiKam at compilation stage.
 
-        if not CraftCore.compiler.isWindows:
-            self.runtimeDependencies['libs/gpgme']                      = 'default'
-
-        self.runtimeDependencies["kde/frameworks/tier2/kfilemetadata"]  = None
-        self.runtimeDependencies["kde/pim/akonadi-contacts"]            = None
+#        if not CraftCore.compiler.isWindows:
+#            self.runtimeDependencies['libs/gpgme']                      = 'default'
+#
+#        self.runtimeDependencies["kde/frameworks/tier2/kfilemetadata"]  = None
+#        self.runtimeDependencies["kde/pim/akonadi-contacts"]            = None
 
         # Install libmarble, plugins and data for geolocation.
         # Marble application will be removed at packaging stage.
