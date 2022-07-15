4450b2e273d2c686e1c6ff5c82c491d0817927ce
Hannah von Reth <vonreth@kde.org>
2022-07-15 21:55:28 +0200

    libssh2 1.10.0

--- libs/libssh2/libssh2.py
+++ libs/libssh2/libssh2.py
@@ -6,15 +6,11 @@ from Package.AutoToolsPackageBase import AutoToolsPackageBase
 class subinfo(info.infoclass):
     def setTargets( self ):
         self.svnTargets['master'] = 'https://github.com/libssh2/libssh2.git'
-        for ver in ["1.8.0", "1.9.0"]:
+        for ver in ["1.10.0"]:
             self.targets[ver] = f"https://www.libssh2.org/download/libssh2-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"libssh2-{ver}"
-        self.targetDigests["1.8.0"] = (['39f34e2f6835f4b992cafe8625073a88e5a28ba78f83e8099610a7b3af4676d4'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["1.9.0"] = (['d5fb8bd563305fd1074dda90bd053fb2d29fc4bce048d182f96eaa466dfadafd'], CraftHash.HashAlgorithm.SHA256)
-        self.patchLevel["1.8.0"] = 6
-        self.patchToApply["1.9.0"] = [("libssh2-configure-no-ws2_32-in-LIBS.patch", 0)]
-        self.patchLevel["1.9.0"] = 1
-        self.defaultTarget = '1.9.0'
+        self.targetDigests["1.10.0"] = (['2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = '1.10.0'
 
     def setDependencies( self ):
         self.buildDependencies['virtual/base'] = 'default'
@@ -27,12 +23,14 @@ if not CraftCore.compiler.isGCCLike():
     class Package(CMakePackageBase):
         def __init__(self, **args):
             CMakePackageBase.__init__(self)
-            # the lib has no exports so we can't build it shared
-            self.subinfo.options.configure.args += " -DENABLE_ZLIB_COMPRESSION=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF"
+            self.subinfo.options.configure.args += ["-DENABLE_ZLIB_COMPRESSION=ON",
+                                                    "-DBUILD_SHARED_LIBS=ON",
+                                                    "-DBUILD_EXAMPLES=OFF",
+                                                    "-DBUILD_TESTING=OFF"]
 else:
     class Package(AutoToolsPackageBase):
         def __init__(self, **args):
             AutoToolsPackageBase.__init__(self)
             # configure.ac:129: error: m4_undefine: undefined macro: backend
             self.subinfo.options.configure.autoreconf = False
-            self.subinfo.options.configure.args = " --disable-static --enable-shared"
+            self.subinfo.options.configure.args += ["--disable-static", "--enable-shared"]
