2dfdb417b4ec3651a384b70f3c5c0103b71016ff
Jasem Mutlaq <mutlaqja@ikarustech.com>
2022-07-29 19:37:15 +0300

    Upgrade INDI client to 1.9.7

--- libs/indiclient/indiclient.py
+++ libs/indiclient/indiclient.py
@@ -6,8 +6,8 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets['1.9.5.1'] = "https://github.com/indilib/indi.git||v1.9.5.1"
-        self.defaultTarget = '1.9.5.1'
+        self.svnTargets['1.9.7'] = "https://github.com/indilib/indi.git||v1.9.7"
+        self.defaultTarget = '1.9.7'
         self.description = 'INDI Library'
 
     def setDependencies(self):
