0cc08783ce7b5915568086d3a141929082b03e60
Robert Lancaster <rlancaste@gmail.com>
2022-07-29 16:24:20 +0000

    Fixing Theora Recipe: Updating mirror location, adding theora checksum,...
    Fixing Theora Recipe: Updating mirror location, adding theora checksum, changing arguments, updating install source folder name, and adding dependency for libogg

--- libs/_mac/theora/theora.py
+++ libs/_mac/theora/theora.py
@@ -5,14 +5,16 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ['1.1.1']:
-            self.targets[ver] = 'https://downloads.xiph.org/releases/theora/libtheora-' + ver + '.tar.bz2'
+            self.targets[ver] = 'https://github.com/xiph/theora/archive/refs/tags/v' + ver + '.tar.gz'
             self.archiveNames[ver] = "libtheora-%s.tar.gz" % ver
-            self.targetInstSrc[ver] = 'libtheora-' + ver
+            self.targetInstSrc[ver] = 'theora-' + ver
+            self.targetDigests['1.1.1'] = (['1d5c3b25bbced448f3e741c42df6796e3d5e57136a74bcd68262318290ec2982'], CraftHash.HashAlgorithm.SHA256)
         self.description = 'Open video compression format'
         self.defaultTarget = '1.1.1'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/libogg"] = None
 
 from Package.AutoToolsPackageBase import *
 
@@ -22,11 +24,10 @@ class Package(AutoToolsPackageBase):
         prefix = str(self.shell.toNativePath(CraftCore.standardDirs.craftRoot()))
        	self.subinfo.options.configure.bootstrap = True
        	self.subinfo.options.useShadowBuild = False
-        self.subinfo.options.configure.args += " --disable-dependency-tracking" \
-        " --prefix=" + prefix + \
-        " --disable-oggtest" + \
-        " --disable-vorbistest" + \
-        " --disable-examples"
+        self.subinfo.options.configure.args += ["--disable-dependency-tracking",
+        "--disable-oggtest",
+        "--disable-vorbistest",
+        "--disable-examples"]
 
 
 
