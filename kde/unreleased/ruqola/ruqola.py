e6f533524c6bf8008c578117f5d9858b693af78a
Nicolas Fella <nicolas.fella@gmx.de>
2022-08-11 13:49:14 +0200

    Ruqola 1.7.4

--- kde/unreleased/ruqola/ruqola.py
+++ kde/unreleased/ruqola/ruqola.py
@@ -32,11 +32,11 @@ class subinfo(info.infoclass):
         self.description = "Rocket.Chat Client"
         self.webpage = "https://apps.kde.org/ruqola"
 
-        self.targets['1.7.3'] = 'https://download.kde.org/stable/ruqola/ruqola-1.7.3.tar.xz'
-        self.targetDigests['1.7.3'] = (['f5415aee9de45e216c33114578ad9d376dfa3b5daea3790317eecd5c2031eab5'], CraftHash.HashAlgorithm.SHA256)
-        self.targetInstSrc['1.7.3'] = 'ruqola-1.7.3'
+        self.targets['1.7.4'] = 'https://download.kde.org/stable/ruqola/ruqola-1.7.4.tar.xz'
+        self.targetDigests['1.7.4'] = (['f55f146651fb5f0f34a6b204f4d71b2baac9f6f93f0b8cb7423012b1ae55a93d'], CraftHash.HashAlgorithm.SHA256)
+        self.targetInstSrc['1.7.4'] = 'ruqola-1.7.4'
         self.svnTargets["master"] = "https://invent.kde.org/network/ruqola.git"
-        self.defaultTarget = "1.7.3"
+        self.defaultTarget = "1.7.4"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
