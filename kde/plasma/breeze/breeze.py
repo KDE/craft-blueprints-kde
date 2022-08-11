57a1dd66dcf0646045e7779555c9d00289f9461a
Nicolas Fella <nicolas.fella@gmx.de>
2022-08-11 14:33:33 +0200

    Breeze needs Kirigami

--- kde/plasma/breeze/breeze.py
+++ kde/plasma/breeze/breeze.py
@@ -12,6 +12,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/libfftw"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kguiaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
