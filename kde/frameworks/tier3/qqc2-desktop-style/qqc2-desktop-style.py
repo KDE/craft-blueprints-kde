907679762eaa656ca27923e78683379f727845a7
Volker Krause <vkrause@kde.org>
2022-08-15 17:37:34 +0200

    Update to KDE Frameworks 5.97

--- kde/frameworks/tier3/qqc2-desktop-style/qqc2-desktop-style.py
+++ kde/frameworks/tier3/qqc2-desktop-style/qqc2-desktop-style.py
@@ -10,10 +10,6 @@ class subinfo(info.infoclass):
 
         self.description = "QtQuickControls 2 style that integrates with the desktop"
 
-        self.patchToApply["5.91.0"] = ('FixWindowsTargets.diff', 1)
-        self.patchLevel["5.91.0"] = 1
-
-
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
