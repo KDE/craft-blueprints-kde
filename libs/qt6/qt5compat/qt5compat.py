201e31352a7151d0352e4e6c00d5d768c32d7cdb
Volker Krause <vkrause@kde.org>
2022-09-18 22:05:16 +0200

    Add Qt 6 Qt5Compat blueprint
    Needed by several KF6 modules.

---
+++ libs/qt6/qt5compat/qt5compat.py
@@ -0,0 +1,17 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt6/qtbase"] = None
+        self.runtimeDependencies["libs/qt6/qtdeclarative"] = None
+
+
+from Blueprints.CraftPackageObject import CraftPackageObject
+
+class Package(CraftPackageObject.get('libs/qt6').pattern):
+    def __init__(self):
+        CraftPackageObject.get('libs/qt6').pattern.__init__(self)
