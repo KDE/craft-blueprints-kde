f6891f42a49b65e3a21cac8263aa52290e84da6b
Jean-Baptiste Mardelle <jb@kdenlive.org>
2022-09-30 14:54:39 +0000

    Update mlt with latest qtblend fix and cairoblend optimization

--- libs/mlt/mlt.py
+++ libs/mlt/mlt.py
@@ -14,12 +14,12 @@ class subinfo(info.infoclass):
 
         self.svnTargets['master'] = "https://github.com/mltframework/mlt.git"
         self.patchLevel['master'] = 20220704
-        self.svnTargets['350fb53'] = "https://github.com/mltframework/mlt.git||350fb53d082b6526478961c91cb7fe1e526d98fb"
-        self.defaultTarget = '350fb53'
+        self.svnTargets['5e3a0db'] = "https://github.com/mltframework/mlt.git||5e3a0db75b40e2174487938ae08d0cdc6dd50ab3"
+        self.defaultTarget = '5e3a0db'
         if CraftCore.compiler.isMacOS:
-            self.patchToApply["350fb53"] = [("001-fix-avformat-mac.diff", 1)]
+            self.patchToApply["5e3a0db"] = [("001-fix-avformat-mac.diff", 1)]
         if CraftCore.compiler.isWindows:
-            self.patchToApply["350fb53"] = [("pi_patch.diff", 1)]
+            self.patchToApply["5e3a0db"] = [("pi_patch.diff", 1)]
 
     def setDependencies( self ):
         self.buildDependencies["dev-utils/pkg-config"] = None
