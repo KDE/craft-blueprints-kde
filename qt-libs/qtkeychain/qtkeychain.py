66817f8751a03f5aa03a59469df0c9ad52a04902
Volker Krause <vkrause@kde.org>
2022-10-07 09:18:40 +0200

    Add Qt 6 support for QtKeychain
    Similar to QCA this doesn't have a proper release with full Qt 6 support
    yet unfortunately.

--- qt-libs/qtkeychain/qtkeychain.py
+++ qt-libs/qtkeychain/qtkeychain.py
@@ -5,12 +5,16 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setDependencies(self):
-        self.buildDependencies["libs/qt5/qttools"] = None
         self.runtimeDependencies["virtual/base"] = None
-        self.runtimeDependencies["libs/qt5/qtbase"] = None
         self.runtimeDependencies["libs/libsecret"] = None
-        if CraftCore.compiler.isAndroid:
-            self.runtimeDependencies["libs/qt5/qtandroidextras"] = None
+        if self.options.dynamic.buildWithQt6:
+            self.buildDependencies["libs/qt6/qttools"] = None
+            self.runtimeDependencies["libs/qt6/qtbase"] = None
+        else:
+            self.buildDependencies["libs/qt5/qttools"] = None
+            self.runtimeDependencies["libs/qt5/qtbase"] = None
+            if CraftCore.compiler.isAndroid:
+                self.runtimeDependencies["libs/qt5/qtandroidextras"] = None
 
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/frankosterfeld/qtkeychain.git'
@@ -27,9 +31,17 @@ class subinfo(info.infoclass):
         self.patchToApply["0.12.0"] = [("qtkeychain-0.12.0-20210128.diff", 1), ("0001-Don-t-find-QtDBus-on-Android.patch", 1), ("0001-Make-QtAndroidExtras-dependency-private.patch", 1)]
         self.patchLevel["0.12.0"] = 2
 
-        self.defaultTarget = '0.13.1'
+        if self.options.dynamic.buildWithQt6:
+             self.defaultTarget = "master" # no release with full Qt 6 support yet
+        else:
+            self.defaultTarget = '0.13.1'
 
+
+    def registerOptions(self):
+        self.options.dynamic.registerOption("buildWithQt6", False)
 
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
+        if self.subinfo.options.dynamic.buildWithQt6:
+            self.subinfo.options.configure.args = "-DBUILD_WITH_QT6=ON"
