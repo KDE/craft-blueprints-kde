faf3cb50618141fc0d67732030ecfcd6b758d75c
Hannah von Reth <vonreth@kde.org>
2022-10-20 18:15:37 +0200

    Disable kinit on windows

--- kde/frameworks/tier3/kinit/kinit.py
+++ kde/frameworks/tier3/kinit/kinit.py
@@ -3,7 +3,7 @@ import info
 
 class subinfo(info.infoclass):
     def registerOptions(self):
-        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NotAndroid
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NotAndroid & CraftCore.compiler.Platforms.NotWindows
 
     def setTargets(self):
         self.versionInfo.setDefaultValues()
