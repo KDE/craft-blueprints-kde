d31eec70eec620766322e6116c45dcdd405a6a37
Hannah von Reth <vonreth@kde.org>
2022-11-06 13:59:54 +0100

    Fix build with llvm 15

--- dev-utils/clazy/clazy.py
+++ dev-utils/clazy/clazy.py
@@ -20,9 +20,9 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://download.kde.org/stable/clazy/{ver}/src/clazy-{ver}.tar.xz"
             self.targetDigestUrls[ver] = f"https://download.kde.org/stable/clazy/{ver}/src/clazy-{ver}.tar.xz.sha256"
             self.targetInstSrc[ver] = f"clazy-{ver}"
-        self.patchToApply["1.11"] = [("0001-Allow-to-build-clazy-on-mac.patch", 1)]
+        self.patchToApply["1.11"] = [("0001-Allow-to-build-clazy-on-mac.patch", 1), ("20fca52da739ebefa47e35f6b338bb99a0da3cfe.patch", 1)]  # llvm 15
         self.patchLevel["1.11"] = 1
-        self.defaultTarget = '1.11'
+        self.defaultTarget = "1.11"
 
 
 class Package(CMakePackageBase):
