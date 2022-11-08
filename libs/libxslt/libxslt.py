d5aceff8dd2ef894b1cd23835de673bbb504c7f1
Hannah von Reth <vonreth@kde.org>
2022-11-08 14:30:00 +0100

    Fix libxslt on linux

--- libs/libxslt/libxslt.py
+++ libs/libxslt/libxslt.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
         for ver in ["1.1.37"]:
             self.targets[ver] = f"https://download.gnome.org/sources/libxslt/1.1/libxslt-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"libxslt-{ver}"
+        self.patchToApply["1.1.37"] = [("libxslt-1.1.37-20221108.diff", 1)]
         self.targetDigests["1.1.37"] = (["3a4b27dc8027ccd6146725950336f1ec520928f320f144eb5fa7990ae6123ab4"], CraftHash.HashAlgorithm.SHA256)
         self.description = "The GNOME XSLT C library and tools"
         self.webpage = "https://gitlab.gnome.org/GNOME/libxslt"
