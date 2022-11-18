141f437a63420cc8c613c646d93c1ccf665f0eca
Ingo Klöcker <dev@ingo-kloecker.de>
2022-11-18 15:50:57 +0100

    grantlee 5.3.1

--- kdesupport/grantlee/grantlee.py
+++ kdesupport/grantlee/grantlee.py
@@ -5,16 +5,16 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = "https://github.com/steveire/grantlee.git"
-        self.targets["5.2.0"] = "https://github.com/steveire/grantlee/archive/v5.2.0.tar.gz"
-        self.targetDigests["5.2.0"] = (['139acee5746b957bdf1327ec0d97c604d4c0b9be42aec5d584297cb5ed6a990a'], CraftHash.HashAlgorithm.SHA256)
-        self.targetInstSrc["5.2.0"] = "grantlee-5.2.0"
-        self.defaultTarget = "5.2.0"
+        self.targets["5.3.1"] = "https://github.com/steveire/grantlee/archive/v5.3.1.tar.gz"
+        self.targetDigests["5.3.1"] = (['ba288ae9ed37ec0c3622ceb40ae1f7e1e6b2ea89216ad8587f0863d64be24f06'], CraftHash.HashAlgorithm.SHA256)
+        self.targetInstSrc["5.3.1"] = "grantlee-5.3.1"
+        self.defaultTarget = "5.3.1"
 
-        self.patchToApply['5.2.0'] = []
+        self.patchToApply['5.3.1'] = []
         if CraftCore.compiler.isMacOS:
-            self.patchToApply['5.2.0'] += [("0001-Don-t-use-dot-in-folder-name-to-prevent-macOS-issues.patch", 1)]
-        self.patchToApply["5.2.0"] += [("grantlee-5.2.0-20201117.diff", 1)]
-        self.patchLevel["5.2.0"] = 2
+            self.patchToApply['5.3.1'] += [("0001-Don-t-use-dot-in-folder-name-to-prevent-macOS-issues.patch", 1)]
+        self.patchToApply["5.3.1"] += [("grantlee-5.3.1-20201117.diff", 1)]
+        self.patchLevel["5.3.1"] = 2
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
