81a4e7728872674af3947d61b0bc19e559351112
Tobias Fella <fella@posteo.de>
2022-11-30 19:24:18 +0100

    Update Mobile gear to 22.11

--- kde/plasma-mobile/kasts/kasts.py
+++ kde/plasma-mobile/kasts/kasts.py
@@ -21,6 +21,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier2/syndication"] = None
         self.runtimeDependencies["kde/frameworks/tier1/threadweaver"] = None
+        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
             self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
