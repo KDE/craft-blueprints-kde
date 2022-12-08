2bf2575dbf10fcc5c40a30e878d652e328a07e58
Jean-Baptiste Mardelle <jb@kdenlive.org>
2022-12-08 12:51:17 +0000

    KImageFormats: add KArchive dependency for Krita support on Mac OS

--- kde/frameworks/tier1/kimageformats/kimageformats.py
+++ kde/frameworks/tier1/kimageformats/kimageformats.py
@@ -7,9 +7,12 @@ class subinfo(info.infoclass):
 
         self.description = "KImageFormats"
 
+        self.patchLevel["5.100.0"] = 1
+
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.buildDependencies["kde/frameworks/tier1/karchive"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
 
 
