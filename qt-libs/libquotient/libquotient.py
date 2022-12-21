e69586fcbd21669712ddb97b7c1bff0d9ea8244f
Gary Wang <wzc782970009@gmail.com>
2022-12-21 10:44:27 +0000

    Update libquotient to 0.7.0

--- qt-libs/libquotient/libquotient.py
+++ qt-libs/libquotient/libquotient.py
@@ -6,17 +6,14 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/quotient-im/libQuotient.git'
 
-        for ver in ['0.6.11']:
+        for ver in ['0.7.0']:
             self.targets[ver] = 'https://github.com/quotient-im/libQuotient/archive/%s.tar.gz' % ver
             self.archiveNames[ver] = 'libQuotient-%s.tar.gz' % ver
             self.targetInstSrc[ver] = 'libQuotient-%s' % ver
 
-        self.patchToApply['0.6.11'] = [('0001-Use-GNUInstallDirs-also-on-Windows.patch', 1)]
-        self.patchLevel['0.6.11'] = 1
+        self.targetDigests['0.7.0'] = (['2e46a9889c2ce86b780f04b0b831896179af6621177f805c1a650a980e041525'], CraftHash.HashAlgorithm.SHA256)
 
-        self.targetDigests['0.6.11'] = (['12b15d1296e630477d5e8f4d32c821dc724b3c5b99d15d383417ba7d88f03c46'], CraftHash.HashAlgorithm.SHA256)
-
-        self.defaultTarget = '0.6.11'
+        self.defaultTarget = '0.7.0'
         self.description = "A Qt5 library to write cross-platform clients for Matrix"
 
     def setDependencies(self):
