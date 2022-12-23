293ae2761dc2f062704a133dd502ee431b4d05c3
Hannah von Reth <vonreth@kde.org>
2022-12-23 12:54:43 +0100

    Fix pixman build on m1 mac

--- libs/pixman/pixman.py
+++ libs/pixman/pixman.py
@@ -3,19 +3,20 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['0.40.0', '0.42.2']:
-            self.targets[ver] = 'http://cairographics.org/releases/pixman-' + ver + '.tar.gz'
-            self.targetInstSrc[ver] = 'pixman-' + ver
+        for ver in ["0.40.0", "0.42.2"]:
+            self.targets[ver] = "http://cairographics.org/releases/pixman-" + ver + ".tar.gz"
+            self.targetInstSrc[ver] = "pixman-" + ver
         self.description = "Image processing and manipulation library"
-        self.targetDigests['0.40.0'] = (['6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc'], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests['0.42.2'] = (['ea1480efada2fd948bc75366f7c349e1c96d3297d09a3fe62626e38e234a625e'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.40.0"] = (["6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.42.2"] = (["ea1480efada2fd948bc75366f7c349e1c96d3297d09a3fe62626e38e234a625e"], CraftHash.HashAlgorithm.SHA256)
         # https://gitlab.freedesktop.org/pixman/pixman/-/commit/bd4e7a9b9e672105bda35ff736c977adbf719c6c
         self.patchToApply["0.40.0"] = [("bd4e7a9b9e672105bda35ff736c977adbf719c6c.diff", 1)]
-        self.defaultTarget = '0.42.2'
+        self.defaultTarget = "0.42.2"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/libpng"] = None
+
 
 from Package.MesonPackageBase import *
 
@@ -23,3 +24,6 @@ from Package.MesonPackageBase import *
 class Package(MesonPackageBase):
     def __init__(self, **args):
         MesonPackageBase.__init__(self)
+        if CraftCore.compiler.isMacOS:
+            # disable neon on arm64 https://gitlab.freedesktop.org/pixman/pixman/-/issues/59
+            self.subinfo.options.configure.args += ["-Da64-neon=disabled"]
