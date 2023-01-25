f42d4202c52d3e40cec4b5fcd2f2da0d12afb6c8
Hannah von Reth <vonreth@kde.org>
2023-01-25 09:52:48 +0100

    Make Qt6 webengine build (most of the time)

--- libs/qt6/qtwebengine/qtwebengine.py
+++ libs/qt6/qtwebengine/qtwebengine.py
@@ -24,3 +24,21 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
+
+    def _getEnv(self):
+        # webengine requires enormous amounts of ram
+        jobs = int(CraftCore.settings.get("Compile", "Jobs", multiprocessing.cpu_count()))
+        env = {"NINJAFLAGS": f"-j{int(jobs/2)}"}
+        if CraftCore.compiler.isWindows:
+            # shorten the path to python
+            shortDevUtils = CraftShortPath(Path(CraftCore.standardDirs.craftRoot()) / "dev-utils/").shortPath
+            env["PATH"] = f"{shortDevUtils}/bin;{os.environ['PATH']}"
+        return env
+
+    def configure(self):
+        with utils.ScopedEnv(self._getEnv()):
+            return super().configure()
+
+    def make(self):
+        with utils.ScopedEnv(self._getEnv()):
+            return super().make()
