69a8d19d3770c14c1a336e62b58882fe3c92021a
Robert Lancaster <rlancaste@gmail.com>
2023-02-01 05:48:10 +0000

    Fixing library debug symbol linking bug for MI drvier and Playerone driver...
    Fixing library debug symbol linking bug for MI drvier and Playerone driver with a recommended Fix from TheOneRing

--- libs/_mac/indiserver-3rdparty-libraries/indiserver-3rdparty-libraries.py
+++ libs/_mac/indiserver-3rdparty-libraries/indiserver-3rdparty-libraries.py
@@ -55,6 +55,7 @@ class Package(CMakePackageBase):
 
     def __init__(self):
         CMakePackageBase.__init__(self)
+        self.subinfo.options.package.disableStriping=True
         root = str(CraftCore.standardDirs.craftRoot())
         craftLibDir = os.path.join(root,  'lib')
         self.subinfo.options.configure.args = "-DCMAKE_INSTALL_PREFIX=" + root + " -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_MACOSX_RPATH=1 -DBUILD_LIBS=1 -DCMAKE_INSTALL_RPATH=" + craftLibDir
