889ada0a886f67523d2b00ac26ad6cccd0aed04b
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-02-18 20:33:03 +0100

    [Qt6 Base] adjustments on the way to Android builds

--- libs/qt6/qtbase/qtbase.py
+++ libs/qt6/qtbase/qtbase.py
@@ -10,8 +10,8 @@ class subinfo(info.infoclass):
         self.options.dynamic.registerOption("buildReleaseAndDebug", False)
         self.options.dynamic.registerOption("libInfix", "")
         self.options.dynamic.registerOption("useLtcg", False)
-        self.options.dynamic.registerOption("withDBus", True)
-        self.options.dynamic.registerOption("withGlib", not CraftCore.compiler.isWindows)
+        self.options.dynamic.registerOption("withDBus", not CraftCore.compiler.isAndroid)
+        self.options.dynamic.registerOption("withGlib", not CraftCore.compiler.isWindows and not CraftCore.compiler.isAndroid)
 
     def setTargets(self):
         self.versionInfo.setDefaultValues()
@@ -47,15 +47,28 @@ class Package(CMakePackageBase):
             "-DFEATURE_pkg_config=ON",
             
             "-DFEATURE_system_sqlite=ON",
-            "-DFEATURE_system_pcre2=ON",
             "-DFEATURE_system_zlib=ON",
-            "-DFEATURE_system_harfbuzz=ON",
             "-DFEATURE_system_freetype=ON",
 
             "-DFEATURE_openssl_linked=ON",
 
             "-DQT_BUILD_EXAMPLES=OFF"
         ]
+
+        if self.subinfo.options.isActive("libs/pcre2"):
+            self.subinfo.options.configure.args += ["-DFEATURE_system_pcre2=ON"]
+
+        if self.subinfo.options.isActive("libs/harfbuzz"):
+            self.subinfo.options.configure.args += ["-DFEATURE_system_harfbuzz=ON"]
+
+        if not self.subinfo.options.dynamic.withDBus:
+            self.subinfo.options.configure.args += ["-DFEATURE_dbus=OFF"]
+
+        if not self.subinfo.options.dynamic.withGlib:
+            self.subinfo.options.configure.args += ["-DFEATURE_glib=OFF"]
+
+        if CraftCore.compiler.isAndroid:
+            self.subinfo.options.configure.args += [f"-DANDROID_ABI={CraftCore.compiler.androidAbi}"]
 
         if self.subinfo.options.dynamic.useLtcg:
             self.subinfo.options.configure.args += ["-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON"]
