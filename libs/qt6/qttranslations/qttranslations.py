3f1b7a6ee20870905d5ad990d44aa89734fc78c3
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-02-18 20:27:05 +0100

    [Qt6] Add blueprints for some yet missing modules
    QtCharts, QtConnectivity, QtSensors, QtTranslations

---
+++ libs/qt6/qttranslations/qttranslations.py
@@ -0,0 +1,14 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/qt6/qtbase"] = None
+        self.runtimeDependencies["libs/qt6/qttools"] = None
+
+from Package.CMakePackageBase import *
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
