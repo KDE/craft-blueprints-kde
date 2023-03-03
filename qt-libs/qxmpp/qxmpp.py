89d38517bf1e0392902e3cd01fae1f303730d87f
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-03-03 15:19:22 +0100

    [qxmpp] Update and adjust to support Qt6

--- qt-libs/qxmpp/qxmpp.py
+++ qt-libs/qxmpp/qxmpp.py
@@ -6,12 +6,17 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
-        self.runtimeDependencies["libs/qt5/qtbase"] = None
-        self.buildDependencies["libs/qt5/qttools"] = None
+        if self.options.isActive("libs/qt5/qtbase"):
+            self.runtimeDependencies["libs/qt5/qtbase"] = None
+            self.buildDependencies["libs/qt5/qttools"] = None
+        else:
+            self.runtimeDependencies["libs/qt6/qtbase"] = None
+            self.buildDependencies["libs/qt6/qttools"] = None
+            self.buildDependencies["libs/qt6/qt5compat"] = None
 
     def setTargets(self):
         self.svnTargets['master'] = 'https://github.com/qxmpp-project/qxmpp.git'
-        for ver in ["0.9.3", "1.0.0", "1.3.1", "1.3.2"]:
+        for ver in ["0.9.3", "1.0.0", "1.3.1", "1.3.2", "1.4.0"]:
             self.targets[ver] = f"https://github.com/qxmpp-project/qxmpp/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"qxmpp-v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"qxmpp-{ver}"
@@ -24,7 +29,9 @@ class subinfo(info.infoclass):
             ['812e718a2dd762ec501a9012a1281b9b6c6d46ec38adbc6eec242309144e1c55'], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests['1.3.2'] = (
             ['016e23c40c604dd43b15e1888e31d48729d0f80775fb6f7faef1130a52fe0641'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = '1.3.2'
+        self.targetDigests['1.4.0'] = (
+            ['2148162138eaf4b431a6ee94104f87877b85a589da803dff9433c698b4cf4f19'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = '1.4.0'
 
 
 class Package(CMakePackageBase):
