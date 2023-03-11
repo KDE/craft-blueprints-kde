091feba099a8d1975dcb6400dd033e7cc76c7245
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-03-11 17:19:22 +0100

    KDE Frameworks 5.104.0 is out!
    Also there is a kf5 branch now

--- kde/frameworks/tier3/kio/kio.py
+++ kde/frameworks/tier3/kio/kio.py
@@ -6,9 +6,7 @@ class subinfo(info.infoclass):
         self.versionInfo.setDefaultValues()
 
         if CraftCore.compiler.isMinGW():
-            self.patchToApply["5.98.0"] = [("fix-mingw-STAT_LNK.patch", 1)]
             self.patchToApply["5.99.0"] = [("fix-mingw-STAT_LNK.patch", 1)]
-            self.patchLevel["5.98.0"] = 1
 
         self.description = "Network transparent access to files and data"
 
