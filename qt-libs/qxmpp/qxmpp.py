cbcc66e057fea81d0e3b6d9d9d31ab32e20fbf86
Melvin Keskin <melvo@olomono.de>
2023-03-22 16:23:22 +0100

    kaidan: Remove unneeded build dependency libomemo-c
    libomemo-c is already set as a runtime dependency.
    Thus, it is also implicitly used as a build dependency.

--- qt-libs/qxmpp/qxmpp.py
+++ qt-libs/qxmpp/qxmpp.py
@@ -14,7 +14,6 @@ class subinfo(info.infoclass):
             self.buildDependencies["libs/qt6/qttools"] = None
             self.buildDependencies["libs/qt6/qt5compat"] = None
         self.runtimeDependencies["libs/libomemo-c"] = None
-        self.buildDependencies["libs/libomemo-c"] = None
         self.runtimeDependencies["kdesupport/qca"] = None
 
     def setTargets(self):
