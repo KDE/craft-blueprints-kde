c3ede677b5ddbd36d7c48b1decdd0c1433d22f45
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-03-23 12:06:56 +0000

    [sdl2] Update
    This fixes build on mac arm

--- libs/_autotools/libsdl2/libsdl2.py
+++ libs/_autotools/libsdl2/libsdl2.py
@@ -4,12 +4,13 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "Simple DirectMedia Layer"
         self.webpage = "https://www.libsdl.org"
-        for ver in ['2.0.9']:
+        for ver in ['2.0.9', '2.26.4']:
             self.targets[ver] = f"https://www.libsdl.org/release/SDL2-{ver}.tar.gz"
             self.targetInstSrc[ ver ] = f"SDL2-{ver}"
-            self.patchToApply[ ver ] = [("libtool_windres.patch", 1)]
-        self.targetDigests['2.0.9'] =(['255186dc676ecd0c1dbf10ec8a2cc5d6869b5079d8a38194c2aecdff54b324b1'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = '2.0.9'
+        self.patchToApply['2.0.9'] = [("libtool_windres.patch", 1)]
+        self.targetDigests['2.0.9'] = (['255186dc676ecd0c1dbf10ec8a2cc5d6869b5079d8a38194c2aecdff54b324b1'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['2.26.4'] = (['1a0f686498fb768ad9f3f80b39037a7d006eac093aad39cb4ebcc832a8887231'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = '2.26.4'
 
     def setDependencies(self):
         self.runtimeDependencies["libs/libsamplerate"] = None
@@ -22,6 +23,6 @@ class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
         self.subinfo.options.configure.autoreconf = False
-        self.subinfo.options.configure.args = " --disable-static --enable-threads --enable-directx --enable-libsamplerate "
+        self.subinfo.options.configure.args = ["--disable-static", "--enable-threads", "--enable-directx", "--enable-libsamplerate"]
         if CraftCore.compiler.isFreeBSD:
-            self.subinfo.options.configure.args += " --enable-video-wayland --disable-wayland-shared "
+            self.subinfo.options.configure.args += ["--disable-static", "--enable-threads", "--enable-directx", "--enable-libsamplerate"]
