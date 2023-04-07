5cda2a1fc23319fe4c4b763b0a2af52583f3d5a0
Jasem Mutlaq <mutlaqja@ikarustech.com>
2023-04-07 16:25:52 +0300

    Use updated version

--- libs/xisf/xisf.py
+++ libs/xisf/xisf.py
@@ -6,11 +6,15 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        ver = "0.2.1"
+        ver = "0.2.2"
         self.targets[ver] = "https://gitea.nouspiro.space/nou/libXISF/archive/v%s.tar.gz" % ver
+        self.svnTargets['master'] = "https://gitea.nouspiro.space/nou/libXISF"
         self.targetInstSrc[ver] = "libxisf"
         self.defaultTarget = ver
         self.description = 'XISF Library'
+
+    def setDependencies(self):
+        self.buildDependencies["libs/zlib"] = None
 
 from Package.CMakePackageBase import *
 
@@ -18,4 +22,4 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args = "-DBUILD_SHARED_LIBS=OFF -DUSE_BUNDLED_LIBS=ON"
+        self.subinfo.options.configure.args = "-DBUILD_SHARED_LIBS=OFF -DUSE_BUNDLED_LZ4=ON -DUSE_BUNDLED_PUGIXML=ON -DUSE_BUNDLED_ZLIB=OFF"
