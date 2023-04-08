91f44c1d4fafc5809ed12ca5e0bc69e91a41aa8f
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-04-08 14:07:32 +0000

    [FFmpeg] update to 6.0
    Also update AMD and NV-codec headers

--- libs/amf/amf.py
+++ libs/amf/amf.py
@@ -3,11 +3,11 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "AMD library for accelerated H.264 and HEVC(only windows) encoding on hardware with Video Coding Engine (VCE)"
-        for ver in ['1.4.23']:
+        for ver in ['1.4.29']:
             self.targets[ ver ] = f"https://github.com/GPUOpen-LibrariesAndSDKs/AMF/archive/refs/tags/v{ver}.tar.gz"
             self.targetInstSrc[ ver ] =  'AMF-' + ver
-        self.targetDigests['1.4.23'] = (['7c8a5c91bca54e257258e4efcc61925921f2efe432efd34cf77dd9dc1803c38a'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = '1.4.23'
+        self.targetDigests['1.4.29'] = (['be42e4acd973fc7a228f087313bee9eaca08df031ec4596f14fb2eabef528628'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = '1.4.29'
 
 from Package.SourceOnlyPackageBase  import *
 
