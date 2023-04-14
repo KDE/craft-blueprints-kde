6fdb4461a027a054a7322224fafbebf063012789
Jonathan Esk-Riddell <jr@jriddell.org>
2023-04-14 13:10:03 +0100

    bad command or file name

--- kde/unreleased/isoimagewriter/isoimagewriter.py
+++ kde/unreleased/isoimagewriter/isoimagewriter.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
         self.versionInfo.setDefaultValues()
         self.svnTargets['master'] = 'https://invent.kde.org/utilities/isoimagewriter.git'
         for ver in ["0.9.2"]:
-            self.targets[ver] = "https://download.kde.org/unstable/isoimagewriter/%s/isoimagewriter-%s.tar.xz" % ver, ver
+            self.targets[ver] = "https://download.kde.org/unstable/isoimagewriter/%s/isoimagewriter-%s.tar.xz" % (ver, ver)
             self.targetInstSrc[ver] = "isoimagewriter-%s" % ver
         self.defaultTarget = "master"
 
