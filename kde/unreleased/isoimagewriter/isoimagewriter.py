95e2a4b86ad4b1aeb868e1e38ed74c5e0c333950
Jonathan Esk-Riddell <jr@jriddell.org>
2023-04-18 15:34:09 +0100

    lets try a release of isoimagewriter

--- kde/unreleased/isoimagewriter/isoimagewriter.py
+++ kde/unreleased/isoimagewriter/isoimagewriter.py
@@ -5,7 +5,7 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
         self.svnTargets['master'] = 'https://invent.kde.org/utilities/isoimagewriter.git'
-        for ver in ["0.9.2"]:
+        for ver in ["1.0"]:
             self.targets[ver] = "https://download.kde.org/unstable/isoimagewriter/%s/isoimagewriter-%s.tar.xz" % (ver, ver)
             self.targetInstSrc[ver] = "isoimagewriter-%s" % ver
         self.defaultTarget = "master"
