c1e5160163ebac039c57bbfab9894e9c64632058
Jean-Baptiste Mardelle <jb@kdenlive.org>
2023-04-25 17:43:27 +0000

    Update MLT to include counter crash fix

--- libs/mlt/mlt.py
+++ libs/mlt/mlt.py
@@ -14,8 +14,8 @@ class subinfo(info.infoclass):
 
         self.svnTargets['master'] = "https://github.com/mltframework/mlt.git"
         self.patchLevel['master'] = 20221103
-        self.svnTargets['ee15927'] = "https://github.com/mltframework/mlt.git||ee15927ec37e2e1073c9a2a70723957cd7d4c421"
-        self.defaultTarget = 'ee15927'
+        self.svnTargets['67d5efd'] = "https://github.com/mltframework/mlt.git||67d5efd034c3cb746468dc37d8885aeacdf7f07c"
+        self.defaultTarget = '67d5efd'
         if CraftCore.compiler.isWindows:
             self.patchToApply["2c614ae"] = [("pi_patch.diff", 1)]
 
