f22e89721b395cb5b4062834aa58f38316c7b40a
Hannah von Reth <vonreth@kde.org>
2023-04-28 15:30:18 +0200

    Try to work around long path issue

--- libs/qt6/qtdeclarative/qtdeclarative.py
+++ libs/qt6/qtdeclarative/qtdeclarative.py
@@ -1,4 +1,5 @@
 import info
+import utils
 
 
 class subinfo(info.infoclass):
@@ -10,7 +11,19 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt6/qtbase"] = None
         self.runtimeDependencies["libs/qt6/qtshadertools"] = None
 
+
 from Package.CMakePackageBase import *
+
+
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
+
+    def make(self):
+        env = {}
+        if CraftCore.compiler.isWindows:
+            # shorten the path to qsb.exe ...
+            shortDevUtils = CraftShortPath(Path(CraftCore.standardDirs.craftRoot()) / "bin").shortPath
+            env["PATH"] = f"{shortDevUtils}/bin;{os.environ['PATH']}"
+        with utils.ScopedEnv(env):
+            return super().make()
