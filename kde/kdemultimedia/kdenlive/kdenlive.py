17388c5760a0be56033b55d7dca86d80cc76fc88
Jean-Baptiste Mardelle <jb@kdenlive.org>
2023-04-29 18:29:41 +0200

    Add patch for Kdenlive 23.04.0 project corruption

--- kde/kdemultimedia/kdenlive/kdenlive.py
+++ kde/kdemultimedia/kdenlive/kdenlive.py
@@ -6,6 +6,7 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
         self.description = "Libre Video Editor, by KDE community"
+        self.patchToApply["23.04.0"] = [("fix-id-corruption.patch", 1)]
 
     def setDependencies(self):
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
