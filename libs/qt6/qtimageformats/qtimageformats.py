30193234bb28abb598a791b167f6f6f5ead86516
Hannah von Reth <vonreth@kde.org>
2023-05-17 11:10:29 +0200

    Make patch apply

--- libs/qt6/qtimageformats/qtimageformats.py
+++ libs/qt6/qtimageformats/qtimageformats.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
         self.versionInfo.setDefaultValues()
 
         # https://raw.githubusercontent.com/microsoft/vcpkg/a618637937298060bdbe5fbcfb628eabd1082c8a/ports/qtimageformats/no_target_promotion_latest.patch
-        self.patchToApply["6.4.3"] = [("no_target_promotion_latest.patch", 1)]
+        self.patchToApply["6.4.3"] = [("qtimageformats-6.4.3-20230517.diff", 1)]
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt6/qtbase"] = None
