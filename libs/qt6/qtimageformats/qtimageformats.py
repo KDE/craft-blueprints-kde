cf6a9a6b9f9e1b094400ccdbe37832e2948e08b4
Hannah von Reth <vonreth@kde.org>
2023-05-17 17:48:34 +0200

    We don't provide jasper, explicitly disable it as a dep

--- libs/qt6/qtimageformats/qtimageformats.py
+++ libs/qt6/qtimageformats/qtimageformats.py
@@ -7,6 +7,7 @@ class subinfo(info.infoclass):
 
         # https://raw.githubusercontent.com/microsoft/vcpkg/a618637937298060bdbe5fbcfb628eabd1082c8a/ports/qtimageformats/no_target_promotion_latest.patch
         self.patchToApply["6.4.3"] = [("qtimageformats-6.4.3-20230517.diff", 1)]
+        self.patchLevel["6.4.3"] = 1
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt6/qtbase"] = None
@@ -20,3 +21,4 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
+        self.subinfo.options.configure.args += ["-DFEATURE_jasper=OFF"]
