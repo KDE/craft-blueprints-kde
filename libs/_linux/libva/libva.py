bb92f92ddb9ed6b4a1ee4d494e59643a532d88a6
Jean-Baptiste Mardelle <jb@kdenlive.org>
2023-05-19 09:00:02 +0200

    update libva

--- libs/_linux/libva/libva.py
+++ libs/_linux/libva/libva.py
@@ -2,12 +2,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2.12.0"]:
+        for ver in ["2.12.0", "2.18.0"]:
             self.targets[ver] = f"https://github.com/intel/libva/archive/refs/tags/{ver}.tar.gz"
             self.targetInstSrc[ ver ] = "libva-" + ver
         self.targetDigests['2.12.0'] = (['7bca8c8a854653e15e602f243e2452e84e4b454b26549bf80a932ab29d7d6b21'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['2.18.0'] = (['9d666c70c12dfefcdd27ae7dea771557f75e24961d0ed4cb050d96fb6136f438'], CraftHash.HashAlgorithm.SHA256)
         self.description = "an implementation for VA-API (Video Acceleration API)"
-        self.defaultTarget = '2.12.0'
+        self.defaultTarget = '2.18.0'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
