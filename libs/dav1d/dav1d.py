48136b138c321cf40c7fd9297f71ece9493d4630
Jean-Baptiste Mardelle <jb@kdenlive.org>
2023-05-19 08:55:57 +0200

    Update dav1d

--- libs/dav1d/dav1d.py
+++ libs/dav1d/dav1d.py
@@ -2,12 +2,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["0.9.2"]:
+        for ver in ["0.9.2", "1.2.0"]:
             self.targets[ver] = f"https://code.videolan.org/videolan/dav1d/-/archive/{ver}/dav1d-{ver}.tar.bz2"
             self.targetInstSrc[ ver ] = "dav1d-" + ver
         self.targetDigests['0.9.2'] = (['0d198c4fe63fe7f0395b1b17de75b21c8c4508cd3204996229355759efa30ef8'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests['1.2.0'] = (['05cedc43127e00a86c68b8a49a5f68e2dc22b9baa10b1e12a5e3bc5b37876a6b'], CraftHash.HashAlgorithm.SHA256)
         self.description = "dav1d is the fastest AV1 decoder on all platforms"
-        self.defaultTarget = '0.9.2'
+        self.defaultTarget = '1.2.0'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
