7780f6f6a93c0d0b5c95cc98f288fe6d11912b12
Hannah von Reth <vonreth@kde.org>
2023-05-24 10:40:24 +0200

    Fix dependency type

--- libs/qt/qtspeech/qtspeech.py
+++ libs/qt/qtspeech/qtspeech.py
@@ -12,9 +12,9 @@ class subinfo(info.infoclass):
 
     def setDependencies(self):
         if CraftPackageObject.get("libs/qt").instance.subinfo.options.dynamic.qtMajorVersion == "6":
-            self.buildDependencies[f"libs/qt6/{self.parent.package.name}"] = None
+            self.runtimeDependencies[f"libs/qt6/{self.parent.package.name}"] = None
         else:
-            self.buildDependencies[f"libs/qt5/{self.parent.package.name}"] = None
+            self.runtimeDependencies[f"libs/qt5/{self.parent.package.name}"] = None
 
 
 class Package(VirtualPackageBase):
