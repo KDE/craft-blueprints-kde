873f6f4c04c952b3c83c4b134ccfd8144dd3def1
Sune Vuorela <sune@vuorela.dk>
2023-06-02 17:35:17 +0200

    Bump gnupg stack

--- libs/_autotools/gcrypt/gcrypt.py
+++ libs/_autotools/gcrypt/gcrypt.py
@@ -3,13 +3,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.9.4"]:
+        for ver in ["1.10.2"]:
             self.targets[ver] = f"ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"libgcrypt-{ver}"
-        self.targetDigests["1.9.4"] = (["ea849c83a72454e3ed4267697e8ca03390aee972ab421e7df69dfe42b65caaf7"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.10.2"] = (["3b9c02a004b68c256add99701de00b383accccf37177e0d6c58289664cce0c03"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = " General purpose crypto library based on the code used in GnuPG."
-        self.defaultTarget = "1.9.4"
+        self.defaultTarget = "1.10.2"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/msys"] = None
