61c448227f68fb2c8d76e971fdb31c66c55211ce
Hannah von Reth <vonreth@kde.org>
2023-06-03 14:08:09 +0200

    update gettext config, apply patch for its search path

--- libs/gettext/gettext.py
+++ libs/gettext/gettext.py
@@ -9,10 +9,11 @@ class subinfo(info.infoclass):
         self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NotAndroid
 
     def setTargets(self):
-        for ver in ["0.21"]:
+        for ver in ["0.21", "0.21.1"]:
             self.targets[ver] = "http://ftp.gnu.org/pub/gnu/gettext/gettext-%s.tar.gz" % ver
             self.targetInstSrc[ver] = "gettext-%s" % ver
         self.targetDigests["0.21"] = (["c77d0da3102aec9c07f43671e60611ebff89a996ef159497ce8e59d075786b12"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.21.1"] = (["e8c3650e1d8cee875c4f355642382c1df83058bd5a11ee8555c0cf276d646d45"], CraftHash.HashAlgorithm.SHA256)
 
         self.patchToApply["0.21"] = [
             ("gettext-0.21-add-missing-ruby.diff", 1),
@@ -20,23 +21,39 @@ class subinfo(info.infoclass):
                 "d1836dbbd6a90b4c0ab79bc5292c023f08b49511.patch",
                 1,
             ),  # https://git.savannah.gnu.org/gitweb/?p=gettext.git;a=commitdiff;h=d1836dbbd6a90b4c0ab79bc5292c023f08b49511
+            ("gettext-0.21.1-20230603.diff", 1),  # fix windows search path
+            # https://raw.githubusercontent.com/microsoft/vcpkg/master/ports/gettext/parallel-gettext-tools.patch
+            ("parallel-gettext-tools.patch", 1),
         ]
 
+        self.patchToApply["0.21.1"] = [
+            ("gettext-0.21.1-20230603.diff", 1),  # fix windows search path
+            # https://raw.githubusercontent.com/microsoft/vcpkg/master/ports/gettext/parallel-gettext-tools.patch
+            ("parallel-gettext-tools.patch", 1),
+        ]
         if CraftCore.compiler.isMinGW():
             self.patchToApply["0.21"] += [
+                ("0011-fix-interference-between-libintl-boost-header-files.patch", 1)
+            ]  # https://github.com/msys2/MINGW-packages/blob/master/mingw-w64-gettext/0011-fix-interference-between-libintl-boost-header-files.patch
+            self.patchToApply["0.21.1"] += [
                 ("0011-fix-interference-between-libintl-boost-header-files.patch", 1)
             ]  # https://github.com/msys2/MINGW-packages/blob/master/mingw-w64-gettext/0011-fix-interference-between-libintl-boost-header-files.patch
             self.patchLevel["0.21"] = 2
 
         self.description = "GNU internationalization (i18n)"
+        # 0.21.1 fails on Windows
+        # libgettextsrc_la-write-catalog.obj : error LNK2019: unresolved external symbol close_used_without_requesting_gnulib_module_close referenced in function msgdomain_list_print
+        # .libs\gettextsrc-0-21-1.dll : fatal error LNK1120: 1 unresolved external
         self.defaultTarget = "0.21"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/automake"] = None
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/iconv"] = None
-        self.runtimeDependencies["libs/libxml2"] = None
-        self.buildDependencies["dev-utils/msys"] = None
+        if not CraftCore.compiler.isWindows:
+            self.runtimeDependencies["libs/libxml2"] = None
+        else:
+            self.buildDependencies["dev-utils/msys"] = None
 
 
 class Package(AutoToolsPackageBase):
@@ -46,14 +63,20 @@ class Package(AutoToolsPackageBase):
         self.subinfo.options.configure.args += [
             "--disable-static",
             "--enable-shared",
+            "--disable-acl",
             "--disable-java",
             "--disable-native-java",
             "--enable-nls",
             "--enable-c++",
+            "--enable-relocatable",
             "--with-included-gettext",
             "--with-included-glib",
             "--with-included-regex",
             "--with-gettext-tools",
+            "--without-cvs",
+            "--without-emacs",
+            "--without-git",
+            "--disable-dependency-tracking",
         ]
 
         if CraftCore.compiler.isMSVC():
@@ -61,15 +84,15 @@ class Package(AutoToolsPackageBase):
             self.subinfo.options.useShadowBuild = False
             # https://github.com/microsoft/vcpkg/blob/c6a4ed75f03a7485cf6fc91794809cd73f8f5aeb/ports/gettext/portfile.cmake#L49
             self.subinfo.options.configure.args += [
+                "--with-included-libxml",
+                # they are not properly detected, and when gnulib tries to define them they clash with the intrinsic definition
+                # memset.c(23): error C2169: 'memset': intrinsic function, cannot be defined
                 "ac_cv_func_wcslen=yes",
-                "ac_cv_func_memmove=yes"
-                # The following are required for a full gettext built (libintl and tools).
-                "gl_cv_func_printf_directive_n=no",  # segfaults otherwise with popup window
-                "ac_cv_func_memset=yes",  # not detected in release builds
-                "ac_cv_header_pthread_h=no",
-                "ac_cv_header_dirent_h=no",
-                "ac_cv_header_getopt_h=no",
+                "ac_cv_func_memmove=yes",
+                "ac_cv_func_memset=yes",
             ]
+        else:
+            self.subinfo.options.configure.args += ["--without-included-libxml"]
 
     def postInstall(self):
         return self.patchInstallPrefix(
