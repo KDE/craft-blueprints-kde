5a10759b4fe2d608566ee03d712b48385843f26e
Joshua Goins <josh@redstrate.com>
2023-06-20 20:59:16 -0400

    Bump libarchive to 3.6.2
    This fixes an important Android compile issue.

--- libs/libarchive/libarchive.py
+++ libs/libarchive/libarchive.py
@@ -3,14 +3,15 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for v in ["3.5.1", "3.4.3"]:
+        for v in ["3.6.2", "3.5.1", "3.4.3"]:
             self.targets[v] = "https://github.com/libarchive/libarchive/archive/v" + v + ".tar.gz"
             self.targetInstSrc[v] = "libarchive-" + v
 
         self.targetDigests["3.4.3"] = (["19556c1c67aacdff547fd719729630444dbc7161c63eca661a310676a022bb01"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["3.5.1"] = (["6d92e669e259a55a0119c135873469778f2718acbe605717f9d341487b4d0cba"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["3.6.2"] = (["652b84588488c2ff38db8f666cd7f781163f85bff4449dcb2e16d3c734f96697"], CraftHash.HashAlgorithm.SHA256)
         self.description = "C library and command-line tools for reading and writing tar, cpio, zip, ISO, and other archive formats"
-        self.defaultTarget = "3.5.1"
+        self.defaultTarget = "3.6.2"
 
     def setDependencies(self):
         self.buildDependencies["libs/liblzma"] = None
