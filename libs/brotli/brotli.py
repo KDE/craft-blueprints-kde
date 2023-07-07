bd7b54bdde19e041313c47f36198cb438fa8117b
Hannah von Reth <vonreth@kde.org>
2023-07-07 18:30:24 +0200

    backport brotli rpath fix

--- libs/brotli/brotli.py
+++ libs/brotli/brotli.py
@@ -8,7 +8,9 @@ class subinfo(info.infoclass):
         for ver in ["1.0.9"]:
             self.targets[ver] = f"https://github.com/google/brotli/archive/refs/tags/v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"brotli-{ver}"
-        self.targetDigests["1.0.9"] = (['f9e8d81d0405ba66d181529af42a3354f838c939095ff99930da6aa9cdf6fe46'], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.0.9"] = (["f9e8d81d0405ba66d181529af42a3354f838c939095ff99930da6aa9cdf6fe46"], CraftHash.HashAlgorithm.SHA256)
+        self.patchToApply["1.0.9"] = [("f842c1bcf9264431cd3b15429a72b7.patch", 1)]
+        self.patchLevel["1.0.9"] = 1
         self.description = "Brotli compression format"
         self.webpage = "https://github.com/google/brotli"
         self.defaultTarget = "1.0.9"
