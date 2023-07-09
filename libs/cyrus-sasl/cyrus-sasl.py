6ef41b6b154ba95879e0e54588aabeb1b41d72e5
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-07-09 11:32:58 +0000

    [cyrus-sasl] Force rebuild to fix macOS packaging after ssl mess during cache update

--- libs/cyrus-sasl/cyrus-sasl.py
+++ libs/cyrus-sasl/cyrus-sasl.py
@@ -12,7 +12,7 @@ class subinfo(info.infoclass):
         if CraftCore.compiler.isWindows:
             self.patchToApply["2.1.26"] += [("cyrus-sasl-2.1.26.patch", 1)]
         self.targetDigests["2.1.26"] = (["8fbc5136512b59bb793657f36fadda6359cae3b08f01fd16b3d406f1345b7bc3"], CraftHash.HashAlgorithm.SHA256)
-        self.patchLevel["2.1.26"] = 1
+        self.patchLevel["2.1.26"] = 2
         self.targetDigests["2.1.28"] = (["7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"], CraftHash.HashAlgorithm.SHA256)
         self.description = "Cyrus SASL implementation"
         self.defaultTarget = "2.1.26"
@@ -24,7 +24,7 @@ class subinfo(info.infoclass):
 class CMakePackage(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args += " -DSTATIC_LIBRARY=OFF"
+        self.subinfo.options.configure.args += ["-DSTATIC_LIBRARY=OFF"]
 
 
 from Package.AutoToolsPackageBase import *
@@ -34,7 +34,7 @@ class PackageAutotools(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
         self.subinfo.options.configure.bootstrap = True
-        self.subinfo.options.configure.args += " --disable-macos-framework"
+        self.subinfo.options.configure.args += ["--disable-macos-framework"]
 
 
 if CraftCore.compiler.isWindows:
