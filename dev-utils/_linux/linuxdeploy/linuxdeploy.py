d3ea37bcd58804fbab7783e189deabaf3b42b9c6
Hannah von Reth <vonreth@kde.org>
2023-07-12 13:41:27 +0200

    linuxdeploy 1-alpha-20230712-1

--- dev-utils/_linux/linuxdeploy/linuxdeploy.py
+++ dev-utils/_linux/linuxdeploy/linuxdeploy.py
@@ -6,13 +6,14 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1-alpha-20230711-2"]:
+        for ver in ["1-alpha-20230711-2", "1-alpha-20230712-1"]:
             self.targets[ver] = f"https://github.com/linuxdeploy/linuxdeploy/releases/download/{ver}/linuxdeploy-x86_64.AppImage"
             # add version to file name to allow downloading multiple versions
             self.archiveNames[ver] = f"linuxdeploy-{ver}-x86_64.AppImage"
             self.targetInstallPath[ver] = "dev-utils/bin"
         self.targetDigests["1-alpha-20230711-2"] = (["d9e7ba27f0d9a45d5ebdcb8fb363339f0cf1cfed918586a2982e4b46c342cc08"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1-alpha-20230711-2"
+        self.targetDigests["1-alpha-20230712-1"] = (["9a86c10bce48e4570d95897092b758d306500dc58c26ce0564706caea3cbb192"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1-alpha-20230712-1"
         self.description = "AppDir creation and maintenance tool. Featuring flexible plugin system."
         self.webpage = "https://github.com/linuxdeploy/linuxdeploy"
 
