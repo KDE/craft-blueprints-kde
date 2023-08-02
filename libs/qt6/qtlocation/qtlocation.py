cd8dd1ca9fa8ceebc74119b66cfaaaa2985ccd64
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-08-02 19:33:25 +0000

    [Qt6] Add QtLocation

---
+++ libs/qt6/qtlocation/qtlocation.py
@@ -0,0 +1,19 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/qt6/qtbase"] = None
+        self.runtimeDependencies["libs/qt6/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt6/qtpositioning"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
