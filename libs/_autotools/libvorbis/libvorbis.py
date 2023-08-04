908a07c01310f93356e10927579a79097f8cb291
Volker Krause <vkrause@kde.org>
2023-08-04 18:29:47 +0200

    Bump libvorbis to 1.3.7 and build it with CMake
    This has the advantage of properly working with Clang for building for
    Android x86[_64].

--- libs/_autotools/libvorbis/libvorbis.py
+++ libs/_autotools/libvorbis/libvorbis.py
@@ -1,24 +1,23 @@
 import info
-from Package.AutoToolsPackageBase import *
+from Package.CMakePackageBase import *
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.3.1", "1.3.3", "1.3.6"]:
+        for ver in ["1.3.6", "1.3.7"]:
             self.targets[ver] = "http://downloads.xiph.org/releases/vorbis/libvorbis-" + ver + ".tar.gz"
             self.targetInstSrc[ver] = "libvorbis-" + ver
-        self.targetDigests["1.3.1"] = "0874dd08699240b868b22979da4c95ae6325006b"
-        self.targetDigests["1.3.3"] = "8dae60349292ed76db0e490dc5ee51088a84518b"
         self.targetDigests["1.3.6"] = (["6ed40e0241089a42c48604dc00e362beee00036af2d8b3f46338031c9e0351cb"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.3.7"] = (["0e982409a9c3fc82ee06e08205b1355e5c6aa4c36bca58146ef399621b0ce5ab"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "reference implementation for the vorbis audio file format"
-        self.defaultTarget = "1.3.6"
+        self.defaultTarget = "1.3.7"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/libogg"] = None
 
 
-class Package(AutoToolsPackageBase):
+class Package(CMakePackageBase):
     def __init__(self):
-        AutoToolsPackageBase.__init__(self)
+        CMakePackageBase.__init__(self)
