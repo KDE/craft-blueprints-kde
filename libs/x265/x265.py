003b6afeca4cb3bf9e38437386ff084ff9a33584
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-08-16 16:19:14 +0000

    [x265]  Update
    Switching to a git target as there has been no release for a while, but important fixes such as support for Mac ARM

--- libs/x265/x265.py
+++ libs/x265/x265.py
@@ -9,8 +9,14 @@ class subinfo(info.infoclass):
             self.patchToApply[ver] = [("mingw-no-pdb.patch", 1)]
         self.targetDigests["3.4"] = (["c2047f23a6b729e5c70280d23223cb61b57bfe4ad4e8f1471eeee2a61d148672"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["3.5"] = (["e70a3335cacacbba0b3a20ec6fecd6783932288ebc8163ad74bcc9606477cae8"], CraftHash.HashAlgorithm.SHA256)
+
+        self.svnTargets["master"] = "https://bitbucket.org/multicoreware/x265_git.git"
+        self.targetConfigurePath["master"] = "source"
+        self.svnTargets["8f18e3a"] = "https://bitbucket.org/multicoreware/x265_git.git||8f18e3ad32684eee95e885e718655f93951128c3"
+        self.targetConfigurePath["8f18e3a"] = "source"
+
         self.description = "H.265/HEVC video stream encoder"
-        self.defaultTarget = "3.5"
+        self.defaultTarget = "8f18e3a"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
@@ -23,4 +29,4 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-        self.subinfo.options.configure.args = "-DEXPORT_C_API=ON -DENABLE_SHARED=ON -DENABLE_ASSEMBLY=ON -DENABLE_CLI=OFF"
+        self.subinfo.options.configure.args = ["-DEXPORT_C_API=ON", "-DENABLE_SHARED=ON", "-DENABLE_ASSEMBLY=ON", "-DENABLE_CLI=OFF"]
