f326ee35490d507d74ad82a2423815fb05d77570
Nicolas Fella <nicolas.fella@gmx.de>
2023-08-30 21:27:06 +0000

    Package FutureSQL and add to Kontrast dependencies

---
+++ kde/libs/futuresql/futuresql.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2023 Nicolas Fella <nicolas.fella@gmx.de>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import info
+from Blueprints.CraftPackageObject import CraftPackageObject
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["master"] = "https://invent.kde.org/libraries/futuresql.git"
+
+        for ver in ["0.1.1"]:
+            self.targets[ver] = f"https://download.kde.org/stable/futuresql/futuresql-{ver}.tar.xz"
+            self.archiveNames[ver] = f"futuresql-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"futuresql-{ver}"
+
+        self.description = "A non-blocking database framework for Qt."
+        self.defaultTarget = "0.1.1"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+
+
+class Package(CraftPackageObject.get("kde").pattern):
+    def __init__(self, **args):
+        CraftPackageObject.get("kde").pattern.__init__(self)
+        self.subinfo.options.configure.args += ["-DBUILD_EXAMPLES=OFF"]
