78e8fef2d15a1fabc67d4e66f6b51db2039bab20
Volker Krause <vkrause@kde.org>
2023-09-09 14:15:24 +0200

    Enable Matrix dependencies unconditional for Itinerary
    The limit to master only made sense during the 23.04 series.

--- kde/applications/itinerary/itinerary.py
+++ kde/applications/itinerary/itinerary.py
@@ -38,8 +38,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/pim/kpkpass"] = None
         self.runtimeDependencies["kde/plasma-mobile/khealthcertificate"] = None
         self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
-        if self.buildTarget == "master":
-            self.runtimeDependencies["qt-libs/libquotient"] = None
+        self.runtimeDependencies["qt-libs/libquotient"] = None
 
 
 from Package.CMakePackageBase import *
