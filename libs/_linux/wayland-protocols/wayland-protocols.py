df17909f10b6889bd36b84f62ce88e733997f26f
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-09-12 14:44:30 +0200

    Update wayland and wayland-protocols

--- libs/_linux/wayland-protocols/wayland-protocols.py
+++ libs/_linux/wayland-protocols/wayland-protocols.py
@@ -3,14 +3,14 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.31"]:
+        for ver in ["1.32"]:
             self.targets[ver] = f"https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/{ver}/downloads/wayland-protocols-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"wayland-protocols-{ver}"
-        self.targetDigests["1.31"] = (["a07fa722ed87676ec020d867714bc9a2f24c464da73912f39706eeef5219e238"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.32"] = (["7459799d340c8296b695ef857c07ddef24c5a09b09ab6a74f7b92640d2b1ba11"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "wayland-protocols contains Wayland protocols that add functionality not available in the Wayland core protocol."
 
-        self.defaultTarget = "1.31"
+        self.defaultTarget = "1.32"
 
     def setDependencies(self):
         self.buildDependencies["python-modules/meson"] = None
