b0f5ede50d63c87b1e99ef7f502453ab92c25c5d
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-09-12 18:36:45 +0200

    [openldap] Make it build on Linux

--- libs/openldap/openldap.py
+++ libs/openldap/openldap.py
@@ -1,21 +1,21 @@
 import info
+from CraftCore import CraftCore
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2.4.28", "2.4.33", "2.4.36", "2.4.45"]:
-            self.targets[ver] = "https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-" + ver + ".tgz"
-            self.targetInstSrc[ver] = "openldap-" + ver
-        self.patchToApply["2.4.28"] = [("openldap-2.4.28-20120212.diff", 1)]
-        self.patchToApply["2.4.33"] = [("openldap-2.4.33-20130124.diff", 1)]
-        self.patchToApply["2.4.36"] = [("openldap-2.4.36-20131003.diff", 1)]
-        # self.patchToApply['2.4.36'] = [('openldap-2.4.36-20170627.diff', 1)]
-        self.patchToApply["2.4.45"] = [("openldap-2.4.45-20170628.diff", 1)]
-        self.targetDigests["2.4.28"] = "d888beae1723002a5a2ff5509d3040df40885774"
-        self.targetDigests["2.4.33"] = "0cea642ba2dae1eb719da41bfedb9eba72ad504d"
-        self.targetDigests["2.4.36"] = "da0e18a28a5dade5c98d9a382fd8f0a676a12aca"
+        for ver in ["2.4.45"]:
+            self.targets[ver] = f"https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-{ver}.tgz"
+            self.targetInstSrc[ver] = f"openldap-{ver}"
+
+        if CraftCore.compiler.isWindows:
+            self.patchToApply["2.4.45"] = [("openldap-2.4.45-20170628.diff", 1)]
+        self.targetDigests["2.4.45"] = (["cdd6cffdebcd95161a73305ec13fc7a78e9707b46ca9f84fb897cd5626df3824"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["2.5.16"] = (["546ba591822e8bb0e467d40c4d4a30f89d937c3a507fe83a578f582f6a211327"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["2.6.6"] = (["082e998cf542984d43634442dbe11da860759e510907152ea579bdc42fe39ea0"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "an open source implementation of the Lightweight Directory Access Protocol"
+
         self.defaultTarget = "2.4.45"
 
     def setDependencies(self):
@@ -25,12 +25,19 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/openssl"] = None
 
 
+from Package.AutoToolsPackageBase import *
 from Package.CMakePackageBase import *
 
+if CraftCore.compiler.isWindows:
 
-class Package(CMakePackageBase):
-    def __init__(self, **args):
-        CMakePackageBase.__init__(self)
+    class Package(CMakePackageBase):
+        def __init__(self, **args):
+            CMakePackageBase.__init__(self)
 
+else:
 
-# self.subinfo.options.configure.args = "-DBUILD_TOOL=ON -DBUILD_TESTS=ON "
+    class Package(AutoToolsPackageBase):
+        def __init__(self, **args):
+            AutoToolsPackageBase.__init__(self)
+            self.subinfo.options.configure.autoreconf = False
+            self.subinfo.options.configure.args += ["--without-cyrus_sasl"]
