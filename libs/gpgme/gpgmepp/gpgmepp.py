22f75133719224588e9708daaa8e1df7a13e6635
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-09-13 18:32:03 +0200

    Only use gpgmepp on MSVC
    Fixes !659

--- libs/gpgme/gpgmepp/gpgmepp.py
+++ libs/gpgme/gpgmepp/gpgmepp.py
@@ -2,6 +2,13 @@ import info
 
 
 class subinfo(info.infoclass):
+    def registerOptions(self):
+        if not CraftCore.compiler.isMSVC():
+            # Theoretically gpgmepp supports other platforms than Windows with MSVC
+            # however not with the patch for MSVC applied below and no one put effort in
+            # making it work on other platforms. So if you are interested in it do it :-)
+            self.parent.package.categoryInfo.compiler = CraftCore.compiler.Compiler.NoCompiler
+
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
@@ -12,15 +19,16 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/gnupg"] = None
         self.runtimeDependencies["libs/qt5/qtbase"] = None
+
         self.patchToApply["1.21.0"] = ("cmake.patch", 1)
 
         self.patchLevel["1.21.0"] = 2
 
+
 from Package.CMakePackageBase import *
+
 
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
         self.subinfo.options.configure.args += ["-DWITH_QT=ON", "-DBUILD_SHARED_LIBS=ON"]
-
-
