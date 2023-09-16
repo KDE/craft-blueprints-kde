8957c0c883374d0fcfc55d60ca0137d07efe87fa
Jasem Mutlaq <mutlaqja@ikarustech.com>
2023-09-16 18:42:18 +0300

    Turn bundled zlib off as proposed by Dusan

--- libs/libxisf/libxisf.py
+++ libs/libxisf/libxisf.py
@@ -25,4 +25,4 @@ class Package(CMakePackageBase):
         CMakePackageBase.__init__(self)
         root = str(CraftCore.standardDirs.craftRoot())
         craftLibDir = os.path.join(root, "lib")
-        self.subinfo.options.configure.args = "-DCMAKE_MACOSX_RPATH=1 -DCMAKE_INSTALL_RPATH=" + craftLibDir
+        self.subinfo.options.configure.args = "-DCMAKE_MACOSX_RPATH=1 -DUSE_BUNDLED_ZLIB=OFF -DCMAKE_INSTALL_RPATH=" + craftLibDir
