2928c73bcd40345e456fff94627d79f24ee4796e
Hannah von Reth <vonreth@kde.org>
2023-09-20 14:30:49 +0200

    webp 1.3.2

--- libs/webp/webp.py
+++ libs/webp/webp.py
@@ -3,11 +3,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.3.0"]:
+        for ver in ["1.3.0", "1.3.2"]:
             self.targets[ver] = f"https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"libwebp-{ver}"
         self.targetDigests["1.3.0"] = (["64ac4614db292ae8c5aa26de0295bf1623dbb3985054cb656c55e67431def17c"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.3.0"
+        self.targetDigests["1.3.2"] = (["2a499607df669e40258e53d0ade8035ba4ec0175244869d1025d460562aa09b4"], CraftHash.HashAlgorithm.SHA256)
+        self.webpage = "https://chromium.googlesource.com/webm/libwebp/"
+        self.defaultTarget = "1.3.2"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
