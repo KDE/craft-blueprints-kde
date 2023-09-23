3b01f991803b30a0f3206027ad4422a7d1b8d49d
Laurent Montel <montel@kde.org>
2023-09-23 14:21:17 +0200

    Add missing dep

--- kde/pim/kdepim-addons/kdepim-addons.py
+++ kde/pim/kdepim-addons/kdepim-addons.py
@@ -38,6 +38,9 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/pim/kitinerary"] = None
         self.runtimeDependencies["kde/libs/ktextaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ktexttemplate"] = None
+        self.runtimeDependencies["kde/libs/kaddressbook"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/prison"] = None
+        self.runtimeDependencies["kde/pim/ktnef"] = None
 
 
 from Package.CMakePackageBase import *
