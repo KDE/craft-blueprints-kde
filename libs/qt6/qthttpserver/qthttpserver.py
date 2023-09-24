0c380929ac50ec93ca2f6d3950f360fad453ce13
Carl Schwan <carl@carlschwan.eu>
2023-09-24 18:17:09 +0000

    Add QtHttpServer
    This is required for arianna in the Qt6 branch as well as soon some part
    of the kleopatra stack

---
+++ libs/qt6/qthttpserver/qthttpserver.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2023 g10 Code GmbH
+# SPDX-Contributor: Carl Schwan <carl.schwan@gnupg.com>
+
+import info
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/qt6/qtbase"] = None
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
