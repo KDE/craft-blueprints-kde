8e53130fb003f593ea3ab021bdae0e45db6732ef
Volker Krause <vkrause@kde.org>
2023-10-06 20:24:05 +0200

    Remove the no longer necessary KF6 deprecation CMake option
    Qt6 builds are only supported against KF6 master nowadays, and those don't
    contain any Qt5-only deprecated API anymore.

--- kde/kde.py
+++ kde/kde.py
@@ -8,4 +8,4 @@ class Pattern(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
         if CraftPackageObject.get("libs/qt").instance.subinfo.options.dynamic.qtMajorVersion == "6":
-            self.subinfo.options.configure.args += ["-DBUILD_WITH_QT6=ON", "-DQT_MAJOR_VERSION=6", "-DEXCLUDE_DEPRECATED_BEFORE_AND_AT=5.99.0"]
+            self.subinfo.options.configure.args += ["-DBUILD_WITH_QT6=ON", "-DQT_MAJOR_VERSION=6"]
