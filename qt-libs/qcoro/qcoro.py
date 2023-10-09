ed1a23c6760ba650bc96a084423043c376863750
Jonah Brüchert <jbb@kaidan.im>
2023-10-09 22:31:29 +0200

    Update qcoro to 0.9.0

--- qt-libs/qcoro/qcoro.py
+++ qt-libs/qcoro/qcoro.py
@@ -6,21 +6,20 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/danvratil/qcoro.git"
 
-        for ver in ["0.7.0"]:
+        for ver in ["0.9.0"]:
             self.targets[ver] = "https://github.com/danvratil/qcoro/archive/refs/tags/v%s.tar.gz" % ver
             self.archiveNames[ver] = "qcoro-%s.tar.gz" % ver
             self.targetInstSrc[ver] = "qcoro-%s" % ver
 
-        self.targetDigests["0.7.0"] = (["23ef0217926e67c8d2eb861cf91617da2f7d8d5a9ae6c62321b21448b1669210"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.9.0"] = (["cfaf6b778450f06adac4ce5e353eb6eae213a3b62b8c8740520d58cf9fe3ec1a"], CraftHash.HashAlgorithm.SHA256)
 
-        self.patchToApply["0.7.0"] = [("0002-Revert-Build-Check-if-libatomic-is-required.patch", 1)]
-
-        self.defaultTarget = "0.7.0"
+        self.defaultTarget = "0.9.0"
         self.description = "C++ Coroutines for Qt"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["libs/qt/qtdeclarative"] = None
         self.runtimeDependencies["libs/qt/qtwebsockets"] = None
 
 
