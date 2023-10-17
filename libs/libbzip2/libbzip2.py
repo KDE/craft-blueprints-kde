7d8bff0f636f2e3fbf6c294d63620f2eacd06582
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-10-17 22:45:06 +0200

    [libbzip2] Use shim instead of symlink
    Possible thanks to the new keepArgv0 option

--- libs/libbzip2/libbzip2.py
+++ libs/libbzip2/libbzip2.py
@@ -10,7 +10,7 @@ class subinfo(info.infoclass):
         self.patchToApply["1.0.6"] = [("bzip.diff", 1), ("libbzip2-1.0.6-20210422.diff", 1)]
         self.targetDigests["1.0.6"] = "3f89f861209ce81a6bab1fd1998c0ef311712002"
         self.description = "shared libraries for handling bzip2 archives (runtime)"
-        self.patchLevel["1.0.6"] = 3
+        self.patchLevel["1.0.6"] = 4
         self.defaultTarget = "1.0.6"
 
     def setDependencies(self):
@@ -30,7 +30,7 @@ class Package(CMakePackageBase):
         # CMake support has been added upstream and hopefully we can drop it all together ones version 1.1 is out
         linkSource = self.imageDir() / "bin" / f"bzip2{CraftCore.compiler.executableSuffix}"
         linkTarget = self.imageDir() / "bin" / f"bunzip2{CraftCore.compiler.executableSuffix}"
-        utils.createSymlink(linkSource, linkTarget)
+        utils.createShim(linkTarget, linkSource, keepArgv0=True)
 
         for file in glob.glob(os.path.join(self.imageDir(), "lib", "libbzip2.*")):
             utils.copyFile(file, os.path.join(self.imageDir(), "lib", "libbz2" + os.path.splitext(file)[1]), linkOnly=True)
