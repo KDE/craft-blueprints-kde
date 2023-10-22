07793cf29bfbaefbb916f4fc7fdb06bb2c3bf561
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-10-22 17:51:41 +0200

    [cfitsio] Update to 4.3.0

--- libs/cfitsio/cfitsio.py
+++ libs/cfitsio/cfitsio.py
@@ -5,10 +5,13 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "library for the FITS (Flexible Image Transport System) file format"
 
-        self.defaultTarget = "4.0.0"
-        self.targets[self.defaultTarget] = f"http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-{self.defaultTarget}.tar.gz"
-        self.targetInstSrc[self.defaultTarget] = f"cfitsio-{self.defaultTarget}"
+        for ver in ["4.0.0", "4.3.0"]:
+            self.targets[ver] = f"https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"cfitsio-{ver}"
         self.targetDigests["4.0.0"] = (["b2a8efba0b9f86d3e1bd619f662a476ec18112b4f27cc441cc680a4e3777425e"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["4.3.0"] = (["fdadc01d09cf9f54253802c5ec87eb10de51ce4130411415ae88c30940621b8b"], CraftHash.HashAlgorithm.SHA256)
+
+        self.defaultTarget = "4.3.0"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
