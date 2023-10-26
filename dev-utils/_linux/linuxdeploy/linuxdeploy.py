e34bbd1a7e872632ea0852727121b15f93222356
Hannah von Reth <vonreth@kde.org>
2023-10-26 14:25:49 +0200

    Use stable version of linuxdeploy

--- dev-utils/_linux/linuxdeploy/linuxdeploy.py
+++ dev-utils/_linux/linuxdeploy/linuxdeploy.py
@@ -3,16 +3,16 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1-alpha-20230711-2", "1-alpha-20230712-1"]:
-            self.targets[ver] = f"https://github.com/linuxdeploy/linuxdeploy/releases/download/{ver}/linuxdeploy-x86_64.AppImage"
+        for ver in ["1-alpha-20231026-1"]:
+            self.targets[ver] = f"https://github.com/linuxdeploy/linuxdeploy/releases/download/{ver}/linuxdeploy-static-x86_64.AppImage"
             # add version to file name to allow downloading multiple versions
             self.archiveNames[ver] = f"linuxdeploy-{ver}-x86_64.AppImage"
             self.targetInstallPath[ver] = "dev-utils/bin"
         self.targets["continous-static"] = "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-static-x86_64.AppImage"
         self.targetInstallPath["continous-static"] = "dev-utils/bin"
-        self.targetDigests["1-alpha-20230711-2"] = (["d9e7ba27f0d9a45d5ebdcb8fb363339f0cf1cfed918586a2982e4b46c342cc08"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["1-alpha-20230712-1"] = (["9a86c10bce48e4570d95897092b758d306500dc58c26ce0564706caea3cbb192"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "continous-static"
+        self.targetDigests["1-alpha-20231026-1"] = (["7e354fb6722b04ac1838e4bde0a49564518400dc9680a33f8d799e877f9f5f6a"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1-alpha-20231026-1"
+
         self.description = "AppDir creation and maintenance tool. Featuring flexible plugin system."
         self.webpage = "https://github.com/linuxdeploy/linuxdeploy"
 
