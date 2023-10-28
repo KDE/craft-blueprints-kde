9742166daf9fa611ea54e4e2e573afd0b9d8b5b6
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-10-28 11:11:54 +0000

    [libvpx] Update to 1.13.1
    Disable cache file because otherwise configure fails

--- libs/_autotools/libvpx/libvpx.py
+++ libs/_autotools/libvpx/libvpx.py
@@ -3,16 +3,20 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.8.0", "1.9.0"]:
+        self.description = "VP8 and VP9 video codec"
+
+        for ver in ["1.9.0", "1.13.1"]:
             self.targets[ver] = f"https://github.com/webmproject/libvpx/archive/v{ver}.tar.gz"
             self.targetInstSrc[ver] = "libvpx-" + ver
-        self.targetDigests["1.8.0"] = (["86df18c694e1c06cc8f83d2d816e9270747a0ce6abe316e93a4f4095689373f6"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.9.0"] = (["d279c10e4b9316bf11a570ba16c3d55791e1ad6faa4404c67422eb631782c80a"], CraftHash.HashAlgorithm.SHA256)
-        self.description = "VP8 and VP9 video codec"
-        self.defaultTarget = "1.9.0"
+        self.targetDigests["1.13.1"] = (["00dae80465567272abd077f59355f95ac91d7809a2d3006f9ace2637dd429d14"], CraftHash.HashAlgorithm.SHA256)
+
         self.patchToApply["1.9.0"] = [("detect-clang.diff", 1)]
         if CraftCore.compiler.isFreeBSD:
             self.patchToApply["1.9.0"] += [("dont-check-for-diff.diff", 1)]
+        self.patchToApply["1.13.1"] = [("detect-clang.diff", 1)]
+
+        self.defaultTarget = "1.13.1"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/pthreads"] = None
@@ -26,6 +30,7 @@ class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
         self.subinfo.options.configure.noDataRootDir = True
+        self.subinfo.options.configure.noCacheFile = True
         self.platform = ""
         self.subinfo.options.configure.args += ["--disable-examples", "--disable-install-docs", "--disable-unit-tests", "--disable-avx512"]
         if CraftCore.compiler.isLinux or CraftCore.compiler.isFreeBSD:
