6f334d999aa75d4231b8d2808d470eb14ffae11c
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-10-29 09:07:34 +0000

    [hunspell-dictionaries] Update to 7.6.2.1

--- data/hunspell-dictionaries/hunspell-dictionaries.py
+++ data/hunspell-dictionaries/hunspell-dictionaries.py
@@ -6,17 +6,16 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "git://anongit.freedesktop.org/libreoffice/dictionaries"
-        for ver in ["6.0.1.1"]:
+        for ver in ["7.6.2.1"]:
             mainVersion = ".".join(ver.split(".")[:3])
             self.targets[ver] = f"https://download.documentfoundation.org/libreoffice/src/{mainVersion}/libreoffice-dictionaries-{ver}.tar.xz"
             self.targetDigestUrls[ver] = f"http://download.documentfoundation.org/libreoffice/src/{mainVersion}/libreoffice-dictionaries-{ver}.tar.xz.sha256"
             self.targetInstSrc[ver] = f"libreoffice-{ver}"
-        self.patchLevel["6.0.1.1"] = 1
 
         self.webpage = "https://cgit.freedesktop.org/libreoffice/dictionaries/"
         self.description = "Hunspell Dictionaries by LibreOffice"
 
-        self.defaultTarget = "6.0.1.1"
+        self.defaultTarget = "7.6.2.1"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
