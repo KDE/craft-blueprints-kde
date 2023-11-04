289c54dd566615247b5941074fca70669e788de1
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-11-04 10:08:14 +0000

    [KUserFeedback] Update to 1.3.0

--- kde/unreleased/kuserfeedback/kuserfeedback.py
+++ kde/unreleased/kuserfeedback/kuserfeedback.py
@@ -7,14 +7,13 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
-        self.targets["1.2.0"] = "https://download.kde.org/stable/kuserfeedback/kuserfeedback-1.2.0.tar.xz"
-        self.targetDigests["1.2.0"] = (["76aac922b153249b274680a6f4c72c238ef14e3df04bad00cb64158b1063f264"], CraftHash.HashAlgorithm.SHA256)
-        self.targetInstSrc["1.2.0"] = "kuserfeedback-1.2.0"
+        for ver in ["1.3.0"]:
+            self.targets[ver] = f"https://download.kde.org/stable/kuserfeedback/kuserfeedback-{ver}.tar.xz"
+            self.targetInstSrc[ver] = f"kuserfeedback-{ver}"
 
-        self.patchToApply["1.2.0"] = [("0001-Hide-finding-docs-dependencies-behind-ENABLE_DOCS.patch", 1), ("disable-qthelp-if-docs-disabled.patch", 1)]
-        self.patchLevel["1.2.0"] = 1
+        self.targetDigests["1.3.0"] = (["252308b822dd4690ea85ab1688c9b0da5512978ac6b435f77a5979fc1d2ffd13"], CraftHash.HashAlgorithm.SHA256)
 
-        self.defaultTarget = "1.2.0"
+        self.defaultTarget = "1.3.0"
 
         self.description = "KUserFeedback"
 
