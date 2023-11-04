5ac27923c6f21f285510f7062d1f78180c03148e
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-11-04 10:27:21 +0000

    [KQuickImageEditor] Update to 0.3.0
    This version adds Qt6 support

--- kde/libs/kquickimageeditor/kquickimageeditor.py
+++ kde/libs/kquickimageeditor/kquickimageeditor.py
@@ -1,24 +1,24 @@
 import info
-from Package.CMakePackageBase import *
-
 
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://invent.kde.org/libraries/kquickimageeditor.git"
 
-        for ver in ["0.2.0"]:
+        for ver in ["0.3.0"]:
             self.targets[ver] = "https://download.kde.org/stable/kquickimageeditor/kquickimageeditor-%s.tar.xz" % ver
             self.archiveNames[ver] = "kquickimageeditor-%s.tar.gz" % ver
             self.targetInstSrc[ver] = "kquickimageeditor-%s" % ver
 
         self.description = "A set of QtQuick components providing basic image editing capabilities"
-        self.defaultTarget = "0.2.0"
+        self.defaultTarget = "0.3.0"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
 
 
-class Package(CMakePackageBase):
-    def __init__(self, **args):
-        CMakePackageBase.__init__(self)
+from Blueprints.CraftPackageObject import CraftPackageObject
+
+class Package(CraftPackageObject.get("kde").pattern):
+    def __init__(self):
+        CraftPackageObject.get("kde").pattern.__init__(self)
