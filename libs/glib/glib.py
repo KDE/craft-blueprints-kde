e48c0fd9c1e80ad2c3e63799ac83faa237463a9f
Hannah von Reth <vonreth@kde.org>
2023-11-05 17:23:10 +0100

    glib 2.78.1

--- libs/glib/glib.py
+++ libs/glib/glib.py
@@ -3,15 +3,12 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2.68.4"]:
+        for ver in ["2.78.1"]:
             majorMinorStr = ".".join(ver.split(".")[0:2])
             self.targets[ver] = f"https://download.gnome.org/sources/glib/{majorMinorStr}/glib-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"glib-{ver}"
-        self.targetDigests["2.68.4"] = (["62fd061d08a75492617e625a73e2c05e259f831acbb8e1f8b9c81f23f7993a3b"], CraftHash.HashAlgorithm.SHA256)
-        self.patchToApply["2.68.4"] = [("glib-2.68.4-20231105.diff", 1)]
-        self.patchLevel["2.68.4"] = 1
-
-        self.defaultTarget = "2.68.4"
+        self.targetDigests["2.78.1"] = (["915bc3d0f8507d650ead3832e2f8fb670fce59aac4d7754a7dab6f1e6fed78b2"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "2.78.1"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
@@ -24,6 +21,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/iconv"] = None
         self.runtimeDependencies["libs/dbus"] = None
         self.runtimeDependencies["libs/gettext"] = None
+        self.runtimeDependencies["libs/pcre2"] = None
         if not OsUtils.isWin():
             self.runtimeDependencies["libs/iconv"] = None
 
@@ -39,9 +37,10 @@ class Package(MesonPackageBase):
                 f"-I{OsUtils.toUnixPath(CraftStandardDirs.craftRoot() / 'include/dbus-1.0')}"
                 f" -I{OsUtils.toUnixPath(CraftStandardDirs.craftRoot() / 'lib/dbus-1.0/include')}"
             )
-        # cmake pcre does not provide .pc files
-        self.subinfo.options.configure.args += ["-Dinternal_pcre=true", "-Diconv=external", "--wrap-mode=nodownload"]
+        self.subinfo.options.configure.args += ["--wrap-mode=nodownload"]
         if CraftCore.compiler.isUnix:
             self.subinfo.options.configure.ldflags += f" -lintl -liconv"
         if CraftCore.compiler.isFreeBSD:
             self.subinfo.options.configure.args += ["-Dxattr=false", "-Dlibmount=disabled", "-Dselinux=disabled", "-Db_lundef=false"]
+        elif CraftCore.compiler.isMacOS:
+            self.subinfo.options.configure.args += ["-Dtests=false"]
