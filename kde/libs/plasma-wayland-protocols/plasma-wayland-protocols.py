5282c24cf0f6519eb7dca41bba90b45420d723e6
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-11-08 20:22:18 +0100

    [plasma-wayland-protocols] Update

--- kde/libs/plasma-wayland-protocols/plasma-wayland-protocols.py
+++ kde/libs/plasma-wayland-protocols/plasma-wayland-protocols.py
@@ -1,4 +1,5 @@
 import info
+from Package.CMakePackageBase import CMakePackageBase
 
 
 class subinfo(info.infoclass):
@@ -10,21 +11,18 @@ class subinfo(info.infoclass):
         self.description = "Plasma-specific protocols for Wayland"
         self.svnTargets["master"] = "https://invent.kde.org/libraries/plasma-wayland-protocols"
 
-        for ver in ["1.8.0", "1.9.0", "1.10.0"]:
+        for ver in ["1.10.0", "1.11.0"]:
             self.targets[ver] = f"https://download.kde.org/stable/plasma-wayland-protocols/plasma-wayland-protocols-{ver}.tar.xz"
             self.targetDigestUrls[ver] = f"https://download.kde.org/stable/plasma-wayland-protocols/plasma-wayland-protocols-{ver}.tar.xz.sha256"
             self.targetInstSrc[ver] = f"plasma-wayland-protocols-{ver}"
 
-        self.defaultTarget = "1.10.0"
+        self.defaultTarget = "1.11.0"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
 
 
-from Package.CMakePackageBase import *
-
-
 class Package(CMakePackageBase):
     def __init__(self):
-        CMakePackageBase.__init__(self)
+        super().__init__()
