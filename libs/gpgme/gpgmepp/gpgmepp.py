5685d7a022f66c3d0d6eecc2d02d9cffe7c3d8d3
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-11-09 15:02:43 +0000

    [gpgme] Bump patch level after patch change

--- libs/gpgme/gpgmepp/gpgmepp.py
+++ libs/gpgme/gpgmepp/gpgmepp.py
@@ -22,7 +22,7 @@ class subinfo(info.infoclass):
 
         self.patchToApply["1.21.0"] = ("cmake.patch", 1)
 
-        self.patchLevel["1.21.0"] = 5
+        self.patchLevel["1.21.0"] = 6
 
 
 from Package.CMakePackageBase import *
