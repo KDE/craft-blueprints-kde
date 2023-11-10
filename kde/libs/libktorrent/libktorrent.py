a0f4c2385d14aeba682883091600251bc1f6118c
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-11-10 09:37:10 +0100

    Move libktorrent, it is part of Gear now

--- kde/libs/libktorrent/libktorrent.py
+++ kde/libs/libktorrent/libktorrent.py
@@ -1,18 +1,12 @@
 import info
-from Package.CMakePackageBase import *
+from Package.CMakePackageBase import CMakePackageBase
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets["master"] = "https://anongit.kde.org/libktorrent"
-        for ver in ["1.3.1"]:
-            self.targets[ver] = "http://ktorrent.org/downloads/4." + ver[2:] + "/libktorrent-" + ver + ".tar.bz2"
-            self.targetInstSrc[ver] = "libktorrent-" + ver
-            self.patchToApply[ver] = [("libktorrent-1.3.1-20130607.diff", 1)]
-        self.patchToApply["master"] = [("libktorrent-1.3.1-20130607.diff", 1)]
+        self.versionInfo.setDefaultValues()
 
         self.description = "A BitTorrent protocol implementation."
-        self.defaultTarget = "master"
 
     def setDependencies(self):
         self.runtimeDependencies["kdesupport/qca"] = None
