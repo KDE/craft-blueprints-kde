d5db5f714f36a0b13f1231659de01f1a34c3a4c4
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-11-10 09:24:21 +0100

    [hunspell] Try to fix on MSVC

--- libs/hunspell/hunspell.py
+++ libs/hunspell/hunspell.py
@@ -1,13 +1,24 @@
 import info
+from CraftCore import CraftCore
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = f"https://github.com/hunspell/hunspell.git"
         for ver in ["1.7.2"]:
-            self.targets[ver] = f"https://github.com/hunspell/hunspell/releases/download/v{ver}/hunspell-{ver}.tar.gz"
+            if CraftCore.compiler.isMSVC():
+                self.targets[ver] = f"https://github.com/hunspell/hunspell/archive/v{ver}.tar.gz"
+            else:
+                self.targets[ver] = f"https://github.com/hunspell/hunspell/releases/download/v{ver}/hunspell-{ver}.tar.gz"
+
             self.targetInstSrc[ver] = f"hunspell-{ver}"
-        self.targetDigests["1.7.2"] = (["11ddfa39afe28c28539fe65fc4f1592d410c1e9b6dd7d8a91ca25d85e9ec65b8"], CraftHash.HashAlgorithm.SHA256)
+
+        if CraftCore.compiler.isMSVC():
+            self.targetDigests["1.7.2"] = (["69fa312d3586c988789266eaf7ffc9861d9f6396c31fc930a014d551b59bbd6e"], CraftHash.HashAlgorithm.SHA256)
+        else:
+            self.targetDigests["1.7.2"] = (["11ddfa39afe28c28539fe65fc4f1592d410c1e9b6dd7d8a91ca25d85e9ec65b8"], CraftHash.HashAlgorithm.SHA256)
+
+
 
         self.description = "Hunspell is the spell checker of LibreOffice, OpenOffice.org, Mozilla Firefox 3 & Thunderbird, Google Chrome, and it is also used by proprietary software packages, like macOS, InDesign, memoQ, Opera and SDL Trados."
         self.webpage = "http://hunspell.github.io/"
