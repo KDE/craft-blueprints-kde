59b238ee01ce6da002d9a77afad6ee25a7c6ca52
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-11-10 12:09:42 +0100

    [quazip] Update to add Qt6 support

--- qt-libs/quazip/quazip.py
+++ qt-libs/quazip/quazip.py
@@ -11,12 +11,13 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/stachenov/quazip"
-        for ver in ["1.1"]:
+        for ver in ["1.1", "1.4"]:
             self.targets[ver] = f"https://github.com/stachenov/quazip/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"quazip-v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"quazip-{ver}"
         self.targetDigests["1.1"] = (["54edce9c11371762bd4f0003c2937b5d8806a2752dd9c0fd9085e90792612ad0"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.1"
+        self.targetDigests["1.4"] = (["79633fd3a18e2d11a7d5c40c4c79c1786ba0c74b59ad752e8429746fe1781dd6"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.4"
 
         self.description = "Qt/C++ wrapper over minizip"
 
