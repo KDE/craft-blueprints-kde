d2828d003dcd254e7d1127abda55d19c8255087d
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-11-11 12:49:22 +0100

    [knotifications] Fix KF6 on macOS

--- kde/frameworks/tier3/knotifications/knotifications.py
+++ kde/frameworks/tier3/knotifications/knotifications.py
@@ -8,8 +8,11 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
-        # Fix macOS build, see https://invent.kde.org/frameworks/knotifications/-/merge_requests/118
-        self.patchToApply["master"] = [("fix-macos-build.patch", 1)]
+        for ver in ["master", "5.245.0"]:
+            # Fix macOS build, see https://invent.kde.org/frameworks/knotifications/-/merge_requests/118
+            self.patchToApply[ver] = [("fix-macos-build.patch", 1)]
+            self.patchToApply[ver] = [("knotifications-5.245.0-20231111.diff", 1)]
+
         self.patchLevel["master"] = 1
 
     def setDependencies(self):
