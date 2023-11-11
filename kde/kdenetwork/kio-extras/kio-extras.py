4b971c835ea254dffe321a72d695c8c719e2cf26
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-11-11 12:50:23 +0100

    [kio-extras] Apply patch for KF6 too

--- kde/kdenetwork/kio-extras/kio-extras.py
+++ kde/kdenetwork/kio-extras/kio-extras.py
@@ -18,6 +18,7 @@ class subinfo(info.infoclass):
         # https://invent.kde.org/network/kio-extras/-/merge_requests/297
         self.patchToApply["23.08.2"] = [("fix-mingw-build.patch", 1)]
         self.patchToApply["23.08.3"] = [("fix-mingw-build.patch", 1)]
+        self.patchToApply["24.01.75"] = [("fix-mingw-build.patch", 1)]
         self.patchLevel["23.08.2"] = 1
 
     def setDependencies(self):
