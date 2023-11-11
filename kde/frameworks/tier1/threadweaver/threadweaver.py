e613e16a9f26dc7dc07d25c56db510ce252cf443
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-11-11 10:23:00 +0100

    [threadweaver] Backport patch to fix MinGW

--- kde/frameworks/tier1/threadweaver/threadweaver.py
+++ kde/frameworks/tier1/threadweaver/threadweaver.py
@@ -8,6 +8,8 @@ class subinfo(info.infoclass):
 
         self.description = "ThreadWeaver"
 
+        self.patchToApply["5.245.0"] = [("fix-non-void-return.diff", 1)]
+
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
