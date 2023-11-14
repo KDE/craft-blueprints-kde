8ab3341f9c3b6c709385b2fca41012ba64802ddd
Hannah von Reth <vonreth@kde.org>
2023-11-14 10:44:01 +0100

    Rebuild qtbase

--- libs/qt6/qtbase/qtbase.py
+++ libs/qt6/qtbase/qtbase.py
@@ -34,6 +34,7 @@ class subinfo(info.infoclass):
             self.patchToApply["6.6.0"] += [("qtbase-6.6.0-mingw-find-pcre2.diff", 1)]
 
         self.patchLevel["6.4.3"] = 4
+        self.patchLevel["6.6.0"] = 1
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
