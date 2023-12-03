5fb7982b1d423d862ea24ed545e7280e3668d808
Joshua Goins <josh@redstrate.com>
2023-12-03 05:24:32 -0500

    Add PlasmaTube blueprint

---
+++ kde/applications/plasmatube/plasmatube.py
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2023 Joshua Goins <josh@redstrate.com>
+
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues(gitUrl="https://invent.kde.org/multimedia/plasmatube.git")
+        self.displayName = "PlasmaTube"
+        self.description = "Kirigami YouTube video player based on QtMultimedia and youtube-dl"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["libs/qt/qtdeclarative"] = None
+        self.runtimeDependencies["kde/unreleased/mpvqt"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kitemmodels"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kitemmodels"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kdeclarative"] = None
+        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
+        self.runtimeDependencies["qt-libs/qtkeychain"] = None
+        if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+            self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+            self.runtimeDependencies["kde/plasma/breeze"] = None
+        else:
+            self.runtimeDependencies["kde/plasma/qqc2-breeze-style"] = None
+
+
+class Package(CraftPackageObject.get("kde").pattern):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def createPackage(self):
+        self.defines["executable"] = r"bin\plasmatube.exe"
+        self.addExecutableFilter(r"(bin|libexec)/(?!(plasmatube|update-mime-database)).*")
+        self.ignoredPackages.append("binary/mysql")
+        if not CraftCore.compiler.isLinux:
+            self.ignoredPackages.append("libs/dbus")
+        return super().createPackage()
