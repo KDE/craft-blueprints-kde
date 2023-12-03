88970bbbd1d4a89f2a34ec3fbbec479a0edf0c81
Volker Krause <vkrause@kde.org>
2023-12-03 13:23:38 +0100

    Add exclusion list for the Qrca APK

--- kde/unreleased/qrca/qrca.py
+++ kde/unreleased/qrca/qrca.py
@@ -39,3 +39,4 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
+        self.blacklist_file.append(self.packageDir() / "blacklist.txt")
