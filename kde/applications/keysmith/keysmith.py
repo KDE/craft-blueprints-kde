c5182e0fb853e0914d03a40df446f7117d2c7db9
Joshua Goins <josh@redstrate.com>
2023-12-03 10:01:28 +0000

    kde/applications/keysmith: Add qqc2-breeze-style dependency
    Only on Android

--- kde/applications/keysmith/keysmith.py
+++ kde/applications/keysmith/keysmith.py
@@ -26,7 +26,8 @@ class subinfo(info.infoclass):
             self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
             self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
             self.runtimeDependencies["kde/plasma/breeze"] = None
-
+        else:
+            self.runtimeDependencies["kde/plasma/qqc2-breeze-style"] = None
 
 class Package(CMakePackageBase):
     def __init__(self):
