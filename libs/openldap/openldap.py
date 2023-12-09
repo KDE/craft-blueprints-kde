7fd36e0924554f19629be2411be2840f9d232b7a
Hannah von Reth <vonreth@kde.org>
2023-12-09 19:40:43 +0100

    Fix autoreconf for openldap

--- libs/openldap/openldap.py
+++ libs/openldap/openldap.py
@@ -8,8 +8,9 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://www.openldap.org/software/download/OpenLDAP/openldap-release/openldap-{ver}.tgz"
             self.targetInstSrc[ver] = f"openldap-{ver}"
 
+        self.patchToApply["2.4.45"] = [("openldap-2.4.45-20231209.diff", 1)] # https://gitweb.gentoo.org/repo/gentoo.git/tree/net-nds/openldap/openldap-2.4.59-r2.ebuild#n380
         if CraftCore.compiler.isWindows:
-            self.patchToApply["2.4.45"] = [("openldap-2.4.45-20170628.diff", 1)]
+            self.patchToApply["2.4.45"] += [("openldap-2.4.45-20170628.diff", 1)]
         self.targetDigests["2.4.45"] = (["cdd6cffdebcd95161a73305ec13fc7a78e9707b46ca9f84fb897cd5626df3824"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.5.16"] = (["546ba591822e8bb0e467d40c4d4a30f89d937c3a507fe83a578f582f6a211327"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.6.6"] = (["082e998cf542984d43634442dbe11da860759e510907152ea579bdc42fe39ea0"], CraftHash.HashAlgorithm.SHA256)
@@ -41,5 +42,4 @@ else:
     class Package(AutoToolsPackageBase):
         def __init__(self, **args):
             AutoToolsPackageBase.__init__(self)
-            self.subinfo.options.configure.autoreconf = False
             self.subinfo.options.configure.args += ["--without-cyrus_sasl", "--disable-bdb", "--disable-hdb"]
