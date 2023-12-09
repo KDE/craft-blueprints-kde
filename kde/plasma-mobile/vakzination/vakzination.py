ffea3a18238e6ddda6246264aaab9986528976f1
Volker Krause <vkrause@kde.org>
2023-12-09 13:06:57 +0100

    Add exclusion list for the Vakzination APK

--- kde/plasma-mobile/vakzination/vakzination.py
+++ kde/plasma-mobile/vakzination/vakzination.py
@@ -34,3 +34,4 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
+        self.blacklist_file.append(self.blueprintDir() / "blacklist.txt")
