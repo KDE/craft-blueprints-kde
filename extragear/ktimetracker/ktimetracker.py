de6387c3a344f6547af9d3fdb8aab896c142a7e1
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-12-16 20:17:42 +0100

    [KTimeTracker] Cleanup and Qt6

--- extragear/ktimetracker/ktimetracker.py
+++ extragear/ktimetracker/ktimetracker.py
@@ -1,16 +1,18 @@
+import os
+
 import info
-from CraftConfig import *
-from CraftOS.osutils import OsUtils
+from Blueprints.CraftPackageObject import CraftPackageObject
+from Package.CMakePackageBase import CMakePackageBase
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets["master"] = "https://anongit.kde.org/ktimetracker|master"
+        self.svnTargets["master"] = "https://invent.kde.org/pim/ktimetracker.git|master"
         self.defaultTarget = "5.0.1"
 
         for ver in ["5.0.0", "5.0.1"]:
-            self.targets[ver] = "https://download.kde.org/stable/ktimetracker/%s/src/ktimetracker-%s.tar.xz" % (ver, ver)
-            self.targetInstSrc[ver] = "ktimetracker-%s" % ver
+            self.targets[ver] = f"https://download.kde.org/stable/ktimetracker/{ver}/src/ktimetracker-{ver}.tar.xz"
+            self.targetInstSrc[ver] = f"ktimetracker-{ver}"
 
         self.displayName = "KTimeTracker"
         self.description = "Personal Time Tracker"
@@ -33,23 +35,23 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
         self.runtimeDependencies["kde/frameworks/tier3/ktextwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
+        if not CraftPackageObject.get("libs/qt").instance.subinfo.options.dynamic.qtMajorVersion == "6":
+            self.runtimeDependencies["kde/frameworks/tier2/kstatusnotifieritem"] = None
+            self.runtimeDependencies["kde/frameworks/tier3/kcmUtils"] = None
         # KTimeTracker forces Breeze style on Windows
         self.runtimeDependencies["kde/plasma/breeze"] = None
 
 
-from Package.CMakePackageBase import *
-
-
 class Package(CMakePackageBase):
     def __init__(self):
-        CMakePackageBase.__init__(self)
+        super().__init__()
 
     def createPackage(self):
         self.defines["productname"] = "KTimeTracker"
         self.defines["website"] = "https://userbase.kde.org/KTimeTracker"
         self.defines["executable"] = "bin\\ktimetracker.exe"
-        self.defines["icon"] = os.path.join(self.packageDir(), "ktimetracker.ico")
+        self.defines["icon"] = os.path.join(self.blueprintDir(), "ktimetracker.ico")
 
-        self.blacklist_file.append(os.path.join(self.packageDir(), "blacklist.txt"))
+        self.blacklist_file.append(os.path.join(self.blueprintDir(), "blacklist.txt"))
 
-        return TypePackager.createPackage(self)
+        return super().createPackage()
