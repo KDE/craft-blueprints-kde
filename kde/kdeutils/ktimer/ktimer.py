1c8ac703b84d90cbb5090f17110fe4a243aa4cec
Laurent Montel <montel@kde.org>
2023-12-20 13:38:38 +0100

    Add missing qt5compat

--- kde/kdeutils/ktimer/ktimer.py
+++ kde/kdeutils/ktimer/ktimer.py
@@ -11,6 +11,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.buildDependencies["libs/qt6/qt5compat"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
