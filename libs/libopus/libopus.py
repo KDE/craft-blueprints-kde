b1f834cdd583b0f44c99a368f8d332de5e4ad199
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-12-30 19:37:02 +0000

    [libopus] fix hash

--- libs/libopus/libopus.py
+++ libs/libopus/libopus.py
@@ -17,7 +17,7 @@ class subinfo(info.infoclass):
             ]  # https://github.com/msys2/MINGW-packages/blob/7c67d9c46554d10276b2017bcfd0290dfe24a1bf/mingw-w64-opus/PKGBUILD#L36
 
         self.targetDigests["1.3.1"] = (["65b58e1e25b2a114157014736a3d9dfeaad8d41be1c8179866f144a2fb44ff9d"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["1.4"] = (["65b58e1e25b2a114157014736a3d9dfeaad8d41be1c8179866f144a2fb44ff9d"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.4"] = (["c9b32b4253be5ae63d1ff16eea06b94b5f0f2951b7a02aceef58e3a3ce49c51f"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "Opus codec library"
         self.defaultTarget = "1.4"
