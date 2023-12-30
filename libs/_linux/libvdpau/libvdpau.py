61dcae8771a2474d1e7ded22ab757a06a8344529
Julius Künzel <jk.kdedev@smartlab.uber.space>
2023-12-30 12:21:38 +0100

    [libvdpau] Update to 1.5

--- libs/_linux/libvdpau/libvdpau.py
+++ libs/_linux/libvdpau/libvdpau.py
@@ -1,23 +1,23 @@
 import info
+from Package.MesonPackageBase import MesonPackageBase
+from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.4"]:
+        for ver in ["1.4", "1.5"]:
             self.targets[ver] = f"https://gitlab.freedesktop.org/vdpau/libvdpau/-/archive/{ver}/libvdpau-{ver}.tar.bz2"
-            self.targetInstSrc[ver] = "libvdpau-" + ver
+            self.targetInstSrc[ver] = f"libvdpau-{ver}"
         self.targetDigests["1.4"] = (["4258824c5a4555ef31de0a7d82b3caf19e75a16a13949f1edafc5f6fb2d33f30"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.5"] = (["a5d50a42b8c288febc07151ab643ac8de06a18446965c7241f89b4e810821913"], CraftHash.HashAlgorithm.SHA256)
         self.description = "Video Decode and Presentation API for Unix. Developed by NVIDIA for Unix/Linux systems."
-        self.defaultTarget = "1.4"
+        self.defaultTarget = "1.5"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["python-modules/meson"] = None
 
 
-from Package.MesonPackageBase import *
-
-
 class Package(MesonPackageBase):
     def __init__(self, **args):
-        MesonPackageBase.__init__(self)
+        super().__init__()
