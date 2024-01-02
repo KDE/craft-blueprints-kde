925441f6e44c27beb203925fd972b5f755ea0a0b
Hannah von Reth <vonreth@kde.org>
2024-01-02 15:53:04 +0100

    aom: 3.8.0

--- libs/aom/aom.py
+++ libs/aom/aom.py
@@ -5,11 +5,11 @@ from Package.CMakePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "An open, royalty-free video coding format designed for video transmissions over the Internet"
-        for ver in ["3.1.3", "3.6.1"]:
-            self.targets[ver] = f"https://aomedia.googlesource.com/aom.git/+archive/v{ver}.tar.gz"
-        self.defaultTarget = "3.6.1"
-        # self.targetDigests[ver] =  (['non-reproducible archive :-('], CraftHash.HashAlgorithm.SHA256)
-
+        for ver in ["3.1.3", "3.6.1", "3.8.0"]:
+            self.targets[ver] = f"https://storage.googleapis.com/aom-releases/libaom-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"libaom-{ver}"
+        self.targetDigests["3.8.0"] = (['a768d3e54c7f00cd38b01208d1ae52d671be410cfc387ff7881ea71c855f3600'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "3.8.0"
     def setDependencies(self):
         self.buildDependencies["dev-utils/cmake"] = None
         self.buildDependencies["dev-utils/perl"] = None
