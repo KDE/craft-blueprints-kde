bb1612bd4954303720f5d42e77a6237b7a511a4b
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-01-03 01:15:01 +0100

    [Qt5Multimedia] Fix build with C++17

--- libs/qt5/qtmultimedia/qtmultimedia.py
+++ libs/qt5/qtmultimedia/qtmultimedia.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 import info
-from Package.Qt5CorePackageBase import Qt5CorePackageBase
 from CraftCore import CraftCore
+from Package.Qt5CorePackageBase import Qt5CorePackageBase
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
+        self.patchToApply["kde/5.15"] = [("0002-fix-c++17-build.patch", 1)]
         if CraftCore.compiler.isMinGW():
             self.patchToApply["5.15.2"] = [("0001-fix-wmf-plugin.patch", 1)]
             self.patchToApply["5.15.5"] = [("0001-fix-wmf-plugin.patch", 1)]
-            self.patchToApply["kde/5.15"] = [("0001-fix-wmf-plugin.patch", 1)]
+            self.patchToApply["kde/5.15"] += [("0001-fix-wmf-plugin.patch", 1)]
             self.patchToApply["kde/before-5.15.11-rebase"] = [("0001-fix-wmf-plugin.patch", 1)]
         self.patchLevel["5.15.2"] = 1
         self.patchLevel["kde/before-5.15.11-rebase"] = 2
