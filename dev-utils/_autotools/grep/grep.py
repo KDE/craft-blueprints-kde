90afb2df542f372ed3f60e571fab9f65d842ada7
Hannah von Reth <vonreth@kde.org>
2024-01-12 10:41:32 +0100

    grep 3.11

--- dev-utils/_autotools/grep/grep.py
+++ dev-utils/_autotools/grep/grep.py
@@ -10,16 +10,18 @@ class subinfo(info.infoclass):
     def setDependencies(self):
         self.buildDependencies["dev-utils/msys"] = None
         self.runtimeDependencies["libs/gettext"] = None
+        self.runtimeDependencies["libs/pcre2"] = None
 
     def setTargets(self):
         self.description = (
             "Grep searches one or more input files for lines containing a match to a specified pattern. By default, Grep outputs the matching lines."
         )
-        for ver in ["3.7"]:
+        for ver in ["3.7", "3.11"]:
             self.targets[ver] = f"https://ftp.gnu.org/gnu/grep/grep-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"grep-{ver}"
         self.targetDigests["3.7"] = (["5c10da312460aec721984d5d83246d24520ec438dd48d7ab5a05dbc0d6d6823c"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "3.7"
+        self.targetDigests["3.11"] = (["1db2aedde89d0dea42b16d9528f894c8d15dae4e190b59aecc78f5a951276eab"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "3.11"
 
 
 class Package(AutoToolsPackageBase):
