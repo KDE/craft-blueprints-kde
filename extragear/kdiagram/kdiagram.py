1ad1da5bfac594d3f5bc206d4aee05b7fabe40c3
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-01-12 15:14:11 +0100

    [KDiagram] Use stable target

--- extragear/kdiagram/kdiagram.py
+++ extragear/kdiagram/kdiagram.py
@@ -1,13 +1,20 @@
 import info
+from Package.CMakePackageBase import CMakePackageBase
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
+        self.description = "Powerful libraries (KChart, KGantt) for creating business diagrams"
+
+        for ver in ["2.8.0", "3.0.1"]:
+            self.targets[ver] = f"https://download.kde.org/stable/kdiagram/{ver}/kdiagram-{ver}.tar.xz"
+            self.targetDigestUrls[ver] = f"https://download.kde.org/stable/kdiagram/{ver}/kdiagram-{ver}.tar.xz.sha256"
+            self.targetInstSrc[ver] = f"kdiagram-{ver}"
+
         self.svnTargets["master"] = "https://invent.kde.org/graphics/kdiagram.git"
         self.targetUpdatedRepoUrl["master"] = ("https://anongit.kde.org/kdiagram", "https://invent.kde.org/graphics/kdiagram.git")
-        self.defaultTarget = "master"
 
-        self.description = "Powerful libraries (KChart, KGantt) for creating business diagrams"
+        self.defaultTarget = "2.8.0"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
@@ -15,9 +22,6 @@ class subinfo(info.infoclass):
         self.buildDependencies["libs/qt/qtsvg"] = None
 
 
-from Package.CMakePackageBase import *
-
-
 class Package(CMakePackageBase):
     def __init__(self):
-        CMakePackageBase.__init__(self)
+        super().__init__()
