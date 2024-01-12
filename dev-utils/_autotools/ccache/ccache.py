8b03cbfe9cea2e9a838252ccaf45e0f6f2cb8ef4
Hannah von Reth <vonreth@kde.org>
2024-01-12 10:41:15 +0100

    ccache 4.9

--- dev-utils/_autotools/ccache/ccache.py
+++ dev-utils/_autotools/ccache/ccache.py
@@ -7,14 +7,15 @@ class subinfo(info.infoclass):
         self.svnTargets["master"] = "https://github.com/ccache/ccache.git"
         self.targetInstallPath["master"] = "dev-utils"
 
-        for ver in ["4.6"]:
+        for ver in ["4.6", "4.9"]:
             self.targets[ver] = f"https://github.com/ccache/ccache/releases/download/v{ver}/ccache-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"ccache-{ver}"
             self.targetInstallPath[ver] = "dev-utils"
         self.targetDigests["4.6"] = (["73a1767ac6b7c0404a1a55f761a746d338e702883c7137fbf587023062258625"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["4.9"] = (["866b2223d59333640f0e7a003cbb85b32d9ca3c9445bd9e3cf142942e69dd3ec"], CraftHash.HashAlgorithm.SHA256)
 
         self.webpage = "https://ccache.dev/"
-        self.defaultTarget = "4.6"
+        self.defaultTarget = "4.9"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
