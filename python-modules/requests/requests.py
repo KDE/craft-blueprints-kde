3a26a63ab47cd1d6c019b8e599b7bbf0b587db2f
Ingo Klöcker <dev@ingo-kloecker.de>
2024-01-18 16:23:57 +0100

    Revert "Move Python modules to Craft"
    This reverts commit 14fee4cfaaa989a967d44b8decdbec0296059678.

---
+++ python-modules/requests/requests.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2021 Volker Krause <vkrause@kde.org>
+
+import info
+from Package.PipPackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["master"] = ""
+        self.defaultTarget = "master"
+
+
+class Package(PipPackageBase):
+    def __init__(self, **args):
+        super().__init__()
