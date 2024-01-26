6b1896513422d852d6afe90c58955ce38144e41a
Thomas Baumgart <thb@net-bembel.de>
2024-01-26 14:39:50 +0100

    Revert "[alkimia] Use stable target"
    This reverts commit 859d26c97e06e7a01c335cb07feb2e4d826481fa.

--- extragear/libalkimia/libalkimia.py
+++ extragear/libalkimia/libalkimia.py
@@ -35,8 +35,7 @@ class subinfo(info.infoclass):
 
         self.svnTargets["master"] = "https://invent.kde.org/office/alkimia.git"
         self.targetUpdatedRepoUrl["master"] = ("https://anongit.kde.org/alkimia", "https://invent.kde.org/office/alkimia.git")
-
-        self.defaultTarget = "8.1.2"
+        self.defaultTarget = "master"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt/qtbase"] = None
