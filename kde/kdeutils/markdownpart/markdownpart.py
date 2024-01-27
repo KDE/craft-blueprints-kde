c8e0596ae9ced6bc6f0c8c5cdcdb9c5201c3727c
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-01-27 17:31:50 +0100

    [markdownpart] Get ready for Qt6

--- kde/kdeutils/markdownpart/markdownpart.py
+++ kde/kdeutils/markdownpart/markdownpart.py
@@ -1,4 +1,5 @@
 import info
+from Blueprints.CraftPackageObject import CraftPackageObject
 
 
 class subinfo(info.infoclass):
@@ -17,9 +18,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/kparts"] = None
 
 
-from Package.CMakePackageBase import *
-
-
-class Package(CMakePackageBase):
+class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self):
         super().__init__()
