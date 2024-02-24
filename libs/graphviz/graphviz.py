1af8a62b09f29843f58a5d658d459a075bc266b2
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-02-24 01:51:49 +0000

    [graphviz] Next try to fix MSVC build

--- libs/graphviz/graphviz.py
+++ libs/graphviz/graphviz.py
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2024 Julius Künzel <jk.kdedev@smartlab.uber.space>
 
 import info
-from Package.AutoToolsPackageBase import AutoToolsPackageBase
+from Package.CMakePackageBase import CMakePackageBase
 
 
 class subinfo(info.infoclass):
@@ -13,6 +13,9 @@ class subinfo(info.infoclass):
             # We need to use git since the tarballs do not contain the submudules needed on Windows
             self.svnTargets[ver] = f"https://gitlab.com/graphviz/graphviz.git||{ver}"
 
+           self.patchToApply["10.0.1"] = [("fix-windows-install.diff", 1)]
+        self.patchLevel["10.0.1"] = 1
+
         self.svnTargets["master"] = "https://gitlab.com/graphviz/graphviz.git"
 
         self.defaultTarget = "10.0.1"
@@ -21,7 +24,7 @@ class subinfo(info.infoclass):
         self.buildDependencies["dev-utils/bison"] = None
 
 
-class Package(AutoToolsPackageBase):
+class Package(CMakePackageBase):
     def __init__(self, **args):
         super().__init__()
-        self.subinfo.options.fetch.checkoutSubmodules = True
+        self.subinfo.options.configure.args = ["-Dwith_gvedit=OFF"]
