6fff53a5437be1ab7f581caa523eb47e04f6d620
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-02-24 12:04:50 +0000

    [graphviz] Add yet another msvc patch

--- libs/graphviz/graphviz.py
+++ libs/graphviz/graphviz.py
@@ -13,8 +13,13 @@ class subinfo(info.infoclass):
             # We need to use git since the tarballs do not contain the submudules needed on Windows
             self.svnTargets[ver] = f"https://gitlab.com/graphviz/graphviz.git||{ver}"
 
-        self.patchToApply["10.0.1"] = [("fix-windows-install.diff", 1)]
-        self.patchLevel["10.0.1"] = 1
+        self.patchToApply["10.0.1"] = [
+            ("fix-windows-install.diff", 1),
+            # https://github.com/microsoft/vcpkg/blob/master/ports/graphviz/disable-pragma-lib.patch
+            ("disable-pragma-lib.patch", 1)
+        ]
+
+        self.patchLevel["10.0.1"] = 2
 
         self.svnTargets["master"] = "https://gitlab.com/graphviz/graphviz.git"
 
@@ -29,4 +34,11 @@ class Package(CMakePackageBase):
     def __init__(self, **args):
         super().__init__()
         self.subinfo.options.fetch.checkoutSubmodules = True
-        self.subinfo.options.configure.args = ["-Dwith_gvedit=OFF"]
+        self.subinfo.options.configure.args = [
+            "-Dwith_gvedit=OFF",
+            "-DCMAKE_DISABLE_FIND_PACKAGE_ANN=ON",
+            "-DCMAKE_REQUIRE_FIND_PACKAGE_CAIRO=ON",
+            "-DCMAKE_REQUIRE_FIND_PACKAGE_EXPAT=ON",
+            "-DCMAKE_REQUIRE_FIND_PACKAGE_LTDL=ON",
+            "-DCMAKE_REQUIRE_FIND_PACKAGE_PANGOCAIRO=ON"
+        ]
