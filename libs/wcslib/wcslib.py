2a61b34480aadbcdcc47c4ff73f3b0aabaf6ef4e
Jasem Mutlaq <mutlaqja@ikarustech.com>
2024-02-27 13:14:59 +0300

    Apply patch to MacOS + Linux

--- libs/wcslib/wcslib.py
+++ libs/wcslib/wcslib.py
@@ -8,7 +8,7 @@ class subinfo(info.infoclass):
             self.targets[ver] = "http://indilib.org/jdownloads/wcslib/wcslib-%s.tar.gz" % ver
             self.targetInstSrc[ver] = "wcslib-%s" % ver
         self.defaultTarget = "7.7"
-        if CraftCore.compiler.isMacOS:
+        if not CraftCore.compiler.isWindows:
             self.patchToApply["7.7"] = [("int64.diff", 1)]
             self.patchLevel["7.7"] = 1
 
