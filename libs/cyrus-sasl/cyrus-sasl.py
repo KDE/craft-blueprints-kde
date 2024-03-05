de5368d90cb20a7d86e15122f771012a4874e59e
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-03-05 22:46:30 +0000

    [cyrus-sasl] Update to 2.1.28 (not for Windows)

--- libs/cyrus-sasl/cyrus-sasl.py
+++ libs/cyrus-sasl/cyrus-sasl.py
@@ -4,24 +4,34 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def registerOptions(self):
-        # cyrus-sasl on MinGW does not work out of the box. It needs someone how cares
+        # cyrus-sasl on MinGW does not work out of the box. It needs someone who cares
         self.parent.package.categoryInfo.platforms = (
             CraftCore.compiler.Compiler.NoCompiler if CraftCore.compiler.isMinGW() else CraftCore.compiler.Platforms.All
         )
 
     def setTargets(self):
+
+        self.description = "Cyrus SASL implementation"
+
         for ver in ["2.1.26", "2.1.28"]:
             self.targets[ver] = f"https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-{ver}/cyrus-sasl-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"cyrus-sasl-{ver}"
             # http://www.linuxfromscratch.org/blfs/view/svn/postlfs/cyrus-sasl.html
-            self.patchToApply["2.1.26"] = [("cyrus-sasl-2.1.26-fixes-3.patch", 1), ("cyrus-sasl-2.1.26-openssl-1.1.0-1.patch", 1)]
+
+        self.patchToApply["2.1.26"] = [("cyrus-sasl-2.1.26-fixes-3.patch", 1), ("cyrus-sasl-2.1.26-openssl-1.1.0-1.patch", 1)]
         if CraftCore.compiler.isWindows:
             self.patchToApply["2.1.26"] += [("cyrus-sasl-2.1.26.patch", 1)]
+
         self.targetDigests["2.1.26"] = (["8fbc5136512b59bb793657f36fadda6359cae3b08f01fd16b3d406f1345b7bc3"], CraftHash.HashAlgorithm.SHA256)
         self.patchLevel["2.1.26"] = 2
         self.targetDigests["2.1.28"] = (["7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"], CraftHash.HashAlgorithm.SHA256)
-        self.description = "Cyrus SASL implementation"
-        self.defaultTarget = "2.1.26"
+
+        if CraftCore.compiler.isWindows:
+            # We use the older version on Windows, because someone needs
+            # to port the CMake patch to a newer version to make it work
+            self.defaultTarget = "2.1.26"
+        else:
+            self.defaultTarget = "2.1.28"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
