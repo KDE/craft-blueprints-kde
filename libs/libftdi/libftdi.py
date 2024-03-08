57b4cd20ba28295ad77f1dfccf2f8f282cb4c116
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-03-08 19:05:13 +0100

    Enable libftdi on all platforms
    Also do some cleanup. As far as I can see the custom rpath and CFLAG magic should not be need as Craft will deal with that

--- libs/libftdi/libftdi.py
+++ libs/libftdi/libftdi.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
-import os
-
 import info
-import utils
-from CraftCore import CraftCore
 from Package.CMakePackageBase import CMakePackageBase
+from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.4"]:
+        for ver in ["1.4", "1.5"]:
             self.targets[ver] = f"https://www.intra2net.com/en/developer/libftdi/download/libftdi1-{ver}.tar.bz2"
             self.archiveNames[ver] = f"libftdi1-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"libftdi1-{ver}"
+        self.targetDigests["1.5"] = (["7c7091e9c86196148bd41177b4590dccb1510bfe6cea5bf7407ff194482eb049"], CraftHash.HashAlgorithm.SHA256)
         self.description = "Library to talk to FTDI chips"
-        self.defaultTarget = "1.4"
+        self.defaultTarget = "1.5"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/pkg-config"] = None
@@ -26,12 +24,6 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self):
         super().__init__()
-        self.subinfo.options.configure.args += ["-DPYTHON_BINDINGS=OFF", "-DEXAMPLES=OFF", "-DBUILD_TESTS=OFF"]
-
-    def postQmerge(self):
-        packageName = "libftdi1"
-        root = str(CraftCore.standardDirs.craftRoot())
-        craftLibDir = os.path.join(root, "lib")
-        utils.system("install_name_tool -add_rpath " + craftLibDir + " " + craftLibDir + "/" + packageName + ".dylib")
-        utils.system("install_name_tool -id @rpath/" + packageName + ".dylib " + craftLibDir + "/" + packageName + ".dylib")
-        return True
+        self.subinfo.options.configure.args += ["-DPYTHON_BINDINGS=OFF", "-DEXAMPLES=OFF"]
+        if not self.subinfo.options.dynamic.buildTests:
+            self.subinfo.options.configure.args += ["-DBUILD_TESTS=OFF"]
