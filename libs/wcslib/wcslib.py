f404c78be7a74d59f077836b06974afdbfe20278
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-03-08 13:04:08 +0000

    [wcslib] Enable PIC to fix stellarsolver build on Linux
    (and some cleanup)

--- libs/wcslib/wcslib.py
+++ libs/wcslib/wcslib.py
@@ -1,24 +1,28 @@
 import info
+from CraftCore import CraftCore
+from Package.CMakePackageBase import CMakePackageBase
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "World Coordinate System Library"
+
         for ver in ["7.7"]:
-            self.targets[ver] = "http://indilib.org/jdownloads/wcslib/wcslib-%s.tar.gz" % ver
-            self.targetInstSrc[ver] = "wcslib-%s" % ver
-        self.defaultTarget = "7.7"
+            self.targets[ver] = f"https://indilib.org/jdownloads/wcslib/wcslib-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"wcslib-{ver}"
+
         if not CraftCore.compiler.isWindows:
             self.patchToApply["7.7"] = [("int64.diff", 1)]
-            self.patchLevel["7.7"] = 1
+
+        self.patchLevel["7.7"] = 2
+
+        self.defaultTarget = "7.7"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/cfitsio"] = None
 
 
-from Package.CMakePackageBase import *
-
-
 class Package(CMakePackageBase):
     def __init__(self):
         super().__init__()
+        self.subinfo.options.configure.args += ["-DCMAKE_POSITION_INDEPENDENT_CODE=ON"]
