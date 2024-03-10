66ec4dbfdc31006c3cb22a75b7d6de78627e9945
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-03-10 19:50:03 +0100

    [dolphin] Cleanup

--- kde/applications/dolphin/dolphin.py
+++ kde/applications/dolphin/dolphin.py
@@ -1,4 +1,6 @@
 import info
+from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
 
 
 class subinfo(info.infoclass):
@@ -8,9 +10,6 @@ class subinfo(info.infoclass):
         self.description = "Dolphin is a lightweight file manager. It has been designed with ease of use and simplicity in mind, while still allowing flexibility and customisation. This means that you can do your file management exactly the way you want to do it."
         self.webpage = "https://www.kde.org/applications/system/dolphin/"
         self.displayName = "Dolphin"
-
-        self.patchToApply["21.12.3"] = [("0001-Port-to-target-based-ecm_add_app_icon.patch", 1)]
-        self.patchLevel["21.12.3"] = 1
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt/qtbase"] = None
@@ -37,17 +36,14 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/unreleased/kuserfeedback"] = None
 
 
-from Package.CMakePackageBase import *
-
-
-class Package(CMakePackageBase):
+class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self):
         super().__init__()
 
     def createPackage(self):
         self.blacklist_file.append(self.blueprintDir() / "blacklist.txt")
         if CraftCore.compiler.isMacOS:
-            self.blacklist_file.append(os.path.join(self.blueprintDir(), "blacklist_mac.txt"))
+            self.blacklist_file.append(self.blueprintDir() / "blacklist_mac.txt")
         self.defines["shortcuts"] = [{"name": "Dolphin", "target": "bin/dolphin.exe", "description": self.subinfo.description, "icon": "$INSTDIR\\dolphin.ico"}]
         self.defines["icon"] = self.buildDir() / "src/dolphin.ico"
 
