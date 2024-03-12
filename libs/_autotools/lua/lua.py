f1acb30d7fea6e33e3c7d5ec4950223ea2be3bd8
Hannah von Reth <vonreth@kde.org>
2024-03-12 15:41:50 +0100

    Fix lua install

--- libs/_autotools/lua/lua.py
+++ libs/_autotools/lua/lua.py
@@ -1,5 +1,6 @@
 import os
 
+import CraftCore
 import info
 from CraftConfig import *
 from CraftOS.osutils import OsUtils
@@ -17,9 +18,10 @@ class subinfo(info.infoclass):
             self.archiveNames[ver] = f"lua-{ver}.tar.gz"
 
         self.targetDigests["5.2.4"] = (["b9e2e4aad6789b3b63a056d442f7b39f0ecfca3ae0f1fc0ae4e9614401b69f4b"], CraftHash.HashAlgorithm.SHA256)
+
+        self.patchToApply["5.2.4"] = [("0002-generate-pc-file.patch", 1)]
         if OsUtils.isUnix():
-            self.patchToApply["5.2.4"] = [("0001-build-shared-library.patch", 1)]
-            self.patchToApply["5.2.4"] += [("0002-generate-pc-file.patch", 1)]
+            self.patchToApply["5.2.4"] += [("0001-build-shared-library.patch", 1)]
         self.patchLevel["5.2.4"] = 1
 
     def setDependencies(self):
@@ -33,56 +35,54 @@ class Package(MakeFilePackageBase):
     def __init__(self):
         super().__init__()
         self.subinfo.options.useShadowBuild = False
+        self.subinfo.options.make.supportsMultijob = not CraftCore.compiler.isWindows
         if OsUtils.isUnix():
             self.subinfo.options.make.args += ["generic"]
         if OsUtils.isWin():
             self.subinfo.options.make.args += ["mingw"]
+        self.subinfo.options.make.args += ["lua5.2.pc"]
 
     def install(self):
         self.enterSourceDir()
-        if OsUtils.isUnix():
-            # generate  lua5.2.pc file
-            if not utils.system(["make", "lua5.2.pc"]):
-                return False
 
-        includeDir = os.path.join(self.installDir(), "include/lua/lua5.2")
+        includeDir = self.installDir() / "include/lua/lua5.2"
         if OsUtils.isWin():
-            includeDir = os.path.join(self.installDir(), "include")
+            includeDir = self.installDir() / "include"
 
-        files = {
-            "lua5.2.pc": os.path.join(self.installDir(), "lib/pkgconfig/lua5.2.pc"),
-            "lua.pc": os.path.join(self.installDir(), "lib/pkgconfig/lua.pc"),
-            "src/liblua.so.5.2.4": os.path.join(self.installDir(), "lib/liblua.so.5.2.4"),
-            "src/lua": os.path.join(self.installDir(), "bin/lua"),
-            "src/luac": os.path.join(self.installDir(), "bin/luac"),
-            "src/liblua.a": os.path.join(self.installDir(), "bin/liblua.a"),
-            "src/lua52.dll": os.path.join(self.installDir(), "bin/lua52.dll"),
-            "src/lua.exe": os.path.join(self.installDir(), "bin/lua.exe"),
-            "src/luac.exe": os.path.join(self.installDir(), "bin/luac.exe"),
-            "src/lua.h": os.path.join(includeDir, "lua.h"),
-            "src/luaconf.h": os.path.join(includeDir, "luaconf.h"),
-            "src/lualib.h": os.path.join(includeDir, "lualib.h"),
-            "src/lauxlib.h": os.path.join(includeDir, "lauxlib.h"),
-            "src/lua.hpp": os.path.join(includeDir, "lua.hpp"),
-            "doc/lua.1": os.path.join(self.installDir(), "man/man1/lua.1"),
-            "doc/luac.1": os.path.join(self.installDir(), "man/man1/luac.1"),
-        }
-
+        files = [
+            ("lua5.2.pc", self.installDir() / "lib/pkgconfig/lua5.2.pc"),
+            ("lua5.2.pc", self.installDir() / "lib/pkgconfig/lua.pc"),
+            ("src/liblua.so.5.2.4", self.installDir() / "lib/liblua.so.5.2.4"),
+            ("src/lua", self.installDir() / "bin/lua"),
+            ("src/luac", self.installDir() / "bin/luac"),
+            ("src/liblua.a", self.installDir() / "bin/liblua.a"),
+            ("src/lua52.dll", self.installDir() / "bin/lua52.dll"),
+            ("src/lua.exe", self.installDir() / "bin/lua.exe"),
+            ("src/luac.exe", self.installDir() / "bin/luac.exe"),
+            ("src/lua.h", includeDir / "lua.h"),
+            ("src/luaconf.h", includeDir / "luaconf.h"),
+            ("src/lualib.h", includeDir / "lualib.h"),
+            ("src/lauxlib.h", includeDir / "lauxlib.h"),
+            ("src/lua.hpp", includeDir / "lua.hpp"),
+            ("doc/lua.1", self.installDir() / "man/man1/lua.1"),
+            ("doc/luac.1", self.installDir() / "man/man1/luac.1"),
+        ]
         # install files
-        for src, dest in files.items():
-            source = os.path.join(self.workDir(), "lua-5.2.4", src)
+        for src, dest in files:
+            source = self.workDir() / "lua-5.2.4" / src
             if os.path.exists(source):
+                CraftCore.log.info(f"Installing {source} -> {dest}")
                 if not utils.copyFile(source, dest):
                     return False
 
         if OsUtils.isUnix():
-            liblua = os.path.join(self.installDir(), "lib/liblua.so.5.2.4")
+            liblua = self.installDir() / "lib/liblua.so.5.2.4"
             if os.path.exists(liblua):
                 if (
-                    not utils.createSymlink(liblua, os.path.join(self.installDir(), "lib/liblua.so.5.2"))
-                    or not utils.createSymlink(liblua, os.path.join(self.installDir(), "lib/liblua.so.5"))
-                    or not utils.createSymlink(liblua, os.path.join(self.installDir(), "lib/liblua.so.0"))
-                    or not utils.createSymlink(liblua, os.path.join(self.installDir(), "lib/liblua.so"))
+                    not utils.createSymlink(liblua, self.installDir() / "lib/liblua.so.5.2")
+                    or not utils.createSymlink(liblua, self.installDir() / "lib/liblua.so.5")
+                    or not utils.createSymlink(liblua, self.installDir() / "lib/liblua.so.0")
+                    or not utils.createSymlink(liblua, self.installDir() / "lib/liblua.so")
                 ):
                     return False
 
