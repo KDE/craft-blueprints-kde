24ae35770c5898333bb31759ace6d75a17dfe0b1
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-03-23 01:29:35 +0100

    [breeze] Workaround for Windows ECM bug with Qt6

--- kde/plasma/breeze/breeze.py
+++ kde/plasma/breeze/breeze.py
@@ -1,7 +1,8 @@
 import info
+import utils
 from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
 from CraftOS.osutils import OsUtils
-
 
 class subinfo(info.infoclass):
     def setTargets(self):
@@ -9,6 +10,8 @@ class subinfo(info.infoclass):
 
         for ver in self.versionInfo.tarballs():
             self.patchToApply[ver] = ("breeze-noWinDrag.diff", 0)
+
+        self.patchLevel["6.0.2"] = 1
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt/qtbase"] = None
@@ -39,3 +42,27 @@ class Package(CraftPackageObject.get("kde/plasma").pattern):
 
         qtMajor = CraftPackageObject.get("libs/qt").instance.subinfo.options.dynamic.qtMajorVersion
         self.subinfo.options.configure.args += [f"-DBUILD_QT5={'ON' if qtMajor == '5' else 'OFF'}", f"-DBUILD_QT6={'ON' if qtMajor == '6' else 'OFF'}"]
+
+
+    def install(self):
+        status = super().install()
+        qtMajor = CraftPackageObject.get("libs/qt").instance.subinfo.options.dynamic.qtMajorVersion
+        if CraftCore.compiler.isWindows and qtMajor == '6':
+            # On Windows files are wrongly installed to "share" while they should go to "bin/data"
+            # This fix is to workaround that bug. It should be removed as soon as the bug is fixed
+            # See https://invent.kde.org/frameworks/extra-cmake-modules/-/merge_requests/428
+            utils.moveFile(self.imageDir() / "share", self.imageDir() / "bin/data")
+
+            # See https://www.qt.io/blog/building-qt-webengine-against-other-qt-versions
+            versionold = self.subinfo.buildTarget
+            versionnew = "5.15"
+            for module in ["Breeze"]:
+                filename = self.imageDir() / "lib" / "cmake" / module / f"{module}Config.cmake"
+                with open(filename, "r") as f:
+                    contents = f.read()
+                with open(filename, "w") as f:
+                    f.write(contents.replace(f"share/color-schemes", f"bin/data/color-schemes"))
+
+        return status
+
+
