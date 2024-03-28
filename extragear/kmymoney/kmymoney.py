6edcad1d52573d6c311eb105434f1e63fb4a8836
Ralf Habacker <ralf.habacker@freenet.de>
2024-03-28 08:53:08 +0100

    [alkimia] Fix bug that CI templates cannot find alkimia
    The CI templates determine the blueprint name from the project name,
    which did not match.
    
    BUG:484633

--- extragear/kmymoney/kmymoney.py
+++ extragear/kmymoney/kmymoney.py
@@ -68,7 +68,7 @@ class subinfo(info.infoclass):
         if not CraftCore.compiler.isMSVC():
             self.runtimeDependencies["libs/aqbanking"] = None
         self.runtimeDependencies["libs/gettext"] = None
-        self.runtimeDependencies["extragear/libalkimia"] = None
+        self.runtimeDependencies["extragear/alkimia"] = None
         self.runtimeDependencies["extragear/kdiagram"] = None
         self.buildDependencies["libs/gettext"] = None
         if self.buildTarget != "master":
