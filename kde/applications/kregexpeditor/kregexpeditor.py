c470aef2cd30d3831a06d80f7361932a996de60e
Laurent Montel <montel@kde.org>
2024-03-30 09:28:22 +0100

    Add kregexpeditor support

---
+++ kde/applications/kregexpeditor/kregexpeditor.py
@@ -0,0 +1,29 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+        self.description = "KRegexpeditor"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kcrash"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/ktextwidgets"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        super().__init__()
