60e09ee85f06a19cc760222842ec18bf84d485f5
Hannah von Reth <vonreth@kde.org>
2024-04-03 11:37:00 +0200

    qttools depend on llvm

--- libs/qt6/qttools/qttools.py
+++ libs/qt6/qttools/qttools.py
@@ -9,6 +9,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/qt6/qtbase"] = None
         self.runtimeDependencies["libs/qt6/qtdeclarative"] = None
+        self.runtimeDependencies["libs/llvm"] = None
         self.patchToApply["6.4.0"] = [("fix-build-clang15.diff", 1)]
         self.patchToApply["6.4.0"] += [("fix-build-clang16.diff", 1)]
         self.patchLevel["6.4.0"] = 1
