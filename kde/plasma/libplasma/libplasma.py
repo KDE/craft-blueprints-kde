77abc90c5c0e3fbdd0fe123c5be23f35e267e10a
Julius Künzel <julius.kuenzel@kde.org>
2024-04-04 17:13:46 +0200

    [libplasma] Depends on QtSvg

--- kde/plasma/libplasma/libplasma.py
+++ kde/plasma/libplasma/libplasma.py
@@ -15,6 +15,8 @@ class subinfo(info.infoclass):
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["libs/qt/qtsvg"] = None
         self.runtimeDependencies["kde/plasma/plasma-activities"] = None
         self.runtimeDependencies["kde/frameworks/tier1/karchive"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
