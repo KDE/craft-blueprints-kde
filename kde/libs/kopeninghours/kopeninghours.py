a8dfb4ab575fcd52171c70ce9ad86becd88c9b3a
Hannah von Reth <vonreth@kde.org>
2024-04-09 20:47:08 +0200

    Fix imports

--- kde/libs/kopeninghours/kopeninghours.py
+++ kde/libs/kopeninghours/kopeninghours.py
@@ -2,6 +2,7 @@
 # SPDX-FileCopyrightText: 2021 Volker Krause <vkrause@kde.org>
 
 import info
+from Blueprints.CraftPackageObject import CraftPackageObject
 
 
 class subinfo(info.infoclass):
@@ -15,9 +16,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt/qtbase"] = None
         self.runtimeDependencies["libs/qt/qtdeclarative"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kholidays"] = None
-
-
-from Package.CMakePackageBase import *
 
 
 class Package(CraftPackageObject.get("kde").pattern):
