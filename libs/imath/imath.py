37f9cf191bab95ccb53b51339426909cf2e9487e
Hannah von Reth <vonreth@kde.org>
2024-04-10 13:28:52 +0200

    Add imath a dependency of openexr which was unasked installed by openexr

---
+++ libs/imath/imath.py
@@ -0,0 +1,21 @@
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ["3.1.11"]:
+            self.targets[ver] = f"https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"imath-{ver}"
+        self.targetDigests["3.1.11"] = (["9057849585e49b8b85abe7cc1e76e22963b01bfdc3b6d83eac90c499cd760063"], CraftHash.HashAlgorithm.SHA256)
+
+        self.description = "Library of 2D and 3D vector, matrix, and math operations"
+        self.defaultTarget = "3.1.11"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
