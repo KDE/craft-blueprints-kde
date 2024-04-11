64ff1cdb28ba1b61bfe4f936cac89e5692f336a1
Hannah von Reth <vonreth@kde.org>
2024-04-11 14:24:13 +0200

    Add abseil

---
+++ libs/abseil-cpp/abseil-cpp.py
@@ -0,0 +1,22 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ["20240116.2"]:
+            self.targets[ver] = f"https://github.com/abseil/abseil-cpp/releases/download/{ver}/abseil-cpp-{ver}.tar.gz"
+            self.archiveNames[ver] = f"abseil-cpp-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"abseil-cpp-{ver}"
+        self.targetDigests["20240116.2"] = (["733726b8c3a6d39a4120d7e45ea8b41a434cdacde401cba500f14236c49b39dc"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "20240116.2"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
