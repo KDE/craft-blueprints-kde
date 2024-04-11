952d7d1859c16e511dccba844a839a2c2685ce2e
Hannah von Reth <vonreth@kde.org>
2024-04-11 22:26:29 +0200

    more settings

--- libs/abseil-cpp/abseil-cpp.py
+++ libs/abseil-cpp/abseil-cpp.py
@@ -8,7 +8,7 @@ class subinfo(info.infoclass):
             self.archiveNames[ver] = f"abseil-cpp-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"abseil-cpp-{ver}"
         self.targetDigests["20240116.2"] = (["733726b8c3a6d39a4120d7e45ea8b41a434cdacde401cba500f14236c49b39dc"], CraftHash.HashAlgorithm.SHA256)
-        self.patchLevel["20240116.2"] = 1
+        self.patchLevel["20240116.2"] = 2
         self.defaultTarget = "20240116.2"
 
     def setDependencies(self):
@@ -21,4 +21,8 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.subinfo.options.configure.args += ["-DCMAKE_CXX_STANDARD=17"]
+        self.subinfo.options.configure.args += [
+            "-DCMAKE_CXX_STANDARD=17",
+            "-DABSL_PROPAGATE_CXX_STD=ON",
+            "-DABSL_ENABLE_INSTALL=ON",
+        ]
