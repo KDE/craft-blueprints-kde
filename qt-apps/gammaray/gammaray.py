254aee67775c2982e407fe279421a32e3bdf4d99
Hannah von Reth <vonreth@kde.org>
2024-04-11 13:25:50 +0200

    Use gammaray master for Qt 6.7

--- qt-apps/gammaray/gammaray.py
+++ qt-apps/gammaray/gammaray.py
@@ -18,7 +18,7 @@ class subinfo(info.infoclass):
         self.description = "GammaRay is a tool to poke around in a Qt-application and also to manipulate the application to some extent"
         self.webpage = "https://www.kdab.com/gammaray"
         self.displayName = "GammaRay"
-        self.defaultTarget = "3.0.0"
+        self.defaultTarget = "master"
 
     def registerOptions(self):
         self.options.dynamic.registerOption("gammarayProbeOnly", False)
