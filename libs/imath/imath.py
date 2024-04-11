44d2cad556eac24954b77890ece1aa9fc40e9fa0
Hannah von Reth <vonreth@kde.org>
2024-04-11 09:08:16 +0200

    Fix name on case sensitive file systems

--- libs/imath/imath.py
+++ libs/imath/imath.py
@@ -6,7 +6,7 @@ class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ["3.1.11"]:
             self.targets[ver] = f"https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v{ver}.tar.gz"
-            self.targetInstSrc[ver] = f"imath-{ver}"
+            self.targetInstSrc[ver] = f"Imath-{ver}"
         self.targetDigests["3.1.11"] = (["9057849585e49b8b85abe7cc1e76e22963b01bfdc3b6d83eac90c499cd760063"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "Library of 2D and 3D vector, matrix, and math operations"
