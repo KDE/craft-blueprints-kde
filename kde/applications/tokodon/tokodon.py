43c24c83d5611a8745f3fb945aa0defc40a0b205
Joshua Goins <josh@redstrate.com>
2024-04-13 18:56:04 +0000

    kde/applications/tokodon: Add KDeclarative dependency
    It's been added as a dependency to master, needs it defined in Craft or else the APK will not launch.

--- kde/applications/tokodon/tokodon.py
+++ kde/applications/tokodon/tokodon.py
@@ -19,6 +19,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kdeclarative"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kitemmodels"] = None
