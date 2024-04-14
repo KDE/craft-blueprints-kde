81c02770fecc4a18f11c0b5187b572d6fe3aad29
Jean-Baptiste Mardelle <jb@kdenlive.org>
2024-04-14 14:37:49 +0200

    Fix frei0r-bigsh0t on MinGW

--- libs/frei0r-bigsh0t/frei0r-bigsh0t.py
+++ libs/frei0r-bigsh0t/frei0r-bigsh0t.py
@@ -14,6 +14,7 @@ class subinfo(info.infoclass):
         self.targetDigests["2.6"] = (["001ed35ef06013a5a83b80c808194f3c5c2339c1759b52dda5bee523ce1f5517"], CraftHash.HashAlgorithm.SHA256)
         self.targetInstSrc["2.6"] = "leo_sutic-bigsh0t-d973312f2c1f"
         self.patchToApply["2.6"] = [("cmake-install-plugins-2.6.diff", 1)]
+        self.patchToApply["2.6"] += [("fix-mingw-include.diff", 1)]
         self.patchLevel["2.6"] = 1
 
         self.defaultTarget = "2.6"
