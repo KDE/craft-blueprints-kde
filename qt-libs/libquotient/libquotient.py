7d737f8105d8c535b37f65a6406b990b0f2ce670
Tobias Fella <fella@posteo.de>
2024-04-15 16:53:48 +0200

    Update libQuotient to 0.8.2

--- qt-libs/libquotient/libquotient.py
+++ qt-libs/libquotient/libquotient.py
@@ -6,14 +6,14 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/quotient-im/libQuotient.git||dev"
 
-        for ver in ["0.8.1.1"]:
+        for ver in ["0.8.2"]:
             self.targets[ver] = "https://github.com/quotient-im/libQuotient/archive/%s.tar.gz" % ver
             self.archiveNames[ver] = "libQuotient-%s.tar.gz" % ver
             self.targetInstSrc[ver] = "libQuotient-%s" % ver
 
-        self.targetDigests["0.8.1.1"] = (["d1ab944a4b42f68d2d2ebfb2782a3e92eac2b7e056c7f72af2ba3b3ddf2fd735"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.8.2"] = (["12ff2fa8b80a934b9dd88fa3416a4b88e94bc0e18a8df0dcebfc90614dd2f5c9"], CraftHash.HashAlgorithm.SHA256)
 
-        self.defaultTarget = "0.8.1.1"
+        self.defaultTarget = "0.8.2"
         self.description = "A Qt library to write cross-platform clients for Matrix"
 
     def setDependencies(self):
