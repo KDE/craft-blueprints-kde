4d914cab69e3aab83a03922ca26a2f4444e5e446
Heiko Becker <mail@heiko-becker.de>
2024-04-16 23:42:01 +0200

    kde/applications/konversation: Update dependencies
    Add kstatusnotifieritem and remove Phonon, which isn't used anymore
    since b9f61d0005932b88c115933054c0dcd98f21182f in konversation.git.

--- kde/applications/konversation/konversation.py
+++ kde/applications/konversation/konversation.py
@@ -29,8 +29,8 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/sonnet"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kwallet"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kstatusnotifieritem"] = None
         self.runtimeDependencies["kdesupport/qca"] = None
-        self.runtimeDependencies["qt-libs/phonon"] = None
 
         if not CraftCore.compiler.isWindows:
             self.runtimeDependencies["kde/frameworks/tier3/kglobalaccel"] = None
