2c2f966d3f171d91a6d6e098ed7aae0774c0180a
Ingo Klöcker <dev@ingo-kloecker.de>
2024-04-17 18:22:41 +0200

    ktextaddons: Ignore on Android

--- kde/libs/ktextaddons/ktextaddons.py
+++ kde/libs/ktextaddons/ktextaddons.py
@@ -3,9 +3,13 @@
 
 import info
 from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
 
 
 class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NotAndroid
+
     def setTargets(self):
         self.description = "Text Addons (autocorrection, grammar checking, text to speak, translator support)"
 
