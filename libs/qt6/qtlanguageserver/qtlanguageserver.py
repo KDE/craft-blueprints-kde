e72107aebb1e8f5a0536f72012d9542989fc6ee7
Hannah von Reth <vonreth@kde.org>
2024-04-26 12:44:11 +0200

    Add qtlanguageserver and rebuild qtdeclarative with it

--- libs/qt6/qtlanguageserver/qtlanguageserver.py
+++ libs/qt6/qtlanguageserver/qtlanguageserver.py
@@ -1,18 +1,12 @@
 import info
-import utils
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
-        self.patchLevel["6.6.0"] = 1
-        self.patchLevel["6.7.0"] = 1
 
     def setDependencies(self):
-        self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/qt6/qtbase"] = None
-        self.runtimeDependencies["libs/qt6/qtshadertools"] = None
-        self.runtimeDependencies["libs/qt6/qtlanguageserver"] = None
 
 
 from Package.CMakePackageBase import *
