8a2907891da8fed62a6640c6ac5dd3f0ef5abe48
Carl Schwan <carl@carlschwan.eu>
2024-04-28 09:43:45 +0200

    Add marknote manifest

---
+++ kde/applications/marknote/marknote.py
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: 2024 Carl Schwan <carl@carlschwan.eu>
+// SPDX-License-Identifier: MIT
+
+import info
+from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues(gitUrl="https://invent.kde.org/office/marknote")
+        self.description = "A simple markdown note management app"
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["libs/qt/qtdeclarative"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kxmlgui"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcolorscheme"] = None
+        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
+        if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+            self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+            self.runtimeDependencies["kde/plasma/breeze"] = None
+        else:
+            self.runtimeDependencies["kde/plasma/qqc2-breeze-style"] = None
+
+
+class Package(CraftPackageObject.get("kde").pattern):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.blacklist_file.append(self.blueprintDir() / "blacklist.txt")
+        self.defines["executable"] = r"bin\marknote.exe"
+        self.addExecutableFilter(r"(bin|libexec)/(?!(marknote|update-mime-database)).*")
+        self.ignoredPackages.append("binary/mysql")
+        self.ignoredPackages.append("libs/dbus")
