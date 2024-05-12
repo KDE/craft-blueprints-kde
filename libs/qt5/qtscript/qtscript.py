d4b6ea3cc8444f1963b4aa578c886868eae54220
Hannah von Reth <vonreth@kde.org>
2024-05-12 13:55:27 +0200

    Fix and update qtscript

--- libs/qt5/qtscript/qtscript.py
+++ libs/qt5/qtscript/qtscript.py
@@ -13,7 +13,10 @@ class subinfo(info.infoclass):
         del self.svnTargets["kde/5.15"]
         del self.svnTargets["kde/before-5.15.11-rebase"]
 
-        self.defaultTarget = "5.15.10"
+        # https://github.com/macports/macports-ports/commit/37210501094e88965242db5aea61f7d3bdc1eed6
+        self.patchToApply["5.15.13"] = [("patch-webkit-remove-wtf_ceil-workaround.diff", 0)]
+
+        self.defaultTarget = "5.15.13"
 
 
 from Package.Qt5CorePackageBase import *
