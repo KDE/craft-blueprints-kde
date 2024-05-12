c5ad0289ef90bd1dc53db8d05686987934e18665
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2024-05-12 08:29:44 +0200

    readstat 1.1.9

--- libs/readstat/readstat.py
+++ libs/readstat/readstat.py
@@ -7,17 +7,13 @@ from Utils import CraftHash
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "[git]https://github.com/WizardMac/ReadStat.git"
-        for ver in ["1.1.6", "1.1.8"]:
+        for ver in ["1.1.6", "1.1.8", "1.1.9"]:
             self.targets[ver] = f"https://github.com/WizardMac/ReadStat/releases/download/v{ver}/readstat-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"readstat-{ver}"
         self.targetDigests["1.1.6"] = (["4b80558ab966ec0e0841b7359ee0b91a647a74f02b1b8816313c6f17ce663511"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.1.8"] = (["b18a0d68059d9665491e53838b3ca89b06c3bdaa3b0a11d4ba87d830b743e178"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.1.9"] = (["89af7c785a7ae3d9a7af6e64a69028829a429f66a14b1b913be22816dc07ddca"], CraftHash.HashAlgorithm.SHA256)
         self.description = "A command-line tool and MIT-licensed C library for reading files from popular stats packages"
-
-        self.patchToApply["1.1.8"] = [
-            ("211c342a1cfe46fb7fb984730dd7a29ff4752f35.patch", 1),  # Add missing `void` to make Clang happy (*.c files)
-            ("5ad80040b94b667e2fef74e9b8d865f142fec820.patch", 1),  # Add missing `void` to make Clang happy (*.h files)
-        ]
 
         self.patchToApply["1.1.8"] = [
             ("211c342a1cfe46fb7fb984730dd7a29ff4752f35.patch", 1),  # Add missing `void` to make Clang happy (*.c files)
@@ -28,9 +24,9 @@ class subinfo(info.infoclass):
             self.patchToApply["1.1.6"] = [("readstat-1.1.6-compiler-flags.diff", 1)]
             self.patchToApply["1.1.8"] += [("readstat-1.1.6-compiler-flags.diff", 1)]
 
-        self.patchLevel["1.1.8"] = 1
+        self.patchLevel["1.1.9"] = 1
 
-        self.defaultTarget = "1.1.8"
+        self.defaultTarget = "1.1.9"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
