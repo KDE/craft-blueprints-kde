41d937bb74139740a5057feafe87ebc76ec53819
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2024-05-14 08:19:35 +0200

    hdf5: add 1.14.3

--- libs/hdf5/hdf5.py
+++ libs/hdf5/hdf5.py
@@ -12,11 +12,15 @@ class subinfo(info.infoclass):
         for ver in ["1.12.1"]:
             self.targets[ver] = f"https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-{ver}/src/hdf5-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"hdf5-{ver}"
+        for ver in ["1.14.3"]:
+            self.targets[ver] = f"https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.14/hdf5-{ver}/src/hdf5-{ver}.tar.bz2"
+            self.targetInstSrc[ver] = f"hdf5-{ver}"
         self.targetDigests["1.10.5"] = (["6d4ce8bf902a97b050f6f491f4268634e252a63dadd6656a1a9be5b7b7726fa8"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.10.6"] = (["5f9a3ee85db4ea1d3b1fa9159352aebc2af72732fc2f58c96a3f0768dba0e9aa"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.10.7"] = (["7a1a0a54371275ce2dfc5cd093775bb025c365846512961e7e5ceaecb437ef15"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.10.8"] = (["d341b80d380dd763753a0ebe22915e11e87aac4e44a084a850646ff934d19c80"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.12.1"] = (["aaf9f532b3eda83d3d3adc9f8b40a9b763152218fa45349c3bc77502ca1f8f1c"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.14.3"] = (["9425f224ed75d1280bb46d6f26923dd938f9040e7eaebf57e66ec7357c08f917"], CraftHash.HashAlgorithm.SHA256)
         self.description = "A data model, library, and file format for storing and managing data"
 
         self.patchToApply["1.10.7"] = [("hdf5-1.10.7-file-locking.diff", 1)]
@@ -34,5 +38,5 @@ class Package(CMakePackageBase):
         self.subinfo.options.dynamic.buildStatic = False
         self.subinfo.options.configure.args += ["-DHDF5_ENABLE_Z_LIB_SUPPORT=ON"]
         # build with the 1.10 APIs
-        if self.buildTarget == "1.12.1":
+        if self.buildTarget == "1.12.1" or self.buildTarget == "1.14.3":
             self.subinfo.options.configure.args += ["-DDEFAULT_API_VERSION:STRING=v110"]
