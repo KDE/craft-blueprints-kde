a496770f07578252ebb254a033e0db15e11aa639
Volker Krause <vkrause@kde.org>
2024-05-14 18:07:29 +0200

    Add Kirigami Addons dependency to Kongress
    Needed with https://invent.kde.org/utilities/kongress/-/merge_requests/44.

--- kde/applications/kongress/kongress.py
+++ kde/applications/kongress/kongress.py
@@ -18,6 +18,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcalendarcore"] = None
+        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
 
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/plasma/breeze"] = None
