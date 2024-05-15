eb70472f60e26a7af3baba88ee5c30533285f17c
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2024-05-15 10:16:37 +0200

    liblz4 1.9.4

--- libs/liblz4/liblz4.py
+++ libs/liblz4/liblz4.py
@@ -6,13 +6,14 @@ from Package.MakeFilePackageBase import *
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "[git]https://github.com/lz4/lz4.git"
-        for ver in ["1.9.2"]:
+        for ver in ["1.9.2", "1.9.4"]:
             self.targets[ver] = f"https://github.com/lz4/lz4/archive/v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"lz4-{ver}"
             self.archiveNames[ver] = f"lz4-{ver}.tar.gz"
             if CraftCore.compiler.isMSVC():
                 self.targetConfigurePath[ver] = "contrib/cmake_unofficial"
         self.targetDigests["1.9.2"] = (["658ba6191fa44c92280d4aa2c271b0f4fbc0e34d249578dd05e50e76d0e5efcc"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.9.4"] = (["0b0e3aa07c8c063ddf40b082bdf7e37a1562bda40a0ff5272957f3e987e0e54b"], CraftHash.HashAlgorithm.SHA256)
         self.description = "Extremely Fast Compression algorithm"
         self.defaultTarget = "1.9.2"
 
