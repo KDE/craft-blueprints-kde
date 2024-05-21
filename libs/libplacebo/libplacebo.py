KDE/craft-blueprints-kde

commit c8973a1fbde3f7f8536fb75684cf02a925304443

libplacebo: update to v6.338.2

libs/libplacebo/libplacebo.py
@@ -13,9 +13,9 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.displayName = "libplacebo"
         self.description = "Reusable library for GPU-accelerated image/video processing primitives and shaders, as well a batteries-included, extensible, high-quality rendering pipeline (similar to mpv's vo_gpu). Supports Vulkan, OpenGL, Metal (via MoltenVK) and Direct3D 11."
-        self.svnTargets["2805a0d0"] = "https://code.videolan.org/videolan/libplacebo||2805a0d01c029084ab36bf5d0e3c8742012a0b27"
+        self.svnTargets["64c19545"] = "https://code.videolan.org/videolan/libplacebo||64c1954570f1cd57f8570a57e51fb0249b57bb90"
         self.svnTargets["master"] = "https://code.videolan.org/videolan/libplacebo"
-        self.defaultTarget = "2805a0d0"
+        self.defaultTarget = "64c19545"
 
         if CraftCore.compiler.isAndroid:
             for ver in ["2805a0d0", "master"]:

commit 7567be979ab8d531a9f00ca5fd37266a9cc870f4

Apply libplacebo Android deployment patch again
Regression from c8973a1fbde.

libs/libplacebo/disable-soversion.patch
@@ -1,12 +1,12 @@
 diff --git a/src/meson.build b/src/meson.build
-index 1260d1b6..e0ab4fa9 100644
+index 63f9d53c..16f14e8e 100644
 --- a/src/meson.build
 +++ b/src/meson.build
-@@ -242,7 +242,6 @@ lib = library('placebo', sources,
+@@ -243,7 +243,6 @@ lib = library('placebo', sources,
    c_args: ['-DPL_EXPORT'],
    install: true,
    dependencies: build_deps + glad_dep,
 -  soversion: apiver,
-   include_directories: [ inc, vulkan_headers_inc, fast_float_inc ],
+   include_directories: [ inc, vulkan_headers_inc, fast_float_inc, xxhash_inc ],
    link_args: link_args,
    link_depends: link_depends,

libs/libplacebo/libplacebo.py
@@ -18,7 +18,7 @@ class subinfo(info.infoclass):
         self.defaultTarget = "64c19545"
 
         if CraftCore.compiler.isAndroid:
-            for ver in ["2805a0d0", "master"]:
+            for ver in ["64c19545", "master"]:
                 self.patchToApply[ver] = [
                     ("disable-soversion.patch", 1)
                 ]