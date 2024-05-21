cbd28d4375b71cd5adcfbab893b5cc1c77c8aeca
Melvin Keskin <melvo@olomono.de>
2024-05-21 20:23:02 +0200

    qxmpp: Update to version 1.7.0

--- qt-libs/qxmpp/qxmpp.py
+++ qt-libs/qxmpp/qxmpp.py
@@ -16,12 +16,12 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/qxmpp-project/qxmpp.git"
-        for ver in ["1.6.0"]:
+        for ver in ["1.7.0"]:
             self.targets[ver] = f"https://github.com/qxmpp-project/qxmpp/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"qxmpp-v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"qxmpp-{ver}"
-        self.targetDigests["1.6.0"] = (["af19b8644ff92f3b38d3e75b89ce4632501c102f17f32b09d7dcde0b27e1c16e"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.6.0"
+        self.targetDigests["1.7.0"] = (["92f23238bc68d0f135a810454729c5da34312ebe2ae8f4bf9d303d2c3cde5e7d"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.7.0"
 
 
 class Package(CMakePackageBase):
