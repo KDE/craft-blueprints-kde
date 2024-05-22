10a3c1cb43e40f06076038941afbbe028d4343a9
Volker Krause <vkrause@kde.org>
2024-05-22 17:02:32 +0200

    Roll Android back to Qt 6.7.0
    Updating Qt for Android needs matching CI image changes to work. Stick
    with 6.7.0 until we have those.

--- libs/qt6/qt6.py
+++ libs/qt6/qt6.py
@@ -6,3 +6,5 @@ from Package.CMakePackageBase import CMakePackageBase
 class Pattern(CMakePackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        if CraftCore.compiler.isAndroid:
+            self.subinfo.defaultTarget = "6.7.0"
