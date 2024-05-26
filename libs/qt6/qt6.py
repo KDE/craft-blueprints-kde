2fb68bee04a56fd174d1fca0c6acd8ad54ed4401
Volker Krause <vkrause@kde.org>
2024-05-26 08:43:38 +0200

    Update Qt on Android to 6.7.1

--- libs/qt6/qt6.py
+++ libs/qt6/qt6.py
@@ -6,5 +6,3 @@ from Package.CMakePackageBase import CMakePackageBase
 class Pattern(CMakePackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        if CraftCore.compiler.isAndroid:
-            self.subinfo.defaultTarget = "6.7.0"
