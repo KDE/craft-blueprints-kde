c6dc57a723c60af329b6b044907a75d9e7b6afba
Hennadii Chernyshchyk <genaloner@gmail.com>
2024-06-17 11:36:29 +0000

    [tesseract] Add blueprint for tesseract
    https://invent.kde.org/packaging/craft-blueprints-kde/-/merge_requests/902, but for the master.

---
+++ libs/leptonica/leptonica.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2024 Hennadii Chernyshchyk <genaloner@gmail.com>
+
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ["1.84.1"]:
+            self.targets[ver] = f"https://github.com/DanBloomberg/leptonica/archive/{ver}/leptonica-{ver}.tar.gz"
+            self.targetInstSrc[ver] = "leptonica-" + ver
+        self.targetDigests["1.84.1"] = "8e37f3f4486df65251d75e1f1c42e34b6b472694"
+
+        self.description = "Software that is broadly useful for image processing and image analysis applications"
+        self.webpage = "http://www.leptonica.com"
+        self.defaultTarget = "1.84.1"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/giflib"] = None
+        self.runtimeDependencies["libs/libjpeg-turbo"] = None
+        self.runtimeDependencies["libs/libpng"] = None
+        self.runtimeDependencies["libs/tiff"] = None
+        self.runtimeDependencies["libs/webp"] = None
+        self.runtimeDependencies["libs/openjpeg"] = None
+        self.runtimeDependencies["libs/zlib"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.subinfo.options.configure.args += ["-DSW_BUILD=OFF"]
---
+++ libs/tesseract/tesseract.py
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2024 Hennadii Chernyshchyk <genaloner@gmail.com>
+
+import info
+from Package.CMakePackageBase import *
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        for ver in ["5.4.1"]:
+            self.targets[ver] = f"https://github.com/tesseract-ocr/tesseract/archive/{ver}/tesseract-{ver}.tar.gz"
+            self.targetInstSrc[ver] = "tesseract-" + ver
+        self.targetDigests["5.4.1"] = "e22bb217ab787fd89a26028391d8572028c8e7dd"
+
+        self.description = "An OCR program"
+        self.webpage = "https://github.com/tesseract-ocr/tesseract"
+        self.defaultTarget = "5.4.1"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/leptonica"] = None
+        self.runtimeDependencies["libs/libarchive"] = None
+        self.runtimeDependencies["libs/libcurl"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.subinfo.options.configure.args += ["-DSW_BUILD=OFF", "-DBUILD_TRAINING_TOOLS=OFF"]
