97ae9c41a811d8793431b2b91152b2b75bf7ce25
Hannah von Reth <vonreth@kde.org>
2024-07-05 12:12:32 +0200

    libpng 1.6.43

--- libs/libpng/libpng.py
+++ libs/libpng/libpng.py
@@ -4,19 +4,21 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.6.39", "1.6.40"]:
+        for ver in ["1.6.39", "1.6.40", "1.6.43"]:
             self.targets[ver] = "https://downloads.sourceforge.net/libpng/libpng-" + ver + ".tar.gz"
             self.targetInstSrc[ver] = "libpng-" + ver
 
         self.patchToApply["1.6.39"] = [("libpng-1.6.39-install.pc-on-windows.diff", 1), ("libpng-fix-android-shared-build.diff", 1)]
         self.patchToApply["1.6.40"] = [("libpng-1.6.40-install.pc-on-windows.diff", 1), ("libpng-android-remove-zlib-dependency.diff", 1)]
+        self.patchToApply["1.6.43"] = [("libpng-1.6.40-install.pc-on-windows.diff", 1), ("libpng-android-remove-zlib-dependency.diff", 1)]
         self.targetDigests["1.6.39"] = (["af4fb7f260f839919e5958e5ab01a275d4fe436d45442a36ee62f73e5beb75ba"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.6.40"] = (["8f720b363aa08683c9bf2a563236f45313af2c55d542b5481ae17dd8d183bb42"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.6.43"] = (["e804e465d4b109b5ad285a8fb71f0dd3f74f0068f91ce3cdfde618180c174925"], CraftHash.HashAlgorithm.SHA256)
 
         self.patchLevel["1.6.39"] = 1
         self.patchLevel["1.6.40"] = 2
         self.description = "A library to display png images"
-        self.defaultTarget = "1.6.40"
+        self.defaultTarget = "1.6.43"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/zlib"] = None
