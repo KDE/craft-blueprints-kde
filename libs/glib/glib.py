e2e702f4686af67d143f5ec13a43182032a500cd
Hannah von Reth <vonreth@kde.org>
2024-07-11 13:11:58 +0300

    glib 2.81.0

--- libs/glib/glib.py
+++ libs/glib/glib.py
@@ -9,12 +9,13 @@ class subinfo(info.infoclass):
         )
 
     def setTargets(self):
-        for ver in ["2.79.0"]:
+        for ver in ["2.79.0", "2.81.0"]:
             majorMinorStr = ".".join(ver.split(".")[0:2])
             self.targets[ver] = f"https://download.gnome.org/sources/glib/{majorMinorStr}/glib-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"glib-{ver}"
         self.targetDigests["2.79.0"] = (["d7ebde5505f5c4741a04ffe32f6927bd165b13caaabe18e962ddc58c811f84c9"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "2.79.0"
+        self.targetDigests["2.79.0"] = (["1665188ed9cc941c0a189dc6295e6859872523d1bfc84a5a84732a7ae87b02e4"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "2.81.0"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
