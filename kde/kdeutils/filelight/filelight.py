940bc5a48a7e4620ade23e857a02a19753aa8164
Harald Sitter <sitter@kde.org>
2024-07-12 09:37:58 +0200

    filelight: drop unused dependencies
    - kquicharts was never used (I think? -- certainly isn't now)
    - kdeclarative is not used any longer (since qt6 port)
    - kdewin is not use any longer (since native windowswalker support)
    - kirigami-addons is not used any longer (relevant use was reverted)

--- kde/kdeutils/filelight/filelight.py
+++ kde/kdeutils/filelight/filelight.py
@@ -15,17 +15,12 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt/qtbase"] = None
         self.runtimeDependencies["libs/qt/qtdeclarative"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
-        self.runtimeDependencies["kde/frameworks/tier1/kquickcharts"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kdeclarative"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
-        self.runtimeDependencies["kdesupport/kdewin"] = None
         self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
         self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
-        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
-
         # More reliable style than the windows style e.g. WRT HiDPI scaling
         self.runtimeDependencies["kde/plasma/breeze"] = None
 
