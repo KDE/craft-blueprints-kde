9d809f0f94699a9b89954fffde7b190d7e2cd281
Hannah von Reth <vonreth@kde.org>
2024-07-16 11:25:53 +0300

    Update url

--- libs/_autotools/gcrypt/gcrypt.py
+++ libs/_autotools/gcrypt/gcrypt.py
@@ -4,7 +4,7 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         for ver in ["1.10.2"]:
-            self.targets[ver] = f"ftp://ftp.gnupg.org/gcrypt/libgcrypt/libgcrypt-{ver}.tar.bz2"
+            self.targets[ver] = f"https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"libgcrypt-{ver}"
             self.patchToApply[ver] = ("fix-getrandom-mac.diff", 1)
         self.targetDigests["1.10.2"] = (["3b9c02a004b68c256add99701de00b383accccf37177e0d6c58289664cce0c03"], CraftHash.HashAlgorithm.SHA256)
