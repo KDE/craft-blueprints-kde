dbeb6465e78b7a49897b24286f35ee01e8b6ab35
Jack Hill <jackhill3103@gmail.com>
2024-07-17 21:14:35 +0100

    KTorrent is in KDE Gear now

--- kde/applications/ktorrent/ktorrent.py
+++ kde/applications/ktorrent/ktorrent.py
@@ -4,20 +4,10 @@ from Package.CMakePackageBase import *
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets["master"] = "https://anongit.kde.org/ktorrent"
-        for version in ["4.3.1"]:
-            self.targets[version] = "https://ktorrent.org/downloads/" + version + "/ktorrent-" + version + ".tar.bz2"
-            self.targetInstSrc[version] = "ktorrent-" + version
-        # these patches were pushed upstream, see:
-        # https://commits.kde.org/ktorrent/72c0379dc75dcbec878ca2ea940d06f771b38438
-        # https://commits.kde.org/ktorrent/aa0060b48afc4f01a4c50a47d0e19047bc4da51e
-        self.patchToApply["4.3.1"] = [
-            ("0001-Do-not-include-signalcatcher.h-in-windows.patch", 1),
-            ("0002-Cast-activateWindow-param-to-improved-portability.patch", 1),
-        ]
+        self.versionInfo.setDefaultValues()
 
+        self.displayName = "KTorrent"
         self.description = "A powerful BitTorrent client."
-        self.defaultTarget = "master"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
@@ -47,6 +37,6 @@ class subinfo(info.infoclass):
             self.runtimeDependencies["kde/frameworks/tier1/kdbusaddons"] = None
             self.runtimeDependencies["kde/frameworks/tier3/kglobalaccel"] = None
 
-class Package(CMakePackageBase):
+class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
