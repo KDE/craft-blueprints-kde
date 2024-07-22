63f88c443a64cc07e89bdfcb6737d51a407cc850
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2024-07-22 23:21:36 +0200

    liblz4: use cmake

--- libs/liblz4/liblz4.py
+++ libs/liblz4/liblz4.py
@@ -10,8 +10,7 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://github.com/lz4/lz4/archive/v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"lz4-{ver}"
             self.archiveNames[ver] = f"lz4-{ver}.tar.gz"
-            if CraftCore.compiler.isMSVC():
-                self.targetConfigurePath[ver] = "build/cmake"
+            self.targetConfigurePath[ver] = "build/cmake"
         self.targetDigests["1.9.4"] = (["0b0e3aa07c8c063ddf40b082bdf7e37a1562bda40a0ff5272957f3e987e0e54b"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.10.0"] = (["537512904744b35e232912055ccf8ec66d768639ff3abe5788d90d792ec5f48b"], CraftHash.HashAlgorithm.SHA256)
         self.description = "Extremely Fast Compression algorithm"
@@ -21,30 +20,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
 
 
-if CraftCore.compiler.isMSVC():
-
-    class Package(CMakePackageBase):
-        def __init__(self, **kwargs):
-            super().__init__(**kwargs)
-            self.subinfo.options.dynamic.buildStatic = True
-
-else:
-
-    class Package(MakeFilePackageBase):
-        def __init__(self, **kwargs):
-            super().__init__(**kwargs)
-            self.subinfo.options.useShadowBuild = False
-            self.subinfo.options.make.supportsMultijob = False
-            self.subinfo.options.make.args += f"liblz4.a"
-
-        def make(self):
-            # fix Makefile
-            makefile = os.path.join(self.sourceDir(), "Makefile")
-            with open(makefile, "rt") as f:
-                content = f.read()
-
-            content = content.replace(r"include Makefile.inc", r"#include Makefile.inc")
-
-            with open(makefile, "wt") as f:
-                f.write(content)
-            return super().make()
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.subinfo.options.dynamic.buildStatic = True
