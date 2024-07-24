eb561584414da56c122887f20d740287737d7625
Volker Krause <vkrause@kde.org>
2024-07-24 18:18:56 +0200

    Kirigami Addons 1.4.0 depends on KCoreAddons

--- kde/unreleased/kirigami-addons/kirigami-addons.py
+++ kde/unreleased/kirigami-addons/kirigami-addons.py
@@ -24,6 +24,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt/qtdeclarative"] = None
         self.runtimeDependencies["libs/qt/qtmultimedia"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kguiaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
