68e4ca9a7a055f74cee580833ba839f3cb0a206b
Julius Künzel <julius.kuenzel@kde.org>
2024-08-09 22:50:14 +0000

    [libpng] Limit patch to MinGW

--- libs/libpng/libpng.py
+++ libs/libpng/libpng.py
@@ -1,5 +1,7 @@
 import info
-from Package.CMakePackageBase import *
+from CraftCore import CraftCore
+from Package.CMakePackageBase import CMakePackageBase
+from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
@@ -13,14 +15,20 @@ class subinfo(info.infoclass):
         self.patchToApply["1.6.43"] = [
             ("libpng-1.6.40-install.pc-on-windows.diff", 1),
             ("libpng-android-remove-zlib-dependency.diff", 1),
-            ("libpng-1.6.43-20240730.diff", 1),
         ]
+
+        if CraftCore.compiler.isMinGW():
+            # disable broken symlink creation
+            self.patchToApply["1.6.43"] += [("libpng-1.6.43-20240730.diff", 1)]
+
         self.targetDigests["1.6.39"] = (["af4fb7f260f839919e5958e5ab01a275d4fe436d45442a36ee62f73e5beb75ba"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.6.40"] = (["8f720b363aa08683c9bf2a563236f45313af2c55d542b5481ae17dd8d183bb42"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.6.43"] = (["e804e465d4b109b5ad285a8fb71f0dd3f74f0068f91ce3cdfde618180c174925"], CraftHash.HashAlgorithm.SHA256)
 
         self.patchLevel["1.6.39"] = 1
         self.patchLevel["1.6.40"] = 2
+        self.patchLevel["1.6.43"] = 1
+
         self.description = "A library to display png images"
         self.defaultTarget = "1.6.43"
 
