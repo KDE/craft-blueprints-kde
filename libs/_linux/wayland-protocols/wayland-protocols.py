b46491c543cb85ba4b9f164c85281cfd8de34025
Julius Künzel <julius.kuenzel@kde.org>
2024-08-09 22:56:42 +0000

    [wayland-protocols] Update to 1.36

--- libs/_linux/wayland-protocols/wayland-protocols.py
+++ libs/_linux/wayland-protocols/wayland-protocols.py
@@ -1,22 +1,26 @@
+import os
+
 import info
+import utils
+
+from CraftCore import CraftCore
+from Package.MesonPackageBase import MesonPackageBase
+from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.32"]:
+        for ver in ["1.36"]:
             self.targets[ver] = f"https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/{ver}/downloads/wayland-protocols-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"wayland-protocols-{ver}"
-        self.targetDigests["1.32"] = (["7459799d340c8296b695ef857c07ddef24c5a09b09ab6a74f7b92640d2b1ba11"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.36"] = (["71fd4de05e79f9a1ca559fac30c1f8365fa10346422f9fe795f74d77b9ef7e92"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "wayland-protocols contains Wayland protocols that add functionality not available in the Wayland core protocol."
 
-        self.defaultTarget = "1.32"
+        self.defaultTarget = "1.36"
 
     def setDependencies(self):
         self.buildDependencies["python-modules/meson"] = None
-
-
-from Package.MesonPackageBase import *
 
 
 class Package(MesonPackageBase):
