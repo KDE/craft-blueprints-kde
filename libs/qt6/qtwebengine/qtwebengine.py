77fe7f3d168fd129a44d38216574a449affaaac5
Julius Künzel <jk.kdedev@smartlab.uber.space>
2024-08-10 20:27:02 +0200

    [QtWebEngine] Try patch to fix MSVC

--- libs/qt6/qtwebengine/qtwebengine.py
+++ libs/qt6/qtwebengine/qtwebengine.py
@@ -37,6 +37,9 @@ class subinfo(info.infoclass):
             ("chromium-119.0.6045.159-icu-74.patch", 1),
         ]
 
+        # https://invent.kde.org/qt/qt/qtwebengine-chromium/-/commit/3aba63e62a86aeb157ad73df0fe0f44f01cf0e02
+        self.patchToApply["6.7.2"] = [("3aba63e62a86aeb157ad73df0fe0f44f01cf0e02.patch", 1)]
+
         self.patchLevel["6.6.1"] = 1
 
     def setDependencies(self):
