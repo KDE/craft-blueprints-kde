0168a82505c6e2c03d0a8bf83eedca09d4f059be
Albert Astals Cid <aacid@kde.org>
2024-08-27 21:55:11 +0200

    Fix Okular Android package
    Don't blacklist QML stuff that we need
    Don't package libavif that depends on dav1d that installs the lib with
    the wrong soname

--- kde/frameworks/tier1/kimageformats/kimageformats.py
+++ kde/frameworks/tier1/kimageformats/kimageformats.py
@@ -13,7 +13,8 @@ class subinfo(info.infoclass):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.buildDependencies["kde/frameworks/tier1/karchive"] = None
-        self.runtimeDependencies["libs/libavif"] = None
+        if not CraftCore.compiler.isAndroid:
+                self.runtimeDependencies["libs/libavif"] = None
         self.runtimeDependencies["libs/libjxl"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
 
