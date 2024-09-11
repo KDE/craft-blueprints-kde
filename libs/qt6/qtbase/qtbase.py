f34ca4b2b0773fefd4a2b8ea68d24f350b865db2
Hannah von Reth <vonreth@kde.org>
2024-09-11 17:32:52 +0200

    Fix static and ios builds

--- libs/qt6/qtbase/qtbase.py
+++ libs/qt6/qtbase/qtbase.py
@@ -1,8 +1,8 @@
 import info
-import utils
 from Blueprints.CraftPackageObject import CraftPackageObject
 from Blueprints.CraftVersion import CraftVersion
 from CraftCore import CraftCore
+import utils
 
 
 class subinfo(info.infoclass):
@@ -11,16 +11,16 @@ class subinfo(info.infoclass):
         self.options.dynamic.registerOption("buildReleaseAndDebug", False)
         self.options.dynamic.registerOption("libInfix", "")
         self.options.dynamic.registerOption("useLtcg", False)
-        self.options.dynamic.registerOption("withDBus", not CraftCore.compiler.isAndroid)
-        self.options.dynamic.registerOption("withGlib", not CraftCore.compiler.isWindows and not CraftCore.compiler.isAndroid)
+        self.options.dynamic.registerOption("withDBus", not CraftCore.compiler.platform.isAndroid)
+        self.options.dynamic.registerOption("withGlib", not CraftCore.compiler.platform.isWindows and not CraftCore.compiler.platform.isAndroid)
         self.options.dynamic.registerOption("withICU", self.options.isActive("libs/icu"))
         self.options.dynamic.registerOption("withHarfBuzz", self.options.isActive("libs/harfbuzz"))
         self.options.dynamic.registerOption("withPCRE2", self.options.isActive("libs/pcre2"))
         self.options.dynamic.registerOption("withEgl", True)
-        self.options.dynamic.registerOption("withCUPS", CraftCore.compiler.isMacOS or self.options.isActive("libs/cups"))
+        self.options.dynamic.registerOption("withCUPS", CraftCore.compiler.platform.isMacOS or self.options.isActive("libs/cups"))
 
         # We need to treat MacOS explicitly because of https://bugreports.qt.io/browse/QTBUG-116083
-        self.options.dynamic.registerOption("withFontConfig", self.options.isActive("libs/fontconfig") and not CraftCore.compiler.isMacOS)
+        self.options.dynamic.registerOption("withFontConfig", self.options.isActive("libs/fontconfig") and not CraftCore.compiler.platform.isApple)
 
     def setTargets(self):
         self.versionInfo.setDefaultValues()
@@ -52,33 +52,33 @@ class subinfo(info.infoclass):
         self.buildDependencies["dev-utils/pkg-config"] = None
         self.buildDependencies["dev-utils/perl"] = None
         self.buildDependencies["dev-utils/flexbison"] = None
-        if not self.options.buildStatic:
-            self.runtimeDependencies["libs/openssl"] = None
-            self.runtimeDependencies["libs/brotli"] = None
-            self.runtimeDependencies["libs/zlib"] = None
-            self.runtimeDependencies["libs/libzstd"] = None
-            self.runtimeDependencies["libs/libpng"] = None
-            self.runtimeDependencies["libs/libjpeg-turbo"] = None
-            self.runtimeDependencies["libs/sqlite"] = None
-            self.runtimeDependencies["libs/freetype"] = None
+        self.runtimeDependencies["libs/libjpeg-turbo"] = None
+        self.runtimeDependencies["libs/libpng"] = None
+        self.runtimeDependencies["libs/libzstd"] = None
+        self.runtimeDependencies["libs/sqlite"] = None
+        self.runtimeDependencies["libs/zlib"] = None
+        self.runtimeDependencies["libs/openssl"] = None
+        self.runtimeDependencies["libs/brotli"] = None
+        self.runtimeDependencies["libs/freetype"] = None
 
+        if self.options.dynamic.withHarfBuzz:
+            self.runtimeDependencies["libs/harfbuzz"] = None
+
+        if self.options.dynamic.withFontConfig:
+            self.runtimeDependencies["libs/fontconfig"] = None
+
+        if self.options.dynamic.withPCRE2:
+            self.runtimeDependencies["libs/pcre2"] = None
+
+        if not self.options.buildStatic:
             if self.options.dynamic.withDBus:
                 self.runtimeDependencies["libs/dbus"] = None
 
             if self.options.dynamic.withICU:
                 self.runtimeDependencies["libs/icu"] = None
 
-            if self.options.dynamic.withHarfBuzz:
-                self.runtimeDependencies["libs/harfbuzz"] = None
-
-            if self.options.dynamic.withFontConfig:
-                self.runtimeDependencies["libs/fontconfig"] = None
-
-            if CraftCore.compiler.isUnix and self.options.dynamic.withGlib:
+            if CraftCore.compiler.platform.isUnix and self.options.dynamic.withGlib:
                 self.runtimeDependencies["libs/glib"] = None
-
-            if self.options.dynamic.withPCRE2:
-                self.runtimeDependencies["libs/pcre2"] = None
 
             if self.options.dynamic.withCUPS:
                 self.runtimeDependencies["libs/cups"] = None
@@ -89,38 +89,55 @@ class Package(CraftPackageObject.get("libs/qt6").pattern):
         super().__init__(**kwargs)
 
         self.subinfo.options.configure.args += [
+            # Qt does weird stuff
+            "--log-level=STATUS",
             # sometimes qt fails to pic up the basic things
             f"-DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES={CraftCore.standardDirs.craftRoot()}/include",
             "-DFEATURE_pkg_config=ON",
+            "-DQT_FEATURE_sql_odbc=OFF",
+            "-DQT_BUILD_EXAMPLES=OFF",
+            f"-DCMAKE_INTERPROCEDURAL_OPTIMIZATION={self.subinfo.options.dynamic.useLtcg.asOnOff}",
+            "-DFEATURE_system_png=ON",
+            "-DFEATURE_system_jpeg=ON",
             "-DFEATURE_system_sqlite=ON",
             "-DFEATURE_system_zlib=ON",
-            f"-DFEATURE_system_freetype={self.subinfo.options.isActive('libs/freetype').asOnOff}",
-            "-DQT_FEATURE_sql_odbc=OFF",
-            "-DFEATURE_openssl_linked=ON",
-            "-DQT_BUILD_EXAMPLES=OFF",
+            "-DFEATURE_system_brotli=ON",
             f"-DFEATURE_system_pcre2={self.subinfo.options.dynamic.withPCRE2.asOnOff}",
+            f"-DFEATURE_system_freetype={self.subinfo.options.isActive('libs/freetype').asOnOff}",
             f"-DFEATURE_system_harfbuzz={self.subinfo.options.dynamic.withHarfBuzz.asOnOff}",
-            f"-DFEATURE_icu={self.subinfo.options.dynamic.withICU.asOnOff}",
-            f"-DFEATURE_dbus={self.subinfo.options.dynamic.withDBus.asOnOff}",
-            "-DFEATURE_dbus_linked=OFF",
-            f"-DFEATURE_glib={self.subinfo.options.dynamic.withGlib.asOnOff}",
             f"-DFEATURE_fontconfig={self.subinfo.options.dynamic.withFontConfig.asOnOff}",
-            f"-DCMAKE_INTERPROCEDURAL_OPTIMIZATION={self.subinfo.options.dynamic.useLtcg.asOnOff}",
+            "-DQT_FEATURE_openssl=ON",
+            "-DFEATURE_openssl_linked=ON",
         ]
-        if CraftCore.compiler.isLinux:
+
+        if not self.subinfo.options.dynamic.buildStatic:
             self.subinfo.options.configure.args += [
+                f"-DFEATURE_icu={self.subinfo.options.dynamic.withICU.asOnOff}",
+                f"-DFEATURE_dbus={self.subinfo.options.dynamic.withDBus.asOnOff}",
+                "-DFEATURE_dbus_linked=OFF",
+                f"-DFEATURE_glib={self.subinfo.options.dynamic.withGlib.asOnOff}",
                 f"-DFEATURE_cups={self.subinfo.options.dynamic.withCUPS.asOnOff}",
+            ]
+        if CraftCore.compiler.platform.isLinux:
+            self.subinfo.options.configure.args += [
                 "-DFEATURE_xcb=ON",
                 f"-DQT_FEATURE_egl={self.subinfo.options.dynamic.withEgl.asOnOff}",
             ]
 
-        if CraftCore.compiler.isAndroid:
-            self.subinfo.options.configure.args += [f"-DANDROID_ABI={CraftCore.compiler.androidAbi}", "-DECM_THREADS_WORKAROUND=OFF"]
-        elif CraftCore.compiler.isMacOS:
+        if CraftCore.compiler.platform.isAndroid:
+            self.subinfo.options.configure.args += [f"-DANDROID_ABI={CraftCore.compiler.platform.androidAbi}", "-DECM_THREADS_WORKAROUND=OFF"]
+        elif CraftCore.compiler.platform.isMacOS:
             self.subinfo.options.configure.args += ["-DQT_NO_HANDLE_APPLE_SINGLE_ARCH_CROSS_COMPILING=ON"]
+        elif CraftCore.compiler.platform.isIOS:
+           self.subinfo.options.configure.args += [
+                "-DQT_FEATURE_printsupport=OFF",
+                f"-DQT_HOST_PATH={CraftCore.standardDirs.craftHostRoot()}",
+                "-DQT_APPLE_SDK=iphonesimulator",
+                "-DCMAKE_SYSTEM_NAME=iOS",
+            ]
 
     def configure(self):
-        if CraftCore.compiler.isAndroid:
+        if CraftCore.compiler.platform.isAndroid:
             env = {}
             env["ECM_THREADS_WORKAROUND"] = "OFF"
             with utils.ScopedEnv(env):
