9b3dc336ceac0b91e9d38f0bae69da56462edbab
Julius Künzel <julius.kuenzel@kde.org>
2024-09-14 19:06:02 +0200

    [pillow] Add deps

--- python-modules/pillow/pillow.py
+++ python-modules/pillow/pillow.py
@@ -11,6 +11,19 @@ class subinfo(info.infoclass):
         self.description = "Pillow is the friendly PIL fork. The Python Imaging Library adds image processing capabilities to your Python interpreter."
         self.defaultTarget = "master"
 
+        # webp: provides the WebP format.
+        self.runtimeDependencies["libs/webp"] = None
+        # tiff: provides compressed TIFF functionality
+        self.runtimeDependencies["libs/tiff"] = None
+        # zlib: provides access to compressed PNGs
+        self.runtimeDependencies["libs/zlib"] = None
+        # openjpeg: provides JPEG 2000 functionality
+        self.runtimeDependencies["libs/openjpeg"] = None
+        # freetype: provides type related services
+        self.runtimeDependencies["libs/freetype"] = None
+        # libjpeg-turbo: provides JPEG functionality
+        self.runtimeDependencies["libs/libjpeg-turbo"] = None
+
 
 class Package(PipPackageBase):
     def __init__(self, **kwargs):
