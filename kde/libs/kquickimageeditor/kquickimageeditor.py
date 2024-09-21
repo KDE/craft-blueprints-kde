d9d45f4c9a5c69f80e6f0d3bc4560f184808f7c5
Volker Krause <vkrause@kde.org>
2024-09-21 10:13:17 +0200

    Update KQuickImageEditor to 0.4.0

--- kde/libs/kquickimageeditor/kquickimageeditor.py
+++ kde/libs/kquickimageeditor/kquickimageeditor.py
@@ -6,13 +6,13 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://invent.kde.org/libraries/kquickimageeditor.git"
 
-        for ver in ["0.3.0"]:
+        for ver in ["0.4.0"]:
             self.targets[ver] = "https://download.kde.org/stable/kquickimageeditor/kquickimageeditor-%s.tar.xz" % ver
             self.archiveNames[ver] = "kquickimageeditor-%s.tar.gz" % ver
             self.targetInstSrc[ver] = "kquickimageeditor-%s" % ver
 
         self.description = "A set of QtQuick components providing basic image editing capabilities"
-        self.defaultTarget = "0.3.0"
+        self.defaultTarget = "0.4.0"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
