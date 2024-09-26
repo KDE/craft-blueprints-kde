a04935ed007826485199d9d91bdab2f2df91791f
Plata Hill <plata.hill@kdemail.net>
2024-09-26 19:15:38 +0000

    Add telly-skout.py

---
+++ kde/applications/telly-skout/telly-skout.py
@@ -0,0 +1,43 @@
+import info
+from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues(gitUrl="https://invent.kde.org/utilities/telly-skout.git")
+        self.displayName = "Telly Skout"
+        self.description = "A convergent TV guide based on Kirigami."
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["libs/qt/qtdeclarative"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kdeclarative"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kitemmodels"] = None
+        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
+        if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+            self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+            self.runtimeDependencies["kde/plasma/breeze"] = None
+        else:
+            self.runtimeDependencies["kde/plasma/qqc2-breeze-style"] = None
+
+
+class Package(CraftPackageObject.get("kde").pattern):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.blacklist_file.append(self.blueprintDir() / "blacklist.txt")
+
+    def createPackage(self):
+        self.defines["executable"] = r"bin\telly-skout.exe"
+        self.addExecutableFilter(r"(bin|libexec)/(?!(telly-skout|update-mime-database)).*")
+        self.ignoredPackages.append("binary/mysql")
+        if not CraftCore.compiler.isLinux:
+            self.ignoredPackages.append("libs/dbus")
+        return super().createPackage()
