b943a15902e0d837ae8b92704a9b8daa52adf463
Carl Schwan <carl@carlschwan.eu>
2024-10-01 14:24:44 +0200

    Use meson for libass
    Available since libass 0.17.2

--- libs/libass/libass.py
+++ libs/libass/libass.py
@@ -1,19 +1,20 @@
 import info
 from CraftCore import CraftCore
-from Package.AutoToolsPackageBase import AutoToolsPackageBase
+from Package.MesonPackageBase import MesonPackageBase
 from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["0.17.1"]:
+        for ver in ["0.17.3"]:
             self.targets[ver] = f"https://github.com/libass/libass/archive/{ver}.tar.gz"
             self.targetInstSrc[ver] = "libass-" + ver
-        self.targetDigests["0.17.1"] = (["5ba42655d7e8c5e87bba3ffc8a2b1bc19c29904240126bb0d4b924f39429219f"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.17.3"] = (["5ba42655d7e8c5e87bba3ffc8a2b1bc19c29904240126bb0d4b924f39429219f"], CraftHash.HashAlgorithm.SHA256)
         self.description = "Portable subtitle renderer"
-        self.defaultTarget = "0.17.1"
+        self.defaultTarget = "0.17.3"
 
     def setDependencies(self):
+        self.buildDependencies["python-modules/meson"] = None
         self.buildDependencies["dev-utils/nasm"] = None
         self.runtimeDependencies["libs/fontconfig"] = None
         self.runtimeDependencies["libs/freetype"] = None
@@ -21,10 +22,10 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/harfbuzz"] = None
 
 
-class Package(AutoToolsPackageBase):
+class Package(MesonPackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.subinfo.options.configure.args += ["--enable-shared", "--disable-static"]
+        self.subinfo.options.configure.args += ["-Ddefault_library=shared"]
 
         if CraftCore.compiler.isAndroid:
-            self.subinfo.options.configure.args += ["--disable-require-system-font-provider"]
+            self.subinfo.options.configure.args += ["-Drequire-system-font-provider=false"]
