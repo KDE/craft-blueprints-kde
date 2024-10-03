f4da22c1862e071d2bb464d9661220e4addf549c
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2024-10-04 01:39:24 +0200

    r-base: fix joining path

--- libs/_linux/r-base/r-base.py
+++ libs/_linux/r-base/r-base.py
@@ -35,9 +35,9 @@ class Package(AutoToolsPackageBase):
 
     def configure(self):
         env = {}
-        env["CFLAGS"] = "-I" + OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot()) / "include"  # Needed in R 4.3.3 to find zlib
+        env["CFLAGS"] = "-I" + OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot()) + "include"  # Needed in R 4.3.3 to find zlib
         env["CPPFLAGS"] = env["CFLAGS"]
-        env["LDFLAGS"] = "-L" + OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot()) / "lib"
+        env["LDFLAGS"] = "-L" + OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot()) + "lib"
         with utils.ScopedEnv(env):
             return super().configure()
 
