b75d6a362546ede428cf359a0a2aa059255b6687
Plata Hill <plata.hill@kdemail.net>
2024-10-08 14:53:59 +0000

    Add Kalm

---
+++ kde/applications/kalm/kalm.py
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2024 Plata Hill <plata.hill@kdemail.net>
+
+import info
+from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues(gitUrl="https://invent.kde.org/utilities/kalm.git")
+        self.description = "Kalm can teach you different breathing techniques."
+
+    def setDependencies(self):
+        self.buildDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["libs/qt/qtdeclarative"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
+        if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+            self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+            self.runtimeDependencies["kde/plasma/breeze"] = None
+        else:
+            self.runtimeDependencies["kde/plasma/qqc2-breeze-style"] = None
+
+
+class Package(CraftPackageObject.get("kde").pattern):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
