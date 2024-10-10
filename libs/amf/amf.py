5f1699acfd4ba8d02356ab80d22aca31914c5d2a
Julius Künzel <julius.kuenzel@kde.org>
2024-10-10 14:03:12 +0200

    [AMF] Version 1.4.35

--- libs/amf/amf.py
+++ libs/amf/amf.py
@@ -7,11 +7,11 @@ from Utils import CraftHash
 class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "AMD library for accelerated H.264 and HEVC(only windows) encoding on hardware with Video Coding Engine (VCE)"
-        for ver in ["1.4.32"]:
+        for ver in ["1.4.35"]:
             self.targets[ver] = f"https://github.com/GPUOpen-LibrariesAndSDKs/AMF/archive/refs/tags/v{ver}.tar.gz"
             self.targetInstSrc[ver] = "AMF-" + ver
-        self.targetDigests["1.4.32"] = (["f08c048e818f71bc7909c447bd810e727818d442db649d479ef87fb5a44a3474"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.4.32"
+        self.targetDigests["1.4.35"] = (["5d93101eae5895247bea58cff40bb84424158170ae7f931105666157cc187636"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.4.35"
 
 
 class Package(SourceOnlyPackageBase):
