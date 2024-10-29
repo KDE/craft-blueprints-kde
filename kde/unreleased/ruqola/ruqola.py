0b814ad04d1665c9d2cbde475e6dc27df5c99aca
Laurent Montel <montel@kde.org>
2024-10-29 13:58:40 +0100

    Add 2.3.1

--- kde/unreleased/ruqola/ruqola.py
+++ kde/unreleased/ruqola/ruqola.py
@@ -33,13 +33,13 @@ class subinfo(info.infoclass):
         self.description = "Rocket.Chat Client"
         self.webpage = "https://apps.kde.org/ruqola"
 
-        for ver in ["2.1.0", "2.1.1", "2.2.0", "2.3.0"]:
+        for ver in ["2.1.0", "2.1.1", "2.2.0", "2.3.0", "2.3.1"]:
             self.targets[ver] = f"https://download.kde.org/stable/ruqola/ruqola-{ver}.tar.xz"
             self.targetDigestUrls[ver] = f"https://download.kde.org/stable/ruqola/ruqola-{ver}.tar.xz.sha256"
             self.targetInstSrc[ver] = f"ruqola-{ver}"
 
         self.svnTargets["master"] = "https://invent.kde.org/network/ruqola.git"
-        self.defaultTarget = "2.3.0"
+        self.defaultTarget = "2.3.1"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
