28c7aa4702a5839e2dc60f4e51e796ba2133cdac
Tobias Fella <fella@posteo.de>
2024-11-03 16:31:15 +0100

    Fix libquotient version

--- qt-libs/libquotient/libquotient.py
+++ qt-libs/libquotient/libquotient.py
@@ -7,14 +7,14 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/quotient-im/libQuotient.git||dev"
 
-        for ver in ["0.9"]:
+        for ver in ["0.9.0"]:
             self.targets[ver] = "https://github.com/quotient-im/libQuotient/archive/%s.tar.gz" % ver
             self.archiveNames[ver] = "libQuotient-%s.tar.gz" % ver
             self.targetInstSrc[ver] = "libQuotient-%s" % ver
 
-        self.targetDigests["0.9"] = (["5e607eb978a5daa82e2186cd92f0d964cb820c72cfad95ed2adda4525ed923b5"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.9.0"] = (["5e607eb978a5daa82e2186cd92f0d964cb820c72cfad95ed2adda4525ed923b5"], CraftHash.HashAlgorithm.SHA256)
 
-        self.defaultTarget = "0.9"
+        self.defaultTarget = "0.9.0"
         self.description = "A Qt library to write cross-platform clients for Matrix"
 
     def setDependencies(self):
