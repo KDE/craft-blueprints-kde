ef70a87cad0ffa506009b199f3dd1adad504b335
Laurent Montel <montel@kde.org>
2024-11-07 18:28:31 +0100

    Remove akonadi-search

--- kde/pim/pimcommon/pimcommon.py
+++ kde/pim/pimcommon/pimcommon.py
@@ -30,7 +30,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
         self.runtimeDependencies["kde/frameworks/tier3/purpose"] = None
-        self.runtimeDependencies["kde/pim/akonadi-search"] = None
 
         self.runtimeDependencies["kde/pim/kmime"] = None
         self.runtimeDependencies["kde/pim/akonadi"] = None
