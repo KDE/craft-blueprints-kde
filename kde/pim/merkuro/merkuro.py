3d2edf65f7e15c583e0994f04028ad92e9b8ddde
Laurent Montel <montel@kde.org>
2024-11-09 19:48:24 +0100

    Add shortcut

--- kde/pim/merkuro/merkuro.py
+++ kde/pim/merkuro/merkuro.py
@@ -37,4 +37,9 @@ class subinfo(info.infoclass):
 class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.defines["executable"] = r"bin\merkuro.exe"
+        self.blacklist_file.append(self.blueprintDir() / "blacklist.txt")
+        self.defines["shortcuts"] = [
+            {"name": "Merkuro Calendar", "target": "bin/merkuro-calendar.exe", "description": self.subinfo.description},
+            {"name": "Merkuro Contact", "target": "bin/merkuro-contact.exe"},
+            {"name": "Merkuro Mail", "target": "bin/merkuro-mail.exe"},
+        ]
