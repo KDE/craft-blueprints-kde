03f305c87c67622cb8a00ae3f55dce12dec12a89
Laurent Montel <montel@kde.org>
2024-11-11 15:08:49 +0100

    Increase kdsingleapplication version

--- libs/kdsingleapplication/kdsingleapplication.py
+++ libs/kdsingleapplication/kdsingleapplication.py
@@ -7,13 +7,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.0.0"]:
+        for ver in ["1.0.0", "1.1.0"]:
             self.targets[ver] = f"https://github.com/KDAB/KDSingleApplication/releases/download/v{ver}/kdsingleapplication-{ver}.tar.gz"
             self.targetInstSrc[ver] = "kdsingleapplication-1.0.0"
         self.svnTargets["master"] = "https://github.com/KDAB/KDSingleApplication.git"
 
         self.targetDigests["1.0.0"] = (["c92355dc10f3ebd39363458458fb5bdd9662e080cf77d91f0437763c4d936520"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.0.0"
+        self.defaultTarget = "1.1.0"
 
         self.description = "KDSingleApplication is a helper class for single-instance policy applications written by KDAB."
         self.webpage = "https://github.com/KDAB/KDSingleApplication"
