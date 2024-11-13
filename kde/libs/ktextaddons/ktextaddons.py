4ce924c808e7dfb2dee29173f499639a8d430db4
Laurent Montel <montel@kde.org>
2024-11-13 06:50:03 +0100

    Disable test building

--- kde/libs/ktextaddons/ktextaddons.py
+++ kde/libs/ktextaddons/ktextaddons.py
@@ -42,3 +42,4 @@ class subinfo(info.infoclass):
 class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.subinfo.options.dynamic.buildTests = False
