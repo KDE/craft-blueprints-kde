202acac8a4a5e8ce61c1613f1b0f8efac8f8de3c
Laurent Montel <montel@kde.org>
2024-11-13 10:51:06 +0100

    Add missing knotifyconfig

--- kde/pim/akregator/akregator.py
+++ kde/pim/akregator/akregator.py
@@ -33,6 +33,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kuserfeedback"] = None
         self.runtimeDependencies["kde/libs/ktextaddons"] = None
         self.runtimeDependencies["kde/plasma/breeze"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/knotifyconfig"] = None
 
 
 class Package(CraftPackageObject.get("kde").pattern):
