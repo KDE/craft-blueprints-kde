c7a0c1e383e082f14517f8ecf94e53278ef89433
Julius Künzel <julius.kuenzel@kde.org>
2024-11-17 20:56:31 +0100

    [kdevelop-pg-qt] Update to 2.3.0

--- extragear/kdevelop-pg-qt/kdevelop-pg-qt.py
+++ extragear/kdevelop-pg-qt/kdevelop-pg-qt.py
@@ -1,19 +1,22 @@
 import info
+from Blueprints.CraftPackageObject import CraftPackageObject
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets["2.2"] = "https://invent.kde.org/kdevelop/kdevelop-pg-qt.git|2.2"
+
+        for ver in ["2.3.0"]:
+            self.targets[ver] = f"https://download.kde.org/stable/kdevelop-pg-qt/{ver}/src/kdevelop-pg-qt-{ver}.tar.xz"
+            self.targetDigestUrls[ver] = f"https://download.kde.org/stable/kdevelop-pg-qt/{ver}/src/kdevelop-pg-qt-{ver}.tar.xz.sha256"
+            self.targetInstSrc[ver] = f"kdevelop-pg-qt-{ver}"
+
         self.svnTargets["master"] = "https://invent.kde.org/kdevelop/kdevelop-pg-qt.git"
-        self.defaultTarget = "2.2"
+        self.defaultTarget = "2.3.0"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt/qtbase"] = None
 
 
-from Package.CMakePackageBase import *
-
-
-class Package(CMakePackageBase):
+class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
