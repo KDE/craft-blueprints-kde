443d24e5852650e9c2daaba6e5d798f2a5d43ade
Julius Künzel <julius.kuenzel@kde.org>
2024-11-19 22:29:06 +0100

    [linuxdeploy-plugin-qt] Revert to previous version
    The 2.0.0-alpha-1-20241106 release does fail with our apps at the moment, see https://github.com/linuxdeploy/linuxdeploy-plugin-qt/issues/189

--- dev-utils/_linux/linuxdeploy-plugin-qt/linuxdeploy-plugin-qt.py
+++ dev-utils/_linux/linuxdeploy-plugin-qt/linuxdeploy-plugin-qt.py
@@ -21,8 +21,8 @@ class subinfo(info.infoclass):
         self.targetInstallPath["continous"] = "dev-utils/bin"
         self.description = "AppDir creation and maintenance tool. Featuring flexible plugin system."
         self.webpage = "https://github.com/linuxdeploy/linuxdeploy"
-        self.targetDigests["2.0.0-alpha-1-20241106"] = (['0615e9218606f14d38be37ef88908dee1ce6c32a04ad0e4f18dff9a85fa2e217'], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "2.0.0-alpha-1-20241106"
+        self.targetDigests["2.0.0-alpha-1-20241106"] = (["0615e9218606f14d38be37ef88908dee1ce6c32a04ad0e4f18dff9a85fa2e217"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1-alpha-20240109-1"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
