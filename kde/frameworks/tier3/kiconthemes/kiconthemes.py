12ecf67efae0009a510d2cc40d42b09f28d89736
Volker Krause <vkrause@kde.org>
2024-11-20 17:31:43 +0100

    Remove KConfigWidgets dependency from KIconThemes for KF >= 6.8
    No longer used there, which benefits especially our Android builds.

--- kde/frameworks/tier3/kiconthemes/kiconthemes.py
+++ kde/frameworks/tier3/kiconthemes/kiconthemes.py
@@ -1,5 +1,6 @@
 import info
 from Blueprints.CraftPackageObject import CraftPackageObject
+from Blueprints.CraftVersion import CraftVersion
 from CraftCore import CraftCore
 
 
@@ -24,9 +25,10 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt/qtsvg"] = None
         self.runtimeDependencies["kde/frameworks/tier1/karchive"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
-        self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kitemviews"] = None
+        if self.buildTarget < CraftVersion("6.8.0"):
+            self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
 
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
