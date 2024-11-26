976f1300e57bb46e3038bc7b848d3629bfcac433
Laurent Montel <montel@kde.org>
2024-11-26 06:47:59 +0100

    Add statemachine

--- kde/kdegames/konquest/konquest.py
+++ kde/kdegames/konquest/konquest.py
@@ -12,6 +12,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["libs/qt/qtscxml"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kcrash"] = None
