23ade622799a6e126558bd537b750cc5c4d856ff
Laurent Montel <montel@kde.org>
2024-11-26 17:15:22 +0100

    Add ecm package

--- kde/applications/kalk/kalk.py
+++ kde/applications/kalk/kalk.py
@@ -23,6 +23,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kunitconversion"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
             self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
