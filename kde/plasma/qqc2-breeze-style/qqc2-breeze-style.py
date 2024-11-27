64d86c618d3fb9910f46a40dfcf7b85dcc191d21
Volker Krause <vkrause@kde.org>
2024-11-27 17:47:41 +0100

    Update to Plasma 6.2.4

--- kde/plasma/qqc2-breeze-style/qqc2-breeze-style.py
+++ kde/plasma/qqc2-breeze-style/qqc2-breeze-style.py
@@ -6,9 +6,6 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
-        self.patchToApply["6.2.3"] = [("fix-combo-box-delegate.diff", 1)]
-        self.patchLevel["6.2.3"] = 1
-
     def setDependencies(self):
         self.runtimeDependencies["libs/qt/qtbase"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
