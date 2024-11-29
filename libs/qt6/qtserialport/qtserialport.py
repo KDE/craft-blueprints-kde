20ff4cbc385f5f42e85e126eca180c3cbf2cc23f
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2024-11-29 15:32:05 +0100

    add qtserialport

---
+++ libs/qt6/qtserialport/qtserialport.py
@@ -0,0 +1,17 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/qt6/qtbase"] = None
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CraftPackageObject.get("libs/qt6").pattern):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
