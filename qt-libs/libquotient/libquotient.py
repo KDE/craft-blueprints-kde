51cc0f438d6286ba87e1bc57e32547bc49544a45
Volker Krause <vkrause@kde.org>
2024-11-30 09:14:50 +0100

    Update to libQuotient 0.9.1

--- qt-libs/libquotient/libquotient.py
+++ qt-libs/libquotient/libquotient.py
@@ -7,18 +7,15 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/quotient-im/libQuotient.git||dev"
 
-        for ver in ["0.9.0"]:
+        for ver in ["0.9.1"]:
             self.targets[ver] = "https://github.com/quotient-im/libQuotient/archive/%s.tar.gz" % ver
             self.archiveNames[ver] = "libQuotient-%s.tar.gz" % ver
             self.targetInstSrc[ver] = "libQuotient-%s" % ver
 
-        self.targetDigests["0.9.0"] = (["5e607eb978a5daa82e2186cd92f0d964cb820c72cfad95ed2adda4525ed923b5"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.9.1"] = (["0a1fd19c9f6e4d93c60fbec5ab4ca84961781e6d00105a4437ecd14aaea36bc9"], CraftHash.HashAlgorithm.SHA256)
 
-        self.defaultTarget = "0.9.0"
+        self.defaultTarget = "0.9.1"
         self.description = "A Qt library to write cross-platform clients for Matrix"
-
-        self.patchToApply["0.9.0"] = [("fix-saving-access-token-to-keychain.diff", 1)]
-        self.patchLevel["0.9.0"] = 1
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
