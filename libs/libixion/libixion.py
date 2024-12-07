1630f7c9903298a1d342ae7c82361c0dba1d9cdc
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2024-12-07 21:42:44 +0100

    mdds+libixion: MSVC build settings

--- libs/libixion/libixion.py
+++ libs/libixion/libixion.py
@@ -1,6 +1,7 @@
 import info
 from Package.AutoToolsPackageBase import AutoToolsPackageBase
 from Utils import CraftHash
+from CraftCore import CraftCore
 
 
 class subinfo(info.infoclass):
@@ -13,6 +14,9 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://gitlab.com/api/v4/projects/ixion%2Fixion/packages/generic/source/{ver}/libixion-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"libixion-{ver}"
         self.targetDigests["0.19.0"] = (["b4864d7a55351a09adbe9be44e5c65b1d417e80e946c947951d0e8428b9dcd15"], CraftHash.HashAlgorithm.SHA256)
+        if CraftCore.compiler.isMSVC():
+            self.patchToApply["0.19.0"] = [("mdds-2.1.1_MSVC-c++17.patch", 1)]
+            self.patchToApply["0.19.0"] += [("libixion-0.19.0_MSVC-boost.patch", 1)]
 
         self.defaultTarget = "0.19.0"
 
@@ -25,4 +29,11 @@ class subinfo(info.infoclass):
 class Package(AutoToolsPackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # self.subinfo.options.configure.args += ["--disable-static", "--enable-shared"]
+        if CraftCore.compiler.isMSVC():
+            # MSVC explicitly needs to update __cplusplus
+            # https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
+            self.subinfo.options.configure.cxxflags += "/Zc:__cplusplus"
+            #TODO: use Boost version
+            self.subinfo.options.configure.cxxflags += f" -I{CraftCore.standardDirs.craftRoot()}/include/boost-1_86"
+            #TODO: Boost version not detected
+            #self.subinfo.options.configure.args += [f"BOOST_ROOT={CraftCore.standardDirs.craftRoot()}/include/boost-1_86"]
--- libs/mdds/mdds.py
+++ libs/mdds/mdds.py
@@ -1,6 +1,7 @@
 import info
 from Package.AutoToolsPackageBase import AutoToolsPackageBase
 from Utils import CraftHash
+from CraftCore import CraftCore
 
 
 class subinfo(info.infoclass):
@@ -11,6 +12,8 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://gitlab.com/api/v4/projects/mdds%2Fmdds/packages/generic/source/{ver}/mdds-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"mdds-{ver}"
         self.targetDigests["2.1.1"] = (["1483d90cefb8aa4563c4d0a85cb7b243aa95217d235d422e9ca6722fd5b97e56"], CraftHash.HashAlgorithm.SHA256)
+        if CraftCore.compiler.isMSVC():
+            self.patchToApply["2.1.1"] = [("mdds-2.1.1_MSVC-c++17.patch", 1)]
 
         self.defaultTarget = "2.1.1"
 
@@ -21,4 +24,8 @@ class subinfo(info.infoclass):
 class Package(AutoToolsPackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # self.subinfo.options.configure.args += ["--disable-static", "--enable-shared"]
+        if CraftCore.compiler.isMSVC():
+            # MSVC explicitly needs to update __cplusplus
+            # https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
+            self.subinfo.options.configure.cxxflags += "/Zc:__cplusplus"
+            # self.subinfo.options.configure.args += ["--disable-static", "--enable-shared"]
