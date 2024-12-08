b51e4ac1ddcc840c99a37d3a45daaa3ae5ff39e5
Volker Krause <vkrause@kde.org>
2024-12-08 14:57:01 +0100

    Update to SDL 2.30.10
    Fixes the build with Android NDK 27.

--- libs/libsdl2/libsdl2.py
+++ libs/libsdl2/libsdl2.py
@@ -8,11 +8,12 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "Simple DirectMedia Layer"
         self.webpage = "https://www.libsdl.org"
-        for ver in ["2.28.5"]:
+        for ver in ["2.28.5", "2.30.10"]:
             self.targets[ver] = f"https://www.libsdl.org/release/SDL2-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"SDL2-{ver}"
         self.targetDigests["2.28.5"] = (["332cb37d0be20cb9541739c61f79bae5a477427d79ae85e352089afdaf6666e4"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "2.28.5"
+        self.targetDigests["2.30.10"] = (["f59adf36a0fcf4c94198e7d3d776c1b3824211ab7aeebeb31fe19836661196aa"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "2.30.10"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/libsamplerate"] = None
