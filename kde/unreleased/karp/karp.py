cd4d332e15696e6510f1ea9889b4de0efb9d1571
Tomasz Bojczuk <seelook@gmail.com>
2025-01-03 14:31:00 +0100

    Unreleased: added karp app to craft
    - qpdf has to be added to CI image(s)

---
+++ kde/unreleased/karp/karp.py
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: 2025 by Tomasz Bojczuk <seelook@gmail.com>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import info
+from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.displayName = "Karp"
+        self.description = "KDE arranger for PDF files"
+
+        self.svnTargets["master"] = "https://invent.kde.org/graphics/karp.git"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["libs/qt/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt/qtwebengine"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
+        self.runtimeDependencies["kde/libs/kweathercore"] = None
+        self.runtimeDependencies["libs/qpdf"] = None
+        if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+            self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+            self.runtimeDependencies["kde/plasma/breeze"] = None
+        else:
+            self.runtimeDependencies["kde/plasma/qqc2-breeze-style"] = None
+
+
+class Package(CraftPackageObject.get("kde").pattern):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.subinfo.options.dynamic.buildTests = False
+
+    def createPackage(self):
+        self.defines["shortcuts"] = [{"name": "Karp", "target": "bin/karp.exe", "description": self.subinfo.description, "appId": "karp"}]
+        return super().createPackage()
