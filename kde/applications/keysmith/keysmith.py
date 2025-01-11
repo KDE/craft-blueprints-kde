df2d7acc235712de2249491f0ebc0defc336372e
Jack Hill <jackhill3103@gmail.com>
2025-01-11 19:27:56 +0000

    Keysmith: add Prison dependency for new QR scanning feature
    Required for utilities/keysmith!148

--- kde/applications/keysmith/keysmith.py
+++ kde/applications/keysmith/keysmith.py
@@ -20,6 +20,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/prison"] = None
         self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
         self.runtimeDependencies["libs/libsodium"] = None
         if not CraftCore.compiler.isAndroid:
