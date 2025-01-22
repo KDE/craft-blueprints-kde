9a6aafbf43490ea9298de3c21da197128a922168
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2025-01-22 13:18:10 +0100

    liborus: fix AppImage build

--- libs/liborcus/liborcus.py
+++ libs/liborcus/liborcus.py
@@ -12,8 +12,7 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://gitlab.com/api/v4/projects/orcus%2Forcus/packages/generic/source/{ver}/liborcus-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"liborcus-{ver}"
         self.targetDigests["0.19.2"] = (["69ed26a00d4aaa7688e62a6e003cbc81928521a45e96605e53365aa499719e39"], CraftHash.HashAlgorithm.SHA256)
-        if CraftCore.compiler.isMacOS:
-            self.patchToApply["0.19.2"] = [("liborcus-0.19.2-boost.patch", 1)]
+        self.patchToApply["0.19.2"] = [("liborcus-0.19.2-boost.patch", 1)]
 
         self.defaultTarget = "0.19.2"
 
