3048aa11ca0c1e2e0360e727193aae9d079b9dba
Julius Künzel <julius.kuenzel@kde.org>
2025-02-01 11:42:24 +0100

    [gyp-next] Install from pip
    Meanwhile pip has an up-to-date version and installing from the tarball does not install the gyp module for some reason properly (on MSVC)

--- python-modules/gyp-next/gyp-next.py
+++ python-modules/gyp-next/gyp-next.py
@@ -6,11 +6,11 @@ from Package.PipPackageBase import PipPackageBase
 class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "GYP can Generate Your Projects."
-        # The version in pip is super old so install directly from release zip
-        for ver in ["0.16.1"]:
-            self.targets[ver] = f"https://github.com/nodejs/gyp-next/archive/refs/tags/v{ver}.zip"
-            self.targetInstSrc[ver] = "gyp-next-" + ver
-        self.defaultTarget = "0.16.1"
+        self.defaultTarget = "master"
+
+    def setDependencies(self):
+        self.buildDependencies["python-modules/packaging"] = None
+        self.buildDependencies["python-modules/setuptools"] = None
 
 
 class Package(PipPackageBase):
