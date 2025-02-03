5fe5b335341c5dc7bb79d784fa0d70d7d72324a9
Laurent Montel <montel@kde.org>
2025-02-03 13:21:17 +0100

    Add kpublicalerts support

---
+++ kde/unreleased/kpublicalerts/kpublicalerts.py
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: 2025 by Laurent Montel <montel@kde.org>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import info
+from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.displayName = "KPublicalerts"
+        self.description = "Client for https://invent.kde.org/webapps/foss-public-alert-server/."
+
+        self.svnTargets["master"] = "https://invent.kde.org/utilities/kpublicalerts.git"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["libs/qt/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt/qtwebengine"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kguiaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
+        self.runtimeDependencies["kde/unreleased/kunifiedpush"] = None
+        self.runtimeDependencies["kde/libs/kweathercore"] = None
+        if not CraftCore.compiler.isAndroid:
+            self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+            self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+            self.runtimeDependencies["kde/plasma/breeze"] = None
+        else:
+            self.runtimeDependencies["kde/plasma/qqc2-breeze-style"] = None
+
+
+class Package(CraftPackageObject.get("kde").pattern):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def createPackage(self):
+        self.defines["shortcuts"] = [{"name": "publicalerts", "target": "bin/publicalerts.exe", "description": self.subinfo.description, "appId": "karp"}]
+        self.blacklist_file.append(self.blueprintDir() / "blacklist.txt")
+        return super().createPackage()
