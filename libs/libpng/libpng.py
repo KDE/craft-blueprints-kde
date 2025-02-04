86536fb8badf61e22e95a8e243232efedcf79169
Hannah von Reth <vonreth@kde.org>
2025-02-04 10:20:18 +0100

    Update png to 1.6.45

--- libs/libpng/libpng.py
+++ libs/libpng/libpng.py
@@ -6,23 +6,23 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.6.39", "1.6.40", "1.6.43"]:
+        for ver in ["1.6.39", "1.6.40", "1.6.43", "1.6.45"]:
             self.targets[ver] = "https://downloads.sourceforge.net/libpng/libpng-" + ver + ".tar.gz"
             self.targetInstSrc[ver] = "libpng-" + ver
 
         self.patchToApply["1.6.39"] = [("libpng-1.6.39-install.pc-on-windows.diff", 1), ("libpng-fix-android-shared-build.diff", 1)]
         self.patchToApply["1.6.40"] = [("libpng-1.6.40-install.pc-on-windows.diff", 1), ("libpng-android-remove-zlib-dependency.diff", 1)]
-        self.patchToApply["1.6.43"] = []
 
-        if CraftCore.compiler.isWindows or CraftCore.compiler.isAndroid:
-            self.patchToApply["1.6.43"] += [
-                ("libpng-1.6.40-install.pc-on-windows.diff", 1),
-                ("libpng-android-remove-zlib-dependency.diff", 1),
-            ]
-
-        if CraftCore.compiler.isMinGW():
-            # disable broken symlink creation
-            self.patchToApply["1.6.43"] += [("libpng-1.6.43-20240730.diff", 1)]
+        for ver in ["1.6.43", "1.6.45"]:
+            self.patchToApply[ver] = []
+            if CraftCore.compiler.isWindows or CraftCore.compiler.isAndroid:
+                self.patchToApply[ver] += [
+                    ("libpng-1.6.40-install.pc-on-windows.diff", 1),
+                    ("libpng-android-remove-zlib-dependency.diff", 1),
+                ]
+                if CraftCore.compiler.isMinGW():
+                    # disable broken symlink creation
+                    self.patchToApply[ver] += [("libpng-1.6.43-20240730.diff", 1)]
 
         self.targetDigests["1.6.39"] = (["af4fb7f260f839919e5958e5ab01a275d4fe436d45442a36ee62f73e5beb75ba"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.6.40"] = (["8f720b363aa08683c9bf2a563236f45313af2c55d542b5481ae17dd8d183bb42"], CraftHash.HashAlgorithm.SHA256)
@@ -33,7 +33,7 @@ class subinfo(info.infoclass):
         self.patchLevel["1.6.43"] = 1
 
         self.description = "A library to display png images"
-        self.defaultTarget = "1.6.43"
+        self.defaultTarget = "1.6.45"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/zlib"] = None
