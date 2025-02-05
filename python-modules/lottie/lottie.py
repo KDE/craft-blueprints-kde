af2db43e9ee7f2143977a5ec8cbc6c332fdf36a8
Julius Künzel <julius.kuenzel@kde.org>
2025-02-05 23:39:13 +0100

    [lottie] Don't install .version_full file on macOS

--- python-modules/lottie/lottie.py
+++ python-modules/lottie/lottie.py
@@ -2,6 +2,8 @@
 # SPDX-FileCopyrightText: 2024 Julius Künzel <julius.kuenzel@kde.org>
 
 import info
+import utils
+from CraftCore import CraftCore
 from Package.PipPackageBase import PipPackageBase
 
 
@@ -15,3 +17,10 @@ class subinfo(info.infoclass):
 class Package(PipPackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    def postInstall(self):
+        # workaround for lottie installing a file that is only needed at build time
+        # and breaks signing on macOS, better would be to fix it upstream
+        if CraftCore.compiler.isMacOS:
+            utils.deleteFile(self.installDir() / "lib/Python.framework/Versions/Current/.version_full")
+        return True
