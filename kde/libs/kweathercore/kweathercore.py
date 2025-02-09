37db923a2d14eec25ddb740869e5089dd44ca1ac
Volker Krause <vkrause@kde.org>
2025-02-09 10:50:53 +0100

    KWeatherCore is part of KDE Gear since 24.12

--- kde/libs/kweathercore/kweathercore.py
+++ kde/libs/kweathercore/kweathercore.py
@@ -2,24 +2,14 @@
 # SPDX-FileCopyrightText: 2021 Tobias Fella <fella@posteo.de>
 
 import info
-from Package.CMakePackageBase import CMakePackageBase
-from Utils import CraftHash
+from Blueprints.CraftPackageObject import CraftPackageObject
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
         self.displayName = "KWeatherCore"
         self.description = "Library to facilitate retrieval of weather information including forecasts and alerts"
-        self.svnTargets["master"] = "https://invent.kde.org/libraries/kweathercore"
-
-        for ver in ["0.5", "0.8.0"]:
-            self.targets[ver] = f"https://download.kde.org/stable/kweathercore/{ver}/kweathercore-{ver}.tar.xz"
-            self.targetInstSrc[ver] = f"kweathercore-{ver}"
-
-        self.targetDigests["0.5"] = (["162c69f758f2e9b1c1ef2b8d0c54f3cee439b4171ef32632df410411d30d4d6f"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["0.8.0"] = (["9bcac13daf98705e2f0d5b06b21a1a8694962078fce1bf620dbbc364873a0efe"], CraftHash.HashAlgorithm.SHA256)
-
-        self.defaultTarget = "0.8.0"
+        self.versionInfo.setDefaultValues(gitUrl="https://invent.kde.org/libraries/kweathercore.git")
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
@@ -30,6 +20,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kholidays"] = None
 
 
-class Package(CMakePackageBase):
+class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
