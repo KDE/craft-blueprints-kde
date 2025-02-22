9ccb9fe515d00b1af6b478eb2756e7013efa1a95
Hannah von Reth <vonreth@kde.org>
2025-02-22 15:36:31 +0100

    Add cython pacakge

--- python-modules/cython/cython.py
+++ python-modules/cython/cython.py
@@ -7,12 +7,9 @@ from Package.PipPackageBase import PipPackageBase
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = ""
-        self.description = "Powerful and Pythonic XML processing library combining libxml2/libxslt with the ElementTree API."
+        self.description = "The Cython compiler for writing C extensions in the Python language."
         self.defaultTarget = "master"
 
-
-    def setDependencies(self):
-        self.buildDependencies["python-modules/cython"] = None
 
 class Package(PipPackageBase):
     def __init__(self, **kwargs):
