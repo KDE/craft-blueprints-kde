1d077afb6e5fc91f1058b28fb6426b592711a4b1
Melvin Keskin <melvo@olomono.de>
2025-02-25 21:58:39 +0100

    qxmpp: Update to version 1.10.1

--- qt-libs/qxmpp/qxmpp.py
+++ qt-libs/qxmpp/qxmpp.py
@@ -15,13 +15,13 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/qxmpp-project/qxmpp.git"
-        for ver in ["1.9.4", "1.10.0"]:
+        for ver in ["1.9.4", "1.10.1"]:
             self.targets[ver] = f"https://github.com/qxmpp-project/qxmpp/archive/v{ver}.tar.gz"
             self.archiveNames[ver] = f"qxmpp-v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"qxmpp-{ver}"
         self.targetDigests["1.9.4"] = (["4403e43a0e8b6afa68f6e1e986e4ec19a08a6bf0db539ab7934a58afa1ddc532"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["1.10.0"] = (["04117461cc608af74ab299a7b96be4e38efbb0b67f50839fb59b17f3903725ee"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.10.0"
+        self.targetDigests["1.10.1"] = (["a9e95847c432cbf9ad36aa6d1596d66aa8f644d6983926457235fb64343bc42c"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.10.1"
 
 
 class Package(CMakePackageBase):
