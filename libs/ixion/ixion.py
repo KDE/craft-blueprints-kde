eb8262ec3e7bb09d5be61c523fec3583cb3d4f65
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2025-03-11 16:14:02 +0100

    add ixion and orcus (libixion/liborcus replacement)

---
+++ libs/ixion/ixion.py
@@ -0,0 +1,33 @@
+import info
+from CraftCore import CraftCore
+from CraftOS.osutils import OsUtils
+from Package.CMakePackageBase import CMakePackageBase
+from Utils import CraftHash
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.description = (
+            "a general purpose formula parser, interpreter, formula cell dependency tracker and spreadsheet document model backend all in one package"
+        )
+
+        for ver in ["0.20.0"]:
+            self.targets[ver] = f"https://gitlab.com/ixion/ixion/-/archive/{ver}/ixion-{ver}.tar.bz2"
+            self.targetInstSrc[ver] = f"ixion-{ver}"
+        self.targetDigests["0.20.0"] = (["39e54cd486fed458c2a6e83a5e658d4c2e818862355b33645bb1342449428463"], CraftHash.HashAlgorithm.SHA256)
+
+        self.defaultTarget = "0.20.0"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["libs/boost"] = None
+        self.buildDependencies["libs/mdds"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # to find mdds header
+        self.subinfo.options.configure.args += f'-DMDDS_INCLUDEDIR="{OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())}/include/mdds-3.0"'
+        if CraftCore.compiler.isMSVC():
+            self.subinfo.options.configure.args += f'-DCMAKE_CXX_FLAGS="-EHsc -DBOOST_ALL_NO_LIB"'
---
+++ libs/orcus/orcus.py
@@ -0,0 +1,39 @@
+import info
+from CraftCore import CraftCore
+from CraftOS.osutils import OsUtils
+from Package.CMakePackageBase import CMakePackageBase
+from Utils import CraftHash
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.description = (
+            "library that provides a collection of standalone file processing filters for spreadsheet formats"
+        )
+
+        for ver in ["0.20.0"]:
+            self.targets[ver] = f"https://gitlab.com/orcus/orcus/-/archive/{ver}/orcus-{ver}.tar.bz2"
+            self.targetInstSrc[ver] = f"orcus-{ver}"
+        self.targetDigests["0.20.0"] = (["767ddd06d72b18c5e6c65af74a78b8c7b9326c7163fde1066b97369769a68f35"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "0.20.0"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["libs/boost"] = None
+        self.buildDependencies["libs/mdds"] = None
+        self.runtimeDependencies["libs/ixion"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # to find mdds and ixion header
+        self.subinfo.options.configure.args += f'-DMDDS_INCLUDEDIR="{OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())}/include/mdds-3.0"'
+        self.subinfo.options.configure.args += f'-DIXION_INCLUDEDIR="{OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())}/include/ixion-0.20"'
+        if CraftCore.compiler.isMSVC():
+            self.subinfo.options.configure.args += '-DCMAKE_CXX_FLAGS="-EHsc -DBOOST_ALL_NO_LIB -DBOOST_PROGRAM_OPTIONS_DYN_LINK"'
+            self.subinfo.options.configure.args += f'-DCMAKE_SHARED_LINKER_FLAGS="-LIBPATH:{OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())}/lib boost_filesystem-vc143-mt-x64-1_86.lib boost_program_options-vc143-mt-x64-1_86.lib"'
+            self.subinfo.options.configure.args += f'-DCMAKE_EXE_LINKER_FLAGS="-LIBPATH:{OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())}/lib boost_program_options-vc143-mt-x64-1_86.lib boost_filesystem-vc143-mt-x64-1_86.lib"'
+        if CraftCore.compiler.isLinux:
+            self.subinfo.options.configure.args += f'-DCMAKE_SHARED_LINKER_FLAGS="-L{OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())}/lib -lboost_filesystem -lboost_program_options"'
+            self.subinfo.options.configure.args += f'-DCMAKE_EXE_LINKER_FLAGS="-L{OsUtils.toUnixPath(CraftCore.standardDirs.craftRoot())}/lib -lboost_filesystem -lboost_program_options"'
