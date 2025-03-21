355544f80a696e577ccfc00c38fe65f5aeb28b10
Melvin Keskin <melvo@olomono.de>
2025-03-21 16:18:41 +0100

    qxmpp: Adapt to new project URL

--- qt-libs/qxmpp/qxmpp.py
+++ qt-libs/qxmpp/qxmpp.py
@@ -14,13 +14,13 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kdesupport/qca"] = None
 
     def setTargets(self):
-        self.svnTargets["master"] = "https://github.com/qxmpp-project/qxmpp.git"
+        self.svnTargets["master"] = "https://invent.kde.org/libraries/qxmpp.git"
         for ver in ["1.9.4", "1.10.1"]:
-            self.targets[ver] = f"https://github.com/qxmpp-project/qxmpp/archive/v{ver}.tar.gz"
+            self.targets[ver] = f"https://invent.kde.org/libraries/qxmpp/-/archive/v{ver}/qxmpp-v{ver}.tar.gz"
             self.archiveNames[ver] = f"qxmpp-v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"qxmpp-{ver}"
-        self.targetDigests["1.9.4"] = (["4403e43a0e8b6afa68f6e1e986e4ec19a08a6bf0db539ab7934a58afa1ddc532"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["1.10.1"] = (["a9e95847c432cbf9ad36aa6d1596d66aa8f644d6983926457235fb64343bc42c"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.9.4"] = (["53ec8c6dbedb647290ae97b7f60de42e08986983de8bdedb2dcb3bd6d4793bf3"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.10.1"] = (["5b0368aa13681148b0d5b6fff9dc1a6a1d83f098e94b8224c9a7cf0981a74038"], CraftHash.HashAlgorithm.SHA256)
         self.defaultTarget = "1.10.1"
 
 
