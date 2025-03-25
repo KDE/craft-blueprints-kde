611f54579e8607ed6a5c18ee29e3e576045a123f
Jean-Baptiste Mardelle <jb@kdenlive.org>
2025-03-25 09:45:36 +0100

    onevpl: don't install examples

--- libs/onevpl/onevpl.py
+++ libs/onevpl/onevpl.py
@@ -20,3 +20,4 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.subinfo.options.configure.args += ["-DINSTALL_EXAMPLES=OFF"]
