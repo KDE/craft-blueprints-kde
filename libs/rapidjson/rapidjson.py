1675c36c403a11c278171b4d157bae33c57bc240
Julius Künzel <julius.kuenzel@kde.org>
2025-04-01 22:22:49 +0200

    [OpenTimelineIO] Update and fix macOS
    - Use rapidjson from Craft (possible only from the OTIO main branch yet)
    - Add patch to not hardcode INSTALL_NAME_DIR to @loader_path

---
+++ libs/rapidjson/rapidjson.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2025 Julius Künzel <julius.kuenzel@kde.org>
+
+import info
+from Package.CMakePackageBase import CMakePackageBase
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.description = "A fast JSON parser/generator for C++ with both SAX/DOM style API"
+        self.webpage = "https://rapidjson.org/"
+        for ver in ["1.1.0"]:
+            self.targets[ver] = f"https://github.com/Tencent/rapidjson/archive/refs/tags/v{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"rapidjson-{ver}"
+
+        self.svnTargets["24b5e7a"] = "https://github.com/Tencent/rapidjson.git||24b5e7a8b27f42fa16b96fc70aade9106cf7102f"
+
+        self.svnTargets["master"] = "https://github.com/Tencent/rapidjson.git"
+
+        self.defaultTarget = "24b5e7a"
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
