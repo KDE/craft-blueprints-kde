4e795b0a028a3495d43b0c19694fae14986dd780
Hannah von Reth <vonreth@kde.org>
2025-04-18 18:51:49 +0200

    libzip 1.11.3

--- libs/libzip/libzip.py
+++ libs/libzip/libzip.py
@@ -6,13 +6,14 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.8.0"]:
+        for ver in ["1.8.0", "1.11.3"]:
             self.targets[ver] = f"https://github.com/nih-at/libzip/releases/download/v{ver}/libzip-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"libzip-{ver}"
         self.targetDigests["1.8.0"] = (["30ee55868c0a698d3c600492f2bea4eb62c53849bcf696d21af5eb65f3f3839e"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.11.3"] = (["76653f135dde3036036c500e11861648ffbf9e1fc5b233ff473c60897d9db0ea"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "a library for handling zip archives"
-        self.defaultTarget = "1.8.0"
+        self.defaultTarget = "1.11.3"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
