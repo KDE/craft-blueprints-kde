2e1b32d42663b09f2c349ef69ec545ea48031f1c
Daniel Novomeský <dnovomesky@gmail.com>
2025-05-05 18:06:12 +0200

    [lcms2] 2.17 version
    meson build with disabled so.version libraries on Android

--- libs/lcms2/lcms2.py
+++ libs/lcms2/lcms2.py
@@ -1,52 +1,30 @@
 import info
-import utils
 from CraftCore import CraftCore
-from Package.AutoToolsPackageBase import AutoToolsPackageBase
-from Package.CMakePackageBase import CMakePackageBase
+from Package.MesonPackageBase import MesonPackageBase
+from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2.3", "2.4", "2.5", "2.7"]:
-            self.targets[ver] = f"http://download.sourceforge.net/lcms/lcms2-{ver}.tar.gz"
-            self.targetInstSrc[ver] = "lcms2-" + ver
-        self.patchToApply["2.3"] = [("lcms2-2.3-20120302.diff", 1)]
-        self.patchToApply["2.4"] = [("lcms2-2.4-20130124.diff", 1)]
-        self.patchToApply["2.5"] = [("lcms2-2.4-20130124.diff", 1)]
-        self.patchToApply["2.7"] = [("lcms2-2.7-20130124.diff", 1)]
-        self.targetDigests["2.3"] = "67d5fabda2f5777ca8387766539b9c871d993133"
-        self.targetDigests["2.4"] = "9944902864283af49e4e21a1ca456db4e04ea7c2"
-        self.targetDigests["2.5"] = "bab3470471fc7756c5fbe71be9a3c7d677d2ee7b"
-        self.targetDigests["2.7"] = "625f0d74bad4a0f6f917120fd992437d26f754d2"
-
-        self.description = "A small-footprint, speed optimized color management engine"
-        self.defaultTarget = "2.7"
+        for ver in ["2.17"]:
+            self.targets[ver] = f"https://github.com/mm2/Little-CMS/releases/download/lcms{ver}/lcms2-{ver}.tar.gz"
+            self.targetInstSrc[ver] = f"lcms2-{ver}"
+        self.targetDigests["2.17"] = (["d11af569e42a1baa1650d20ad61d12e41af4fead4aa7964a01f93b08b53ab074"], CraftHash.HashAlgorithm.SHA256)
+        self.patchToApply["2.17"] = [("lcms2-2.17-ability_to_disable_so.version_libs.patch", 1)]
+        self.description = "A free, open source, CMM engine. It provides fast transforms between ICC profiles."
+        self.defaultTarget = "2.17"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["python-modules/meson"] = None
 
 
-if not CraftCore.compiler.isWindows:
-
-    class Package(AutoToolsPackageBase):
-        def __init__(self, **kwargs):
-            super().__init__(**kwargs)
-
-        def configure(self):
-            if CraftCore.compiler.isAndroid:
-                env = {}
-                env["LDFLAGS"] = "-lm"
-                with utils.ScopedEnv(env):
-                    return super().configure()
-            else:
-                return super().configure()
-
-else:
-
-    class Package(CMakePackageBase):
-        def __init__(self, **kwargs):
-            super().__init__(**kwargs)
-            if self.subinfo.options.buildStatic:
-                self.subinfo.options.configure.args += ["-DBUILD_STATIC=ON", "-DBUILD_TESTS=ON", "-DBUILD_UTILS=ON"]
-            else:
-                self.subinfo.options.configure.args += ["-DBUILD_STATIC=OFF", "-DBUILD_TESTS=OFF", "-DBUILD_UTILS=OFF"]
+class Package(MesonPackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.subinfo.options.configure.args += [
+            "-Djpeg=disabled",
+            "-Dtiff=disabled",
+        ]
+        if CraftCore.compiler.isAndroid:
+            self.subinfo.options.configure.args += ["-Dversionedlibs=false"]
