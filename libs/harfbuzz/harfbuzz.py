de9c92455a6dfbe10d49debbbaa2a59e1ba787ea
Hannah von Reth <vonreth@kde.org>
2025-05-10 21:01:47 +0200

    Update harfbuzz

--- libs/harfbuzz/harfbuzz.py
+++ libs/harfbuzz/harfbuzz.py
@@ -1,23 +1,26 @@
 import info
-from Package.CMakePackageBase import CMakePackageBase
+from Package.MesonPackageBase import MesonPackageBase
 from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2.7.2"]:
+        for ver in ["11.2.0"]:
             self.targets[ver] = f"https://github.com/harfbuzz/harfbuzz/archive/{ver}.tar.gz"
-            self.targetInstSrc[ver] = "harfbuzz-" + ver
-        self.targetDigests["2.7.2"] = (["8ec112ee108642477478b75fc7906422abed404d7530e47ba0a4875f553f1b59"], CraftHash.HashAlgorithm.SHA256)
-        self.patchToApply["2.7.2"] = [("cmake-generate-pkgconfig.patch", 0)]
+            self.targetInstSrc[ver] = f"harfbuzz-{ver}"
+        self.targetDigests["11.2.0"] = (["16c0204704f3ebeed057aba100fe7db18d71035505cb10e595ea33d346457fc8"], CraftHash.HashAlgorithm.SHA256)
         self.description = "Text shaping library"
-        self.defaultTarget = "2.7.2"
+        self.defaultTarget = "11.2.0"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["python-modules/meson"] = None
+        self.runtimeDependencies["libs/icu"] = None
+        self.runtimeDependencies["libs/freetype"] = None
+        self.runtimeDependencies["libs/cairo"] = None
 
 
-class Package(CMakePackageBase):
+class Package(MesonPackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.subinfo.options.configure.args += ["-DCMAKE_CXX_FLAGS=-DHB_NO_PRAGMA_GCC_DIAGNOSTIC_ERROR=1"]
+        self.subinfo.options.configure.args += ["-Ddocs=disabled", "-Dbenchmark=disabled", "-Dglib=disabled", "-Dgobject=disabled"]
