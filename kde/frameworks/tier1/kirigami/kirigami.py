0edfcf6e27d6911bda1c4531182344ec4e8250f5
Volker Krause <vkrause@kde.org>
2025-05-10 10:26:59 +0200

    Update to KDE Frameworks 6.14.0

--- kde/frameworks/tier1/kirigami/kirigami.py
+++ kde/frameworks/tier1/kirigami/kirigami.py
@@ -6,6 +6,9 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
+        self.patchToApply["6.14.0"] = [("1380952bd517e83ef2bf5305f26e55a4bfbc57fb.diff", 1)]
+        self.patchLevel["6.14.0"] = 1
+
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
