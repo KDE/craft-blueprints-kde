6dfef349b780686d30871b156bfd54c48353cf8b
Laurent Montel <montel@kde.org>
2025-05-12 14:00:50 +0200

    Add kaichat.py

---
+++ kde/unreleased/kaichat/kaichat.py
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: 2025 by Laurent Montel <montel@kde.org>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import info
+from Blueprints.CraftPackageObject import CraftPackageObject
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.displayName = "KAIChat"
+        self.description = "Chat with AI"
+
+        self.svnTargets["master"] = "https://invent.kde.org/utilities/kaichat.git"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kcrash"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
+        self.runtimeDependencies["kde/plasma/breeze"] = None
+
+        self.runtimeDependencies["kde/frameworks/tier1/kidletime"] = None
+        self.runtimeDependencies["kde/libs/ktextaddons"] = None
+        self.runtimeDependencies["libs/kdsingleapplication"] = None
+
+
+class Package(CraftPackageObject.get("kde").pattern):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def createPackage(self):
+        self.defines["shortcuts"] = [{"name": "KAIChat", "target": "bin/kaichat.exe", "description": self.subinfo.description, "appId": "kaichat"}]
+        self.blacklist_file.append(self.blueprintDir() / "blacklist.txt")
+        self.defines["alias"] = "kaichat"
+        return super().createPackage()
