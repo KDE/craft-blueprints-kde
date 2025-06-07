b538e0cd0f2b674ee3fbfe8b06488745967be9d7
Julius Künzel <julius.kuenzel@kde.org>
2025-06-07 15:05:20 +0000

    [alligator] Add QImageFormats dep to support webp images

--- kde/applications/alligator/alligator.py
+++ kde/applications/alligator/alligator.py
@@ -15,6 +15,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt/qtdeclarative"] = None
         self.runtimeDependencies["libs/qt/qtwebsockets"] = None
         self.runtimeDependencies["libs/qt/qtwebview"] = None
+        self.runtimeDependencies["libs/qt/qtimageformats"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
