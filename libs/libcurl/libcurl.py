2c0785b24fd085cf5da9b01e5a970b7613df498c
Hannah von Reth <vonreth@kde.org>
2025-07-03 11:56:43 +0200

    curl 8.14.1

--- libs/libcurl/libcurl.py
+++ libs/libcurl/libcurl.py
@@ -15,16 +15,17 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/bagder/curl.git"
-        for ver in ["7.78.0", "7.84.0", "8.13.0"]:
+        for ver in ["7.78.0", "7.84.0", "8.13.0", "8.14.1"]:
             self.targets[ver] = f"https://curl.haxx.se/download/curl-{ver}.tar.bz2"
             self.targetInstSrc[ver] = "curl-" + ver
         self.targetDigests["7.78.0"] = (["98530b317dc95ccb324bbe4f834f07bb642fbc393b794ddf3434f246a71ea44a"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["7.84.0"] = (["702fb26e73190a3bd77071aa146f507b9817cc4dfce218d2ab87f00cd3bc059d"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["8.13.0"] = (["e0d20499260760f9865cb6308928223f4e5128910310c025112f592a168e1473"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["8.14.1"] = (["5760ed3c1a6aac68793fc502114f35c3e088e8cd5c084c2d044abdf646ee48fb"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "a free and easy-to-use client-side URL transfer library"
         self.patchLevel["7.84.0"] = 2
-        self.defaultTarget = "8.13.0"
+        self.defaultTarget = "8.14.1"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
