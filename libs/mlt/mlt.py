447905c013d2176c9707d954e73d09efc654dc32
Julius Künzel <julius.kuenzel@kde.org>
2025-07-24 22:25:44 +0200

    [mlt] Modify blueprint for MSVC build
    Draft as it breaks other platforms

--- libs/mlt/mlt.py
+++ libs/mlt/mlt.py
@@ -5,8 +5,6 @@ from Package.CMakePackageBase import CMakePackageBase
 
 
 class subinfo(info.infoclass):
-    def registerOptions(self):
-        self.parent.package.categoryInfo.compiler = CraftCore.compiler.Compiler.GCCLike
 
     def setTargets(self):
         self.description = "Open source multimedia framework"
@@ -60,8 +58,8 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/ladspa-cmt"] = None
         self.runtimeDependencies["libs/ladspa-rnnoise"] = None
         self.runtimeDependencies["libs/ladspa-tap"] = None
-        self.runtimeDependencies["libs/spatialaudio"] = None
-        self.runtimeDependencies["libs/lilv"] = None
+        # self.runtimeDependencies["libs/spatialaudio"] = None
+        #self.runtimeDependencies["libs/lilv"] = None
         self.runtimeDependencies["libs/opencv/opencv_contrib"] = None
         self.runtimeDependencies["libs/opencv/opencv"] = None
         # dependencies for glaxnimate module
@@ -72,24 +70,75 @@ class Package(CMakePackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # enable submodule checkout to get glaximate
-        if not CraftCore.compiler.isAndroid:
-            self.subinfo.options.fetch.checkoutSubmodules = True
+        # if not CraftCore.compiler.isAndroid:
+        #     self.subinfo.options.fetch.checkoutSubmodules = True
         self.subinfo.options.configure.args += [
-            "-DMOD_DECKLINK=ON",
+            "-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON",
+            # "-DMOD_DECKLINK=ON",
             "-DWINDOWS_DEPLOY=OFF",
             "-DRELOCATABLE=ON",
-            "-DMOD_GDK=OFF",  # don't pull in gtk
-            "-DMOD_SDL2=ON",
+            # "-DMOD_GDK=OFF",  # don't pull in gtk
+            # "-DMOD_SDL2=ON",
             "-DBUILD_TESTING=OFF",
+        ]
+
+        self.subinfo.options.configure.args += [
+            "-DGPL=ON",
+            "-DGPL3=ON",
+            "-DBUILD_TESTING=OFF",
+            "-DBUILD_DOCS=OFF",
+            "-DCLANG_FORMAT=ON",
+            "-DBUILD_TESTS_WITH_QT6=OFF",
+
+            "-DMOD_AVFORMAT=OFF", #default: ON
+            "-DMOD_DECKLINK=OFF", #TODO: handled below in blueprint
+            "-DMOD_FREI0R=ON",
+            "-DMOD_GDK=OFF", #default: ON
+            "-DMOD_GLAXNIMATE=OFF",
+            "-DMOD_GLAXNIMATE_QT6=OFF",
+            "-DMOD_JACKRACK=ON",
+            "-DUSE_LV2=ON",
+            "-DUSE_VST2=OFF", #default: ON
+            "-DMOD_KDENLIVE=ON",
+            "-DMOD_NDI=OFF",
+            "-DMOD_NORMALIZE=ON",
+            "-DMOD_OLDFILM=ON",
+            "-DMOD_OPENCV=OFF",
+            "-DMOD_MOVIT=OFF", #default: ON
+            "-DMOD_PLUS=OFF", #default: ON
+            "-DMOD_PLUSGPL=OFF", #default: ON
+            "-DMOD_QT=OFF",
+            "-DMOD_QT6=ON",
+            "-DMOD_RESAMPLE=ON",
+            "-DMOD_RTAUDIO=ON",
+            "-DMOD_RUBBERBAND=ON",
+            "-DMOD_SDL1=OFF", #default: ON
+            "-DMOD_SDL2=ON",
+            "-DMOD_SOX=ON",
+            "-DMOD_SPATIALAUDIO=ON",
+            "-DMOD_VIDSTAB=ON",
+            "-DMOD_VORBIS=ON",
+            "-DMOD_XINE=OFF", #default: ON
+            "-DMOD_XML=ON",
+
+            "-DSWIG_CSHARP=OFF",
+            "-DSWIG_JAVA=OFF",
+            "-DSWIG_LUA=OFF",
+            "-DSWIG_NODEJS=OFF",
+            "-DSWIG_PERL=OFF",
+            "-DSWIG_PHP=OFF",
+            "-DSWIG_PYTHON=OFF",
+            "-DSWIG_RUBY=OFF",
+            "-DSWIG_TCL=OFF"
         ]
 
         if CraftCore.compiler.isAndroid:
             self.subinfo.options.configure.args += ["-DMOD_RTAUDIO=OFF", "-DMOD_SOX=OFF"]
 
-        if self.subinfo.options.isActive("libs/libarchive"):
-            self.subinfo.options.configure.args += ["-DMOD_GLAXNIMATE_QT6=ON"]
-        else:
-            self.subinfo.options.configure.args += ["-DMOD_GLAXNIMATE_QT6=OFF"]
+        # if self.subinfo.options.isActive("libs/libarchive"):
+        #     self.subinfo.options.configure.args += ["-DMOD_GLAXNIMATE_QT6=ON"]
+        # else:
+        #     self.subinfo.options.configure.args += ["-DMOD_GLAXNIMATE_QT6=OFF"]
 
         if CraftCore.compiler.isMSVC():
             # TODO Fix decklink module with MSVC
