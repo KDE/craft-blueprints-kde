050b7ef618aa6ab3ad58a4b6a3ade7161c0d91e9
Thomas Baumgart <thb@net-bembel.de>
2025-08-03 14:57:11 +0200

    [alkimia] Add version 8.2.1

--- extragear/alkimia/alkimia.py
+++ extragear/alkimia/alkimia.py
@@ -29,7 +29,7 @@ from Package.CMakePackageBase import CMakePackageBase
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["7.0.2", "8.0.4", "8.1.1", "8.1.2", "8.2.0"]:
+        for ver in ["7.0.2", "8.0.4", "8.1.1", "8.1.2", "8.2.0", "8.2.1"]:
             self.targets[ver] = f"https://download.kde.org/stable/alkimia/{ver}/alkimia-{ver}.tar.xz"
             self.targetDigestUrls[ver] = f"https://download.kde.org/stable/alkimia/{ver}/alkimia-{ver}.tar.xz.sha256"
             self.targetInstSrc[ver] = f"alkimia-{ver}"
@@ -37,7 +37,7 @@ class subinfo(info.infoclass):
         self.svnTargets["master"] = "https://invent.kde.org/office/alkimia.git"
         self.targetUpdatedRepoUrl["master"] = ("https://anongit.kde.org/alkimia", "https://invent.kde.org/office/alkimia.git")
 
-        self.defaultTarget = "8.2.0"
+        self.defaultTarget = "8.2.1"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt6/qtbase"] = None
