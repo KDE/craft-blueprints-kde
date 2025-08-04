6ee09683adffafc70f45fbd32d28e4051f13e0b7
Volker Krause <vkrause@kde.org>
2025-08-04 17:27:35 +0200

    Update to Poppler 25.08.0

--- qt-libs/poppler/poppler.py
+++ qt-libs/poppler/poppler.py
@@ -35,10 +35,10 @@ class subinfo(info.infoclass):
         self.svnTargets["master"] = "git://git.freedesktop.org/git/poppler/poppler"
 
         # always try to use latest libpoppler with all security fixes
-        ver = "25.07.0"
+        ver = "25.08.0"
         self.targets[ver] = f"https://invent.kde.org/mirrors/poppler/-/archive/poppler-{ver}/poppler-poppler-{ver}.tar.bz2"
         self.targetInstSrc[ver] = f"poppler-poppler-{ver}"
-        self.targetDigests[ver] = (["8c1ca818e0df06ad3a956fdb6362367bc810d4daababf135d232bb479dd04291"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests[ver] = (["95c23dde967f09ce5571e0fdbc13b775e3620b26074fa06dc9bddba9e310f9dc"], CraftHash.HashAlgorithm.SHA256)
         self.defaultTarget = ver
 
     def setDependencies(self):
