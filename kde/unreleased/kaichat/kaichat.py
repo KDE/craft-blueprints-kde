1efd31db8e101abc0d93ca47a800797958709a1e
Laurent Montel <montel@kde.org>
2025-08-07 07:34:07 +0200

    Kaichat needs KIO/Purpose now

--- kde/unreleased/kaichat/kaichat.py
+++ kde/unreleased/kaichat/kaichat.py
@@ -25,7 +25,9 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kidletime"] = None
         self.runtimeDependencies["kde/libs/ktextaddons"] = None
         self.runtimeDependencies["libs/kdsingleapplication"] = None
-
+        if not CraftCore.compiler.isMacOS:
+            self.runtimeDependencies["kde/frameworks/tier3/purpose"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
 
 class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self, **kwargs):
