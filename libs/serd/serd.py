458852de60092bc1dc995233eccf2a370f09732b
Julius Künzel <julius.kuenzel@kde.org>
2025-08-08 22:05:15 +0200

    Update lilv and its dependencies
    As a site effect this fixes build on MSVC

--- libs/serd/serd.py
+++ libs/serd/serd.py
@@ -8,11 +8,19 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["0.32.2"]:
+        self.description = "A lightweight C library for reading and writing RDF"
+        self.webpage = "https://drobilla.net/software/serd"
+
+        self.svnTargets["master"] = "https://gitlab.com/drobilla/serd.git"
+        self.patchLevel["master"] = 20250808
+
+        for ver in ["0.32.2", "0.32.4"]:
             self.targets[ver] = f"https://download.drobilla.net/serd-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"serd-{ver}"
         self.targetDigests["0.32.2"] = (["df7dc2c96f2ba1decfd756e458e061ded7d8158d255554e7693483ac0963c56b"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "0.32.2"
+        self.targetDigests["0.32.4"] = (["cbefb569e8db686be8c69cb3866a9538c7cb055e8f24217dd6a4471effa7d349"], CraftHash.HashAlgorithm.SHA256)
+
+        self.defaultTarget = "0.32.4"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
--- libs/sord/sord.py
+++ libs/sord/sord.py
@@ -8,11 +8,19 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["0.16.16"]:
+        self.description = "Lightweight C library for storing RDF statements in memory"
+        self.webpage = "https://drobilla.net/software/sord"
+
+        self.svnTargets["master"] = "https://gitlab.com/drobilla/sord.git"
+        self.patchLevel["master"] = 20250808
+
+        for ver in ["0.16.16", "0.16.18"]:
             self.targets[ver] = f"https://download.drobilla.net/sord-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"sord-{ver}"
         self.targetDigests["0.16.16"] = (["257f876d756143da02ee84c9260af93559d6249dd87f317e70ab5fffcc975fd0"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "0.16.16"
+        self.targetDigests["0.16.18"] = (["4f398b635894491a4774b1498959805a08e11734c324f13d572dea695b13d3b3"], CraftHash.HashAlgorithm.SHA256)
+
+        self.defaultTarget = "0.16.18"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
