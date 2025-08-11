886e5b8ce5bc6b4c189399bd6a1761f75fab04a4
Robert Lancaster <rlancaste@gmail.com>
2025-08-11 06:43:37 +0000

    KCMUtils is needed for the KnewStuff Dialog to work on MacOS
    Without the KCMUtils module, at least on MacOS, the Download New Stuff / KF6::KnewStuff download window is a blank screen.  This installs the needed QT Quick based KCMUtils module so that the dialog can work properly.

--- kde/applications/kstars/kstars.py
+++ kde/applications/kstars/kstars.py
@@ -28,6 +28,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier2/kpackage"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwidgetsaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier3/knewstuff"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kcmutils"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kdbusaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kjobwidgets"] = None
