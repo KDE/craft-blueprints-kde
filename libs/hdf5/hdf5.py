2571e6f11751c943f573038313797c4bf765572e
Julius Künzel <julius.kuenzel@kde.org>
2025-08-21 23:00:31 +0200

    [hdf5] Cleanup old versions

--- libs/hdf5/hdf5.py
+++ libs/hdf5/hdf5.py
@@ -9,19 +9,13 @@ from Utils import CraftHash
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "[git]https://bitbucket.hdfgroup.org/scm/hdffv/hdf5.git"
-        for ver in ["1.10.8"]:
-            self.targets[ver] = f"https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-{ver}/src/hdf5-{ver}.tar.gz"
-            self.archiveNames[ver] = f"hdf5-{ver}.tar.gz"
-        for ver in ["1.12.1"]:
-            self.targets[ver] = f"https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-{ver}/src/hdf5-{ver}.tar.bz2"
+
         for ver in ["1.14.3"]:
             self.targets[ver] = f"https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.14/hdf5-{ver}/src/hdf5-{ver}.tar.bz2"
         self.targets["1.14.6"] = "https://support.hdfgroup.org/releases/hdf5/v1_14/v1_14_6/downloads/hdf5-1.14.6.tar.gz"
-        for ver in ["1.10.8", "1.12.1", "1.14.3", "1.14.6"]:
+        for ver in ["1.14.3", "1.14.6"]:
             self.targetInstSrc[ver] = f"hdf5-{ver}"
 
-        self.targetDigests["1.10.8"] = (["d341b80d380dd763753a0ebe22915e11e87aac4e44a084a850646ff934d19c80"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["1.12.1"] = (["aaf9f532b3eda83d3d3adc9f8b40a9b763152218fa45349c3bc77502ca1f8f1c"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.14.3"] = (["9425f224ed75d1280bb46d6f26923dd938f9040e7eaebf57e66ec7357c08f917"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.14.6"] = (["e4defbac30f50d64e1556374aa49e574417c9e72c6b1de7a4ff88c4b1bea6e9b"], CraftHash.HashAlgorithm.SHA256)
         self.description = "A data model, library, and file format for storing and managing data"
@@ -37,6 +31,3 @@ class Package(CMakePackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.subinfo.options.configure.args += ["-DHDF5_ENABLE_Z_LIB_SUPPORT=ON"]
-        # build with the 1.10 APIs
-        if self.buildTarget == "1.12.1" or self.buildTarget == "1.14.3":
-            self.subinfo.options.configure.args += ["-DDEFAULT_API_VERSION:STRING=v110"]
