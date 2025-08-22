9bb0a8edec6b8d52e624b50b8750decb3acab028
Julius Künzel <julius.kuenzel@kde.org>
2025-08-23 00:06:22 +0200

    [sox] Add patch to fix build

--- libs/_autotools/sox/sox.py
+++ libs/_autotools/sox/sox.py
@@ -11,7 +11,7 @@ class subinfo(info.infoclass):
         for ver in ["14.4.2"]:
             self.targets[ver] = f"https://downloads.sourceforge.net/project/sox/sox/{ver}/sox-{ver}.tar.bz2"
             self.targetInstSrc[ver] = "sox-" + ver
-            self.patchToApply[ver] = [("mingw-sox-install.patch", 1)]
+            self.patchToApply[ver] = [("mingw-sox-install.patch", 1), ("sox-14.4.2-20250822.diff", 1)]
 
         self.targetDigests["14.4.2"] = (["81a6956d4330e75b5827316e44ae381e6f1e8928003c6aa45896da9041ea149c"], CraftHash.HashAlgorithm.SHA256)
 
