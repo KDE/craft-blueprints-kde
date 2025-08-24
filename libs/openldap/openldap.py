be56165765a2f0d486e46b8ddd0b5553688b8309
Julius Künzel <julius.kuenzel@kde.org>
2025-08-24 15:30:23 +0200

    [openldap] Patch borked openssl check on macOS

--- libs/openldap/openldap.py
+++ libs/openldap/openldap.py
@@ -20,6 +20,10 @@ class subinfo(info.infoclass):
             self.patchToApply["2.4.45"] += [("openldap-2.4.45-20170628.diff", 1)]
             self.patchToApply["2.4.45"] += [("openldap-2.4.45-20250809.diff", 1)]  # Force usage of compat getopt
             self.patchToApply["2.6.10"] = [("openldap-2.6.10-20250807.diff", 1)]
+
+        if CraftCore.compiler.isMacOS:
+            self.patchToApply["2.6.10"] = [("no-openssl-version-check.diff", 1)]
+
         self.targetDigests["2.4.45"] = (["cdd6cffdebcd95161a73305ec13fc7a78e9707b46ca9f84fb897cd5626df3824"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.5.16"] = (["546ba591822e8bb0e467d40c4d4a30f89d937c3a507fe83a578f582f6a211327"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.6.6"] = (["082e998cf542984d43634442dbe11da860759e510907152ea579bdc42fe39ea0"], CraftHash.HashAlgorithm.SHA256)
