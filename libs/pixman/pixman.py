870803d39b9dff38b50a312fd2e057ea5c9735d8
Julius Künzel <julius.kuenzel@kde.org>
2025-08-25 12:34:03 +0200

    [pixman] Fix version number

--- libs/pixman/pixman.py
+++ libs/pixman/pixman.py
@@ -6,13 +6,13 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["0.40.0", "0.42.2", "0.42.4"]:
+        for ver in ["0.40.0", "0.42.2", "0.46.4"]:
             self.targets[ver] = "http://cairographics.org/releases/pixman-" + ver + ".tar.gz"
             self.targetInstSrc[ver] = "pixman-" + ver
         self.description = "Image processing and manipulation library"
         self.targetDigests["0.40.0"] = (["6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["0.42.2"] = (["ea1480efada2fd948bc75366f7c349e1c96d3297d09a3fe62626e38e234a625e"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["0.42.4"] = (["d09c44ebc3bd5bee7021c79f922fe8fb2fb57f7320f55e97ff9914d2346a591c"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.46.4"] = (["d09c44ebc3bd5bee7021c79f922fe8fb2fb57f7320f55e97ff9914d2346a591c"], CraftHash.HashAlgorithm.SHA256)
 
         # https://gitlab.freedesktop.org/pixman/pixman/-/commit/bd4e7a9b9e672105bda35ff736c977adbf719c6c
         self.patchToApply["0.40.0"] = [("bd4e7a9b9e672105bda35ff736c977adbf719c6c.diff", 1)]
