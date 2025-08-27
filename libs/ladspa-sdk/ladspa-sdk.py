b9c75574aa8b5fcd11ec8481421ed92dc6dac80b
Julius Künzel <julius.kuenzel@kde.org>
2025-08-28 00:10:54 +0200

    [ladspa-sdk] Use source code from our mirror

--- libs/ladspa-sdk/ladspa-sdk.py
+++ libs/ladspa-sdk/ladspa-sdk.py
@@ -11,7 +11,8 @@ class subinfo(info.infoclass):
 
         # Remember to update the CMake patch (at least the project version), when updating to a new version
         for ver in ["1.17"]:
-            self.targets[ver] = f"https://www.ladspa.org/download/ladspa_sdk_{ver}.tgz"
+            # self.targets[ver] = f"https://www.ladspa.org/download/ladspa_sdk_{ver}.tgz"
+            self.targets[ver] = f"https://files.kde.org/craft/sources/libs/ladspa/ladspa_sdk_{ver}.tgz"
             self.targetInstSrc[ver] = f"ladspa_sdk_{ver}/src"
             self.patchToApply[ver] = [("ladspa-sdk-cmake.diff", 2), ("ladspa-sdk-1.17-msvc.diff", 2)]
         self.targetDigests["1.17"] = (["27d24f279e4b81bd17ecbdcc38e4c42991bb388826c0b200067ce0eb59d3da5b"], CraftHash.HashAlgorithm.SHA256)
