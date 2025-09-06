8440e5f8c6de1c18fdbb0bb7cf0dfdf3032585be
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2025-09-06 22:00:26 +0200

    [vlc] Fix compilation with gcc 14

--- libs/_autotools/vlc/vlc.py
+++ libs/_autotools/vlc/vlc.py
@@ -38,8 +38,9 @@ class subinfo(info.infoclass):
         self.targetDigests["3.0.19"] = (["643e3294bafe922324663ca499515b7564f2794575fd7d2b7992d20896381745"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["3.0.21"] = (["24dbbe1d7dfaeea0994d5def0bbde200177347136dbfe573f5b6a4cee25afbb0"], CraftHash.HashAlgorithm.SHA256)
         self.patchToApply["3.0.21"] = [
-            ("5574.diff", 1)
-        ]  # upstream in-progress MR to fix compilation with ffmpeg 7.x: https://code.videolan.org/videolan/vlc/-/merge_requests/5574/diffs; retrieved 2024-09-02
+            ("5574.diff", 1),  # upstream in-progress MR to fix compilation with ffmpeg 7.x: https://code.videolan.org/videolan/vlc/-/merge_requests/5574/diffs; retrieved 2024-09-02
+            ("6168.diff", 1),  # upstream MR to fix compilation with stricter compilers: https://code.videolan.org/videolan/vlc/-/merge_requests/6168; retrieved 2025-09-08
+        ]
         self.description = "VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."
         self.defaultTarget = "3.0.21"
 
