a58dc45c5cd242a15d5c300d24afb3e459add7b6
Julius Künzel <julius.kuenzel@kde.org>
2025-09-09 14:42:56 +0200

    [gavl] Force rebuild to fix crash

--- libs/_autotools/gavl/gavl.py
+++ libs/_autotools/gavl/gavl.py
@@ -16,6 +16,8 @@ class subinfo(info.infoclass):
 
         self.targetDigests["1.4.0"] = (["51aaac41391a915bd9bad07710957424b046410a276e7deaff24a870929d33ce"], CraftHash.HashAlgorithm.SHA256)
         self.patchToApply["1.4.0"] = ("FixCputest.patch", 1)
+        self.patchLevel["1.4.0"] = 1
+
         self.svnTargets["master"] = "https://github.com/bplaum/gavl.git||main"
         self.patchLevel["master"] = 20220321
         # when adding new commit based targets, keep gnutls dependency (below) in mind!
