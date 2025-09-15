639b1d34f6275df8db0868d234b7b7da3a16983a
Johnny Jazeix <jazeix@gmail.com>
2025-09-15 13:52:29 +0200

    add QtGraphs and QtQuick3D available since Qt6.9

---
+++ libs/qt6/qtgraphs/qtgraphs.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2017 Hannah von Reth <vonreth@kde.org>
+import info
+from Blueprints.CraftPackageObject import CraftPackageObject
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/qt6/qtbase"] = None
+
+
+class Package(CraftPackageObject.get("libs/qt6").pattern):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
