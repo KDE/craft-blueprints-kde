83cb882428041dd5174d7dd638422639e1546ba7
Volker Krause <vkrause@kde.org>
2025-09-20 14:03:21 +0200

    Update libquotient to 0.9.5

--- qt-libs/libquotient/libquotient.py
+++ qt-libs/libquotient/libquotient.py
@@ -12,16 +12,14 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/quotient-im/libQuotient.git||dev"
 
-        for ver in ["0.9.4"]:
+        for ver in ["0.9.5"]:
             self.targets[ver] = "https://github.com/quotient-im/libQuotient/archive/%s.tar.gz" % ver
             self.archiveNames[ver] = "libQuotient-%s.tar.gz" % ver
             self.targetInstSrc[ver] = "libQuotient-%s" % ver
-            self.patchToApply[ver] = [("nested-transaction-fix.diff", 1)]
-            self.patchLevel[ver] = 1
 
-        self.targetDigests["0.9.4"] = (["f5dfd8dd7652161e6699baef6cd7e8ded5c91bd2ceb691b7ba4379e9fe319502"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.9.5"] = (["24366dd59aca7b991756e45c3c8823c43fc6a8fdb161ea6dcce5b666c6300766"], CraftHash.HashAlgorithm.SHA256)
 
-        self.defaultTarget = "0.9.4"
+        self.defaultTarget = "0.9.5"
         self.description = "A Qt library to write cross-platform clients for Matrix"
 
     def setDependencies(self):
