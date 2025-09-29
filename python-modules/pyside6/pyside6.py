d5220a22b409c738afb3cb50b0556c1d658b58a4
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2025-09-29 15:58:00 +0200

    add pyside6 blueprint

---
+++ python-modules/pyside6/pyside6.py
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2025 Stefan Gerlach <stefan.gerlach@uni.kn>
+
+import info
+import utils
+from CraftCore import CraftCore
+from Package.PipPackageBase import PipPackageBase
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.description = "Python Qt bindings project"
+        #self.defaultTarget = "master"
+        self.defaultTarget = "6.9.2"
+ 
+
+class Package(PipPackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.allowPrebuildBinaries = True
+        self.subinfo.options.configure.args += ["-v", "-v", "-v"]
+
+    def install(self):
+        if not super().install():
+            return False
+
+        utils.createSymlink(CraftCore.standardDirs.craftRoot() / "lib/python3.11/site-packages/PySide6", CraftCore.standardDirs.craftRoot() / "share/PySide6")
+
+        return True
