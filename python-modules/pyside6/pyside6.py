d0e718914a9fdadc23ddb2c2cf3ef2f33859d64e
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2025-10-09 18:32:10 +0200

    pyside6: build from source and remove redundant shiboken6-generator

--- python-modules/pyside6/pyside6.py
+++ python-modules/pyside6/pyside6.py
@@ -2,27 +2,25 @@
 # SPDX-FileCopyrightText: 2025 Stefan Gerlach <stefan.gerlach@uni.kn>
 
 import info
-import utils
 from CraftCore import CraftCore
-from Package.PipPackageBase import PipPackageBase
+from Package.CMakePackageBase import CMakePackageBase
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
         self.description = "Python Qt bindings project"
-        self.defaultTarget = "6.9.2"
+
+        for ver in ["6.9.3"]:
+            self.svnTargets[ver] = f"git://code.qt.io/pyside/pyside-setup.git||v{ver}"
+            self.targetInstSrc[ver] = "."
+
+        self.defaultTarget = "6.9.3"
+
+    def setDependencies(self):
+        self.runtimeDependencies["libs/python"] = None
 
 
-class Package(PipPackageBase):
+class Package(CMakePackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.allowPrebuildBinaries = True
-        self.subinfo.options.configure.args += ["-v", "-v", "-v"]
-
-    def install(self):
-        if not super().install():
-            return False
-
-        utils.createSymlink(CraftCore.standardDirs.craftRoot() / "lib/python3.11/site-packages/PySide6", CraftCore.standardDirs.craftRoot() / "share/PySide6")
-
-        return True
+        self.subinfo.options.configure.args = f'-DCMAKE_C_FLAGS="-isystem {CraftCore.standardDirs.craftRoot()}/include/python3.11"'
