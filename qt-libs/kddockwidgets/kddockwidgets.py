fc50efbcbef66ab4babdc035201ab838ec8d94e6
Jean-Baptiste Mardelle <jb@kdenlive.org>
2025-10-21 13:22:09 +0200

    Update KDDockWidgets with latest fixes (correctly paint south tabs)

--- qt-libs/kddockwidgets/kddockwidgets.py
+++ qt-libs/kddockwidgets/kddockwidgets.py
@@ -15,7 +15,8 @@ class subinfo(info.infoclass):
             self.targetInstSrc[ver] = f"KDDockWidgets-{ver}"
             self.archiveNames[ver] = f"KDDockWidgets-{ver}.tar.gz"
         self.targetDigests["2.3.0"] = (["843baf9e1812c1ab82fd81d85b57cbc0d29bb43245efeb2539039780004b1056"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "2.3.0"
+        self.svnTargets["1e25b35"] = "https://github.com/KDAB/KDDockWidgets.git||1e25b357bbc7e0485c172dde77c653fa30c26ba8"
+        self.defaultTarget = "1e25b35"
 
         self.description = "KDDockWidgets is a Qt dock widget library written by KDAB, suitable for replacing QDockWidget and implementing advanced functionalities missing in Qt."
         self.webpage = "https://www.kdab.com/introducing-kddockwidgets/"
