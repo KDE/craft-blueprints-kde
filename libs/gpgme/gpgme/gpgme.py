c8c8b588a16cd8372949fd3eb0a4059f1f1e0810
Ingo Klöcker <dev@ingo-kloecker.de>
2025-10-22 10:19:20 +0200

    gpgme 2.0.1, gpgmepp 2.0.0, qgpgme 2.0.0
    This change introduces qgpgme as separate package.

--- libs/gpgme/gpgme/gpgme.py
+++ libs/gpgme/gpgme/gpgme.py
@@ -32,14 +32,14 @@ from Utils import CraftHash
 if not CraftCore.compiler.isMSVC():
 
     class subinfo(info.infoclass):
-        def registerOptions(self):
-            # Be aware that ther is also gpgmepp to provide C++
-            self.options.dynamic.registerOption("enableCPP", False)
-
         def setTargets(self):
-            self.versionInfo.setDefaultValues()
-            self.targetDigests["1.23.2"] = (["9499e8b1f33cccb6815527a1bc16049d35a6198a6c5fae0185f2bd561bce5224"], CraftHash.HashAlgorithm.SHA256)
-            self.patchLevel["1.23.2"] = 1
+            for ver in ["2.0.1"]:
+                self.targets[ver] = f"https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-{ver}.tar.bz2"
+                self.targetInstSrc[ver] = f"gpgme-{ver}"
+
+            self.targetDigests["2.0.1"] = (["821ab0695c842eab51752a81980c92b0410c7eadd04103f791d5d2a526784966"], CraftHash.HashAlgorithm.SHA256)
+
+            self.defaultTarget = "2.0.1"
 
         def setDependencies(self):
             self.buildDependencies["dev-utils/msys"] = None
@@ -47,18 +47,12 @@ if not CraftCore.compiler.isMSVC():
             self.runtimeDependencies["libs/gpg-error"] = None
             self.runtimeDependencies["libs/assuan2"] = None
             self.runtimeDependencies["libs/gnupg"] = None
-            if self.options.dynamic.enableCPP:
-                self.runtimeDependencies["libs/qt/qtbase"] = None
 
     class Package(AutoToolsPackageBase):
         def __init__(self, **kwargs):
             super().__init__(**kwargs)
             self.subinfo.options.configure.cflags += " -Wno-incompatible-pointer-types"
             self.subinfo.options.configure.args += ["--disable-gpg-test"]
-            if not self.subinfo.options.dynamic.enableCPP:
-                self.subinfo.options.configure.args += ["--enable-languages=no"]
-            else:
-                self.subinfo.options.configure.args += ["--enable-languages=cpp,qt"]
 
         def install(self):
             if not super().install():
--- libs/gpgme/qgpgme/qgpgme.py
+++ libs/gpgme/qgpgme/qgpgme.py
@@ -7,18 +7,19 @@ from Utils import CraftHash
 class subinfo(info.infoclass):
     def registerOptions(self):
         if CraftCore.compiler.isMinGW():
-            # Theoretically gpgmepp supports mingw but the cmake patches are incomplete
+            # Theoretically qgpgme supports mingw but the cmake patches are incomplete
             self.parent.package.categoryInfo.compiler = CraftCore.compiler.Compiler.NoCompiler
 
     def setTargets(self):
         for ver in ["2.0.0"]:
-            self.targets[ver] = f"https://gnupg.org/ftp/gcrypt/gpgmepp/gpgmepp-{ver}.tar.xz"
-            self.targetInstSrc[ver] = f"gpgmepp-{ver}"
+            self.targets[ver] = f"https://gnupg.org/ftp/gcrypt/qgpgme/qgpgme-{ver}.tar.xz"
+            self.targetInstSrc[ver] = f"qgpgme-{ver}"
 
-        self.targetDigests["2.0.0"] = (["d4796049c06708a26f3096f748ef095347e1a3c1e570561701fe952c3f565382"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["2.0.0"] = (["15645b2475cca6118eb2ed331b3a8d9442c9d4019c3846ba3f6d25321b4a61ad"], CraftHash.HashAlgorithm.SHA256)
         if CraftCore.compiler.isMSVC():
             self.patchToApply["2.0.0"] += [
                 ("msvc.patch", 1),
+                ("0001-Workaround-compile-errors-with-MSVC-2022.patch", 1),
             ]
 
         self.defaultTarget = "2.0.0"
@@ -27,10 +28,13 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/assuan2"] = None
         self.runtimeDependencies["libs/gpg-error"] = None
         self.runtimeDependencies["libs/gpgme/gpgme"] = None
+        self.runtimeDependencies["libs/gpgme/gpgmepp"] = None
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/gnupg"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
 
 
 class Package(CMakePackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.subinfo.options.configure.args += ["-DBUILD_WITH_QT5=OFF"]
