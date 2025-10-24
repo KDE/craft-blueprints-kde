b9a23497ddcacbef659a042a4c9095599120c005
Hannah von Reth <vonreth@kde.org>
2025-10-24 10:25:02 +0200

    libsamplerate cmake 4 patch

--- libs/libsamplerate/libsamplerate.py
+++ libs/libsamplerate/libsamplerate.py
@@ -9,6 +9,8 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://github.com/libsndfile/libsamplerate/archive/{ver}.tar.gz"
             self.targetInstSrc[ver] = f"libsamplerate-{ver}"
         self.targetDigests["0.2.2"] = (["16e881487f184250deb4fcb60432d7556ab12cb58caea71ef23960aec6c0405a"], CraftHash.HashAlgorithm.SHA256)
+        # cmake 4, based on 15c392d47e71b9395a759544b3818a1235fe1a1d.patch
+        self.patchToApply["0.2.2"] = [("libsamplerate-0.2.2-20251024.diff", 1)]
         self.description = "an audio sample rate converter library"
         self.defaultTarget = "0.2.2"
 
