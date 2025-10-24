c704e7986c1731a77da05bcb7aa966afa3be2feb
Ingo Klöcker <dev@ingo-kloecker.de>
2025-10-24 17:25:27 +0200

    gpgmepp: Add upstream patches to fix build with MSVC

--- libs/gpgme/gpgmepp/gpgmepp.py
+++ libs/gpgme/gpgmepp/gpgmepp.py
@@ -14,6 +14,7 @@ class subinfo(info.infoclass):
         if CraftCore.compiler.isMSVC():
             self.patchToApply["2.0.0"] = [
                 ("msvc.patch", 1),
+                ("gpgmepp-2.0.0.patch", 1),
             ]
 
         self.defaultTarget = "2.0.0"
