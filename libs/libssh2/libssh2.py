37add30c3837b3203daaaf38dcabbcd4b715409d
Hannah von Reth <vonreth@kde.org>
2025-10-27 17:00:17 +0100

    libssh2 1.11.1

--- libs/libssh2/libssh2.py
+++ libs/libssh2/libssh2.py
@@ -9,16 +9,19 @@ from Utils import CraftHash
 class subinfo(info.infoclass):
     def setTargets(self):
         self.svnTargets["master"] = "https://github.com/libssh2/libssh2.git"
-        for ver in ["1.10.0", "1.11.0"]:
+        for ver in ["1.10.0", "1.11.0", "1.11.1"]:
             self.targets[ver] = f"https://www.libssh2.org/download/libssh2-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"libssh2-{ver}"
         self.targetDigests["1.10.0"] = (["2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["1.11.0"] = (["3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.11.1"] = (["d9ec76cbe34db98eec3539fe2c899d26b0c837cb3eb466a56b0f109cabf658f7"], CraftHash.HashAlgorithm.SHA256)
         self.patchToApply["1.10.0"] = [("libssh2-1.10.0-20221026.diff", 1)]  # don't let pkg-config search for ws2_32
         if CraftCore.compiler.isMSVC():
             self.patchToApply["1.11.0"] = [("libssh2-1.11.0-MSVC-libprefix.pc.diff", 1)]
+            self.patchToApply["1.11.1"] = [("libssh2-1.11.1-20251027.diff", 1)]
         self.patchLevel["1.10.0"] = 1
-        self.defaultTarget = "1.11.0"
+        self.webpage = "https://github.com/libssh2/libssh2"
+        self.defaultTarget = "1.11.1"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
