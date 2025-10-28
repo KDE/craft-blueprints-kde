f69317cadad7e07f3ba66f781f33fc8aaabda53e
Hannah von Reth <vonreth@kde.org>
2025-10-28 10:13:53 +0100

    snoretoast v0.9.1, to fix cmake 4 support

--- dev-utils/_msvc/snoretoast/snoretoast.py
+++ dev-utils/_msvc/snoretoast/snoretoast.py
@@ -2,7 +2,6 @@ import info
 from CraftCore import CraftCore
 from CraftOS.osutils import OsUtils
 from Package.CMakePackageBase import CMakePackageBase
-from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
@@ -14,17 +13,11 @@ class subinfo(info.infoclass):
         self.svnTargets["master"] = "https://invent.kde.org/libraries/snoretoast.git"
         self.targetUpdatedRepoUrl["master"] = ("https://anongit.kde.org/snoretoast", "https://invent.kde.org/libraries/snoretoast.git")
 
-        for ver in ["0.5.2", "0.6.0", "0.7.0"]:
-            self.targets[ver] = f"https://download.kde.org/stable/snoretoast/{ver}/src/snoretoast-{ver}.tar.xz"
-            self.targetDigestUrls[ver] = f"https://download.kde.org/stable/snoretoast/{ver}/src/snoretoast-{ver}.tar.xz.sha256"
-            self.targetInstSrc[ver] = f"snoretoast-{ver}"
-
-        for ver in ["0.8.0", "0.9.0"]:
-            self.targets[ver] = f"https://invent.kde.org/libraries/snoretoast/-/archive/v{ver}/snoretoast-v{ver}.tar.gz"
+        for ver in ["0.9.1"]:
+            self.targets[ver] = f"https://download.kde.org/stable/snoretoast/snoretoast-v{ver}.tar.bz2"
+            self.targetDigestUrls[ver] = f"https://download.kde.org/stable/snoretoast/snoretoast-v{ver}.tar.bz2.sha256"
             self.targetInstSrc[ver] = f"snoretoast-v{ver}"
-        self.targetDigests["0.8.0"] = (["3d77ae76dd47929c088b3bbf4bd4fa2984dfa3c8c4c959ad4a5a427002a2ab64"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["0.9.0"] = (["2d19d793b665ec0357a506111528110f87823c3efcd1a08599e1c4571ae86066"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "0.9.0"
+        self.defaultTarget = "0.9.1"
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
