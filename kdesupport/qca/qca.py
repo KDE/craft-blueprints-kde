8e6001165f90129e6651075cee92fc165b36d2f3
Hannah von Reth <vonreth@kde.org>
2025-10-29 17:52:46 +0100

    qca 2.3.10

--- kdesupport/qca/qca.py
+++ kdesupport/qca/qca.py
@@ -5,6 +5,21 @@ from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
+    def setTargets(self):
+        self.description = "Qt Cryptographic Architecture (QCA)"
+
+        self.svnTargets["master"] = "https://invent.kde.org/libraries/qca.git"
+
+        for ver in ["2.3.7", "2.3.10"]:
+            self.targets[ver] = f"https://download.kde.org/stable/qca/{ver}/qca-{ver}.tar.xz"
+            self.targetInstSrc[ver] = f"qca-{ver}"
+        self.targetDigests["2.3.7"] = (["fee2343b54687d5be3e30fb33ce296ee50ac7ae5e23d7ab725f63ffdf7af3f43"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["2.3.10"] = (["1c5b722da93d559365719226bb121c726ec3c0dc4c67dea34f1e50e4e0d14a02"], CraftHash.HashAlgorithm.SHA256)
+
+        self.webpage = "https://invent.kde.org/libraries/qca"
+        # latest stable version
+        self.defaultTarget = "2.3.10"
+
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
@@ -18,19 +33,6 @@ class subinfo(info.infoclass):
         # cyrus-sasl currently fails to build with mingw / for android
         if not CraftCore.compiler.isMinGW() and not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["libs/cyrus-sasl"] = None
-
-    def setTargets(self):
-        self.description = "Qt Cryptographic Architecture (QCA)"
-
-        self.svnTargets["master"] = "https://anongit.kde.org/qca.git"
-
-        for ver in ["2.3.7"]:
-            self.targets[ver] = f"https://download.kde.org/stable/qca/{ver}/qca-{ver}.tar.xz"
-            self.targetDigests[ver] = (["fee2343b54687d5be3e30fb33ce296ee50ac7ae5e23d7ab725f63ffdf7af3f43"], CraftHash.HashAlgorithm.SHA256)
-            self.targetInstSrc[ver] = f"qca-{ver}"
-
-        # latest stable version
-        self.defaultTarget = "2.3.7"
 
 
 class Package(CMakePackageBase):
