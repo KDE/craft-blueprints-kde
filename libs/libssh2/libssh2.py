77cdc5857e20bcf02f5b605751f77942db8b0f34
Ingo Klöcker <dev@ingo-kloecker.de>
2025-10-30 21:13:48 +0100

    Update patch for 1.11.1

--- libs/libssh2/libssh2.py
+++ libs/libssh2/libssh2.py
@@ -20,7 +20,7 @@ class subinfo(info.infoclass):
             self.patchToApply["1.11.0"] = [("libssh2-1.11.0-MSVC-libprefix.pc.diff", 1)]
             self.patchToApply["1.11.1"] = [("libssh2-1.11.1-20251027.diff", 1)]
         else:
-            self.patchToApply["1.11.1"] = [("libssh2-1.10.0-20221026.diff", 1)]  # don't let pkg-config search for ws2_32
+            self.patchToApply["1.11.1"] = [("libssh2-1.11.1-non-MSVC.diff", 1)]  # don't let pkg-config search for ws2_32
         self.patchLevel["1.10.0"] = 1
         self.patchLevel["1.11.1"] = 1
         self.webpage = "https://github.com/libssh2/libssh2"
