d490d90de084ed06e669074732d89b9e086a1cf6
Farid Abdelnour <farid@gunga.com.br>
2025-11-01 11:50:42 -0300

    Update to latest version

--- qt-libs/kddockwidgets/kddockwidgets.py
+++ qt-libs/kddockwidgets/kddockwidgets.py
@@ -10,13 +10,12 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets["master"] = "[git]https://github.com/KDAB/KDDockWidgets.git"
-        for ver in ["2.3.0"]:
+        for ver in ["2.4.0"]:
             self.targets[ver] = f"https://github.com/KDAB/KDDockWidgets/releases/download/v{ver}/kddockwidgets-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"KDDockWidgets-{ver}"
             self.archiveNames[ver] = f"KDDockWidgets-{ver}.tar.gz"
-        self.targetDigests["2.3.0"] = (["843baf9e1812c1ab82fd81d85b57cbc0d29bb43245efeb2539039780004b1056"], CraftHash.HashAlgorithm.SHA256)
-        self.svnTargets["1e25b35"] = "https://github.com/KDAB/KDDockWidgets.git||1e25b357bbc7e0485c172dde77c653fa30c26ba8"
-        self.defaultTarget = "1e25b35"
+        self.targetDigests["2.4.0"] = (["51dbf24fe72e43dd7cb9a289d3cab47112010f1a2ed69b6fc8ac0dff31991ed2"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "2.4.0"
 
         self.description = "KDDockWidgets is a Qt dock widget library written by KDAB, suitable for replacing QDockWidget and implementing advanced functionalities missing in Qt."
         self.webpage = "https://www.kdab.com/introducing-kddockwidgets/"
