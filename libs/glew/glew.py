7eb47a7789ad57702b820c98c20af6798f32bb38
Hannah von Reth <vonreth@kde.org>
2025-11-11 17:02:31 +0100

    glew ignore cmake version check

--- libs/glew/glew.py
+++ libs/glew/glew.py
@@ -15,6 +15,8 @@ class subinfo(info.infoclass):
 
         self.targetDigests["2.2.0"] = (["d4fc82893cfb00109578d0a1a2337fb8ca335b3ceccf97b97e5cc7f08e4353e1"], CraftHash.HashAlgorithm.SHA256)
 
+        self.releaseManagerId = 7878
+        self.webpage = "https://github.com/nigels-com/glew"
         self.defaultTarget = "2.2.0"
 
     def setDependencies(self):
@@ -24,3 +26,4 @@ class subinfo(info.infoclass):
 class Package(CMakePackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.subinfo.options.configure.args += ["-DCMAKE_POLICY_VERSION_MINIMUM=3.5"]
