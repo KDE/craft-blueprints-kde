1c7ad7f872320ebd1154cf1b2ffff24ee7e0e45e
Hannah von Reth <vonreth@kde.org>
2025-11-11 15:52:13 +0100

    deprecate pcre on all platforms but win, reduce and fix build

--- libs/pcre/pcre.py
+++ libs/pcre/pcre.py
@@ -5,11 +5,17 @@ from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
+    def registerOptions(self):
+        # this blueprint is deprecated and only used to provides a regex replacement for openldap
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.Windows
+
     def setTargets(self):
         for ver in ["8.45"]:
             self.targets[ver] = f"https://files.kde.org/craft/sources/libs/pcre/pcre-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"pcre-{ver}"
-        self.patchToApply["8.45"] = [("pcre-8.10-20101125.diff", 1)]
+        self.patchToApply["8.45"] = [
+            ("pcre-8.45_suppress_cmake_and_compiler_warnings-errors.patch", 1)
+        ]  # https://github.com/microsoft/vcpkg/blob/8fbf295ab5283313932f65a597a2c1f09a73fafd/ports/pcre/pcre-8.45_suppress_cmake_and_compiler_warnings-errors.patch
         self.targetDigests["8.45"] = (["4dae6fdcd2bb0bb6c37b5f97c33c2be954da743985369cddac3546e3218bffb8"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "Perl-Compatible Regular Expressions"
@@ -19,8 +25,6 @@ class subinfo(info.infoclass):
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
-        self.runtimeDependencies["libs/libbzip2"] = None
-        self.runtimeDependencies["libs/zlib"] = None
 
 
 class Package(CMakePackageBase):
@@ -31,6 +35,10 @@ class Package(CMakePackageBase):
             "-DPCRE_SUPPORT_UNICODE_PROPERTIES=ON",
             "-DPCRE_SUPPORT_UTF8=ON",
             "-DPCRE_EBCDIC=OFF",
+            "-DPCRE_BUILD_PCREGREP=OFF",
+            "-DPCRE_BUILD_TESTS=OFF",
+            "-DPCRE_SUPPORT_LIBBZ2=OFF",
+            "-DPCRE_SUPPORT_LIBZ=OFF",
+            "-DPCRE_SUPPORT_LIBEDIT=OFF",
+            "-DPCRE_SUPPORT_LIBREADLINE=OFF",
         ]
-        if CraftCore.compiler.isAndroid:
-            self.subinfo.options.configure.args += ["-DHAVE_STRTOQ=FALSE", "-DPCRE_BUILD_PCREGREP=FALSE", "-DPCRE_BUILD_TEST=FALSE"]
