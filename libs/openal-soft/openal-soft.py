9ad2b3912497b0e262245a4a1cfc5001e8e04268
Hannah von Reth <vonreth@kde.org>
2025-11-12 12:04:14 +0100

    openal-soft 1.24.3

--- libs/openal-soft/openal-soft.py
+++ libs/openal-soft/openal-soft.py
@@ -5,12 +5,16 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.21.1"]:
+        for ver in ["1.21.1", "1.24.3"]:
             self.targets[ver] = f"https://github.com/kcat/openal-soft/archive/refs/tags/{ver}.tar.gz"
             self.targetInstSrc[ver] = f"openal-soft-{ver}"
         self.targetDigests["1.21.1"] = (["8ac17e4e3b32c1af3d5508acfffb838640669b4274606b7892aa796ca9d7467f"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.24.3"] = (["7e1fecdeb45e7f78722b776c5cf30bd33934b961d7fd2a11e0494e064cc631ce"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.24.3"
+
+        self.releaseManagerId = 8172
+        self.webpage = "https://www.openal-soft.org/"
         self.description = "a library for audio support"
-        self.defaultTarget = "1.21.1"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
