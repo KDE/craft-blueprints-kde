f0a186d46cc18d18dbd345e8bb079a5bb51872bf
Laurent Montel <montel@kde.org>
2025-11-17 22:40:08 +0100

    Add kaichat-addons rules

---
+++ kde/unreleased/kaichat-addons/kaichat-addons.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2025 by Laurent Montel <montel@kde.org>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import info
+from Package.CMakePackageBase import CMakePackageBase
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues(gitUrl="https://invent.kde.org/utilities/kaichat-addons.git")
+        self.versionInfo.setDefaultValues()
+        self.description = "KAIChat Addons"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/libs/ktextaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/libs/kweathercore"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.subinfo.options.configure.args += ["-DUSE_UNITY_CMAKE_SUPPORT=ON"]
