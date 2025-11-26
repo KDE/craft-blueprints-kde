b9b90ac16c50f4062d44ff5cdf1601288c727afa
Hannah von Reth <vonreth@kde.org>
2025-11-26 16:58:13 +0100

    Fix lmdb name on linux

--- libs/lmdb/lmdb.py
+++ libs/lmdb/lmdb.py
@@ -10,9 +10,10 @@ class subinfo(info.infoclass):
         for ver in ["0.9.33"]:
             self.targets[ver] = f"https://git.openldap.org/openldap/openldap/-/archive/LMDB_{ver}/openldap-LMDB_{ver}.tar.gz"
             self.targetInstSrc[ver] = f"openldap-LMDB_{ver}/libraries/liblmdb"
-        self.patchToApply["0.9.33"] = [("lmdb-LMDB_0.9.16-20151004.diff", 3)]
+        self.patchToApply["0.9.33"] = [("lmdb-LMDB_0.9.16-20151004.diff", 3), ("lmdb-0.9.33-20251126.diff", 3)]
         self.targetDigests["0.9.33"] = (["476801f5239c88c7de61c3390502a5d13965ecedef80105b5fb0fcb8373d1e53"], CraftHash.HashAlgorithm.SHA256)
 
+        self.patchLevel["0.9.33"] = 1
         self.defaultTarget = "0.9.33"
 
         self.description = "in memory database from the openldap project"
