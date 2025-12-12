77d14aa93930dc7ece0c89dadc2acb1120a9a298
Jean-Baptiste Mardelle <jb@kdenlive.org>
2025-12-12 10:52:34 +0100

    Update libva to enable HW accel on recent processors

--- libs/_linux/libva/libva.py
+++ libs/_linux/libva/libva.py
@@ -5,14 +5,14 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2.12.0", "2.18.0", "2.20.0"]:
+        for ver in ["2.18.0", "2.20.0", "2.22.0"]:
             self.targets[ver] = f"https://github.com/intel/libva/archive/refs/tags/{ver}.tar.gz"
             self.targetInstSrc[ver] = "libva-" + ver
-        self.targetDigests["2.12.0"] = (["7bca8c8a854653e15e602f243e2452e84e4b454b26549bf80a932ab29d7d6b21"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.18.0"] = (["9d666c70c12dfefcdd27ae7dea771557f75e24961d0ed4cb050d96fb6136f438"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.20.0"] = (["117f8d658a5fc9ea406ca80a3eb4ae1d70b15a54807c9ed77199c812bed73b60"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["2.22.0"] = (["467c418c2640a178c6baad5be2e00d569842123763b80507721ab87eb7af8735"], CraftHash.HashAlgorithm.SHA256)
         self.description = "an implementation for VA-API (Video Acceleration API)"
-        self.defaultTarget = "2.20.0"
+        self.defaultTarget = "2.22.0"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
