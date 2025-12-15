cadec606339600fab5e20fe93c1d4ba26d499f82
Hannah von Reth <vonreth@kde.org>
2025-12-15 10:17:25 +0100

    Update nlohmmann-json

--- libs/nlohmann-json/nlohmann-json.py
+++ libs/nlohmann-json/nlohmann-json.py
@@ -5,14 +5,16 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["3.11.2"]:
+        for ver in ["3.11.2", "3.12.0"]:
             self.targets[ver] = f"https://github.com/nlohmann/json/releases/download/v{ver}/json.tar.xz"
+            self.archiveNames[ver] = f"json-{ver}.tar.xz"
             self.targetInstSrc[ver] = "json"
 
         self.targetDigests["3.11.2"] = (["8c4b26bf4b422252e13f332bc5e388ec0ab5c3443d24399acb675e68278d341f"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["3.12.0"] = (["42f6e95cad6ec532fd372391373363b62a14af6d771056dbfc86160e6dfff7aa"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "A library to display png images"
-        self.defaultTarget = "3.11.2"
+        self.defaultTarget = "3.12.0"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
