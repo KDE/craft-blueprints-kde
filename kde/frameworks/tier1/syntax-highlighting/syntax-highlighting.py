a499d095537e36d5b7e4feb64edd97b431e0ecbb
Volker Krause <vkrause@kde.org>
2025-12-17 12:14:31 +0100

    Fix host tools build on Android
    That fails if we accidentally force it to use the target compiler via
    CC/CXX environment variables. Not setting these on the other hand is fine,
    it'll find all that in the CMake toolchain file.

--- kde/frameworks/tier1/syntax-highlighting/syntax-highlighting.py
+++ kde/frameworks/tier1/syntax-highlighting/syntax-highlighting.py
@@ -1,5 +1,7 @@
 import info
+import utils
 from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
 
 
 class subinfo(info.infoclass):
@@ -19,3 +21,15 @@ class subinfo(info.infoclass):
 class Package(CraftPackageObject.get("kde/frameworks").pattern):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    def make(self):
+        if CraftCore.compiler.isAndroid:
+            # this does build a host tool, which fails if we force the compiler to the one of the target
+            # not setting CC/CXX OTOH is fine, it'll find everything it needs for the target in the CMake toolchain file
+            env = {}
+            env["CC"] = None
+            env["CXX"] = None
+            with utils.ScopedEnv(env):
+                return super().make()
+        else:
+            return super().make()
