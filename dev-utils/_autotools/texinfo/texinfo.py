a932e045fef5c92325f4d1ecd13a98e13366908e
Hannah von Reth <vonreth@kde.org>
2025-12-22 11:06:52 +0100

    texinfo add 7.2

--- dev-utils/_autotools/texinfo/texinfo.py
+++ dev-utils/_autotools/texinfo/texinfo.py
@@ -13,7 +13,9 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.description = "Texinfo is the official documentation format of the GNU project. It is used by many non-GNU projects as well."
-        for ver in ["6.4", "7.0.2"]:
+        self.releaseManagerId = 4958
+
+        for ver in ["6.4", "7.0.2", "7.2"]:
             self.targets[ver] = f"https://ftp.gnu.org/gnu/texinfo/texinfo-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"texinfo-{ver}"
         self.targetDigests["6.4"] = (["3714c129e37fe4b58fa3dc2f30b6b8c867de683a5cfe74e74e6a9f7c0e9a8e77"], CraftHash.HashAlgorithm.SHA256)
