fb9fc39561517c6e159269443a8b647b54a37e77
Laurent Montel <montel@kde.org>
2025-12-29 08:36:46 +0100

    [kaichat] add kuserfeedback dep

--- kde/unreleased/kaichat/kaichat.py
+++ kde/unreleased/kaichat/kaichat.py
@@ -32,6 +32,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kidletime"] = None
         self.runtimeDependencies["kde/frameworks/tier3/knotifications"] = None
         self.runtimeDependencies["kde/frameworks/tier3/knotifyconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kuserfeedback"] = None
         self.runtimeDependencies["kde/libs/ktextaddons"] = None
         self.runtimeDependencies["libs/kdsingleapplication"] = None
         if not CraftCore.compiler.isMacOS:
