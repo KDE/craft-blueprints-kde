e7e912b5571a19342627ece11852f38507f3437f
Laurent Montel <montel@kde.org>
2025-12-31 13:30:01 +0100

    [drawy] add drawy rule

---
+++ kde/unreleased/drawy/drawy.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2025 by Laurent Montel <montel@kde.org>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import info
+from Blueprints.CraftPackageObject import CraftPackageObject
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.displayName = "Drawy"
+        self.description = "Your handy, infinite, brainstorming tool!"
+
+        self.svnTargets["master"] = "https://invent.kde.org/graphics/drawy.git"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kcrash"] = None
+
+
+class Package(CraftPackageObject.get("kde").pattern):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def createPackage(self):
+        self.defines["shortcuts"] = [{"name": "Drawy", "target": "bin/drawy.exe", "description": self.subinfo.description, "appId": "drawy"}]
+        self.blacklist_file.append(self.blueprintDir() / "blacklist.txt")
+        self.defines["alias"] = "drawy"
+        return super().createPackage()
