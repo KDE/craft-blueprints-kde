185f952336d7a8dad63c1046110383942221ac91
Volker Krause <vkrause@kde.org>
2026-01-04 11:35:54 +0100

    Backport Kirigami Android icon bundling fix
    Fixes all current APK builds lacking icons.

--- kde/frameworks/tier1/kirigami/kirigami.py
+++ kde/frameworks/tier1/kirigami/kirigami.py
@@ -6,6 +6,9 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
+        self.patchToApply["6.21.0"] = [("fix-android-icon-bundling.patch", 1)]
+        self.patchLevel["6.21.0"] = 1
+
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
