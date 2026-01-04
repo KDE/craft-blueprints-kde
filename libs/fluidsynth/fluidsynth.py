73fb639f3565f2f7bf2d1211465d584c0566e078
Sandro Andrade <sandroandrade@kde.org>
2026-01-04 17:33:53 -0300

    Bump fluidsynth version to 2.5.1 and add a patch to not use mlock/munlock on Windows

--- libs/fluidsynth/fluidsynth.py
+++ libs/fluidsynth/fluidsynth.py
@@ -13,6 +13,7 @@ class subinfo(info.infoclass):
         self.targetDigests["2.3.5"] = (["f89e8e983ecfb4a5b4f5d8c2b9157ed18d15ed2e36246fa782f18abaea550e0d"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.5.1"] = (["10b2e32ba78c72ac1384965c66df06443a4bd0ab968dcafaf8fa17086001bf03"], CraftHash.HashAlgorithm.SHA256)
         self.patchToApply["2.1.6"] = [("fluidsynth-2.1.6-20210129.diff", 1)]
+        self.patchToApply["2.5.1"] = [("fluidsynth-2.5.1-20260104.diff", 1)]
 
         self.description = "FluidSynth is a real-time software synthesizer based on the SoundFont 2 specifications and has reached widespread distribution."
         self.webpage = "http://www.fluidsynth.org/"
