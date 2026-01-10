50ce1b566d51f7bd7f736fdeab318f5ebda8fffa
Stefan Gerlach <stefan.gerlach@uni-konstanz.de>
2026-01-10 13:53:25 +0100

    labplot: add system-python3

--- kde/applications/labplot/labplot.py
+++ kde/applications/labplot/labplot.py
@@ -101,7 +101,8 @@ class subinfo(info.infoclass):
             self.runtimeDependencies["libs/libpng"] = None
             self.buildDependencies["python-modules/build"] = None
         if not CraftCore.compiler.isWindows:
-            self.runtimeDependencies["libs/python"] = None
+            self.buildDependencies["libs/python"] = None
+            self.buildDependencies["dev-utils/system-python3"] = None
             self.runtimeDependencies["python-modules/pyside6"] = None
 
 
