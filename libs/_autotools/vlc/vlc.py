218b826a745632abe91efa223d00e0aadd899cb3
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2026-01-14 20:50:56 +0100

    [vlc] Update to 3.0.23 (fixes FTBFS)

--- libs/_autotools/vlc/vlc.py
+++ libs/_autotools/vlc/vlc.py
@@ -32,17 +32,17 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["3.0.19", "3.0.21"]:
+        for ver in ["3.0.21", "3.0.23"]:
             self.targets[ver] = f"http://get.videolan.org/vlc/{ver}/vlc-{ver}.tar.xz"
             self.targetInstSrc[ver] = f"vlc-{ver}"
-        self.targetDigests["3.0.19"] = (["643e3294bafe922324663ca499515b7564f2794575fd7d2b7992d20896381745"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["3.0.21"] = (["24dbbe1d7dfaeea0994d5def0bbde200177347136dbfe573f5b6a4cee25afbb0"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["3.0.23"] = (["e891cae6aa3ccda69bf94173d5105cbc55c7a7d9b1d21b9b21666e69eff3e7e0"], CraftHash.HashAlgorithm.SHA256)
         self.patchToApply["3.0.21"] = [
             ("5574.diff", 1),  # upstream in-progress MR to fix compilation with ffmpeg 7.x: https://code.videolan.org/videolan/vlc/-/merge_requests/5574/diffs; retrieved 2024-09-02
             ("6168.diff", 1),  # upstream MR to fix compilation with stricter compilers: https://code.videolan.org/videolan/vlc/-/merge_requests/6168; retrieved 2025-09-08
         ]
         self.description = "VLC is a free and open source cross-platform multimedia player and framework that plays most multimedia files as well as DVDs, Audio CDs, VCDs, and various streaming protocols."
-        self.defaultTarget = "3.0.21"
+        self.defaultTarget = "3.0.23"
 
     def setDependencies(self):
         self.buildDependencies["dev-utils/msys"] = None
