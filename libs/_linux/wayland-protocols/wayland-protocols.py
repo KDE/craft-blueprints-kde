f51cb0eee31f6138e0e707c7d81f86729fcfef62
Volker Krause <vkrause@kde.org>
2026-01-17 14:35:38 +0100

    Update wayland-protocols to 1.45
    Seems to be needed by KF 6.22.

--- libs/_linux/wayland-protocols/wayland-protocols.py
+++ libs/_linux/wayland-protocols/wayland-protocols.py
@@ -9,14 +9,14 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.39"]:
+        for ver in ["1.45"]:
             self.targets[ver] = f"https://invent.kde.org/mirrors/wayland-protocols/-/archive/{ver}/wayland-protocols-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"wayland-protocols-{ver}"
-        self.targetDigests["1.39"] = (["5778d7a99a7ef34d4ee2e4e2896236311fa49a3fb3dd01f358e508637cafe657"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.39"] = (["b73315c26d6c3374c19c2dff7e491f019d8597dc4dcd4473d9181f676f15f48f"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "wayland-protocols contains Wayland protocols that add functionality not available in the Wayland core protocol."
 
-        self.defaultTarget = "1.39"
+        self.defaultTarget = "1.45"
 
     def setDependencies(self):
         self.buildDependencies["python-modules/meson"] = None
