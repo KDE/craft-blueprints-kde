d5fe94440591eaffc7f6e4175a0cce02d1e3cabd
Stéphane MANKOWSKI <stephane@mankowski.fr>
2026-02-02 22:38:42 +0100

    Update skrooge script

--- extragear/skrooge/skrooge.py
+++ extragear/skrooge/skrooge.py
@@ -2,23 +2,20 @@ import info
 from Package.CMakePackageBase import CMakePackageBase
 from Utils import CraftHash
 
-
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.svnTargets["master"] = "https://anongit.kde.org/skrooge|master"
-        for ver in ["2.27.0"]:
-            self.targets[ver] = f"https://download.kde.org/stable/skrooge/skrooge-{ver}.tar.xz"
-            self.targetInstSrc[ver] = f"skrooge-{ver}"
-        self.targetDigests["2.27.0"] = (["c649745322440ce7983aaa977f7c2808331bf19c82d9ce428507431451116711"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "2.27.0"
+        self.svnTargets["master"] = "https://invent.kde.org/office/skrooge|master"
         self.description = "personal finance manager for KDE"
         self.displayName = "Skrooge"
+        self.defaultTarget = "master"
+
 
     def setDependencies(self):
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
-        self.runtimeDependencies["libs/qt/qtbase"] = None
-        self.runtimeDependencies["libs/qt/qtdeclarative"] = None
-        self.runtimeDependencies["libs/qt/qtsvg"] = None
+        self.runtimeDependencies["libs/qt6/qtbase"] = None
+        self.runtimeDependencies["libs/qt6/qtdeclarative"] = None
+        self.runtimeDependencies["libs/qt6/qtsvg"] = None
+        self.runtimeDependencies["libs/qt6/qtwebengine"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
@@ -27,7 +24,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/kwallet"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
         self.runtimeDependencies["kde/frameworks/tier3/knotifyconfig"] = None
-        self.runtimeDependencies["kdesupport/grantlee"] = None
         self.runtimeDependencies["kdesupport/qca"] = None
         self.runtimeDependencies["libs/sqlcipher"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
