3320724aa02b7831995e78ff875314a23486a4b1
Julius Künzel <julius.kuenzel@kde.org>
2026-02-03 22:35:34 +0100

    [knotifyconfig] Remove phonon dependency
    The dependency has been removed over a year ago, see frameworks/knotifyconfig!33

--- kde/frameworks/tier3/knotifyconfig/knotifyconfig.py
+++ kde/frameworks/tier3/knotifyconfig/knotifyconfig.py
@@ -22,7 +22,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kservice"] = None
-        self.runtimeDependencies["qt-libs/phonon"] = None
 
 
 class Package(CraftPackageObject.get("kde/frameworks").pattern):
