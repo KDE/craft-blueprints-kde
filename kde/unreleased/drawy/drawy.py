04f26da75c2741cd84ddb788bea74c13921c4988
Laurent Montel <montel@kde.org>
2026-02-11 07:51:13 +0100

    [drawy] add new dependancy : i18n

--- kde/unreleased/drawy/drawy.py
+++ kde/unreleased/drawy/drawy.py
@@ -22,6 +22,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
         self.runtimeDependencies["kde/plasma/breeze"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
 
 
 class Package(CraftPackageObject.get("kde").pattern):
