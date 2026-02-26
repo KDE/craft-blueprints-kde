6284eb6e4aee010353c7e5d743c72170086d2347
Stéphane MANKOWSKI <stephane@mankowski.fr>
2026-02-26 18:32:06 +0100

    [sqlcipher] Update to version 4.13.0

--- libs/sqlcipher/sqlcipher.py
+++ libs/sqlcipher/sqlcipher.py
@@ -66,6 +66,7 @@ class PackageAutotools(AutoToolsPackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.subinfo.options.configure.args += ["--with-tempstore=yes"]
+        self.subinfo.options.configure.noCacheFile = True
         if CraftCore.compiler.isMinGW():
             self.subinfo.options.make.supportsMultijob = False
             self.subinfo.options.configure.args += ["CFLAGS='-DSQLITE_HAS_CODEC'"]
