ff9435417086c07ffe3ffe1d10c1a991d6a2f4c4
Albert Vaca Cintora <albertvaka@gmail.com>
2026-03-08 16:05:27 +0100

    Add kdominate

---
+++ kde/kdegames/kdominate/kdominate.py
@@ -0,0 +1,35 @@
+import info
+from Package.CMakePackageBase import CMakePackageBase
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.versionInfo.setDefaultValues()
+        self.description = "KDominate"
+
+    def setDependencies(self):
+        self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
+        self.runtimeDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kcrash"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kdbusaddons"] = None
+        self.runtimeDependencies["kde/frameworks/tier2/kdoctools"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
+        self.runtimeDependencies["kde/kdegames/libkdegames"] = None
+        #self.runtimeDependencies["kde/plasma/breeze"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
+
+
+class Package(CMakePackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def createPackage(self):
+        self.blacklist_file.append(self.blueprintDir() / "blacklist.txt")
+        self.defines["alias"] = "kdominate"
+        self.defines["shortcuts"] = [{"name": "KDominate", "target": "bin/kdominate.exe", "description": self.subinfo.description}]
+        return super().createPackage()
