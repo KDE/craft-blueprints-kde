146d2f784cb9b268905c555a00b5a32fe7962179
Jean-Baptiste Mardelle <jb@kdenlive.org>
2026-03-18 17:22:58 +0100

    Add missing deps for 6.24

--- kde/frameworks/tier3/purpose/purpose.py
+++ kde/frameworks/tier3/purpose/purpose.py
@@ -17,7 +17,10 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kitemmodels"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/prison"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kcmutils"] = None
 
 
 class Package(CraftPackageObject.get("kde/frameworks").pattern):
