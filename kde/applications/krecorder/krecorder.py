d0163aa11389eef0ea38fc59624c3480fa55bc8a
krebs geo <redbeana44945@gmail.com>
2026-03-25 10:11:38 +0800

    Add new dependency for krecorder.

--- kde/applications/krecorder/krecorder.py
+++ kde/applications/krecorder/krecorder.py
@@ -19,6 +19,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kconfig"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kwindowsystem"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
         self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None
