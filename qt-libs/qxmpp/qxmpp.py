ed7e4e988973c3a99ce1576befd5bea6633cba5a
Linus Jahn <lnj@kaidan.im>
2026-03-29 22:15:15 +0200

    Update qxmpp to 1.14.5

--- qt-libs/qxmpp/qxmpp.py
+++ qt-libs/qxmpp/qxmpp.py
@@ -16,12 +16,12 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets["master"] = "https://invent.kde.org/libraries/qxmpp.git"
-        for ver in ["1.14.4"]:
+        for ver in ["1.14.5"]:
             self.targets[ver] = f"https://download.kde.org/unstable/qxmpp/qxmpp-{ver}.tar.xz"
             self.archiveNames[ver] = f"qxmpp-v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"qxmpp-{ver}"
-        self.targetDigests["1.14.4"] = (["06a3a484eccbb9488d66b75d41b8afffcc9741304f1a96b930e39dc8b4ef6243"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "1.14.4"
+        self.targetDigests["1.14.5"] = (["fcf809743cf9bf10884d700ad7d47e4d3e7063bdda30f6341d96f88e93809576"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "1.14.5"
 
 
 class Package(CMakePackageBase):
