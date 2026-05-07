ba63dead244f5ca42cbb6faba0f585984d84acdb
Sandro S. Andrade <sandroandrade@kde.org>
2026-05-07 11:35:05 -0300

    Downgrade default fluidsynth version to 2.5.3

--- libs/fluidsynth/fluidsynth.py
+++ libs/fluidsynth/fluidsynth.py
@@ -6,22 +6,24 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["2.1.6", "2.2.3", "2.3.5", "2.5.1", "2.5.4"]:
+        for ver in ["2.1.6", "2.2.3", "2.3.5", "2.5.1", "2.5.3", "2.5.4"]:
             self.targets[ver] = f"https://github.com/FluidSynth/fluidsynth/archive/v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"fluidsynth-{ver}"
         self.targetDigests["2.1.6"] = (["328fc290b5358544d8dea573f81cb1e97806bdf49e8507db067621242f3f0b8a"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.2.3"] = (["b31807cb0f88e97f3096e2b378c9815a6acfdc20b0b14f97936d905b536965c4"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.3.5"] = (["f89e8e983ecfb4a5b4f5d8c2b9157ed18d15ed2e36246fa782f18abaea550e0d"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.5.1"] = (["10b2e32ba78c72ac1384965c66df06443a4bd0ab968dcafaf8fa17086001bf03"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["2.5.3"] = (["6f247edfb4b91b927efc68c8884cec2ec345c8007afe6b59558cc52a67ef2517"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["2.5.4"] = (["72f5720328fe44e2e5c67813885f0a6b4b004d048bd2eeeb0c0064074ebff530"], CraftHash.HashAlgorithm.SHA256)
         self.patchToApply["2.1.6"] = [("fluidsynth-2.1.6-20210129.diff", 1)]
         self.patchToApply["2.5.1"] = [("fluidsynth-2.5.1-20260104.diff", 1)]
+        self.patchToApply["2.5.3"] = [("fluidsynth-2.5.1-20260104.diff", 1)]
         self.patchToApply["2.5.4"] = [("fluidsynth-2.5.1-20260104.diff", 1)]
 
         self.description = "FluidSynth is a real-time software synthesizer based on the SoundFont 2 specifications and has reached widespread distribution."
         self.webpage = "http://www.fluidsynth.org/"
         self.releaseManagerId = 10437
-        self.defaultTarget = "2.5.4"
+        self.defaultTarget = "2.5.3"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
