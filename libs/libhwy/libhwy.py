83095bc9b9eadd3ce796370191e730e822c6b488
Daniel Novomeský <dnovomesky@gmail.com>
2026-05-08 15:47:08 +0200

    [libhwy] 1.4.0 version

--- libs/libhwy/libhwy.py
+++ libs/libhwy/libhwy.py
@@ -8,12 +8,13 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.2.0"]:
+        for ver in ["1.2.0", "1.4.0"]:
             self.targets[ver] = f"https://github.com/google/highway/releases/download/{ver}/highway-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"highway-{ver}"
         self.targetDigests["1.2.0"] = (["58e9d5d41d6573ad15245ad76aec53a69499ca7480c092d899c4424812ed906f"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.4.0"] = (["36f672ab48ddb3c8555e9e89e16fe400cd7d16c6eb455a1a3d0c146a63ababdc"], CraftHash.HashAlgorithm.SHA256)
         self.description = "Performance-portable, length-agnostic SIMD with runtime dispatch"
-        self.defaultTarget = "1.2.0"
+        self.defaultTarget = "1.4.0"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
