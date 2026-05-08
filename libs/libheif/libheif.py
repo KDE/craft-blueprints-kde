1ab47642ec86f156f12e661cceeece47f250d9de
Daniel Novomeský <dnovomesky@gmail.com>
2026-05-08 15:12:37 +0200

    [libheif] 1.21.2 version

--- libs/libheif/libheif.py
+++ libs/libheif/libheif.py
@@ -8,13 +8,13 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.19.8"]:
+        for ver in ["1.21.2"]:
             self.targets[ver] = f"https://github.com/strukturag/libheif/releases/download/v{ver}/libheif-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"libheif-{ver}"
-        self.targetDigests["1.19.8"] = (["6c4a5b08e6eae66d199977468859dea3b5e059081db8928f7c7c16e53836c906"], CraftHash.HashAlgorithm.SHA256)
-        self.patchToApply["1.19.8"] = [("libheif-1.18.2-20250415.diff", 1)]
+        self.targetDigests["1.21.2"] = (["75f530b7154bc93e7ecf846edfc0416bf5f490612de8c45983c36385aa742b42"], CraftHash.HashAlgorithm.SHA256)
+        self.patchToApply["1.21.2"] = [("libheif-1.18.2-20250415.diff", 1)]
         self.description = "libheif is an HEIF and AVIF file format decoder and encoder"
-        self.defaultTarget = "1.19.8"
+        self.defaultTarget = "1.21.2"
 
     def setDependencies(self):
         self.runtimeDependencies["libs/ffmpeg"] = None
@@ -27,6 +27,7 @@ class Package(CMakePackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.subinfo.options.configure.args += [
+            "-DBUILD_DOCUMENTATION=OFF",
             "-DENABLE_PLUGIN_LOADING=OFF",
             "-DWITH_LIBSHARPYUV=OFF",
             "-DWITH_EXAMPLES=OFF",
@@ -38,4 +39,6 @@ class Package(CMakePackageBase):
             "-DWITH_JPEG_DECODER=ON",
             "-DWITH_OpenJPEG_DECODER=ON",
             "-DWITH_FFMPEG_DECODER=ON",
+            "-DWITH_OpenH264_DECODER=OFF",
+            "-DWITH_X264=OFF",
         ]
