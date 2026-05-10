932aca7adf0c9e1d2218d13943cdac2b38b8e016
Volker Krause <vkrause@kde.org>
2026-05-10 20:16:36 +0200

    Update Wayland Protocols to 1.48
    KF 6.26 needs at least 1.46.

--- libs/_linux/wayland-protocols/wayland-protocols.py
+++ libs/_linux/wayland-protocols/wayland-protocols.py
@@ -4,19 +4,17 @@ import info
 import utils
 from CraftCore import CraftCore
 from Package.MesonPackageBase import MesonPackageBase
-from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.45"]:
+        for ver in ["1.48"]:
             self.targets[ver] = f"https://invent.kde.org/mirrors/wayland-protocols/-/archive/{ver}/wayland-protocols-{ver}.tar.bz2"
             self.targetInstSrc[ver] = f"wayland-protocols-{ver}"
-        self.targetDigests["1.39"] = (["b73315c26d6c3374c19c2dff7e491f019d8597dc4dcd4473d9181f676f15f48f"], CraftHash.HashAlgorithm.SHA256)
 
         self.description = "wayland-protocols contains Wayland protocols that add functionality not available in the Wayland core protocol."
 
-        self.defaultTarget = "1.45"
+        self.defaultTarget = "1.48"
 
     def setDependencies(self):
         self.buildDependencies["python-modules/meson"] = None
