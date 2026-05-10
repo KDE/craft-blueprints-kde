eafcbc2d2b70a445b392a159d35f2b7e4e92eae9
Julius Künzel <julius.kuenzel@kde.org>
2026-05-10 23:42:23 +0200

    [spatialaudio] Add patch to fix linking on MSVC
    Also add few versions and a note why we use a specific commit

--- libs/spatialaudio/spatialaudio.py
+++ libs/spatialaudio/spatialaudio.py
@@ -4,6 +4,7 @@
 
 import info
 from Package.CMakePackageBase import CMakePackageBase
+from Utils import CraftHash
 
 
 class subinfo(info.infoclass):
@@ -11,9 +12,23 @@ class subinfo(info.infoclass):
         self.description = "Ambisonic encoding / decoding and binauralization library in C++ "
         self.webpage = "https://github.com/videolabs/libspatialaudio"
 
+        for ver in ["0.3.0", "0.4.0"]:
+            self.targets[ver] = f"https://github.com/videolan/libspatialaudio/releases/download/{ver}/libspatialaudio-{ver}.tar.xz"
+            self.targetInstSrc[ver] = "libspatialaudio-" + ver
+        self.targetDigests["0.4.0"] = (["79f00d6f2695844764604897f704f4520440e1c9e63ccb2aff482717f66b6187"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["0.4.0"] = (["79f00d6f2695844764604897f704f4520440e1c9e63ccb2aff482717f66b6187"], CraftHash.HashAlgorithm.SHA256)
+
         self.svnTargets["master"] = "https://github.com/videolabs/libspatialaudio.git"
         self.patchLevel["master"] = 20240907
+
         self.svnTargets["0b6b25e"] = "https://github.com/videolabs/libspatialaudio.git||0b6b25eba39fe1d2f4a981867957b9dcf62016db"
+        # https://github.com/videolan/libspatialaudio/pull/91
+        self.patchToApply["0b6b25e"] = [("fix-pkgconfig-file-msvc.patch", 1)]
+        self.patchLevel["0b6b25e"] = 1
+
+        self.svnTargets["5dda58e"] = "https://github.com/videolabs/libspatialaudio.git||5dda58ee0bee3ec808d01b044dcf7392b7d30c3d"
+
+        # Version 0.4.0 contains breaking changes and MLT is not yet compatible with it
         self.defaultTarget = "0b6b25e"
 
 
