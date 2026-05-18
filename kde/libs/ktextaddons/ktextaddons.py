1e7a2e637e03746d31b51b6ba0f90bd034ce6500
Laurent Montel <montel@kde.org>
2026-05-18 07:33:44 +0200

    [ktextaddons] add unity support

--- kde/libs/ktextaddons/ktextaddons.py
+++ kde/libs/ktextaddons/ktextaddons.py
@@ -43,3 +43,4 @@ class subinfo(info.infoclass):
 class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.subinfo.options.configure.args += ["-DUSE_UNITY_CMAKE_SUPPORT=ON"]
