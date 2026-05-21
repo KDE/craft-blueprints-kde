afdf83be756bbb3f4eb4a54484613c95b4113993
Jean-Baptiste Mardelle <jb@kdenlive.org>
2026-05-21 16:10:56 +0200

    Add rnnoise audio denoiser

---
+++ libs/_autotools/rnnoise/rnnoise.py
@@ -0,0 +1,20 @@
+import info
+from CraftCore import CraftCore
+from Package.AutoToolsPackageBase import AutoToolsPackageBase
+from Utils import CraftHash
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets["0.2"] = "https://github.com/xiph/rnnoise.git||v0.2"
+        self.defaultTarget = "0.2"
+        self.description = "A noise suppression library based on a recurrent neural network"
+        self.webpage = "https://github.com/xiph/rnnoise"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = None
+
+class Package(AutoToolsPackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.subinfo.options.configure.args += ["--enable-shared", "--disable-static"]
