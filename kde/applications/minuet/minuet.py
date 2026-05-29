fe3825c499cfe78aa684645100ce7f145287f947
Sandro S. Andrade <sandroandrade@kde.org>
2026-05-29 15:29:04 -0300

    Add shortcuts packaging config for Minuet's Windows .appxupload generation

--- kde/applications/minuet/minuet.py
+++ kde/applications/minuet/minuet.py
@@ -33,3 +33,6 @@ class subinfo(info.infoclass):
 class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.defines["shortcuts"] = [
+            {"name": "Minuet", "target": "bin/minuet.exe", "description": self.subinfo.description}
+        ]
