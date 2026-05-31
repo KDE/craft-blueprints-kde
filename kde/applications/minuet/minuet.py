0b550615ae99c8d642973d86ed8b63bc8ad01937
Sandro S. Andrade <sandro.andrade@gmail.com>
2026-05-31 13:23:21 -0300

    Fix minuet packaging for macOS

--- kde/applications/minuet/minuet.py
+++ kde/applications/minuet/minuet.py
@@ -1,4 +1,5 @@
 import info
+import utils
 from Blueprints.CraftPackageObject import CraftPackageObject
 from CraftCore import CraftCore
 
@@ -38,5 +39,40 @@ class Package(CraftPackageObject.get("kde").pattern):
     def createPackage(self):
         if CraftCore.compiler.isMacOS:
             self.blacklist_file.append(self.blueprintDir() / "blacklist_macos.txt")
+            self.addExecutableFilter(r"(bin|libexec)/(?!(minuet|minuet-bin)).*")
 
         return super().createPackage()
+
+    def preArchive(self):
+        if CraftCore.compiler.isMacOS:
+            archiveDir = self.archiveDir()
+            fluidsynthFramework = archiveDir / "Library/Frameworks/FluidSynth.framework"
+            if fluidsynthFramework.exists():
+                if not utils.createDir(archiveDir / "lib"):
+                    return False
+                if not utils.moveFile(fluidsynthFramework, archiveDir / "lib"):
+                    return False
+
+            for launcher in (
+                archiveDir / "bin/minuet",
+                archiveDir / "Applications/KDE/minuet.app/Contents/MacOS/minuet",
+            ):
+                realExecutable = launcher.with_name("minuet-bin")
+                if launcher.exists() and not realExecutable.exists():
+                    if not utils.moveFile(launcher, realExecutable):
+                        return False
+                if realExecutable.exists():
+                    launcher.write_text(
+                        """#!/bin/sh
+unset QT_PLUGIN_PATH
+unset QML_IMPORT_PATH
+unset QML2_IMPORT_PATH
+
+SELF_DIR=$(CDPATH= cd "$(dirname "$0")" && pwd)
+exec "$SELF_DIR/minuet-bin" "$@"
+""",
+                        encoding="UTF-8",
+                    )
+                    launcher.chmod(0o755)
+
+        return super().preArchive()
