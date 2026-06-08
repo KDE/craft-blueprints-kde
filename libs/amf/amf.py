5be7271ed0acbe58eb9aa589a4ec495a92338f40
Julius Künzel <julius.kuenzel@kde.org>
2026-06-08 18:12:20 +0200

    [amf] Fix hash for tarball

--- libs/amf/amf.py
+++ libs/amf/amf.py
@@ -11,7 +11,7 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://github.com/GPUOpen-LibrariesAndSDKs/AMF/archive/refs/tags/v{ver}.tar.gz"
             self.targetInstSrc[ver] = "AMF-" + ver
         self.targetDigests["1.4.36"] = (["240a42033babc7920e5476506d5ac0c5628f67908833168e746406808d0ef146"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["1.5.2"] = (["d3c12eb324edf05e214608b6a395a51dd95770ed9d45520185d6c3a206811c99"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.5.2"] = (["8a70b6dc85261e6e6e57769bd81ac1e09c0a4c96bbd5e358ffbc2dee51e8e50a"], CraftHash.HashAlgorithm.SHA256)
         self.webpage = "https://github.com/GPUOpen-LibrariesAndSDKs/AMF"
         self.defaultTarget = "1.5.2"
 
