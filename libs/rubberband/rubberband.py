6a28ee37f0b3d4ae79cd4e4c7f7be8f4bcaf13bb
Julius Künzel <julius.kuenzel@kde.org>
2026-06-09 13:25:50 +0200

    [rubberband] Try to fix macOS build

--- libs/rubberband/rubberband.py
+++ libs/rubberband/rubberband.py
@@ -5,14 +5,18 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
+        self.description = "An audio time-stretching and pitch-shifting library and utility program"
+        self.webpage = "https://breakfastquay.com/rubberband/"
+
         for ver in ["3.1.3", "4.0.0"]:
             self.targets[ver] = f"https://github.com/breakfastquay/rubberband/archive/v{ver}.tar.gz"
             self.targetInstSrc[ver] = f"rubberband-{ver}"
-            self.patchToApply[ver] = ("fftw3-linking.patch", 0)
+            self.patchToApply[ver] = [("fftw3-linking.patch", 0), ("fix-undefined-size_t.diff", 1)]
         self.targetDigests["4.0.0"] = (["24300f48a8014b7c863b573a9647e61b1b19b37875e2cdd92005e64c6424d266"], CraftHash.HashAlgorithm.SHA256)
         self.targetDigests["3.1.3"] = (["85f7fde23383a94c38955c3bb3fc29f59d2c0a1cad4aa31da539a92a3b5621db"], CraftHash.HashAlgorithm.SHA256)
-        self.description = "An audio time-stretching and pitch-shifting library and utility program"
-        self.webpage = "https://breakfastquay.com/rubberband/"
+
+        self.patchLevel["4.0.0"] = 1
+
         self.defaultTarget = "4.0.0"
 
     def setDependencies(self):
