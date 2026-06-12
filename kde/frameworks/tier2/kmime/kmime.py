c39143b0c76f04695bad7a426f871fd2e98a1ed9
Volker Krause <vkrause@kde.org>
2026-06-12 16:05:03 +0200

    Update to KDE Frameworks 6.27
    Also add KMime in Frameworks and enables KCrash on Android (downstream
    dependency adjustments to follow for both).

--- kde/frameworks/tier2/kmime/kmime.py
+++ kde/frameworks/tier2/kmime/kmime.py
@@ -6,13 +6,13 @@ class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
 
-        self.description = "KCrash"
+        self.description = "KMime library"
 
     def setDependencies(self):
-        self.buildDependencies["virtual/base"] = None
+        self.runtimeDependencies["virtual/base"] = None
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
-        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["libs/qt/qtbase"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcodecs"] = None
 
 
 class Package(CraftPackageObject.get("kde/frameworks").pattern):
