99a5e6db726482b73b7aa4cb28dc32fd30285ea3
Julius Künzel <julius.kuenzel@kde.org>
2026-06-15 13:47:40 +0200

    [qtgraphs] declare dependency on qtshadertools

--- libs/qt6/qtgraphs/qtgraphs.py
+++ libs/qt6/qtgraphs/qtgraphs.py
@@ -10,6 +10,7 @@ class subinfo(info.infoclass):
 
     def setDependencies(self):
         self.runtimeDependencies["libs/qt6/qtbase"] = None
+        self.runtimeDependencies["libs/qt6/qtshadertools"] = None
 
 
 class Package(CraftPackageObject.get("libs/qt6").pattern):
