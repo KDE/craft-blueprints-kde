68dd8fc915aaddf1e86925639889ec466bd03030
Julius Künzel <julius.kuenzel@kde.org>
2026-06-20 12:27:07 +0200

    [qtwebengine] Enable another patch that is still required for 6.12.0-beta1

--- libs/qt6/qtwebengine/qtwebengine.py
+++ libs/qt6/qtwebengine/qtwebengine.py
@@ -29,6 +29,7 @@ class subinfo(info.infoclass):
         self.patchToApply["6.9.1"] += [(".6.9.1", 1)]
         self.patchToApply["6.9.2"] += [(".6.9.2", 1)]
         self.patchToApply["6.11.1"] += [(".6.11.1", 1)]
+        self.patchToApply["6.12.0-beta1"] += [(".6.11.1", 1)]
         self.patchLevel["6.7.0"] = 3
 
         for ver in ["6.6.1", "6.6.2"]:
