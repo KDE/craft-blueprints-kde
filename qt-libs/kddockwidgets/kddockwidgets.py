108519e0b810383f315eb4876ec2c95b39ae7a22
Jean-Baptiste Mardelle <jb@kdenlive.org>
2026-08-14 10:43:43 +0200

    Update KDDockWidgets, drop custom tabbar patch, fixed by in app implementation, see https://github.com/KDAB/KDDockWidgets/issues/635

--- qt-libs/kddockwidgets/kddockwidgets.py
+++ qt-libs/kddockwidgets/kddockwidgets.py
@@ -10,15 +10,12 @@ class subinfo(info.infoclass):
 
     def setTargets(self):
         self.svnTargets["master"] = "[git]https://github.com/KDAB/KDDockWidgets.git"
-        for ver in ["2.4.0"]:
+        for ver in ["2.4.0", "2.4.1"]:
             self.targets[ver] = f"https://github.com/KDAB/KDDockWidgets/releases/download/v{ver}/kddockwidgets-{ver}.tar.gz"
             self.targetInstSrc[ver] = f"KDDockWidgets-{ver}"
             self.archiveNames[ver] = f"KDDockWidgets-{ver}.tar.gz"
-        self.targetDigests["2.4.0"] = (["51dbf24fe72e43dd7cb9a289d3cab47112010f1a2ed69b6fc8ac0dff31991ed2"], CraftHash.HashAlgorithm.SHA256)
-        self.defaultTarget = "2.4.0"
-
-        self.patchToApply["2.4.0"] = [("qtabbar_style.diff", 1)]
-        self.patchLevel["2.4.0"] = 2
+        self.targetDigests["2.4.1"] = (["5cd9495d9316cf6cc937bb328a7fd491c3248ef2469cfa42511f1039f6148aca"], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "2.4.1"
 
         self.description = "KDDockWidgets is a Qt dock widget library written by KDAB, suitable for replacing QDockWidget and implementing advanced functionalities missing in Qt."
         self.webpage = "https://www.kdab.com/introducing-kddockwidgets/"
