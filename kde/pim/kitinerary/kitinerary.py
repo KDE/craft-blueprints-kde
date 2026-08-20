c3bf3cb5c753dcc62a1734865337b705329958a4
Volker Krause <vkrause@kde.org>
2026-08-20 17:13:30 +0200

    Update to KDE Gear 26.08.0

--- kde/pim/kitinerary/kitinerary.py
+++ kde/pim/kitinerary/kitinerary.py
@@ -14,8 +14,6 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["libs/qt/qtbase"] = None
         self.runtimeDependencies["kde/frameworks/tier1/karchive"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kmime"] = None
-        # remove after 26.04
-        self.runtimeDependencies["kde/pim/kmime"] = None
         self.runtimeDependencies["kde/pim/kpkpass"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kcalendarcore"] = None
         self.runtimeDependencies["kde/frameworks/tier2/kcontacts"] = None
