f3bab47c3042bd862d908c6a560b67aca3c9f935
Manuel Alcaraz Zambrano <manuel@alcarazzam.dev>
2026-08-25 22:30:56 +0200

    chessament: Add kirigami-app-components dependency

--- kde/kdegames/chessament/chessament.py
+++ kde/kdegames/chessament/chessament.py
@@ -34,6 +34,7 @@ class subinfo(info.infoclass):
         self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
         self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
         self.runtimeDependencies["kde/frameworks/tier3/qqc2-desktop-style"] = None
+        self.runtimeDependencies["kde/libs/kirigami-app-components"] = None
         self.runtimeDependencies["kde/plasma/breeze"] = None
         self.runtimeDependencies["kde/unreleased/kirigami-addons"] = None
 
