a1f89112e41efee0ff3dcff569a056486a3317bf
Michael Reeves <reeves.87@gmail.com>
2026-09-08 11:41:18 -0400

    [kdiff3] Suppress policy warnings from KDE internal files under craft.

--- extragear/kdiff3/kdiff3.py
+++ extragear/kdiff3/kdiff3.py
@@ -27,7 +27,8 @@ class subinfo(info.infoclass):
 class Package(CraftPackageObject.get("kde").pattern):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.subinfo.options.configure.args += ["-DBUILD_WITH_QT6=ON"]
+        # Use -Wno-dev to suppress policys warnings from KDE internal files in craft setups
+        self.subinfo.options.configure.args += ["-Wno-dev","-DBUILD_WITH_QT6=ON"]
 
     def createPackage(self):
         self.addExecutableFilter(r"bin/(?!(kdiff3|kbuildsycoca5|update-mime-database|kioworker|QtWebEngineProcess)).*")
