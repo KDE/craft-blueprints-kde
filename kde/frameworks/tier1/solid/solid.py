f447b3b64feae6b1c6e6b1d1473f53c63189fe76
Jean-Baptiste Mardelle <jb@kdenlive.org>
2026-09-10 13:55:06 +0200

    Fix Solid crash on MacOS
    See https://invent.kde.org/frameworks/solid/-/merge_requests/266
    
    BUG: 521270

--- kde/frameworks/tier1/solid/solid.py
+++ kde/frameworks/tier1/solid/solid.py
@@ -1,11 +1,17 @@
 import info
 from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
 
 
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
         self.description = "Solid"
+        if CraftCore.compiler.isMacOS:
+            # patch will be upstreamed in 6.31.0, fixes BUG 521270
+            for ver in ["6.29.0", "6.30.0"]:
+                self.patchToApply[ver] = [("mac_cfrelease.patch", 1)]
+            self.patchLevel["6.29.0"] = 1
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
