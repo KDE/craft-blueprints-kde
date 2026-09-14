a8fe5bd8560c69f53a7c24442552cfdc96cf2419
Hannah von Reth <vonreth@kde.org>
2026-09-14 10:57:43 +0200

    Add qtopenapi and deps

---
+++ dev-utils/jdk/jdk.py
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2026 Hannah von Reth <vonreth@kde.org>
+import urllib
+from pathlib import Path
+from urllib import parse
+
+import info
+from CraftCore import CraftCore
+from Package.BinaryPackageBase import BinaryPackageBase
+from Utils import CraftHash
+
+
+class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NotAndroid
+
+    def setDependencies( self ):
+        self.buildDependencies["virtual/base"] = None
+
+    def setTargets(self):
+        for ver in ["25.0.4.1+1"]:
+            urlVer = urllib.parse.quote(ver)
+            tarVer = ver.replace("+", "_")
+            self.targetInstSrc[ver] = f"jdk-{ver}"
+            if CraftCore.compiler.isWindows:
+                self.targets[ver] = f"https://github.com/adoptium/temurin25-binaries/releases/download/jdk-{urlVer}/OpenJDK25U-jdk_x64_windows_hotspot_{tarVer}.zip"
+                self.targetDigestUrls[ver] = ([f"https://github.com/adoptium/temurin25-binaries/releases/download/jdk-{urlVer}/OpenJDK25U-jdk_x64_windows_hotspot_{tarVer}.zip.sha256.txt"], CraftHash.HashAlgorithm.SHA256)
+            elif CraftCore.compiler.isMacOS:
+                self.targets[ver] = f"https://github.com/adoptium/temurin25-binaries/releases/download/jdk-{urlVer}/OpenJDK25U-jdk_{CraftCore.compiler.appImageArchitecture}_mac_hotspot_{tarVer}.tar.gz"
+                self.targetDigestUrls[ver] = ([f"https://github.com/adoptium/temurin25-binaries/releases/download/jdk-{urlVer}/OpenJDK25U-jdk_{CraftCore.compiler.appImageArchitecture}_mac_hotspot_{tarVer}.tar.gz.sha256.txt"], CraftHash.HashAlgorithm.SHA256)
+            elif CraftCore.compiler.isLinux:
+                self.targets[ver] = f"https://github.com/adoptium/temurin25-binaries/releases/download/jdk-{urlVer}/OpenJDK25U-jdk_{CraftCore.compiler.appImageArchitecture}_linux_hotspot_{tarVer}.tar.gz"
+                self.targetDigestUrls[ver] = ([f"https://github.com/adoptium/temurin25-binaries/releases/download/jdk-{urlVer}/OpenJDK25U-jdk_{CraftCore.compiler.appImageArchitecture}_linux_hotspot_{tarVer}.tar.gz.sha256.txt"], CraftHash.HashAlgorithm.SHA256)
+            else:
+                raise NotImplementedError()
+            self.targetInstallPath[ver] = "dev-utils/jdk"
+            self.defaultTarget = "25.0.4.1+1"
+        self.description = "Java Runtime Environment"
+
+class Package(BinaryPackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+    @property
+    def JAVA_HOME(self) -> Path:
+        if CraftCore.compiler.isMacOS:
+            return self.installPrefix() / "Contents/Home"
+        return self.installPrefix()
---
+++ dev-utils/openapi-generator-cli/openapi-generator-cli.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2026 Hannah von Reth <vonreth@kde.org>
+import info
+import utils
+from Blueprints.CraftPackageObject import CraftPackageObject
+from CraftCore import CraftCore
+from Package.BinaryPackageBase import BinaryPackageBase
+from Utils import CraftHash
+
+
+class subinfo(info.infoclass):
+    def registerOptions(self):
+        self.parent.package.categoryInfo.platforms = CraftCore.compiler.Platforms.NotAndroid
+
+    def setDependencies( self ):
+        self.runtimeDependencies["dev-utils/jdk"] = None
+
+    def setTargets(self):
+        self.targets["7.25.0"] = "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.25.0/openapi-generator-cli-7.25.0.jar"
+        self.targetDigests["7.25.0"] = (['41ce4f6b07f196676439d710759fa1ced7a08066d06ff1bf314681470289efae'], CraftHash.HashAlgorithm.SHA256)
+        self.defaultTarget = "7.25.0"
+        self.targetInstallPath["7.25.0"] = "dev-utils/bin"
+        self.description = "OpenAPI Generator CLI is a command-line tool for generating code from OpenAPI specifications."
+
+
+class Package(BinaryPackageBase):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+    def postInstall(self):
+        return utils.createShim(self.installDir() / "openapi-generator-cli", CraftPackageObject.get('dev-utils/jdk').instance.JAVA_HOME / "bin/java",["-jar", self.installDir() /self.subinfo.archiveName()[0]], useAbsolutePath=True,
+                                env={"JAVA_HOME": CraftPackageObject.get("dev-utils/jdk").instance.JAVA_HOME})
