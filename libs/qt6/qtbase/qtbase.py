83f1372d4b871f64ec7524a9ea409bcd37853bef
Hannah von Reth <vonreth@kde.org>
2026-09-16 13:56:11 +0200

    Mac: Backport patches fixing a crash on tray icon interaction

--- libs/qt6/qtbase/qtbase.py
+++ libs/qt6/qtbase/qtbase.py
@@ -50,13 +50,18 @@ class subinfo(info.infoclass):
             self.patchToApply["6.8.1"] += [("8814bb1e81adcc74f504fb3c7fb1508dff4b68d9.diff", 1), ("0be9ebcc222c14266e6330c58de794d60d6d35ed.diff", 1)]
             self.patchLevel["6.8.1"] = 1
 
+        # https://qt-project.atlassian.net/browse/QTBUG-147449 (fixed in 6.11.3)
+        self.patchToApply["6.11.0"] += [(".6.11.1", 1)]
+        self.patchToApply["6.11.1"] += [(".6.11.1", 1)]
+        self.patchToApply["6.11.2"] += [(".6.11.1", 1)]
+
         self.patchLevel["6.4.3"] = 4
         self.patchLevel["6.6.0"] = 4
         self.patchLevel["6.6.1"] = 3
         self.patchLevel["6.8.0"] = 2
         self.patchLevel["6.8.1"] = 2
         self.patchLevel["6.11.0"] = 1
-        self.patchLevel["6.11.1"] = 2
+        self.patchLevel["6.11.1"] = 3
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = None
