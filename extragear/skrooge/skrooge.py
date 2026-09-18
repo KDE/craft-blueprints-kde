357de39f98e938d9e99a48eab97bee99f6e5099c
Stéphane MANKOWSKI <stephane@mankowski.fr>
2026-09-18 18:15:06 +0200

    Fix skrooge

--- extragear/skrooge/skrooge.py
+++ extragear/skrooge/skrooge.py
@@ -14,6 +14,7 @@ class subinfo(info.infoclass):
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt6/qtbase"] = None
         self.runtimeDependencies["libs/sqlcipher"] = None
+        self.runtimeDependencies["libs/openssl"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
         if not CraftCore.compiler.isAndroid:
             self.runtimeDependencies["libs/qt6/qtdeclarative"] = None
