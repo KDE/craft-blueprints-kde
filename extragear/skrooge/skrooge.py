ac0855b0917af35978942d62a6908acab6f41c1e
Stéphane MANKOWSKI <stephane@mankowski.fr>
2026-09-20 18:53:14 +0200

    Build skrooge on android

--- extragear/skrooge/skrooge.py
+++ extragear/skrooge/skrooge.py
@@ -14,21 +14,23 @@ class subinfo(info.infoclass):
         self.buildDependencies["kde/frameworks/extra-cmake-modules"] = None
         self.runtimeDependencies["libs/qt6/qtbase"] = None
         self.runtimeDependencies["libs/qt6/qt5compat"] = None
-        self.runtimeDependencies["libs/qt6/qtwebengine"] = None
+        self.runtimeDependencies["libs/sqlcipher"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/ktexttemplate"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kparts"] = None
+        self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
         if not CraftCore.compiler.isAndroid:
-            self.runtimeDependencies["libs/sqlcipher"] = None
+            self.runtimeDependencies["libs/qt6/qtwebengine"] = None
             self.runtimeDependencies["libs/qt6/qtdeclarative"] = None
             self.runtimeDependencies["libs/qt6/qtsvg"] = None
             self.runtimeDependencies["kde/frameworks/tier3/kconfigwidgets"] = None
             self.runtimeDependencies["kde/frameworks/tier3/kiconthemes"] = None
-            self.runtimeDependencies["kde/frameworks/tier3/kio"] = None
             self.runtimeDependencies["kde/frameworks/tier3/knewstuff"] = None
-            self.runtimeDependencies["kde/frameworks/tier3/kparts"] = None
             self.runtimeDependencies["kde/frameworks/tier3/kwallet"] = None
-            self.runtimeDependencies["kde/frameworks/tier3/kxmlgui"] = None
+        else:
+            self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
             self.runtimeDependencies["kde/frameworks/tier3/knotifyconfig"] = None
-            self.runtimeDependencies["kde/frameworks/tier1/ktexttemplate"] = None
 
 
 class Package(CraftPackageObject.get("kde").pattern):
