7b45fa677258aeff4d0a06b8be430c8840513654
Carl Schwan <carl@carlschwan.eu>
2026-09-23 10:14:00 +0200

    qpdf: Add more new dependencies

--- libs/qpdf/qpdf.py
+++ libs/qpdf/qpdf.py
@@ -17,6 +17,8 @@ class subinfo(info.infoclass):
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = None
+        self.runtimeDependencies["libs/libzstd"] = None
+        self.runtimeDependencies["libs/openjpeg"] = None
         self.runtimeDependencies["libs/gnutls"] = None
 
 
