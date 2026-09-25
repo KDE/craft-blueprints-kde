a05f813fd3a92dbc6990193e42b8f5c360156ff0
Stéphane MANKOWSKI <stephane@mankowski.fr>
2026-09-25 17:17:12 +0200

    build skrooge for macos

--- extragear/skrooge/skrooge.py
+++ extragear/skrooge/skrooge.py
@@ -1,6 +1,7 @@
 import info
 from Blueprints.CraftPackageObject import CraftPackageObject
 from CraftCore import CraftCore
+import utils
 
 
 class subinfo(info.infoclass):
@@ -44,5 +45,17 @@ class Package(CraftPackageObject.get("kde").pattern):
         self.defines["file_types"] = [".skg", ".kmy", ".mny", ".gnucash", ".gsb", ".xhb", ".mmb", ".afb120", ".mt940", ".iif", ".ofx", ".qfx", ".qif", ".csv"]
         self.defines["website"] = "https://skrooge.org/"
         # self.defines["icon"] = self.blueprintDir() / "skrooge.ico"
+
+        # Correctif macOS : Réparation du chemin durci de qca-qt6 issu des binaires pré-compilés du CI
+        if CraftCore.compiler.isMacOS:
+            ksecretd_path = self.imageDir() / "bin" / "ksecretd"
+            if not ksecretd_path.exists():
+                ksecretd_path = CraftCore.standardDirs.craftRoot() / "bin" / "ksecretd"
+
+            if ksecretd_path.exists():
+                # On remplace le chemin obsolète du CI sysadmin par un @rpath générique
+                old_path = "/Users/gitlab/builds/GZwHuM5xu/0/sysadmin/craft-ci/macos-64-clang/lib/qca-qt6.framework/Versions/2/qca-qt6"
+                new_path = "@rpath/qca-qt6.framework/Versions/2/qca-qt6"
+                utils.system(["install_name_tool", "-change", old_path, new_path, str(ksecretd_path)], ignoreSuccess=True)
 
         return super().createPackage()
