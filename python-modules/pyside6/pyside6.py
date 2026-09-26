e9e059573e92af661a4ff93f0d48735b1cb30291
Alexander Semke <alexander.semke@web.de>
2026-09-26 12:07:31 +0200

    [pyside6] windows specific changes and workarounds to get pyside6 compiled also on windows:
    1. skip-plugins.patch  -  disables Designer plugin build (Windows linker wants python311.lib but only python3.lib exists in limited API)
    2. skip-designer-copy.patch  -  prevents installer from copying non-existent plugin directory
    3. qml directory deletion  -  empty qml dir triggers copy escaping bug on Windows
    4. --skip-build flag  -  revents install from rebuilding and recreating qml directory
    
    While all the underlying issues are ideally addressed at some point in time and pyside is working on windows for all modules and plugins,
    there is no need at the moment for everything from Qt and we follow the pragmatic approach for now by skipping/disabling everything that
    is not required at the moment.

--- python-modules/pyside6/pyside6.py
+++ python-modules/pyside6/pyside6.py
@@ -4,6 +4,7 @@
 import info
 import utils
 from CraftCore import CraftCore
+from CraftStandardDirs import CraftStandardDirs
 from Package.PipPackageBase import PipPackageBase
 
 
@@ -16,8 +17,13 @@ class subinfo(info.infoclass):
             self.targets[ver] = f"https://download.qt.io/official_releases/QtForPython/pyside6/PySide6-{ver}-src/pyside-setup-everywhere-src-{ver}.zip"
             self.targetInstSrc[ver] = "pyside-setup-everywhere-src-%s" % ver
 
-        self.patchToApply["6.11.2"] = [("shiboken-include-pep384impl.patch", 1)]
-        self.patchLevel["6.11.2"] = 1
+        self.patchToApply["6.11.2"] = [
+            ("shiboken-include-pep384impl.patch", 1),
+            ("python-libdir-fallback.patch", 1),
+            ("skip-plugins.patch", 1),
+            ("skip-designer-copy.patch", 1)
+        ]
+        self.patchLevel["6.11.2"] = 2
 
     def setDependencies(self):
         self.buildDependencies["python-modules/setuptools"] = None
@@ -32,20 +38,64 @@ class Package(PipPackageBase):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def make(self):
+        """Build PySide6 from source."""
+        sourceDir = self.sourceDir()
+        env = {}
+        if CraftCore.compiler.isWindows:
+            env["LLVM_INSTALL_DIR"] = str(CraftStandardDirs.craftRoot())
+            env["CLANG_INSTALL_DIR"] = str(CraftStandardDirs.craftRoot())
+
+        with utils.ScopedEnv(env):
+            if CraftCore.compiler.isMacOS:
+                return utils.system(
+                    f"SDKROOT=$(xcrun --show-sdk-path) PYSIDE_DISABLE_UNITY=1 python setup.py build --verbose-build --macos-use-libc++ --disable-pyi --skip-modules=WebEngineCore,WebEngineWidgets,WebEngineQuick",
+                    cwd=sourceDir
+                )
+            else:
+                # Skip QML/Designer modules: QML directory copy fails with escaping errors on Windows
+                # Skip WebEngine modules: require Chromium dependencies not available in Craft
+                return utils.system(
+                    ["python", "setup.py", "build",
+                     "--limited-api=yes",
+                     "--disable-pyi",
+                     "--skip-modules=Designer,Positioning,WebEngineCore,WebEngineWidgets,WebEngineQuick,WebChannel,Quick,Qml,QuickControls2,QuickTest,QuickWidgets,UiTools"],
+                    cwd=sourceDir
+                )
+
     def install(self):
-        """Override default pip install with setup.py install."""
+        """Install PySide6 without rebuilding."""
+        import shutil
+        import glob
         sourceDir = self.sourceDir()
         imageDir = self.imageDir()
-        # see https://doc.qt.io/qtforpython-6/building_from_source/index.html
-        # options: --build-type=pyside6 (only pyside6), --macos-use-libc++, --macos-sysroot=, --shiboken-extra-include-paths=,  --standalone, --verbose
+        env = {}
+        if CraftCore.compiler.isWindows:
+            env["LLVM_INSTALL_DIR"] = str(CraftStandardDirs.craftRoot())
+            env["CLANG_INSTALL_DIR"] = str(CraftStandardDirs.craftRoot())
+
+        # Windows: Delete qml directory before install to prevent copy escaping error
+        # The directory is empty (QML modules skipped) but copy still fails with backslash escaping bug
+        qml_dirs = glob.glob(str(sourceDir / "build" / "*" / "package" / "PySide6" / "qml"))
+        for qml_dir in qml_dirs:
+            shutil.rmtree(qml_dir)
+
+        # See https://doc.qt.io/qtforpython-6/building_from_source/index.html
         # macOS: SDKROOT required to find type_traits and skip failing WebEngineCore and dependencies
-        if CraftCore.compiler.isMacOS:
-            return utils.system(
-                f"SDKROOT=$(xcrun --show-sdk-path) PYSIDE_DISABLE_UNITY=1 python setup.py install --prefix={imageDir} --verbose-build --macos-use-libc++ --disable-pyi --skip-mypy-test --skip-modules=WebEngineCore,WebEngineWidgets,WebEngineQuick",
-                cwd=sourceDir
-            )
-        else:
-            return utils.system(
-                ["python", "setup.py", "install", f"--prefix={imageDir}", "--verbose-build", "--disable-pyi", "--skip-mypy-test", "--skip-modules=Positioning,WebEngineCore,WebEngineWidgets,WebEngineQuick,WebChannel,Quick,QuickControls2,QuickTest,QuickWidgets,Qml,UiTools"],
-                cwd=sourceDir
-            )
+        with utils.ScopedEnv(env):
+            if CraftCore.compiler.isMacOS:
+                return utils.system(
+                    f"SDKROOT=$(xcrun --show-sdk-path) PYSIDE_DISABLE_UNITY=1 python setup.py install --prefix={imageDir} --verbose-build --macos-use-libc++ --disable-pyi --skip-mypy-test --skip-modules=WebEngineCore,WebEngineWidgets,WebEngineQuick",
+                    cwd=sourceDir
+                )
+            else:
+                # --skip-build: prevents setup.py from rebuilding (which would recreate qml dir)
+                # --skip-modules: must match make() to prevent module mismatch errors
+                return utils.system(
+                    ["python", "setup.py", "install",
+                     f"--prefix={imageDir}",
+                     "--skip-build",
+                     "--skip-mypy-test",
+                     "--skip-modules=Designer,Positioning,WebEngineCore,WebEngineWidgets,WebEngineQuick,WebChannel,Quick,Qml,QuickControls2,QuickTest,QuickWidgets,UiTools"],
+                    cwd=sourceDir
+                )
