00ab915e362ca7cfb80f02b7e49be5f07d7080c0
Stéphane MANKOWSKI <stephane@mankowski.fr>
2026-09-26 21:35:12 +0200

    build of skrooge on macos

--- extragear/skrooge/skrooge.py
+++ extragear/skrooge/skrooge.py
@@ -43,19 +43,39 @@ class Package(CraftPackageObject.get("kde").pattern):
     def createPackage(self):
         self.defines["executable"] = "bin\\skrooge.exe"  # Windows-only, mac is handled implicitly
         self.defines["icon"] = self.blueprintDir() / "skrooge.ico"
-        self.defines["file_types"] = [".skg", ".kmy", ".mny", ".gnucash", ".gsb", ".xhb", ".mmb", ".afb120", ".mt940", ".iif", ".ofx", ".qfx", ".qif", ".csv"]
+        self.defines["file_types"] = [
+            ".skg",
+            ".kmy",
+            ".mny",
+            ".gnucash",
+            ".gsb",
+            ".xhb",
+            ".mmb",
+            ".afb120",
+            ".mt940",
+            ".iif",
+            ".ofx",
+            ".qfx",
+            ".qif",
+            ".csv",
+        ]
         self.defines["website"] = "https://skrooge.org/"
-        # self.defines["icon"] = self.blueprintDir() / "skrooge.ico"
 
-        # Correctif macOS : Réparation du chemin durci de qca-qt6 issu des binaires pré-compilés du CI
         if CraftCore.compiler.isMacOS:
-            ksecretd_path = self.imageDir() / "bin" / "ksecretd"
-            if not ksecretd_path.exists():
-                ksecretd_path = CraftCore.standardDirs.craftRoot() / "bin" / "ksecretd"
+            old_path = "/Users/gitlab/builds/GZwHuM5xu/0/sysadmin/craft-ci/macos-64-clang/lib/qca-qt6.framework/Versions/2/qca-qt6"
+            new_path = "@rpath/qca-qt6.framework/Versions/2/qca-qt6"
 
-            if ksecretd_path.exists():
-                old_path = "/Users/gitlab/builds/GZwHuM5xu/0/sysadmin/craft-ci/macos-64-clang/lib/qca-qt6.framework/Versions/2/qca-qt6"
-                new_path = "@rpath/qca-qt6.framework/Versions/2/qca-qt6"
-                subprocess.run(["install_name_tool", "-change", old_path, new_path, str(ksecretd_path)], check=False)
+            candidate_paths = [
+                self.imageDir() / "bin" / "ksecretd",
+                CraftCore.standardDirs.craftRoot() / "bin" / "ksecretd",
+                self.archiveDir() / "Applications/KDE/skrooge.app/Contents/MacOS/ksecretd",
+            ]
+
+            for ksecretd_path in candidate_paths:
+                if ksecretd_path.exists():
+                    subprocess.run(
+                        ["install_name_tool", "-change", old_path, new_path, str(ksecretd_path)],
+                        check=False,
+                    )
 
         return super().createPackage()
